paidaxingyo666/Manta

feat(native-chat): resume an Agent Session History row into a new structured chat (#19176)
* feat(native-chat): resume an Agent Session History row into a new structured chat

A Claude or Codex row in Agent Session History gains "Resume in New Chat": it
opens a new structured native-chat tab that continues that provider
conversation, with the prior turns already in the journal. Until now those rows
could only be resumed into a PTY terminal; the structured branch could reveal a
chat Orca already owned but could not adopt one it had never held.

Almost all of the machinery existed. Both lanes already resume from the record's
provider handle chain, the journal already has a transcript importer, and the
handle chain already models `adopted` as an origin. The gap was that a create
always minted an empty chain, so the adapters started a fresh conversation. This
seeds that chain.

The client names only the conversation. `agentSession.create` is reachable by
paired mobile clients, so the transcript path and the account home are derived
by the executing host and validated against the account homes it recognises —
a client-supplied path would choose which file the host imports and which
credential directory the provider child launches against.

Failure refuses rather than degrades. A transcript that cannot be found refuses
before anything is created; one that fails or decodes empty *after* the provider
has resumed fails the attach, tearing the child down and publishing no tab,
because an empty journal beside a context-carrying agent claims a continuity the
provider never gave.

Codex can resume into any workspace since it is handed the rollout path; Claude
resolves transcripts under a project key derived from the launch cwd, so it is
offered only for the workspace the conversation was recorded in.

* fix(native-chat): widen adopted-home discovery and keep ordinary launches untouched

Three corrections from review of the first commit.

The adoption's account-home candidates no

src/main/runtime/structured-agent-session-create-adoption.ts
@@ -1,0 +1,113 @@
+import { homedir } from 'node:os'
+import { join } from 'node:path'
+import type { AgentSessionExecutionLocation } from '../../shared/agent-session-record'
+import { agentSessionExecutionLocationsEqual } from '../../shared/agent-session-record'
+import type { AgentSessionAttachParams } from '../native-chat/agent-session-wire/structured-agent-session-attach'
+import type { StructuredAgentSessionHost } from '../native-chat/agent-session-wire/structured-agent-session-host'
+import { listStructuredProviderSessionOwnership } from '../native-chat/agent-session-wire/structured-provider-session-ownership'
+import {
+  findCommittedStructuredAgentSessionAdoptionReplay,
+  findConflictingStructuredAdoption,
+  resolveStructuredAgentSessionAdoption,
+  structuredAdoptionConflictError
+} from '../native-chat/structured-agent-session-history-adoption'
+import { resolveSessionFilePath } from '../native-chat/session-file-resolver'
+import { configuredAdditionalCodexHomePaths } from '../ai-vault/cached-session-list'
+import { getMantaManagedCodexHomePath, getSystemCodexHomePath } from '../codex/codex-home-paths'
+
+type AdoptionSettings = {
+  codexManagedAccounts?: readonly { managedHomePath: string }[]
+}
+
+export function resolveCommittedStructuredAgentSessionAdoptionIntent(input: {
+  host: StructuredAgentSessionHost | null
+  envelope: { sessionId: string; clientOperationId: string }
+  agent: 'claude' | 'codex'
+  callerKey?: string
+  resumeFrom?: { providerSessionId: string }
+  location: AgentSessionExecutionLocation
+  options?: Readonly<Record<string, string>>
+}): AgentSessionAttachParams | null {
+  const replay =
+    input.resumeFrom && input.callerKey && input.host
+      ? findCommittedStructuredAgentSessionAdoptionReplay({
+          agent: input.agent,
+          providerSessionId: input.resumeFrom.providerSessionId,
+          selfSessionId: input.envelope.sessionId,
+          callerKey: input.callerKey,
+          operationId: input.envelope.clientOperationId,
+          record: input.host.deps.store.getRecord(input.envelope.sessionId),
+          operations: input.host.deps.store.listOperationRows()
+        })
+      : null
+  if (!replay || !agentSessionExecutionLocationsEqual(replay.record.location, input.location)) {
+    return null
+  }
+  return {
+    envelope: {
+      sessionId: input.envelope.sessionId,
+      clientOperationId: input.envelope.clientOperationId,
+      expectedRuntimeFence: null,
+      payloadFingerprint: ''
+    },
+    location: input.location,
+    provider: input.agent,
+    agent: input.agent,
+    accountHome: replay.record.accountHome,
+    ...(input.options ? { options: input.options } : {}),
+    adopt: { providerHandle: replay.providerHandle },
+    runtimeKind: replay.record.lease.runtimeKind
+  }
+}
+
+export async function resolveStructuredAgentSessionAdoptionForCreate(input: {
+  host: StructuredAgentSessionHost | null
+  settings: AdoptionSettings
+  agent: 'claude' | 'codex'
+  providerSessionId: string
+  selfSessionId: string
+  selectedAccountHomePath: string
+}) {
+  const conflict = input.host
+    ? findConflictingStructuredAdoption({
+        agent: input.agent,
+        providerSessionId: input.providerSessionId,
+        selfSessionId: input.selfSessionId,
+        ownership: listStructuredProviderSessionOwnership(input.host.deps.store.listRecords())
+      })
+    : null
+  if (conflict) {
+    throw structuredAdoptionConflictError(conflict)
+  }
+  return resolveStructuredAgentSessionAdoption({
+    agent: input.agent,
+    providerSessionId: input.providerSessionId,
+    candidateAccountHomes: structuredAdoptionAccountHomeCandidates(input),
+    resolveTranscript: async ({ agent, providerSessionId, accountHomePath }) =>
+      resolveSessionFilePath(
+        agent,
+        providerSessionId,
+        agent === 'claude'
+          ? { claudeProjectsDir: join(accountHomePath, 'projects') }
+          : { codexSessionsDirs: [join(accountHomePath, 'sessions')] }
+      )
+  })
+}
+
+/** Recognised adoption homes, most-preferred first. */
+function structuredAdoptionAccountHomeCandidates(input: {
+  settings: AdoptionSettings
+  agent: 'claude' | 'codex'
+  selectedAccountHomePath: string
+}): string[] {
+  if (input.agent === 'claude') {
+    return [input.selectedAccountHomePath, join(homedir(), '.claude')]
+  }
+  return [
+    input.selectedAccountHomePath,
+    ...(input.settings.codexManagedAccounts ?? []).map((account) => account.managedHomePath),
+    ...configuredAdditionalCodexHomePaths(),
+    getMantaManagedCodexHomePath(),
+    getSystemCodexHomePath()
+  ]
+}

src/shared/structured-agent-session-create.test.ts
@@ -1,0 +1,82 @@
+import { describe, expect, it } from 'vitest'
+import { structuredAgentSessionCreateParams } from './structured-agent-session-create'
+import {
+  structuredAgentSessionCreateFingerprint,
+  structuredAgentSessionPayloadFingerprint
+} from './structured-agent-session-mutation'
+
+const SESSION_ID = 'codex_11111111_2222_3333_4444_555555555555'
+const RESUME = { providerSessionId: 'thread-abc' }
+
+/** Distinct per call so two envelopes never share an operation id by accident. */
+let uuidCounter = 0
+function nextUuid(): string {
+  uuidCounter += 1
+  return `00000000-0000-4000-8000-${String(uuidCounter).padStart(12, '0')}`
+}
+
+function createParams(overrides: { resumeFrom?: { providerSessionId: string } } = {}) {
+  return structuredAgentSessionCreateParams({
+    sessionId: SESSION_ID,
+    worktree: 'id:repo-1::/repo/manta',
+    agent: 'codex',
+    ...overrides,
+    randomUuid: nextUuid,
+    now: 1_800_000_000_000
+  })
+}
+
+describe('structured agent session create params', () => {
+  it('carries resumeFrom only when the create adopts a conversation', () => {
+    expect(createParams()).not.toHaveProperty('resumeFrom')
+    expect(createParams({ resumeFrom: RESUME })).toMatchObject({ resumeFrom: RESUME })
+  })
+
+  it('declares a fingerprint the host can recompute from the same fields', () => {
+    const params = createParams({ resumeFrom: RESUME })
+
+    expect(params.envelope.payloadFingerprint).toBe(
+      structuredAgentSessionCreateFingerprint({
+        sessionId: SESSION_ID,
+        worktree: 'id:repo-1::/repo/manta',
+        agent: 'codex',
+        resumeFrom: RESUME
+      })
+    )
+  })
+
+  it('separates an adopting create from a blank one and from another row', () => {
+    const blank = createParams().envelope.payloadFingerprint
+    const adopted = createParams({ resumeFrom: RESUME }).envelope.payloadFingerprint
+    const otherRow = createParams({
+      resumeFrom: { providerSessionId: 'thread-other' }
+    }).envelope.payloadFingerprint
+
+    expect(adopted).not.toBe(blank)
+    expect(otherRow).not.toBe(adopted)
+  })
+
+  it('gives a replay of the same adoption the same digest under a new operation id', () => {
+    const first = createParams({ resumeFrom: RESUME })
+    const second = createParams({ resumeFrom: RESUME })
+
+    expect(second.envelope.clientOperationId).not.toBe(first.envelope.clientOperationId)
+    expect(second.envelope.payloadFingerprint).toBe(first.envelope.payloadFingerprint)
+  })
+
+  it('leaves a blank create byte-identical to the pre-resume digest', () => {
+    // Pinned literal: a create with no `resumeFrom` must keep the digest older clients and hosts
+    // already compute, so adding a field to the create fingerprint fails here rather than in the
+    // field on a mixed-version pair.
+    expect(createParams().envelope.payloadFingerprint).toBe(
+      structuredAgentSessionPayloadFingerprint({
+        method: 'agentSession.create',
+        sessionId: SESSION_ID,
+        fields: { worktree: 'id:repo-1::/repo/manta', agent: 'codex' }
+      })
+    )
+    expect(createParams().envelope.payloadFingerprint).toBe(
+      '56cb15e22414c0f62fd89d77d00d2d6a0a422f16e95edee154fb8b5bf53fbbc3'
+    )
+  })
+})

ivanvykopal/orca

src/main/runtime/structured-agent-session-create-adoption.ts
@@ -1,0 +1,113 @@
+import { homedir } from 'node:os'
+import { join } from 'node:path'
+import type { AgentSessionExecutionLocation } from '../../shared/agent-session-record'
+import { agentSessionExecutionLocationsEqual } from '../../shared/agent-session-record'
+import type { AgentSessionAttachParams } from '../native-chat/agent-session-wire/structured-agent-session-attach'
+import type { StructuredAgentSessionHost } from '../native-chat/agent-session-wire/structured-agent-session-host'
+import { listStructuredProviderSessionOwnership } from '../native-chat/agent-session-wire/structured-provider-session-ownership'
+import {
+  findCommittedStructuredAgentSessionAdoptionReplay,
+  findConflictingStructuredAdoption,
+  resolveStructuredAgentSessionAdoption,
+  structuredAdoptionConflictError
+} from '../native-chat/structured-agent-session-history-adoption'
+import { resolveSessionFilePath } from '../native-chat/session-file-resolver'
+import { configuredAdditionalCodexHomePaths } from '../ai-vault/cached-session-list'
+import { getOrcaManagedCodexHomePath, getSystemCodexHomePath } from '../codex/codex-home-paths'
+
+type AdoptionSettings = {
+  codexManagedAccounts?: readonly { managedHomePath: string }[]
+}
+
+export function resolveCommittedStructuredAgentSessionAdoptionIntent(input: {
+  host: StructuredAgentSessionHost | null
+  envelope: { sessionId: string; clientOperationId: string }
+  agent: 'claude' | 'codex'
+  callerKey?: string
+  resumeFrom?: { providerSessionId: string }
+  location: AgentSessionExecutionLocation
+  options?: Readonly<Record<string, string>>
+}): AgentSessionAttachParams | null {
+  const replay =
+    input.resumeFrom && input.callerKey && input.host
+      ? findCommittedStructuredAgentSessionAdoptionReplay({
+          agent: input.agent,
+          providerSessionId: input.resumeFrom.providerSessionId,
+          selfSessionId: input.envelope.sessionId,
+          callerKey: input.callerKey,
+          operationId: input.envelope.clientOperationId,
+          record: input.host.deps.store.getRecord(input.envelope.sessionId),
+          operations: input.host.deps.store.listOperationRows()
+        })
+      : null
+  if (!replay || !agentSessionExecutionLocationsEqual(replay.record.location, input.location)) {
+    return null
+  }
+  return {
+    envelope: {
+      sessionId: input.envelope.sessionId,
+      clientOperationId: input.envelope.clientOperationId,
+      expectedRuntimeFence: null,
+      payloadFingerprint: ''
+    },
+    location: input.location,
+    provider: input.agent,
+    agent: input.agent,
+    accountHome: replay.record.accountHome,
+    ...(input.options ? { options: input.options } : {}),
+    adopt: { providerHandle: replay.providerHandle },
+    runtimeKind: replay.record.lease.runtimeKind
+  }
+}
+
+export async function resolveStructuredAgentSessionAdoptionForCreate(input: {
+  host: StructuredAgentSessionHost | null
+  settings: AdoptionSettings
+  agent: 'claude' | 'codex'
+  providerSessionId: string
+  selfSessionId: string
+  selectedAccountHomePath: string
+}) {
+  const conflict = input.host
+    ? findConflictingStructuredAdoption({
+        agent: input.agent,
+        providerSessionId: input.providerSessionId,
+        selfSessionId: input.selfSessionId,
+        ownership: listStructuredProviderSessionOwnership(input.host.deps.store.listRecords())
+      })
+    : null
+  if (conflict) {
+    throw structuredAdoptionConflictError(conflict)
+  }
+  return resolveStructuredAgentSessionAdoption({
+    agent: input.agent,
+    providerSessionId: input.providerSessionId,
+    candidateAccountHomes: structuredAdoptionAccountHomeCandidates(input),
+    resolveTranscript: async ({ agent, providerSessionId, accountHomePath }) =>
+      resolveSessionFilePath(
+        agent,
+        providerSessionId,
+        agent === 'claude'
+          ? { claudeProjectsDir: join(accountHomePath, 'projects') }
+          : { codexSessionsDirs: [join(accountHomePath, 'sessions')] }
+      )
+  })
+}
+
+/** Recognised adoption homes, most-preferred first. */
+function structuredAdoptionAccountHomeCandidates(input: {
+  settings: AdoptionSettings
+  agent: 'claude' | 'codex'
+  selectedAccountHomePath: string
+}): string[] {
+  if (input.agent === 'claude') {
+    return [input.selectedAccountHomePath, join(homedir(), '.claude')]
+  }
+  return [
+    input.selectedAccountHomePath,
+    ...(input.settings.codexManagedAccounts ?? []).map((account) => account.managedHomePath),
+    ...configuredAdditionalCodexHomePaths(),
+    getOrcaManagedCodexHomePath(),
+    getSystemCodexHomePath()
+  ]
+}

src/shared/structured-agent-session-create.test.ts
@@ -1,0 +1,82 @@
+import { describe, expect, it } from 'vitest'
+import { structuredAgentSessionCreateParams } from './structured-agent-session-create'
+import {
+  structuredAgentSessionCreateFingerprint,
+  structuredAgentSessionPayloadFingerprint
+} from './structured-agent-session-mutation'
+
+const SESSION_ID = 'codex_11111111_2222_3333_4444_555555555555'
+const RESUME = { providerSessionId: 'thread-abc' }
+
+/** Distinct per call so two envelopes never share an operation id by accident. */
+let uuidCounter = 0
+function nextUuid(): string {
+  uuidCounter += 1
+  return `00000000-0000-4000-8000-${String(uuidCounter).padStart(12, '0')}`
+}
+
+function createParams(overrides: { resumeFrom?: { providerSessionId: string } } = {}) {
+  return structuredAgentSessionCreateParams({
+    sessionId: SESSION_ID,
+    worktree: 'id:repo-1::/repo/orca',
+    agent: 'codex',
+    ...overrides,
+    randomUuid: nextUuid,
+    now: 1_800_000_000_000
+  })
+}
+
+describe('structured agent session create params', () => {
+  it('carries resumeFrom only when the create adopts a conversation', () => {
+    expect(createParams()).not.toHaveProperty('resumeFrom')
+    expect(createParams({ resumeFrom: RESUME })).toMatchObject({ resumeFrom: RESUME })
+  })
+
+  it('declares a fingerprint the host can recompute from the same fields', () => {
+    const params = createParams({ resumeFrom: RESUME })
+
+    expect(params.envelope.payloadFingerprint).toBe(
+      structuredAgentSessionCreateFingerprint({
+        sessionId: SESSION_ID,
+        worktree: 'id:repo-1::/repo/orca',
+        agent: 'codex',
+        resumeFrom: RESUME
+      })
+    )
+  })
+
+  it('separates an adopting create from a blank one and from another row', () => {
+    const blank = createParams().envelope.payloadFingerprint
+    const adopted = createParams({ resumeFrom: RESUME }).envelope.payloadFingerprint
+    const otherRow = createParams({
+      resumeFrom: { providerSessionId: 'thread-other' }
+    }).envelope.payloadFingerprint
+
+    expect(adopted).not.toBe(blank)
+    expect(otherRow).not.toBe(adopted)
+  })
+
+  it('gives a replay of the same adoption the same digest under a new operation id', () => {
+    const first = createParams({ resumeFrom: RESUME })
+    const second = createParams({ resumeFrom: RESUME })
+
+    expect(second.envelope.clientOperationId).not.toBe(first.envelope.clientOperationId)
+    expect(second.envelope.payloadFingerprint).toBe(first.envelope.payloadFingerprint)
+  })
+
+  it('leaves a blank create byte-identical to the pre-resume digest', () => {
+    // Pinned literal: a create with no `resumeFrom` must keep the digest older clients and hosts
+    // already compute, so adding a field to the create fingerprint fails here rather than in the
+    // field on a mixed-version pair.
+    expect(createParams().envelope.payloadFingerprint).toBe(
+      structuredAgentSessionPayloadFingerprint({
+        method: 'agentSession.create',
+        sessionId: SESSION_ID,
+        fields: { worktree: 'id:repo-1::/repo/orca', agent: 'codex' }
+      })
+    )
+    expect(createParams().envelope.payloadFingerprint).toBe(
+      '56cb15e22414c0f62fd89d77d00d2d6a0a422f16e95edee154fb8b5bf53fbbc3'
+    )
+  })
+})