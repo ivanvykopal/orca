paidaxingyo666/Manta

fix(runtime): apply the structured-chat setting to every RPC caller (#18700)
* fix(runtime): apply the structured-chat setting to every RPC caller

supportsStructuredAgentSessions only consulted experimentalStructuredNativeChat
when clientKind === 'mobile', so identical host settings admitted desktop and
in-process callers while refusing a phone. The server branched on client surface.

The setting is now one rule for every caller. The negotiated capability stays a
wire term asked of remote clients only, so a capability-less in-process caller is
still admitted on the setting alone.

Making the projection's structuredNativeChatEnabled argument required surfaced
eight call sites that passed `undefined` for non-mobile clients; they now read the
host setting, so tab projection follows the same single rule.

Announced behaviour change: with the flag off, session.tabs.list/listAll no longer
restore structured tabs for desktop. The desktop renderer already discards them in
that state, and startup record/lease reconciliation is unaffected.

* fix(runtime): keep structured session cleanup available

* test(runtime): enable structured chat in desktop projection fixture

* test(agent-session): settle merged fixtures against the all-clients structured policy

The merge with main left three fixtures written for the old mobile-only rule:
a duplicate getClientSettings key, a create fixture with no host settings at
all, and a projection call whose 'old client' is now the mobile fallback-title
case.

* fix(native-chat): let an admitted caller close a chat after the setting is off

Turning `experimentalStructuredNativeChat` off revoked admission for every
`agentSession.*` method, including `close`. A chat opened while the setting was
on stays mounted, so its owner was left with a live provider child and an X
button that answered `structured_agent_session_unsupported`.

Split the surface by what a method does to work in flight rather than by how it
sounds, and write that rule where the

src/main/runtime/rpc/methods/session-tabs-structured-restore.test.ts
@@ -1,22 +1,79 @@
-import { describe, expect, it, vi } from 'vitest'
+import { describe, expect, it, vi, type Mock } from 'vitest'
 import { RpcDispatcher } from '../dispatcher'
 import type { RpcRequest } from '../core'
 import type { MantaRuntimeService } from '../../manta-runtime'
 import { STRUCTURED_AGENT_SESSION_RUNTIME_CAPABILITY } from '../../../../shared/protocol-version'
 import { SESSION_TAB_METHODS } from './session-tabs'
+import { visibleSnapshot } from './session-tabs-snapshot.test-fixture'
 
 function makeRequest(method: string, params?: unknown): RpcRequest {
   return { id: 'req-1', authToken: 'tok', method, params }
 }
 
+function makeRuntime(experimentalStructuredNativeChat: boolean): MantaRuntimeService {
+  return {
+    getRuntimeId: () => 'test-runtime',
+    getClientSettings: vi.fn(() => ({ experimentalStructuredNativeChat })),
+    restoreStructuredAgentSessionTabs: vi.fn(),
+    listMobileSessionTabs: vi.fn().mockResolvedValue(visibleSnapshot())
+  } as unknown as MantaRuntimeService
+}
+
+describe('structured session tab restoration follows one rule for every caller', () => {
+  it('does not restore for the desktop renderer while the host setting is off', async () => {
+    const runtime = makeRuntime(false)
+    const dispatcher = new RpcDispatcher({ runtime, methods: SESSION_TAB_METHODS })
+
+    const response = await dispatcher.dispatch(
+      makeRequest('session.tabs.list', { worktree: 'id:wt-1' }),
+      {
+        clientKind: 'runtime',
+        clientCapabilities: [STRUCTURED_AGENT_SESSION_RUNTIME_CAPABILITY]
+      }
+    )
+
+    expect(response.ok).toBe(true)
+    expect(runtime.restoreStructuredAgentSessionTabs).not.toHaveBeenCalled()
+  })
+
+  it('restores for the desktop renderer once the host setting is on', async () => {
+    const runtime = makeRuntime(true)
+    const dispatcher = new RpcDispatcher({ runtime, methods: SESSION_TAB_METHODS })
+
+    const response = await dispatcher.dispatch(
+      makeRequest('session.tabs.list', { worktree: 'id:wt-1' }),
+      {
+        clientKind: 'runtime',
+        clientCapabilities: [STRUCTURED_AGENT_SESSION_RUNTIME_CAPABILITY]
+      }
+    )
+
+    expect(response.ok).toBe(true)
+    expect(runtime.restoreStructuredAgentSessionTabs).toHaveBeenCalledTimes(1)
+  })
+
+  it('restores for an in-process caller on the same setting that admits remote clients', async () => {
+    const restoreCallsBySetting = new Map<boolean, number>()
+    for (const enabled of [false, true]) {
+      const runtime = makeRuntime(enabled)
+      const dispatcher = new RpcDispatcher({ runtime, methods: SESSION_TAB_METHODS })
+
+      await dispatcher.dispatch(makeRequest('session.tabs.list', { worktree: 'id:wt-1' }))
+
+      restoreCallsBySetting.set(
+        enabled,
+        (runtime.restoreStructuredAgentSessionTabs as unknown as Mock).mock.calls.length
+      )
+    }
+
+    expect(restoreCallsBySetting.get(false)).toBe(0)
+    expect(restoreCallsBySetting.get(true)).toBe(1)
+  })
+})
+
 describe('session tab structured restore gating', () => {
   it('does not restore structured tabs for mobile while the host setting is off', async () => {
-    const runtime = {
-      getRuntimeId: () => 'test-runtime',
-      getClientSettings: vi.fn(() => ({ experimentalStructuredNativeChat: false })),
-      restoreStructuredAgentSessionTabs: vi.fn(),
-      listMobileSessionTabs: vi.fn().mockResolvedValue(visibleSnapshot())
-    } as unknown as MantaRuntimeService
+    const runtime = makeRuntime(false)
     const dispatcher = new RpcDispatcher({ runtime, methods: SESSION_TAB_METHODS })
 
     const response = await dispatcher.dispatch(
@@ -34,12 +91,7 @@ describe('session tab structured restore gating', () => {
   // Why: an old build has no capability to advertise, and skipping the restore left it with
   // nothing to project after a desktop restart — neither the chat nor its fallback row.
   it('restores structured tabs for a mobile client that advertises no capability', async () => {
-    const runtime = {
-      getRuntimeId: () => 'test-runtime',
-      getClientSettings: vi.fn(() => ({ experimentalStructuredNativeChat: true })),
-      restoreStructuredAgentSessionTabs: vi.fn(),
-      listMobileSessionTabs: vi.fn().mockResolvedValue(visibleSnapshot())
-    } as unknown as MantaRuntimeService
+    const runtime = makeRuntime(true)
     const dispatcher = new RpcDispatcher({ runtime, methods: SESSION_TAB_METHODS })
 
     const response = await dispatcher.dispatch(
@@ -52,12 +104,7 @@ describe('session tab structured restore gating', () => {
   })
 
   it('restores structured tabs for mobile once the setting is present', async () => {
-    const runtime = {
-      getRuntimeId: () => 'test-runtime',
-      getClientSettings: vi.fn(() => ({ experimentalStructuredNativeChat: true })),
-      restoreStructuredAgentSessionTabs: vi.fn(),
-      listMobileSessionTabs: vi.fn().mockResolvedValue(visibleSnapshot())
-    } as unknown as MantaRuntimeService
+    const runtime = makeRuntime(true)
     const dispatcher = new RpcDispatcher({ runtime, methods: SESSION_TAB_METHODS })
 
     const response = await dispatcher.dispatch(
@@ -72,27 +119,3 @@ describe('session tab structured restore gating', () => {
     expect(runtime.restoreStructuredAgentSessionTabs).toHaveBeenCalledTimes(1)
   })
 })
-
-function visibleSnapshot() {
-  return {
-    worktree: 'wt-1',
-    publicationEpoch: 'epoch-1',
-    snapshotVersion: 1,
-    activeGroupId: 'group-1',
-    activeTabId: 'tab-1::leaf-1',
-    activeTabType: 'terminal' as const,
-    tabGroups: [{ id: 'group-1', activeTabId: 'tab-1', tabOrder: ['tab-1'] }],
-    tabs: [
-      {
-        type: 'terminal' as const,
-        id: 'tab-1::leaf-1',
-        parentTabId: 'tab-1',
-        leafId: 'leaf-1',
-        title: 'Terminal',
-        status: 'ready' as const,
-        terminal: 'pty-1',
-        isActive: true
-      }
-    ]
-  }
-}

src/main/runtime/rpc/methods/structured-agent-session-hold.test.ts
@@ -41,6 +41,7 @@ let runtime: MantaRuntimeService
 let dispatcher: RpcDispatcher
 let closeSession: Mock<NonNullable<StructuredAgentSessionAdapter['closeSession']>>
 let requests = 0
+let structuredNativeChatEnabled = true
 
 async function call(method: string, params: unknown): Promise<RpcResponse> {
   const replies: RpcResponse[] = []
@@ -57,6 +58,7 @@ beforeEach(async () => {
   root = await mkdtemp(join(tmpdir(), 'orca-hold-wire-'))
   resetHostTestOperationIds()
   requests = 0
+  structuredNativeChatEnabled = true
   closeSession = vi.fn(async () => true)
   store = await AgentSessionRecordStore.open({ directory: join(root, 'store'), hostId: 'local' })
   host = new StructuredAgentSessionHost({
@@ -86,6 +88,13 @@ beforeEach(async () => {
   })
   setStructuredAgentSessionHost(host)
   runtime = new MantaRuntimeService()
+  // The structured surface is settings-gated for every caller, in-process included.
+  vi.spyOn(runtime, 'getClientSettings').mockImplementation(
+    () =>
+      ({ experimentalStructuredNativeChat: structuredNativeChatEnabled }) as ReturnType<
+        MantaRuntimeService['getClientSettings']
+      >
+  )
   dispatcher = new RpcDispatcher({ runtime, methods: STRUCTURED_AGENT_SESSION_METHODS })
   expect(await host.attach({ callerKey: 'client-1' }, hostTestAttachParams(null))).toMatchObject({
     ok: true
@@ -117,6 +126,23 @@ describe('a client that holds a session', () => {
       await call('agentSession.release', { sessionId: SESSION, holderId: 'chat-1' })
     ).toMatchObject({ ok: true })
 
+    await vi.waitFor(() => expect(host.hasSession(SESSION)).toBe(false))
+    expect(closeSession).toHaveBeenCalledWith(SESSION)
+  })
+
+  it('releases its hold and cleanup after the setting is disabled', async () => {
+    const release = vi.spyOn(host, 'release')
+    await call('agentSession.hold', { sessionId: SESSION, holderId: 'chat-1' })
+    structuredNativeChatEnabled = false
+
+    expect(
+      await call('agentSession.release', { sessionId: SESSION, holderId: 'chat-1' })
+    ).toMatchObject({ ok: true })
+    const releaseCallsAfterRpc = release.mock.calls.length
+    runtime.cleanupSubscriptionsForConnection(CONNECTION)
+
+    expect(releaseCallsAfterRpc).toBe(2)
+    expect(release).toHaveBeenCalledTimes(releaseCallsAfterRpc)
     await vi.waitFor(() => expect(host.hasSession(SESSION)).toBe(false))
     expect(closeSession).toHaveBeenCalledWith(SESSION)
   })
@@ -208,6 +234,31 @@ describe('a client that disappears without cleanup', () => {
     expect(host.isHeld(SESSION)).toBe(true)
 
     transport.abort()
+
+    await vi.waitFor(() => expect(host.hasSession(SESSION)).toBe(false))
+    expect(closeSession).toHaveBeenCalledWith(SESSION)
+  })
+
+  it('unsubscribes and releases stream retention after the setting is disabled', async () => {
+    await dispatcher.dispatchStreaming(
+      {
+        id: 'stream-disabled-cleanup',
+        authToken: 'token',
+        method: 'agentSession.subscribe',
+        params: { sessionId: SESSION }
+      },
+      () => {},
+      CLIENT
+    )
+    expect(host.isHeld(SESSION)).toBe(true)
+    structuredNativeChatEnabled = false
+
+    expect(
+      await call('agentSession.unsubscribe', {
+        sessionId: SESSION,
+        subscriptionId: 'stream-disabled-cleanup'
+      })
+    ).toMatchObject({ ok: true })
 
     await vi.waitFor(() => expect(host.hasSession(SESSION)).toBe(false))
     expect(closeSession).toHaveBeenCalledWith(SESSION)

src/main/runtime/rpc/methods/structured-agent-session-policy.test.ts
@@ -1,0 +1,101 @@
+import { describe, expect, it } from 'vitest'
+import { STRUCTURED_AGENT_SESSION_RUNTIME_CAPABILITY } from '../../../../shared/protocol-version'
+import type { MantaRuntimeService } from '../../manta-runtime'
+import { supportsStructuredAgentSessions } from './structured-agent-session-policy'
+
+function runtimeWithSetting(
+  experimentalStructuredNativeChat: boolean
+): Pick<MantaRuntimeService, 'getClientSettings'> {
+  return {
+    getClientSettings: () => ({ experimentalStructuredNativeChat })
+  } as unknown as Pick<MantaRuntimeService, 'getClientSettings'>
+}
+
+const CAPABLE = [STRUCTURED_AGENT_SESSION_RUNTIME_CAPABILITY]
+
+/** Every caller shape that reaches the policy: desktop renderer, paired phone, in-process. */
+const CALLERS = [
+  { name: 'desktop renderer', clientKind: 'runtime' as const, clientCapabilities: CAPABLE },
+  { name: 'paired mobile', clientKind: 'mobile' as const, clientCapabilities: CAPABLE },
+  { name: 'in-process', clientKind: undefined, clientCapabilities: undefined }
+]
+
+describe('supportsStructuredAgentSessions', () => {
+  it.each([true, false])('admits every caller alike when the setting is %s', (enabled) => {
+    const decisions = CALLERS.map((caller) =>
+      supportsStructuredAgentSessions({
+        clientKind: caller.clientKind,
+        clientCapabilities: caller.clientCapabilities,
+        runtime: runtimeWithSetting(enabled)
+      })
+    )
+
+    expect(decisions).toEqual([enabled, enabled, enabled])
+  })
+
+  it('admits a capability-less in-process caller, which negotiates nothing', () => {
+    expect(
+      supportsStructuredAgentSessions({
+        clientKind: undefined,
+        clientCapabilities: undefined,
+        runtime: runtimeWithSetting(true)
+      })
+    ).toBe(true)
+  })
+
+  it('still refuses a remote client that did not advertise the capability', () => {
+    for (const clientKind of ['runtime', 'mobile'] as const) {
+      expect(
+        supportsStructuredAgentSessions({
+          clientKind,
+          clientCapabilities: [],
+          runtime: runtimeWithSetting(true)
+        })
+      ).toBe(false)
+    }
+  })
+
+  it('leaves desktop launch admission unchanged, because launches require the setting anyway', () => {
+    // `agent-launch-routing.ts` refuses to route a structured launch unless
+    // `experimentalStructuredNativeChat` is on, so the only state a desktop launch can
+    // reach the host in is setting-on — which admits exactly as it did before.
+    expect(
+      supportsStructuredAgentSessions({
+        clientKind: 'runtime',
+        clientCapabilities: CAPABLE,
+        runtime: runtimeWithSetting(true)
+      })
+    ).toBe(true)
+  })
+
+  it('reads the setting from the caller-supplied value when no runtime is available', () => {
+    expect(
+      supportsStructuredAgentSessions({
+        clientKind: 'runtime',
+        clientCapabilities: CAPABLE,
+        structuredNativeChatEnabled: true
+      })
+    ).toBe(true)
+    expect(
+      supportsStructuredAgentSessions({
+        clientKind: 'runtime',
+        clientCapabilities: CAPABLE,
+        structuredNativeChatEnabled: false
+      })
+    ).toBe(false)
+  })
+
+  it('treats an unreadable settings store as off rather than admitting', () => {
+    expect(
+      supportsStructuredAgentSessions({
+        clientKind: 'runtime',
+        clientCapabilities: CAPABLE,
+        runtime: {
+          getClientSettings: () => {
+            throw new Error('settings unavailable')
+          }
+        } as unknown as Pick<MantaRuntimeService, 'getClientSettings'>
+      })
+    ).toBe(false)
+  })
+})

ivanvykopal/orca

src/main/runtime/rpc/methods/session-tabs-structured-restore.test.ts
@@ -1,22 +1,79 @@
-import { describe, expect, it, vi } from 'vitest'
+import { describe, expect, it, vi, type Mock } from 'vitest'
 import { RpcDispatcher } from '../dispatcher'
 import type { RpcRequest } from '../core'
 import type { OrcaRuntimeService } from '../../orca-runtime'
 import { STRUCTURED_AGENT_SESSION_RUNTIME_CAPABILITY } from '../../../../shared/protocol-version'
 import { SESSION_TAB_METHODS } from './session-tabs'
+import { visibleSnapshot } from './session-tabs-snapshot.test-fixture'
 
 function makeRequest(method: string, params?: unknown): RpcRequest {
   return { id: 'req-1', authToken: 'tok', method, params }
 }
 
+function makeRuntime(experimentalStructuredNativeChat: boolean): OrcaRuntimeService {
+  return {
+    getRuntimeId: () => 'test-runtime',
+    getClientSettings: vi.fn(() => ({ experimentalStructuredNativeChat })),
+    restoreStructuredAgentSessionTabs: vi.fn(),
+    listMobileSessionTabs: vi.fn().mockResolvedValue(visibleSnapshot())
+  } as unknown as OrcaRuntimeService
+}
+
+describe('structured session tab restoration follows one rule for every caller', () => {
+  it('does not restore for the desktop renderer while the host setting is off', async () => {
+    const runtime = makeRuntime(false)
+    const dispatcher = new RpcDispatcher({ runtime, methods: SESSION_TAB_METHODS })
+
+    const response = await dispatcher.dispatch(
+      makeRequest('session.tabs.list', { worktree: 'id:wt-1' }),
+      {
+        clientKind: 'runtime',
+        clientCapabilities: [STRUCTURED_AGENT_SESSION_RUNTIME_CAPABILITY]
+      }
+    )
+
+    expect(response.ok).toBe(true)
+    expect(runtime.restoreStructuredAgentSessionTabs).not.toHaveBeenCalled()
+  })
+
+  it('restores for the desktop renderer once the host setting is on', async () => {
+    const runtime = makeRuntime(true)
+    const dispatcher = new RpcDispatcher({ runtime, methods: SESSION_TAB_METHODS })
+
+    const response = await dispatcher.dispatch(
+      makeRequest('session.tabs.list', { worktree: 'id:wt-1' }),
+      {
+        clientKind: 'runtime',
+        clientCapabilities: [STRUCTURED_AGENT_SESSION_RUNTIME_CAPABILITY]
+      }
+    )
+
+    expect(response.ok).toBe(true)
+    expect(runtime.restoreStructuredAgentSessionTabs).toHaveBeenCalledTimes(1)
+  })
+
+  it('restores for an in-process caller on the same setting that admits remote clients', async () => {
+    const restoreCallsBySetting = new Map<boolean, number>()
+    for (const enabled of [false, true]) {
+      const runtime = makeRuntime(enabled)
+      const dispatcher = new RpcDispatcher({ runtime, methods: SESSION_TAB_METHODS })
+
+      await dispatcher.dispatch(makeRequest('session.tabs.list', { worktree: 'id:wt-1' }))
+
+      restoreCallsBySetting.set(
+        enabled,
+        (runtime.restoreStructuredAgentSessionTabs as unknown as Mock).mock.calls.length
+      )
+    }
+
+    expect(restoreCallsBySetting.get(false)).toBe(0)
+    expect(restoreCallsBySetting.get(true)).toBe(1)
+  })
+})
+
 describe('session tab structured restore gating', () => {
   it('does not restore structured tabs for mobile while the host setting is off', async () => {
-    const runtime = {
-      getRuntimeId: () => 'test-runtime',
-      getClientSettings: vi.fn(() => ({ experimentalStructuredNativeChat: false })),
-      restoreStructuredAgentSessionTabs: vi.fn(),
-      listMobileSessionTabs: vi.fn().mockResolvedValue(visibleSnapshot())
-    } as unknown as OrcaRuntimeService
+    const runtime = makeRuntime(false)
     const dispatcher = new RpcDispatcher({ runtime, methods: SESSION_TAB_METHODS })
 
     const response = await dispatcher.dispatch(
@@ -34,12 +91,7 @@ describe('session tab structured restore gating', () => {
   // Why: an old build has no capability to advertise, and skipping the restore left it with
   // nothing to project after a desktop restart — neither the chat nor its fallback row.
   it('restores structured tabs for a mobile client that advertises no capability', async () => {
-    const runtime = {
-      getRuntimeId: () => 'test-runtime',
-      getClientSettings: vi.fn(() => ({ experimentalStructuredNativeChat: true })),
-      restoreStructuredAgentSessionTabs: vi.fn(),
-      listMobileSessionTabs: vi.fn().mockResolvedValue(visibleSnapshot())
-    } as unknown as OrcaRuntimeService
+    const runtime = makeRuntime(true)
     const dispatcher = new RpcDispatcher({ runtime, methods: SESSION_TAB_METHODS })
 
     const response = await dispatcher.dispatch(
@@ -52,12 +104,7 @@ describe('session tab structured restore gating', () => {
   })
 
   it('restores structured tabs for mobile once the setting is present', async () => {
-    const runtime = {
-      getRuntimeId: () => 'test-runtime',
-      getClientSettings: vi.fn(() => ({ experimentalStructuredNativeChat: true })),
-      restoreStructuredAgentSessionTabs: vi.fn(),
-      listMobileSessionTabs: vi.fn().mockResolvedValue(visibleSnapshot())
-    } as unknown as OrcaRuntimeService
+    const runtime = makeRuntime(true)
     const dispatcher = new RpcDispatcher({ runtime, methods: SESSION_TAB_METHODS })
 
     const response = await dispatcher.dispatch(
@@ -72,27 +119,3 @@ describe('session tab structured restore gating', () => {
     expect(runtime.restoreStructuredAgentSessionTabs).toHaveBeenCalledTimes(1)
   })
 })
-
-function visibleSnapshot() {
-  return {
-    worktree: 'wt-1',
-    publicationEpoch: 'epoch-1',
-    snapshotVersion: 1,
-    activeGroupId: 'group-1',
-    activeTabId: 'tab-1::leaf-1',
-    activeTabType: 'terminal' as const,
-    tabGroups: [{ id: 'group-1', activeTabId: 'tab-1', tabOrder: ['tab-1'] }],
-    tabs: [
-      {
-        type: 'terminal' as const,
-        id: 'tab-1::leaf-1',
-        parentTabId: 'tab-1',
-        leafId: 'leaf-1',
-        title: 'Terminal',
-        status: 'ready' as const,
-        terminal: 'pty-1',
-        isActive: true
-      }
-    ]
-  }
-}

src/main/runtime/rpc/methods/structured-agent-session-hold.test.ts
@@ -41,6 +41,7 @@ let runtime: OrcaRuntimeService
 let dispatcher: RpcDispatcher
 let closeSession: Mock<NonNullable<StructuredAgentSessionAdapter['closeSession']>>
 let requests = 0
+let structuredNativeChatEnabled = true
 
 async function call(method: string, params: unknown): Promise<RpcResponse> {
   const replies: RpcResponse[] = []
@@ -57,6 +58,7 @@ beforeEach(async () => {
   root = await mkdtemp(join(tmpdir(), 'orca-hold-wire-'))
   resetHostTestOperationIds()
   requests = 0
+  structuredNativeChatEnabled = true
   closeSession = vi.fn(async () => true)
   store = await AgentSessionRecordStore.open({ directory: join(root, 'store'), hostId: 'local' })
   host = new StructuredAgentSessionHost({
@@ -86,6 +88,13 @@ beforeEach(async () => {
   })
   setStructuredAgentSessionHost(host)
   runtime = new OrcaRuntimeService()
+  // The structured surface is settings-gated for every caller, in-process included.
+  vi.spyOn(runtime, 'getClientSettings').mockImplementation(
+    () =>
+      ({ experimentalStructuredNativeChat: structuredNativeChatEnabled }) as ReturnType<
+        OrcaRuntimeService['getClientSettings']
+      >
+  )
   dispatcher = new RpcDispatcher({ runtime, methods: STRUCTURED_AGENT_SESSION_METHODS })
   expect(await host.attach({ callerKey: 'client-1' }, hostTestAttachParams(null))).toMatchObject({
     ok: true
@@ -117,6 +126,23 @@ describe('a client that holds a session', () => {
       await call('agentSession.release', { sessionId: SESSION, holderId: 'chat-1' })
     ).toMatchObject({ ok: true })
 
+    await vi.waitFor(() => expect(host.hasSession(SESSION)).toBe(false))
+    expect(closeSession).toHaveBeenCalledWith(SESSION)
+  })
+
+  it('releases its hold and cleanup after the setting is disabled', async () => {
+    const release = vi.spyOn(host, 'release')
+    await call('agentSession.hold', { sessionId: SESSION, holderId: 'chat-1' })
+    structuredNativeChatEnabled = false
+
+    expect(
+      await call('agentSession.release', { sessionId: SESSION, holderId: 'chat-1' })
+    ).toMatchObject({ ok: true })
+    const releaseCallsAfterRpc = release.mock.calls.length
+    runtime.cleanupSubscriptionsForConnection(CONNECTION)
+
+    expect(releaseCallsAfterRpc).toBe(2)
+    expect(release).toHaveBeenCalledTimes(releaseCallsAfterRpc)
     await vi.waitFor(() => expect(host.hasSession(SESSION)).toBe(false))
     expect(closeSession).toHaveBeenCalledWith(SESSION)
   })
@@ -208,6 +234,31 @@ describe('a client that disappears without cleanup', () => {
     expect(host.isHeld(SESSION)).toBe(true)
 
     transport.abort()
+
+    await vi.waitFor(() => expect(host.hasSession(SESSION)).toBe(false))
+    expect(closeSession).toHaveBeenCalledWith(SESSION)
+  })
+
+  it('unsubscribes and releases stream retention after the setting is disabled', async () => {
+    await dispatcher.dispatchStreaming(
+      {
+        id: 'stream-disabled-cleanup',
+        authToken: 'token',
+        method: 'agentSession.subscribe',
+        params: { sessionId: SESSION }
+      },
+      () => {},
+      CLIENT
+    )
+    expect(host.isHeld(SESSION)).toBe(true)
+    structuredNativeChatEnabled = false
+
+    expect(
+      await call('agentSession.unsubscribe', {
+        sessionId: SESSION,
+        subscriptionId: 'stream-disabled-cleanup'
+      })
+    ).toMatchObject({ ok: true })
 
     await vi.waitFor(() => expect(host.hasSession(SESSION)).toBe(false))
     expect(closeSession).toHaveBeenCalledWith(SESSION)

src/main/runtime/rpc/methods/structured-agent-session-policy.test.ts
@@ -1,0 +1,101 @@
+import { describe, expect, it } from 'vitest'
+import { STRUCTURED_AGENT_SESSION_RUNTIME_CAPABILITY } from '../../../../shared/protocol-version'
+import type { OrcaRuntimeService } from '../../orca-runtime'
+import { supportsStructuredAgentSessions } from './structured-agent-session-policy'
+
+function runtimeWithSetting(
+  experimentalStructuredNativeChat: boolean
+): Pick<OrcaRuntimeService, 'getClientSettings'> {
+  return {
+    getClientSettings: () => ({ experimentalStructuredNativeChat })
+  } as unknown as Pick<OrcaRuntimeService, 'getClientSettings'>
+}
+
+const CAPABLE = [STRUCTURED_AGENT_SESSION_RUNTIME_CAPABILITY]
+
+/** Every caller shape that reaches the policy: desktop renderer, paired phone, in-process. */
+const CALLERS = [
+  { name: 'desktop renderer', clientKind: 'runtime' as const, clientCapabilities: CAPABLE },
+  { name: 'paired mobile', clientKind: 'mobile' as const, clientCapabilities: CAPABLE },
+  { name: 'in-process', clientKind: undefined, clientCapabilities: undefined }
+]
+
+describe('supportsStructuredAgentSessions', () => {
+  it.each([true, false])('admits every caller alike when the setting is %s', (enabled) => {
+    const decisions = CALLERS.map((caller) =>
+      supportsStructuredAgentSessions({
+        clientKind: caller.clientKind,
+        clientCapabilities: caller.clientCapabilities,
+        runtime: runtimeWithSetting(enabled)
+      })
+    )
+
+    expect(decisions).toEqual([enabled, enabled, enabled])
+  })
+
+  it('admits a capability-less in-process caller, which negotiates nothing', () => {
+    expect(
+      supportsStructuredAgentSessions({
+        clientKind: undefined,
+        clientCapabilities: undefined,
+        runtime: runtimeWithSetting(true)
+      })
+    ).toBe(true)
+  })
+
+  it('still refuses a remote client that did not advertise the capability', () => {
+    for (const clientKind of ['runtime', 'mobile'] as const) {
+      expect(
+        supportsStructuredAgentSessions({
+          clientKind,
+          clientCapabilities: [],
+          runtime: runtimeWithSetting(true)
+        })
+      ).toBe(false)
+    }
+  })
+
+  it('leaves desktop launch admission unchanged, because launches require the setting anyway', () => {
+    // `agent-launch-routing.ts` refuses to route a structured launch unless
+    // `experimentalStructuredNativeChat` is on, so the only state a desktop launch can
+    // reach the host in is setting-on — which admits exactly as it did before.
+    expect(
+      supportsStructuredAgentSessions({
+        clientKind: 'runtime',
+        clientCapabilities: CAPABLE,
+        runtime: runtimeWithSetting(true)
+      })
+    ).toBe(true)
+  })
+
+  it('reads the setting from the caller-supplied value when no runtime is available', () => {
+    expect(
+      supportsStructuredAgentSessions({
+        clientKind: 'runtime',
+        clientCapabilities: CAPABLE,
+        structuredNativeChatEnabled: true
+      })
+    ).toBe(true)
+    expect(
+      supportsStructuredAgentSessions({
+        clientKind: 'runtime',
+        clientCapabilities: CAPABLE,
+        structuredNativeChatEnabled: false
+      })
+    ).toBe(false)
+  })
+
+  it('treats an unreadable settings store as off rather than admitting', () => {
+    expect(
+      supportsStructuredAgentSessions({
+        clientKind: 'runtime',
+        clientCapabilities: CAPABLE,
+        runtime: {
+          getClientSettings: () => {
+            throw new Error('settings unavailable')
+          }
+        } as unknown as Pick<OrcaRuntimeService, 'getClientSettings'>
+      })
+    ).toBe(false)
+  })
+})