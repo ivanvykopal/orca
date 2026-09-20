paidaxingyo666/Manta

fix(relay): one malformed pre-auth handshake frame closes its connection, not the daemon (#19879)
* fix(relay): one malformed pre-auth handshake frame closes its connection, not the daemon

parseHandshakeMessage returned whatever JSON.parse produced, and the daemon
interpolates the peer's version into a log line before any credential check.
A version that is an object with a non-callable toString throws TypeError
there, inside the frame-decoder callback. FrameDecoder.drainTurn wrapped its
synchronous dispatch in try/finally with no catch, so the throw escaped
feed(), escaped the socket data handler, and reached uncaughtException: the
relay daemon exited and every PTY and agent session it held died with it.

Two layers, because only the second closes the class:

- parseHandshakeMessage now requires the string fields each arm carries
  (version; expected/got) and rejects a non-object payload. Both readers
  share the parser, so neither side can interpolate a non-string again.
- FrameDecoder contains a frame owner that throws on the synchronous turn
  the same way it already contained one on a continuation turn: reset the
  residue and report one FrameDecoderContinuationError to onError. Every
  owner's onError already closes its own connection, so any future throw
  of this shape costs one connection instead of the process.

The relay CLI channel gains an explicit onError so a malformed reply still
ends that one-shot command instead of parking it.

* fix(relay): keep the diagnostic the refusal path exists to produce

Two error paths that destroy their own evidence.

`parseHandshakeMessage`'s unknown-type refusal interpolated `String(t)` on a
peer-supplied value: `{"type":{"toString":1}}` makes String() throw "Cannot
convert object to primitive value", so the refusal arrives without naming what
was refused. `describeRelayProtocolVersion` guards this exact hazard two files
away; the sibling was missed.

`runRelayOrcaCliChannel`'s new `onDecodeError` wrote to stderr and 

src/relay/protocol-handshake.test.ts
@@ -61,6 +61,84 @@ describe('handshake framing', () => {
     expect(() => parseHandshakeMessage(bogus)).toThrow(/Unknown handshake type/)
   })
 
+  // `type` is peer-supplied, so it can be an object whose String() conversion throws — which
+  // replaced the one diagnostic this refusal exists to produce with a primitive-conversion error.
+  it('still names the refusal when the peer type cannot be stringified', () => {
+    const hostile = Buffer.from(JSON.stringify({ type: { toString: 1 } }))
+    expect(() => parseHandshakeMessage(hostile)).toThrow(/Unknown handshake type: object/)
+  })
+
+  // The daemon logs the peer's version before any credential check, and `JSON.parse` can hand
+  // back a value a template literal throws on. The parser is the one place every reader shares.
+  it('rejects a version that is not a string on both arms that carry one', () => {
+    for (const type of ['manta-relay-handshake', 'manta-relay-handshake-ok']) {
+      for (const version of [{ toString: 1 }, 7, null, undefined, ['0.1.0']]) {
+        const payload = Buffer.from(JSON.stringify({ type, version }))
+        expect(
+          () => parseHandshakeMessage(payload),
+          `${type} version=${JSON.stringify(version)}`
+        ).toThrow(/Handshake field version is not a string/)
+      }
+    }
+  })
+
+  it('rejects a mismatch reply whose expected or got is not a string', () => {
+    const type = 'manta-relay-handshake-mismatch'
+    expect(() =>
+      parseHandshakeMessage(Buffer.from(JSON.stringify({ type, expected: {}, got: 'b' })))
+    ).toThrow(/Handshake field expected is not a string/)
+    expect(() =>
+      parseHandshakeMessage(Buffer.from(JSON.stringify({ type, expected: 'a', got: 1 })))
+    ).toThrow(/Handshake field got is not a string/)
+  })
+
+  it('rejects payloads that are not objects', () => {
+    for (const payload of ['null', '"manta-relay-handshake"', '42']) {
+      expect(() => parseHandshakeMessage(Buffer.from(payload)), payload).toThrow(
+        /Handshake payload is not an object/
+      )
+    }
+  })
+
+  // endpointCredential is the one optional field, and it is the most pre-auth thing on the frame.
+  // Its only reader compares it, so a non-string refuses today by inequality rather than by type —
+  // which is luck, not a guarantee. Prove it at the parser, where every reader shares it.
+  it('rejects a present endpointCredential that is not a string', () => {
+    for (const endpointCredential of [{ toString: 1 }, 7, null, ['secret'], true]) {
+      const payload = Buffer.from(
+        JSON.stringify({ type: 'manta-relay-handshake', version: '0.1.0', endpointCredential })
+      )
+      expect(
+        () => parseHandshakeMessage(payload),
+        `endpointCredential=${JSON.stringify(endpointCredential)}`
+      ).toThrow(/Handshake field endpointCredential is not a string/)
+    }
+  })
+
+  // Absent must stay absent: a bridge that legitimately presents no credential is the common case,
+  // and refusing it here would close every unauthenticated-endpoint connection in the fleet.
+  it('still accepts a handshake with no endpointCredential, and one with a string', () => {
+    const bare = Buffer.from(JSON.stringify({ type: 'manta-relay-handshake', version: '0.1.0' }))
+    expect(parseHandshakeMessage(bare)).toEqual({ type: 'manta-relay-handshake', version: '0.1.0' })
+    const withCredential = Buffer.from(
+      JSON.stringify({ type: 'manta-relay-handshake', version: '0.1.0', endpointCredential: 'sec' })
+    )
+    expect(parseHandshakeMessage(withCredential)).toEqual({
+      type: 'manta-relay-handshake',
+      version: '0.1.0',
+      endpointCredential: 'sec'
+    })
+  })
+
+  it('still accepts a credential-mismatch reply, which carries no fields', () => {
+    const payload = Buffer.from(
+      JSON.stringify({ type: 'manta-relay-handshake-credential-mismatch' })
+    )
+    expect(parseHandshakeMessage(payload)).toEqual({
+      type: 'manta-relay-handshake-credential-mismatch'
+    })
+  })
+
   it('handshake frames use a distinct MessageType from Regular and KeepAlive', () => {
     expect(MessageType.Handshake).not.toBe(MessageType.Regular)
     expect(MessageType.Handshake).not.toBe(MessageType.KeepAlive)

src/relay/protocol.ts
@@ -50,18 +50,62 @@ export function encodeHandshakeFrame(msg: HandshakeMessage): Buffer {
   return encodeFrame(MessageType.Handshake, 0, 0, payload)
 }
 
+// Why the fields are checked and not just the type: this frame arrives before any credential, and
+// both sides interpolate its version fields into log lines. `JSON.parse` can produce values a
+// template literal throws on, so anything that reaches a reader must already be a string.
+const HANDSHAKE_STRING_FIELDS: Readonly<Record<HandshakeMessage['type'], readonly string[]>> = {
+  'manta-relay-handshake': ['version'],
+  'manta-relay-handshake-ok': ['version'],
+  'manta-relay-handshake-mismatch': ['expected', 'got'],
+  'manta-relay-handshake-credential-mismatch': []
+}
+
+// Optional fields are peer-supplied too, so the parser only proves the type of what it returns if
+// it refuses a present-but-wrong one. `endpointCredential` survives today only because its single
+// reader compares it and never interpolates it; the next reader to log it would restore the bug
+// this function exists to stop. Absent stays absent — refusing that would break a bridge that
+// legitimately presents no credential.
+const HANDSHAKE_OPTIONAL_STRING_FIELDS: Readonly<
+  Record<HandshakeMessage['type'], readonly string[]>
+> = {
+  'manta-relay-handshake': ['endpointCredential'],
+  'manta-relay-handshake-ok': [],
+  'manta-relay-handshake-mismatch': [],
+  'manta-relay-handshake-credential-mismatch': []
+}
+
 export function parseHandshakeMessage(payload: Buffer): HandshakeMessage {
-  const msg = JSON.parse(payload.toString('utf-8')) as HandshakeMessage
-  const t = (msg as { type?: string }).type
-  if (
-    t !== 'manta-relay-handshake' &&
-    t !== 'manta-relay-handshake-ok' &&
-    t !== 'manta-relay-handshake-mismatch' &&
-    t !== 'manta-relay-handshake-credential-mismatch'
-  ) {
-    throw new Error(`Unknown handshake type: ${t}`)
+  const parsed: unknown = JSON.parse(payload.toString('utf-8'))
+  if (typeof parsed !== 'object' || parsed === null) {
+    throw new Error('Handshake payload is not an object')
   }
-  return msg
+  // oxlint-disable-next-line typescript/consistent-type-assertions -- SAFETY: the typeof/null guard directly above is exactly what makes this an index-able object; every read below still proves its own field.
+  const msg = parsed as Record<string, unknown>
+  const t = msg.type
+  const required =
+    typeof t === 'string' && Object.hasOwn(HANDSHAKE_STRING_FIELDS, t)
+      ? // oxlint-disable-next-line typescript/consistent-type-assertions -- SAFETY: reached only when Object.hasOwn proved t is a key of this record, on the same line.
+        HANDSHAKE_STRING_FIELDS[t as HandshakeMessage['type']]
+      : null
+  if (required === null) {
+    // Why typeof and not String(t): a peer-supplied `{ "type": { "toString": 1 } }` makes String()
+    // itself throw "Cannot convert object to primitive value", replacing the one diagnostic this
+    // line exists to produce.
+    throw new Error(`Unknown handshake type: ${typeof t === 'string' ? t : typeof t}`)
+  }
+  for (const field of required) {
+    if (typeof msg[field] !== 'string') {
+      throw new Error(`Handshake field ${field} is not a string`)
+    }
+  }
+  // oxlint-disable-next-line typescript/consistent-type-assertions -- SAFETY: the required === null bail above already refused every t that is not one of the four keys.
+  for (const field of HANDSHAKE_OPTIONAL_STRING_FIELDS[t as HandshakeMessage['type']]) {
+    if (msg[field] !== undefined && typeof msg[field] !== 'string') {
+      throw new Error(`Handshake field ${field} is not a string`)
+    }
+  }
+  // oxlint-disable-next-line typescript/consistent-type-assertions -- SAFETY: this is the one place the shape is proved: the type is one of the four literals and every field the union declares has been checked to be a string.
+  return msg as unknown as HandshakeMessage
 }
 
 export const KEEPALIVE_SEND_MS = 5_000

src/relay/relay-handshake-roundtrip.test.ts
@@ -14,6 +14,7 @@ import {
   encodeJsonRpcFrame,
   FrameDecoder,
   type DecodedFrame,
+  type HandshakeMessage,
   MessageType
 } from './protocol'
 import { relayTestSocketPath } from './relay-test-socket-path'
@@ -245,5 +246,70 @@ describe('handshake round-trip over a real Socket pair', () => {
     expect(acceptedCb).not.toHaveBeenCalled()
 
     bridgeSock.destroy()
+  })
+
+  // The daemon reads one handshake frame before any credential check, so every field on it is
+  // untrusted input. `JSON.parse` hands back objects a template literal cannot stringify, and the
+  // frame callback runs inside the decoder: a throw there used to escape the socket's data
+  // handler and take the daemon — and every PTY and agent session it held — down with it.
+  it('closes a connection whose handshake version is not a string and keeps serving', async () => {
+    const { accepted } = await startDaemon('0.1.0+server-version')
+
+    const hostile = connect(sockPath)
+    await new Promise<void>((r) => hostile.once('connect', () => r()))
+    const hostileClosed = new Promise<void>((r) => hostile.once('close', () => r()))
+    // The annotation is deliberately a lie: this is the frame a hostile peer sends, and
+    // HandshakeMessage cannot describe it. JSON.parse answers `any`, so it needs no assertion.
+    const hostileFrame: HandshakeMessage = JSON.parse(
+      '{"type":"manta-relay-handshake","version":{"toString":1}}'
+    )
+    hostile.write(encodeHandshakeFrame(hostileFrame))
+    await hostileClosed
+
+    const good = connect(sockPath)
+    await new Promise<void>((r) => good.once('connect', () => r()))
+    const acceptedCb = vi.fn<(leftover: Buffer) => void>()
+    runConnectHandshake(good, '0.1.0+server-version', { onAccepted: acceptedCb })
+    await accepted
+    await vi.waitFor(() => expect(acceptedCb).toHaveBeenCalledTimes(1))
+
+    good.destroy()
+  })
+
+  // Same class, different instance: `onAccepted` runs inside the frame callback too, so a throw
+  // from the accept path must cost that one connection and nothing else.
+  it('closes only the connection whose accept path throws', async () => {
+    let connections = 0
+    const acceptedSockets: Socket[] = []
+    server = createServer((sock) => {
+      trackServerSocket(sock)
+      connections += 1
+      const failThisOne = connections === 1
+      setupDaemonHandshake(sock, {
+        launchVersion: '0.1.0+server-version',
+        onAccepted: (s) => {
+          if (failThisOne) {
+            throw new Error('accept path failed')
+          }
+          acceptedSockets.push(s)
+        }
+      })
+    })
+    await new Promise<void>((r) => server.listen(sockPath, () => r()))
+
+    const first = connect(sockPath)
+    await new Promise<void>((r) => first.once('connect', () => r()))
+    const firstClosed = new Promise<void>((r) => first.once('close', () => r()))
+    runConnectHandshake(first, '0.1.0+server-version', { onAccepted: vi.fn() })
+    await firstClosed
+
+    const second = connect(sockPath)
+    await new Promise<void>((r) => second.once('connect', () => r()))
+    const acceptedCb = vi.fn<(leftover: Buffer) => void>()
+    runConnectHandshake(second, '0.1.0+server-version', { onAccepted: acceptedCb })
+    await vi.waitFor(() => expect(acceptedCb).toHaveBeenCalledTimes(1))
+    expect(acceptedSockets).toHaveLength(1)
+
+    second.destroy()
   })
 })

ivanvykopal/orca

src/relay/protocol-handshake.test.ts
@@ -61,6 +61,84 @@ describe('handshake framing', () => {
     expect(() => parseHandshakeMessage(bogus)).toThrow(/Unknown handshake type/)
   })
 
+  // `type` is peer-supplied, so it can be an object whose String() conversion throws — which
+  // replaced the one diagnostic this refusal exists to produce with a primitive-conversion error.
+  it('still names the refusal when the peer type cannot be stringified', () => {
+    const hostile = Buffer.from(JSON.stringify({ type: { toString: 1 } }))
+    expect(() => parseHandshakeMessage(hostile)).toThrow(/Unknown handshake type: object/)
+  })
+
+  // The daemon logs the peer's version before any credential check, and `JSON.parse` can hand
+  // back a value a template literal throws on. The parser is the one place every reader shares.
+  it('rejects a version that is not a string on both arms that carry one', () => {
+    for (const type of ['orca-relay-handshake', 'orca-relay-handshake-ok']) {
+      for (const version of [{ toString: 1 }, 7, null, undefined, ['0.1.0']]) {
+        const payload = Buffer.from(JSON.stringify({ type, version }))
+        expect(
+          () => parseHandshakeMessage(payload),
+          `${type} version=${JSON.stringify(version)}`
+        ).toThrow(/Handshake field version is not a string/)
+      }
+    }
+  })
+
+  it('rejects a mismatch reply whose expected or got is not a string', () => {
+    const type = 'orca-relay-handshake-mismatch'
+    expect(() =>
+      parseHandshakeMessage(Buffer.from(JSON.stringify({ type, expected: {}, got: 'b' })))
+    ).toThrow(/Handshake field expected is not a string/)
+    expect(() =>
+      parseHandshakeMessage(Buffer.from(JSON.stringify({ type, expected: 'a', got: 1 })))
+    ).toThrow(/Handshake field got is not a string/)
+  })
+
+  it('rejects payloads that are not objects', () => {
+    for (const payload of ['null', '"orca-relay-handshake"', '42']) {
+      expect(() => parseHandshakeMessage(Buffer.from(payload)), payload).toThrow(
+        /Handshake payload is not an object/
+      )
+    }
+  })
+
+  // endpointCredential is the one optional field, and it is the most pre-auth thing on the frame.
+  // Its only reader compares it, so a non-string refuses today by inequality rather than by type —
+  // which is luck, not a guarantee. Prove it at the parser, where every reader shares it.
+  it('rejects a present endpointCredential that is not a string', () => {
+    for (const endpointCredential of [{ toString: 1 }, 7, null, ['secret'], true]) {
+      const payload = Buffer.from(
+        JSON.stringify({ type: 'orca-relay-handshake', version: '0.1.0', endpointCredential })
+      )
+      expect(
+        () => parseHandshakeMessage(payload),
+        `endpointCredential=${JSON.stringify(endpointCredential)}`
+      ).toThrow(/Handshake field endpointCredential is not a string/)
+    }
+  })
+
+  // Absent must stay absent: a bridge that legitimately presents no credential is the common case,
+  // and refusing it here would close every unauthenticated-endpoint connection in the fleet.
+  it('still accepts a handshake with no endpointCredential, and one with a string', () => {
+    const bare = Buffer.from(JSON.stringify({ type: 'orca-relay-handshake', version: '0.1.0' }))
+    expect(parseHandshakeMessage(bare)).toEqual({ type: 'orca-relay-handshake', version: '0.1.0' })
+    const withCredential = Buffer.from(
+      JSON.stringify({ type: 'orca-relay-handshake', version: '0.1.0', endpointCredential: 'sec' })
+    )
+    expect(parseHandshakeMessage(withCredential)).toEqual({
+      type: 'orca-relay-handshake',
+      version: '0.1.0',
+      endpointCredential: 'sec'
+    })
+  })
+
+  it('still accepts a credential-mismatch reply, which carries no fields', () => {
+    const payload = Buffer.from(
+      JSON.stringify({ type: 'orca-relay-handshake-credential-mismatch' })
+    )
+    expect(parseHandshakeMessage(payload)).toEqual({
+      type: 'orca-relay-handshake-credential-mismatch'
+    })
+  })
+
   it('handshake frames use a distinct MessageType from Regular and KeepAlive', () => {
     expect(MessageType.Handshake).not.toBe(MessageType.Regular)
     expect(MessageType.Handshake).not.toBe(MessageType.KeepAlive)

src/relay/protocol.ts
@@ -50,18 +50,62 @@ export function encodeHandshakeFrame(msg: HandshakeMessage): Buffer {
   return encodeFrame(MessageType.Handshake, 0, 0, payload)
 }
 
+// Why the fields are checked and not just the type: this frame arrives before any credential, and
+// both sides interpolate its version fields into log lines. `JSON.parse` can produce values a
+// template literal throws on, so anything that reaches a reader must already be a string.
+const HANDSHAKE_STRING_FIELDS: Readonly<Record<HandshakeMessage['type'], readonly string[]>> = {
+  'orca-relay-handshake': ['version'],
+  'orca-relay-handshake-ok': ['version'],
+  'orca-relay-handshake-mismatch': ['expected', 'got'],
+  'orca-relay-handshake-credential-mismatch': []
+}
+
+// Optional fields are peer-supplied too, so the parser only proves the type of what it returns if
+// it refuses a present-but-wrong one. `endpointCredential` survives today only because its single
+// reader compares it and never interpolates it; the next reader to log it would restore the bug
+// this function exists to stop. Absent stays absent — refusing that would break a bridge that
+// legitimately presents no credential.
+const HANDSHAKE_OPTIONAL_STRING_FIELDS: Readonly<
+  Record<HandshakeMessage['type'], readonly string[]>
+> = {
+  'orca-relay-handshake': ['endpointCredential'],
+  'orca-relay-handshake-ok': [],
+  'orca-relay-handshake-mismatch': [],
+  'orca-relay-handshake-credential-mismatch': []
+}
+
 export function parseHandshakeMessage(payload: Buffer): HandshakeMessage {
-  const msg = JSON.parse(payload.toString('utf-8')) as HandshakeMessage
-  const t = (msg as { type?: string }).type
-  if (
-    t !== 'orca-relay-handshake' &&
-    t !== 'orca-relay-handshake-ok' &&
-    t !== 'orca-relay-handshake-mismatch' &&
-    t !== 'orca-relay-handshake-credential-mismatch'
-  ) {
-    throw new Error(`Unknown handshake type: ${t}`)
+  const parsed: unknown = JSON.parse(payload.toString('utf-8'))
+  if (typeof parsed !== 'object' || parsed === null) {
+    throw new Error('Handshake payload is not an object')
   }
-  return msg
+  // oxlint-disable-next-line typescript/consistent-type-assertions -- SAFETY: the typeof/null guard directly above is exactly what makes this an index-able object; every read below still proves its own field.
+  const msg = parsed as Record<string, unknown>
+  const t = msg.type
+  const required =
+    typeof t === 'string' && Object.hasOwn(HANDSHAKE_STRING_FIELDS, t)
+      ? // oxlint-disable-next-line typescript/consistent-type-assertions -- SAFETY: reached only when Object.hasOwn proved t is a key of this record, on the same line.
+        HANDSHAKE_STRING_FIELDS[t as HandshakeMessage['type']]
+      : null
+  if (required === null) {
+    // Why typeof and not String(t): a peer-supplied `{ "type": { "toString": 1 } }` makes String()
+    // itself throw "Cannot convert object to primitive value", replacing the one diagnostic this
+    // line exists to produce.
+    throw new Error(`Unknown handshake type: ${typeof t === 'string' ? t : typeof t}`)
+  }
+  for (const field of required) {
+    if (typeof msg[field] !== 'string') {
+      throw new Error(`Handshake field ${field} is not a string`)
+    }
+  }
+  // oxlint-disable-next-line typescript/consistent-type-assertions -- SAFETY: the required === null bail above already refused every t that is not one of the four keys.
+  for (const field of HANDSHAKE_OPTIONAL_STRING_FIELDS[t as HandshakeMessage['type']]) {
+    if (msg[field] !== undefined && typeof msg[field] !== 'string') {
+      throw new Error(`Handshake field ${field} is not a string`)
+    }
+  }
+  // oxlint-disable-next-line typescript/consistent-type-assertions -- SAFETY: this is the one place the shape is proved: the type is one of the four literals and every field the union declares has been checked to be a string.
+  return msg as unknown as HandshakeMessage
 }
 
 export const KEEPALIVE_SEND_MS = 5_000

src/relay/relay-handshake-roundtrip.test.ts
@@ -14,6 +14,7 @@ import {
   encodeJsonRpcFrame,
   FrameDecoder,
   type DecodedFrame,
+  type HandshakeMessage,
   MessageType
 } from './protocol'
 import { relayTestSocketPath } from './relay-test-socket-path'
@@ -245,5 +246,70 @@ describe('handshake round-trip over a real Socket pair', () => {
     expect(acceptedCb).not.toHaveBeenCalled()
 
     bridgeSock.destroy()
+  })
+
+  // The daemon reads one handshake frame before any credential check, so every field on it is
+  // untrusted input. `JSON.parse` hands back objects a template literal cannot stringify, and the
+  // frame callback runs inside the decoder: a throw there used to escape the socket's data
+  // handler and take the daemon — and every PTY and agent session it held — down with it.
+  it('closes a connection whose handshake version is not a string and keeps serving', async () => {
+    const { accepted } = await startDaemon('0.1.0+server-version')
+
+    const hostile = connect(sockPath)
+    await new Promise<void>((r) => hostile.once('connect', () => r()))
+    const hostileClosed = new Promise<void>((r) => hostile.once('close', () => r()))
+    // The annotation is deliberately a lie: this is the frame a hostile peer sends, and
+    // HandshakeMessage cannot describe it. JSON.parse answers `any`, so it needs no assertion.
+    const hostileFrame: HandshakeMessage = JSON.parse(
+      '{"type":"orca-relay-handshake","version":{"toString":1}}'
+    )
+    hostile.write(encodeHandshakeFrame(hostileFrame))
+    await hostileClosed
+
+    const good = connect(sockPath)
+    await new Promise<void>((r) => good.once('connect', () => r()))
+    const acceptedCb = vi.fn<(leftover: Buffer) => void>()
+    runConnectHandshake(good, '0.1.0+server-version', { onAccepted: acceptedCb })
+    await accepted
+    await vi.waitFor(() => expect(acceptedCb).toHaveBeenCalledTimes(1))
+
+    good.destroy()
+  })
+
+  // Same class, different instance: `onAccepted` runs inside the frame callback too, so a throw
+  // from the accept path must cost that one connection and nothing else.
+  it('closes only the connection whose accept path throws', async () => {
+    let connections = 0
+    const acceptedSockets: Socket[] = []
+    server = createServer((sock) => {
+      trackServerSocket(sock)
+      connections += 1
+      const failThisOne = connections === 1
+      setupDaemonHandshake(sock, {
+        launchVersion: '0.1.0+server-version',
+        onAccepted: (s) => {
+          if (failThisOne) {
+            throw new Error('accept path failed')
+          }
+          acceptedSockets.push(s)
+        }
+      })
+    })
+    await new Promise<void>((r) => server.listen(sockPath, () => r()))
+
+    const first = connect(sockPath)
+    await new Promise<void>((r) => first.once('connect', () => r()))
+    const firstClosed = new Promise<void>((r) => first.once('close', () => r()))
+    runConnectHandshake(first, '0.1.0+server-version', { onAccepted: vi.fn() })
+    await firstClosed
+
+    const second = connect(sockPath)
+    await new Promise<void>((r) => second.once('connect', () => r()))
+    const acceptedCb = vi.fn<(leftover: Buffer) => void>()
+    runConnectHandshake(second, '0.1.0+server-version', { onAccepted: acceptedCb })
+    await vi.waitFor(() => expect(acceptedCb).toHaveBeenCalledTimes(1))
+    expect(acceptedSockets).toHaveLength(1)
+
+    second.destroy()
   })
 })