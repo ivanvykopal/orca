paidaxingyo666/Manta

fix(mobile): two known main bugs the RPC migration preserved (#20563)
* fix(mobile): two known main bugs the RPC migration preserved

A malformed host `error` and a null settings result both reach a property read that throws.
Both are deliberate behaviour changes; the goldens move in the follow-up commit.

`hostReplyErrorTextOrFallback` passed a truthy non-string through under a `string` annotation.
Its one caller is the in-band `git.commit` failure, and every consumer of that text is display or
prompt copy: `use-mobile-create-pr-runner` and `PrSidebarCreateEmptyState` record it as a commit
failure, `use-mobile-commit-failure-recovery` hands it to `summarizeCommitFailure`, which starts
with `raw.slice(...).replace(...)`. So no consumer needs the value, and the decision is the
fallback rather than `String(value)` — the relay handler declares
`commit(): Promise<{ success: boolean; error?: string }>`, so a non-string is a malformed reply,
and `generatedCommitMessageReader` in the same domain already reads a non-string host error as
absent. The parameter stays `unknown`, which it honestly is, and the `SAFETY` cast is gone.

`useNewWorkspaceRuntimeContext` read settings through `settingsRead`, whose reader preserves
main's `boxed!.settings` throw, so a `null` or absent result threw a TypeError out of the effect —
losing the trusted-hooks publish and the available-provider computation that follow it, not just
the settings. It now uses `optionalSettingsRead`, the operation that already reads a null or
absent result as absent settings, so the reply degrades exactly the way a reply with no `settings`
member does. Reply-side only: same method, same params, same barrier, no wire change.

Claude-Session: https://claude.ai/code/session_01JNnE9qzUZMMnqpZWCqM3nb

* test(mobile): re-record the goldens the two bug fixes move

Baseline bumped to 3f71999237. Two goldens move an observation; the other 151 move only
`baseline` and `recorderSha256`, which `pilot-scenarios.json` is still 

mobile/src/components/use-new-workspace-runtime-context.test.ts
@@ -1,0 +1,115 @@
+import { createElement } from 'react'
+import { act, create, type ReactTestRenderer } from 'react-test-renderer'
+import { afterEach, describe, expect, it } from 'vitest'
+import { FakeSession } from '../transport/mobile-endpoint-supervisor-test-fakes'
+import type { RpcClient } from '../transport/rpc-client'
+import type { RpcResponse } from '../transport/types'
+import { useNewWorkspaceRuntimeContext } from './use-new-workspace-runtime-context'
+
+type RuntimeContext = ReturnType<typeof useNewWorkspaceRuntimeContext>
+type PublishedState = Pick<
+  RuntimeContext,
+  'runtimeSettings' | 'trustedMantaHooks' | 'availableProviders'
+>
+
+const TRUSTED_HOOKS = { '/repo/manta.yaml': 'sha-1' }
+const UI_WITH_TRUST = { ui: { trustedMantaHooks: TRUSTED_HOOKS } }
+const SETTINGS = { defaultTuiAgent: 'codex', visibleTaskProviders: ['github', 'linear'] }
+
+function reply(result: unknown): RpcResponse {
+  return { id: 'r', ok: true, result, _meta: { runtimeId: 'runtime-1' } }
+}
+
+/** Every prerequisite answers normally; only the two reads under test vary. */
+function clientAnswering(settingsResult: unknown, uiResult: unknown): RpcClient {
+  const client = new FakeSession('connected')
+  client.sendRequest.mockImplementation(async (method: string) => {
+    switch (method) {
+      case 'settings.get':
+        return reply(settingsResult)
+      case 'ui.get':
+        return reply(uiResult)
+      case 'preflight.check':
+        return reply({ glab: { installed: false } })
+      default:
+        return reply({ connected: false })
+    }
+  })
+  return client
+}
+
+describe('useNewWorkspaceRuntimeContext', () => {
+  let renderer: ReactTestRenderer | null = null
+  let context: RuntimeContext | null = null
+
+  afterEach(() => {
+    act(() => renderer?.unmount())
+    renderer = null
+    context = null
+  })
+
+  function Harness({ client }: { client: RpcClient }): null {
+    context = useNewWorkspaceRuntimeContext(client, true)
+    return null
+  }
+
+  /** Answering twice re-renders the live harness, so the second call is a host swap, not a remount. */
+  async function answer(settingsResult: unknown, uiResult: unknown): Promise<PublishedState> {
+    const element = createElement(Harness, { client: clientAnswering(settingsResult, uiResult) })
+    await act(async () => {
+      if (renderer) {
+        renderer.update(element)
+      } else {
+        renderer = create(element)
+      }
+    })
+    await act(async () => {})
+    const { runtimeSettings, trustedMantaHooks, availableProviders } = context!
+    return { runtimeSettings, trustedMantaHooks, availableProviders }
+  }
+
+  // A null result used to throw the `settings` property read out of the effect, skipping the
+  // provider commit the absent case still reached.
+  it.each([
+    ['null', null],
+    ['absent', undefined],
+    ['without a settings member', {}]
+  ])('degrades a %s settings result to absent settings', async (_label, settingsResult) => {
+    expect(await answer(settingsResult, UI_WITH_TRUST)).toEqual({
+      runtimeSettings: null,
+      trustedMantaHooks: TRUSTED_HOOKS,
+      availableProviders: ['github']
+    })
+  })
+
+  // Same defect on the sibling leg: `reading 'ui'` threw after the settings commit and before
+  // the provider commit.
+  it.each([
+    ['null', null],
+    ['absent', undefined],
+    ['without a ui member', {}]
+  ])('degrades a %s ui result to untrusted hooks', async (_label, uiResult) => {
+    expect(await answer({ settings: SETTINGS }, uiResult)).toEqual({
+      runtimeSettings: SETTINGS,
+      trustedMantaHooks: {},
+      availableProviders: ['github']
+    })
+  })
+
+  // The blank, not just the absence of a throw: trustedMantaHooks gates the setup-hook approval
+  // prompt in use-new-workspace-create-submit.ts, so a stale value would skip it.
+  it('blanks the trust an earlier host published when the next ui result is null', async () => {
+    expect((await answer({ settings: SETTINGS }, UI_WITH_TRUST)).trustedMantaHooks).toEqual(
+      TRUSTED_HOOKS
+    )
+    expect((await answer({ settings: SETTINGS }, null)).trustedMantaHooks).toEqual({})
+  })
+
+  it('publishes the settings and trust a host does send', async () => {
+    expect(await answer({ settings: SETTINGS }, UI_WITH_TRUST)).toEqual({
+      runtimeSettings: SETTINGS,
+      trustedMantaHooks: TRUSTED_HOOKS,
+      availableProviders: ['github']
+    })
+  })
+})

mobile/src/components/use-new-workspace-runtime-context.ts
@@ -1,4 +1,4 @@
-import { settingsRead } from '../transport/settings-read-operations'
+import { optionalSettingsRead } from '../transport/settings-read-operations'
 import { useEffect, useState } from 'react'
 import type { PersistedTrustedMantaHooks } from '../../../src/shared/manta-yaml-hook-types'
 import type { RpcClient } from '../transport/rpc-client'
@@ -9,6 +9,8 @@ import {
   type TaskProvider
 } from '../tasks/mobile-task-providers'
 import type { NewWorktreeRuntimeSettings } from './new-worktree-agent-selection'
+
+type UiGetResult = { ui?: { trustedMantaHooks?: PersistedTrustedMantaHooks } } | null | undefined
 
 function settledSuccess(entry: PromiseSettledResult<RpcResponse>): RpcSuccess | null {
   return entry.status === 'fulfilled' && entry.value.ok ? (entry.value as RpcSuccess) : null
@@ -40,7 +42,7 @@ export function useNewWorkspaceRuntimeContext(
         client.sendRequest('linear.status')
       ])
       const [settingsRes, uiRes] = await Promise.allSettled([
-        settingsRead.request(client),
+        optionalSettingsRead.request(client),
         client.sendRequest('ui.get')
       ])
       if (stale) {
@@ -48,7 +50,9 @@ export function useNewWorkspaceRuntimeContext(
       }
 
       const settingsResult =
-        settingsRes.status === 'fulfilled' ? settingsRead.interpret(settingsRes.value) : null
+        settingsRes.status === 'fulfilled'
+          ? optionalSettingsRead.interpret(settingsRes.value)
+          : null
       const settingsValue = settingsResult?.accepted
         ? // oxlint-disable-next-line typescript/consistent-type-assertions -- SAFETY: Preserve the established response shape at this boundary.
           (settingsResult.value as NewWorktreeRuntimeSettings & { visibleTaskProviders?: unknown })
@@ -58,7 +62,8 @@ export function useNewWorkspaceRuntimeContext(
       }
       const uiResult = settledSuccess(uiRes)
       if (uiResult) {
-        const ui = (uiResult.result as { ui?: { trustedMantaHooks?: PersistedTrustedMantaHooks } }).ui
+        // oxlint-disable-next-line typescript/consistent-type-assertions -- SAFETY: Preserve the established response shape at this boundary; a missing result reads as untrusted.
+        const ui = (uiResult.result as UiGetResult)?.ui
         setTrustedMantaHooks(ui?.trustedMantaHooks ?? {})
       }
 

ivanvykopal/orca

mobile/src/components/use-new-workspace-runtime-context.test.ts
@@ -1,0 +1,115 @@
+import { createElement } from 'react'
+import { act, create, type ReactTestRenderer } from 'react-test-renderer'
+import { afterEach, describe, expect, it } from 'vitest'
+import { FakeSession } from '../transport/mobile-endpoint-supervisor-test-fakes'
+import type { RpcClient } from '../transport/rpc-client'
+import type { RpcResponse } from '../transport/types'
+import { useNewWorkspaceRuntimeContext } from './use-new-workspace-runtime-context'
+
+type RuntimeContext = ReturnType<typeof useNewWorkspaceRuntimeContext>
+type PublishedState = Pick<
+  RuntimeContext,
+  'runtimeSettings' | 'trustedOrcaHooks' | 'availableProviders'
+>
+
+const TRUSTED_HOOKS = { '/repo/orca.yaml': 'sha-1' }
+const UI_WITH_TRUST = { ui: { trustedOrcaHooks: TRUSTED_HOOKS } }
+const SETTINGS = { defaultTuiAgent: 'codex', visibleTaskProviders: ['github', 'linear'] }
+
+function reply(result: unknown): RpcResponse {
+  return { id: 'r', ok: true, result, _meta: { runtimeId: 'runtime-1' } }
+}
+
+/** Every prerequisite answers normally; only the two reads under test vary. */
+function clientAnswering(settingsResult: unknown, uiResult: unknown): RpcClient {
+  const client = new FakeSession('connected')
+  client.sendRequest.mockImplementation(async (method: string) => {
+    switch (method) {
+      case 'settings.get':
+        return reply(settingsResult)
+      case 'ui.get':
+        return reply(uiResult)
+      case 'preflight.check':
+        return reply({ glab: { installed: false } })
+      default:
+        return reply({ connected: false })
+    }
+  })
+  return client
+}
+
+describe('useNewWorkspaceRuntimeContext', () => {
+  let renderer: ReactTestRenderer | null = null
+  let context: RuntimeContext | null = null
+
+  afterEach(() => {
+    act(() => renderer?.unmount())
+    renderer = null
+    context = null
+  })
+
+  function Harness({ client }: { client: RpcClient }): null {
+    context = useNewWorkspaceRuntimeContext(client, true)
+    return null
+  }
+
+  /** Answering twice re-renders the live harness, so the second call is a host swap, not a remount. */
+  async function answer(settingsResult: unknown, uiResult: unknown): Promise<PublishedState> {
+    const element = createElement(Harness, { client: clientAnswering(settingsResult, uiResult) })
+    await act(async () => {
+      if (renderer) {
+        renderer.update(element)
+      } else {
+        renderer = create(element)
+      }
+    })
+    await act(async () => {})
+    const { runtimeSettings, trustedOrcaHooks, availableProviders } = context!
+    return { runtimeSettings, trustedOrcaHooks, availableProviders }
+  }
+
+  // A null result used to throw the `settings` property read out of the effect, skipping the
+  // provider commit the absent case still reached.
+  it.each([
+    ['null', null],
+    ['absent', undefined],
+    ['without a settings member', {}]
+  ])('degrades a %s settings result to absent settings', async (_label, settingsResult) => {
+    expect(await answer(settingsResult, UI_WITH_TRUST)).toEqual({
+      runtimeSettings: null,
+      trustedOrcaHooks: TRUSTED_HOOKS,
+      availableProviders: ['github']
+    })
+  })
+
+  // Same defect on the sibling leg: `reading 'ui'` threw after the settings commit and before
+  // the provider commit.
+  it.each([
+    ['null', null],
+    ['absent', undefined],
+    ['without a ui member', {}]
+  ])('degrades a %s ui result to untrusted hooks', async (_label, uiResult) => {
+    expect(await answer({ settings: SETTINGS }, uiResult)).toEqual({
+      runtimeSettings: SETTINGS,
+      trustedOrcaHooks: {},
+      availableProviders: ['github']
+    })
+  })
+
+  // The blank, not just the absence of a throw: trustedOrcaHooks gates the setup-hook approval
+  // prompt in use-new-workspace-create-submit.ts, so a stale value would skip it.
+  it('blanks the trust an earlier host published when the next ui result is null', async () => {
+    expect((await answer({ settings: SETTINGS }, UI_WITH_TRUST)).trustedOrcaHooks).toEqual(
+      TRUSTED_HOOKS
+    )
+    expect((await answer({ settings: SETTINGS }, null)).trustedOrcaHooks).toEqual({})
+  })
+
+  it('publishes the settings and trust a host does send', async () => {
+    expect(await answer({ settings: SETTINGS }, UI_WITH_TRUST)).toEqual({
+      runtimeSettings: SETTINGS,
+      trustedOrcaHooks: TRUSTED_HOOKS,
+      availableProviders: ['github']
+    })
+  })
+})

mobile/src/components/use-new-workspace-runtime-context.ts
@@ -1,4 +1,4 @@
-import { settingsRead } from '../transport/settings-read-operations'
+import { optionalSettingsRead } from '../transport/settings-read-operations'
 import { useEffect, useState } from 'react'
 import type { PersistedTrustedOrcaHooks } from '../../../src/shared/orca-yaml-hook-types'
 import type { RpcClient } from '../transport/rpc-client'
@@ -9,6 +9,8 @@ import {
   type TaskProvider
 } from '../tasks/mobile-task-providers'
 import type { NewWorktreeRuntimeSettings } from './new-worktree-agent-selection'
+
+type UiGetResult = { ui?: { trustedOrcaHooks?: PersistedTrustedOrcaHooks } } | null | undefined
 
 function settledSuccess(entry: PromiseSettledResult<RpcResponse>): RpcSuccess | null {
   return entry.status === 'fulfilled' && entry.value.ok ? (entry.value as RpcSuccess) : null
@@ -40,7 +42,7 @@ export function useNewWorkspaceRuntimeContext(
         client.sendRequest('linear.status')
       ])
       const [settingsRes, uiRes] = await Promise.allSettled([
-        settingsRead.request(client),
+        optionalSettingsRead.request(client),
         client.sendRequest('ui.get')
       ])
       if (stale) {
@@ -48,7 +50,9 @@ export function useNewWorkspaceRuntimeContext(
       }
 
       const settingsResult =
-        settingsRes.status === 'fulfilled' ? settingsRead.interpret(settingsRes.value) : null
+        settingsRes.status === 'fulfilled'
+          ? optionalSettingsRead.interpret(settingsRes.value)
+          : null
       const settingsValue = settingsResult?.accepted
         ? // oxlint-disable-next-line typescript/consistent-type-assertions -- SAFETY: Preserve the established response shape at this boundary.
           (settingsResult.value as NewWorktreeRuntimeSettings & { visibleTaskProviders?: unknown })
@@ -58,7 +62,8 @@ export function useNewWorkspaceRuntimeContext(
       }
       const uiResult = settledSuccess(uiRes)
       if (uiResult) {
-        const ui = (uiResult.result as { ui?: { trustedOrcaHooks?: PersistedTrustedOrcaHooks } }).ui
+        // oxlint-disable-next-line typescript/consistent-type-assertions -- SAFETY: Preserve the established response shape at this boundary; a missing result reads as untrusted.
+        const ui = (uiResult.result as UiGetResult)?.ui
         setTrustedOrcaHooks(ui?.trustedOrcaHooks ?? {})
       }
 