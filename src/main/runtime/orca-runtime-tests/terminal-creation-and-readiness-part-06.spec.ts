paidaxingyo666/Manta

feat(agent-launch): one executor for agent launches, exposed as agent.launch (#19849)
* refactor(agent-launch): make the launch-mode decision surface-neutral

`decideWorkerStartMode` was the only shared answer to "structured chat session
or terminal agent?", but it lived in an orchestration-named module and spoke
orchestration's vocabulary, so the other launch surfaces could not call it.
Move the decision to `main/agent-launch/agent-launch-mode` unchanged and leave
`orchestration-worker-start-mode` as the adapter that supplies the noun.

A worker is not a special kind of launch; it is the same launch with a dispatch
attached. Naming the receipt's subject is the only thing orchestration actually
contributed, so that is the only thing the adapter keeps: "worker" in both
sentences, plus the `--terminal` wording, which reads as nonsense anywhere a
`--terminal` flag does not exist. Both are pinned, because they are asserted.

No behavior change. The receipts are byte-identical for every reachable case,
proven by running the new pin against both implementations.

Also pins the wording, which nothing was holding. The existing suites assert
`toContain` fragments ('terminal agent', 'cannot create') and the CLI suite
asserts a receipt handed to it by a mock rather than one this code produced;
all six files stayed green against a deliberately corrupted vocabulary. A
dispatch receipt is the only place a structured-to-terminal downgrade explains
itself, so the whole sentence is the contract, not a fragment of it.

* feat(agent-launch): add the launch intent and the one executor that runs it

The sequencing around the launch decision was duplicated per surface, and the
duplicate is where the bug lives. A new worktree was created agent-first, so
its startup terminal WAS the agent and the structured branch below it could
never be reached — every new-worktree launch was a PTY regardless of the user's
default. Orchestration fixed that for itself in #19431; mobile and the CLI
still ha

src/main/runtime/manta-runtime-tests/terminal-creation-and-readiness-part-06.spec.ts
@@ -103,6 +103,37 @@ describe('MantaRuntimeService', () => {
     await expect(waiting).resolves.toEqual({ exitCode: 9 })
   })
 
+  it('cancels setup completion observation when the caller aborts', async () => {
+    const runtime = new MantaRuntimeService(store)
+    runtime.setPtyController({
+      spawn: vi.fn().mockResolvedValue({ id: 'pty-cancelled-setup' }),
+      write: () => true,
+      kill: () => true,
+      getForegroundProcess: async () => null
+    })
+    runtime.attachWindow(1)
+    runtime.syncWindowGraph(1, { tabs: [], leaves: [] })
+    const { handle } = await runtime.createTerminal(`path:${TEST_WORKTREE_PATH}`)
+    ;(
+      runtime as unknown as { setupCompletionTokenByPtyId: Map<string, string> }
+    ).setupCompletionTokenByPtyId.set('pty-cancelled-setup', 'token-cancelled')
+    const unsubscribe = vi.fn()
+    vi.spyOn(runtime, 'subscribeToTerminalData').mockReturnValue(unsubscribe)
+    const controller = new AbortController()
+
+    const waiting = runtime.waitForSetupTerminalCompletion(handle, controller.signal)
+    expect(runtime.subscribeToTerminalData).toHaveBeenCalledWith(
+      'pty-cancelled-setup',
+      expect.any(Function)
+    )
+
+    const reason = new Error('cancelled')
+    controller.abort(reason)
+
+    await expect(waiting).rejects.toBe(reason)
+    expect(unsubscribe).toHaveBeenCalledOnce()
+  })
+
   it('keeps observing after an uncertain setup terminal status', async () => {
     const runtime = new MantaRuntimeService(store)
     runtime.setPtyController({

src/main/runtime/rpc/methods/agent-launch-worktree-creation.ts
@@ -1,0 +1,118 @@
+/**
+ * Creating the worktree an `agent.launch` asks for.
+ *
+ * `startupAgent` is the whole fork, and it is the same one `worker-worktree-creation` makes: a
+ * terminal launch creates the worktree agent-first, so the startup terminal IS the agent, while a
+ * structured launch creates it with no agent at all and its session is created for the worktree
+ * afterwards. Setup, default tabs, provenance and lineage are identical either way.
+ *
+ * The executor owns which side of that fork this call lands on; nothing here re-decides it.
+ */
+
+import { buildCliWorkspaceProvenance } from '../../../../shared/cli-workspace-provenance'
+import type { TuiAgent } from '../../../../shared/tui-agent'
+import type { MantaRuntimeService } from '../../manta-runtime'
+import {
+  finishAutomationWorkspaceProvenanceRequest,
+  releaseAutomationWorkspaceProvenanceRequest,
+  resolveAutomationWorkspaceProvenance
+} from '../../../automations/workspace-provenance'
+import type { AgentLaunchWorkspaceFactory } from '../../../agent-launch/agent-launch-executor'
+import type { RpcContext } from '../core'
+import { resolveRpcWorkspaceCreatorProvenance } from '../workspace-creator-context'
+import { buildManagedWorktreeCreateArgs } from './worktree-create-args'
+import type { AgentLaunchParams } from './agent-launch-schemas'
+
+type WorktreeCreateParams = Extract<
+  AgentLaunchParams['target'],
+  { kind: 'create-worktree' }
+>['create']
+
+const STRUCTURED_SETUP_WAIT_TIMEOUT_MS = 60_000
+
+export function agentLaunchWorkspaceFactory(
+  context: RpcContext,
+  agent: TuiAgent
+): AgentLaunchWorkspaceFactory {
+  return {
+    createWorktree: async ({ create, startupAgent }) => {
+      // oxlint-disable-next-line typescript/consistent-type-assertions -- SAFETY: already validated by `AgentLaunch`; the executor only removed the reserved agent fields, so the rest of the payload is the parsed shape.
+      const params = create as WorktreeCreateParams
+      const { runtime } = context
+      const repo = await runtime.showRepo(params.repo)
+      const automationProvenance = resolveAutomationWorkspaceProvenance({
+        authority: runtime,
+        repoSelector: params.repo,
+        repo,
+        request: params.automationProvenanceRequest
+      })
+      // Reserved before creation so a retry can recover; a failed attempt has to release it.
+      try {
+        const result = await runtime.createManagedWorktree({
+          ...buildManagedWorktreeCreateArgs(
+            { ...params, ...(startupAgent ? { startupAgent } : {}) },
+            {
+              automationProvenance,
+              cliProvenance: buildCliWorkspaceProvenance(params.cliProvenanceRequest, {
+                startupAgent: agent,
+                createdAt: Date.now()
+              }),
+              creatorProvenance: resolveRpcWorkspaceCreatorProvenance(context)
+            },
+            context.clientKind ? { clientKind: context.clientKind } : {}
+          ),
+          // The launch owns the agent whichever surface it settles on, so the workspace records
+          // it even when no startup terminal was created for it.
+          createdWithAgent: agent,
+          // Structured sessions have no startup command to sequence behind setup. Provision the
+          // setup terminal synchronously and attach a completion token so the launch can wait
+          // before creating the chat surface.
+          awaitTerminalProvisioning: true,
+          observeSetupCompletion: true
+        })
+        if (!startupAgent) {
+          await waitForStructuredSetup(runtime, result.setupReceipt)
+        }
+        finishAutomationWorkspaceProvenanceRequest(params.automationProvenanceRequest)
+        return {
+          worktreeId: result.worktree.id,
+          startupTerminalHandle: result.startupTerminal?.handle
+        }
+      } catch (error) {
+        releaseAutomationWorkspaceProvenanceRequest(params.automationProvenanceRequest)
+        throw error
+      }
+    }
+  }
+}
+
+async function waitForStructuredSetup(
+  runtime: Pick<MantaRuntimeService, 'waitForSetupTerminalCompletion'>,
+  receipt: Awaited<ReturnType<MantaRuntimeService['createManagedWorktree']>>['setupReceipt']
+): Promise<void> {
+  if (
+    !receipt ||
+    receipt.startupPolicy !== 'wait-for-setup' ||
+    receipt.state !== 'running' ||
+    !receipt.terminalHandle
+  ) {
+    return
+  }
+  const abort = new AbortController()
+  let timer: ReturnType<typeof setTimeout> | undefined
+  try {
+    await Promise.race([
+      runtime.waitForSetupTerminalCompletion(receipt.terminalHandle, abort.signal),
+      new Promise<void>((resolve) => {
+        timer = setTimeout(() => {
+          abort.abort(new Error('structured_setup_wait_timeout'))
+          resolve()
+        }, STRUCTURED_SETUP_WAIT_TIMEOUT_MS)
+      })
+    ])
+  } catch {
+    // Setup completion is evidence, not a reason to strand a launch when the PTY disappears.
+  } finally {
+    clearTimeout(timer)
+  }
+}

src/main/runtime/rpc/methods/agent-launch.ts
@@ -1,0 +1,116 @@
+/**
+ * `agent.launch` — the one method that starts an agent, whatever surface it turns out to be.
+ *
+ * It exists because the routing decision had no host-side home: `worktree.create` never consulted
+ * it, so any client that created a worktree with `startupAgent` got a PTY agent no matter what the
+ * user's default said. That is not fixable inside `worktree.create`, because its contract is
+ * exactly "spawn a PTY agent and hand me its `agentTerminalHandle`" — a host that quietly answered
+ * it with a structured session would hand every older client a response with no handle and no
+ * error. So `worktree.create` keeps that meaning verbatim, forever, and everything that has to
+ * choose a surface comes here instead, behind a negotiated capability.
+ *
+ * A caller therefore never asks for a mode, and must read `outcome.kind` rather than assume one:
+ * the receipt always says which surface ran and why, so a downgrade is never silent.
+ */
+
+import { AGENT_LAUNCH_RUNTIME_CAPABILITY } from '../../../../shared/protocol-version'
+import type { AgentLaunchIntent, AgentLaunchTarget } from '../../../../shared/agent-launch-intent'
+import { executeAgentLaunch } from '../../../agent-launch/agent-launch-executor'
+import type { MantaRuntimeService } from '../../manta-runtime'
+import { defineMethod, type RpcContext } from '../core'
+import { AgentLaunch, type AgentLaunchParams } from './agent-launch-schemas'
+import { agentLaunchSurfaceFactory } from './agent-launch-surfaces'
+import { agentLaunchWorkspaceFactory } from './agent-launch-worktree-creation'
+
+/**
+ * Advertising `agent.launch.v1` is a client's statement that it understands EITHER outcome — a
+ * structured session it can open, or a terminal agent. A client that can only render one of the
+ * two must keep using the surface-specific methods instead. In-process callers are the same build
+ * as the host and negotiate nothing.
+ */
+export function supportsAgentLaunch(
+  context: Pick<RpcContext, 'clientKind' | 'clientCapabilities'>
+): boolean {
+  return (
+    context.clientKind === undefined ||
+    context.clientCapabilities?.includes(AGENT_LAUNCH_RUNTIME_CAPABILITY) === true
+  )
+}
+
+/**
+ * A client addresses a workspace by selector, but the result's `worktreeId` is an id and every
+ * step below the executor re-prefixes it as `id:<worktreeId>`. Resolving here is what keeps a
+ * caller's `id:wt-7` from reaching the runtime as `id:id:wt-7`; the terminal-workspace resolver is
+ * used rather than the git-worktree one so a folder workspace is addressable too.
+ */
+async function agentLaunchTarget(
+  params: AgentLaunchParams,
+  runtime: Pick<MantaRuntimeService, 'showManagedTerminalWorkspace'>
+): Promise<AgentLaunchTarget> {
+  if (params.target.kind === 'create-worktree') {
+    return { kind: 'create-worktree', create: { ...params.target.create } }
+  }
+  const workspace = await runtime.showManagedTerminalWorkspace(params.target.worktree)
+  return { kind: 'existing', worktree: workspace.id }
+}
+
+async function agentLaunchIntent(
+  params: AgentLaunchParams,
+  runtime: MantaRuntimeService
+): Promise<AgentLaunchIntent> {
+  return {
+    agent: params.agent,
+    target: await agentLaunchTarget(params, runtime),
+    ...(params.prompt ? { prompt: params.prompt } : {}),
+    ...(params.sessionOptions ? { sessionOptions: params.sessionOptions } : {}),
+    ...(params.reuseTerminal ? { reuseTerminal: params.reuseTerminal } : {})
+  }
+}
+
+async function validateReusedTerminal(
+  intent: AgentLaunchIntent,
+  runtime: Pick<MantaRuntimeService, 'showTerminal' | 'isTerminalRunningAgent'>
+): Promise<void> {
+  if (!intent.reuseTerminal) {
+    return
+  }
+  if (intent.target.kind !== 'existing') {
+    throw new Error('agent_launch_reuse_requires_existing_workspace')
+  }
+  const terminal = await runtime.showTerminal(intent.reuseTerminal.handle)
+  if (terminal.worktreeId !== intent.target.worktree) {
+    throw new Error('agent_launch_terminal_worktree_mismatch')
+  }
+  if (!(await runtime.isTerminalRunningAgent(intent.reuseTerminal.handle))) {
+    throw new Error('agent_launch_terminal_not_running_agent')
+  }
+}
+
+export const AGENT_LAUNCH_METHODS = [
+  defineMethod({
+    name: 'agent.launch',
+    params: AgentLaunch,
+    handler: async (params, context) => {
+      if (!supportsAgentLaunch(context)) {
+        throw new Error('agent_launch_unsupported')
+      }
+      const intent = await agentLaunchIntent(params, context.runtime)
+      await validateReusedTerminal(intent, context.runtime)
+      const execute = () =>
+        executeAgentLaunch({
+          runtime: context.runtime,
+          intent,
+          surfaces: agentLaunchSurfaceFactory(context),
+          workspaces: agentLaunchWorkspaceFactory(context, intent.agent)
+        })
+      if (params.target.kind === 'create-worktree' && params.target.create.clientMutationId) {
+        return context.runtime.dedupeWorktreeCreate(
+          params.target.create.repo,
+          `agent.launch:${params.target.create.clientMutationId}`,
+          execute
+        )
+      }
+      return execute()
+    }
+  })
+]

ivanvykopal/orca

src/main/runtime/orca-runtime-tests/terminal-creation-and-readiness-part-06.spec.ts
@@ -103,6 +103,37 @@ describe('OrcaRuntimeService', () => {
     await expect(waiting).resolves.toEqual({ exitCode: 9 })
   })
 
+  it('cancels setup completion observation when the caller aborts', async () => {
+    const runtime = new OrcaRuntimeService(store)
+    runtime.setPtyController({
+      spawn: vi.fn().mockResolvedValue({ id: 'pty-cancelled-setup' }),
+      write: () => true,
+      kill: () => true,
+      getForegroundProcess: async () => null
+    })
+    runtime.attachWindow(1)
+    runtime.syncWindowGraph(1, { tabs: [], leaves: [] })
+    const { handle } = await runtime.createTerminal(`path:${TEST_WORKTREE_PATH}`)
+    ;(
+      runtime as unknown as { setupCompletionTokenByPtyId: Map<string, string> }
+    ).setupCompletionTokenByPtyId.set('pty-cancelled-setup', 'token-cancelled')
+    const unsubscribe = vi.fn()
+    vi.spyOn(runtime, 'subscribeToTerminalData').mockReturnValue(unsubscribe)
+    const controller = new AbortController()
+
+    const waiting = runtime.waitForSetupTerminalCompletion(handle, controller.signal)
+    expect(runtime.subscribeToTerminalData).toHaveBeenCalledWith(
+      'pty-cancelled-setup',
+      expect.any(Function)
+    )
+
+    const reason = new Error('cancelled')
+    controller.abort(reason)
+
+    await expect(waiting).rejects.toBe(reason)
+    expect(unsubscribe).toHaveBeenCalledOnce()
+  })
+
   it('keeps observing after an uncertain setup terminal status', async () => {
     const runtime = new OrcaRuntimeService(store)
     runtime.setPtyController({

src/main/runtime/rpc/methods/agent-launch-worktree-creation.ts
@@ -1,0 +1,118 @@
+/**
+ * Creating the worktree an `agent.launch` asks for.
+ *
+ * `startupAgent` is the whole fork, and it is the same one `worker-worktree-creation` makes: a
+ * terminal launch creates the worktree agent-first, so the startup terminal IS the agent, while a
+ * structured launch creates it with no agent at all and its session is created for the worktree
+ * afterwards. Setup, default tabs, provenance and lineage are identical either way.
+ *
+ * The executor owns which side of that fork this call lands on; nothing here re-decides it.
+ */
+
+import { buildCliWorkspaceProvenance } from '../../../../shared/cli-workspace-provenance'
+import type { TuiAgent } from '../../../../shared/tui-agent'
+import type { OrcaRuntimeService } from '../../orca-runtime'
+import {
+  finishAutomationWorkspaceProvenanceRequest,
+  releaseAutomationWorkspaceProvenanceRequest,
+  resolveAutomationWorkspaceProvenance
+} from '../../../automations/workspace-provenance'
+import type { AgentLaunchWorkspaceFactory } from '../../../agent-launch/agent-launch-executor'
+import type { RpcContext } from '../core'
+import { resolveRpcWorkspaceCreatorProvenance } from '../workspace-creator-context'
+import { buildManagedWorktreeCreateArgs } from './worktree-create-args'
+import type { AgentLaunchParams } from './agent-launch-schemas'
+
+type WorktreeCreateParams = Extract<
+  AgentLaunchParams['target'],
+  { kind: 'create-worktree' }
+>['create']
+
+const STRUCTURED_SETUP_WAIT_TIMEOUT_MS = 60_000
+
+export function agentLaunchWorkspaceFactory(
+  context: RpcContext,
+  agent: TuiAgent
+): AgentLaunchWorkspaceFactory {
+  return {
+    createWorktree: async ({ create, startupAgent }) => {
+      // oxlint-disable-next-line typescript/consistent-type-assertions -- SAFETY: already validated by `AgentLaunch`; the executor only removed the reserved agent fields, so the rest of the payload is the parsed shape.
+      const params = create as WorktreeCreateParams
+      const { runtime } = context
+      const repo = await runtime.showRepo(params.repo)
+      const automationProvenance = resolveAutomationWorkspaceProvenance({
+        authority: runtime,
+        repoSelector: params.repo,
+        repo,
+        request: params.automationProvenanceRequest
+      })
+      // Reserved before creation so a retry can recover; a failed attempt has to release it.
+      try {
+        const result = await runtime.createManagedWorktree({
+          ...buildManagedWorktreeCreateArgs(
+            { ...params, ...(startupAgent ? { startupAgent } : {}) },
+            {
+              automationProvenance,
+              cliProvenance: buildCliWorkspaceProvenance(params.cliProvenanceRequest, {
+                startupAgent: agent,
+                createdAt: Date.now()
+              }),
+              creatorProvenance: resolveRpcWorkspaceCreatorProvenance(context)
+            },
+            context.clientKind ? { clientKind: context.clientKind } : {}
+          ),
+          // The launch owns the agent whichever surface it settles on, so the workspace records
+          // it even when no startup terminal was created for it.
+          createdWithAgent: agent,
+          // Structured sessions have no startup command to sequence behind setup. Provision the
+          // setup terminal synchronously and attach a completion token so the launch can wait
+          // before creating the chat surface.
+          awaitTerminalProvisioning: true,
+          observeSetupCompletion: true
+        })
+        if (!startupAgent) {
+          await waitForStructuredSetup(runtime, result.setupReceipt)
+        }
+        finishAutomationWorkspaceProvenanceRequest(params.automationProvenanceRequest)
+        return {
+          worktreeId: result.worktree.id,
+          startupTerminalHandle: result.startupTerminal?.handle
+        }
+      } catch (error) {
+        releaseAutomationWorkspaceProvenanceRequest(params.automationProvenanceRequest)
+        throw error
+      }
+    }
+  }
+}
+
+async function waitForStructuredSetup(
+  runtime: Pick<OrcaRuntimeService, 'waitForSetupTerminalCompletion'>,
+  receipt: Awaited<ReturnType<OrcaRuntimeService['createManagedWorktree']>>['setupReceipt']
+): Promise<void> {
+  if (
+    !receipt ||
+    receipt.startupPolicy !== 'wait-for-setup' ||
+    receipt.state !== 'running' ||
+    !receipt.terminalHandle
+  ) {
+    return
+  }
+  const abort = new AbortController()
+  let timer: ReturnType<typeof setTimeout> | undefined
+  try {
+    await Promise.race([
+      runtime.waitForSetupTerminalCompletion(receipt.terminalHandle, abort.signal),
+      new Promise<void>((resolve) => {
+        timer = setTimeout(() => {
+          abort.abort(new Error('structured_setup_wait_timeout'))
+          resolve()
+        }, STRUCTURED_SETUP_WAIT_TIMEOUT_MS)
+      })
+    ])
+  } catch {
+    // Setup completion is evidence, not a reason to strand a launch when the PTY disappears.
+  } finally {
+    clearTimeout(timer)
+  }
+}

src/main/runtime/rpc/methods/agent-launch.ts
@@ -1,0 +1,116 @@
+/**
+ * `agent.launch` — the one method that starts an agent, whatever surface it turns out to be.
+ *
+ * It exists because the routing decision had no host-side home: `worktree.create` never consulted
+ * it, so any client that created a worktree with `startupAgent` got a PTY agent no matter what the
+ * user's default said. That is not fixable inside `worktree.create`, because its contract is
+ * exactly "spawn a PTY agent and hand me its `agentTerminalHandle`" — a host that quietly answered
+ * it with a structured session would hand every older client a response with no handle and no
+ * error. So `worktree.create` keeps that meaning verbatim, forever, and everything that has to
+ * choose a surface comes here instead, behind a negotiated capability.
+ *
+ * A caller therefore never asks for a mode, and must read `outcome.kind` rather than assume one:
+ * the receipt always says which surface ran and why, so a downgrade is never silent.
+ */
+
+import { AGENT_LAUNCH_RUNTIME_CAPABILITY } from '../../../../shared/protocol-version'
+import type { AgentLaunchIntent, AgentLaunchTarget } from '../../../../shared/agent-launch-intent'
+import { executeAgentLaunch } from '../../../agent-launch/agent-launch-executor'
+import type { OrcaRuntimeService } from '../../orca-runtime'
+import { defineMethod, type RpcContext } from '../core'
+import { AgentLaunch, type AgentLaunchParams } from './agent-launch-schemas'
+import { agentLaunchSurfaceFactory } from './agent-launch-surfaces'
+import { agentLaunchWorkspaceFactory } from './agent-launch-worktree-creation'
+
+/**
+ * Advertising `agent.launch.v1` is a client's statement that it understands EITHER outcome — a
+ * structured session it can open, or a terminal agent. A client that can only render one of the
+ * two must keep using the surface-specific methods instead. In-process callers are the same build
+ * as the host and negotiate nothing.
+ */
+export function supportsAgentLaunch(
+  context: Pick<RpcContext, 'clientKind' | 'clientCapabilities'>
+): boolean {
+  return (
+    context.clientKind === undefined ||
+    context.clientCapabilities?.includes(AGENT_LAUNCH_RUNTIME_CAPABILITY) === true
+  )
+}
+
+/**
+ * A client addresses a workspace by selector, but the result's `worktreeId` is an id and every
+ * step below the executor re-prefixes it as `id:<worktreeId>`. Resolving here is what keeps a
+ * caller's `id:wt-7` from reaching the runtime as `id:id:wt-7`; the terminal-workspace resolver is
+ * used rather than the git-worktree one so a folder workspace is addressable too.
+ */
+async function agentLaunchTarget(
+  params: AgentLaunchParams,
+  runtime: Pick<OrcaRuntimeService, 'showManagedTerminalWorkspace'>
+): Promise<AgentLaunchTarget> {
+  if (params.target.kind === 'create-worktree') {
+    return { kind: 'create-worktree', create: { ...params.target.create } }
+  }
+  const workspace = await runtime.showManagedTerminalWorkspace(params.target.worktree)
+  return { kind: 'existing', worktree: workspace.id }
+}
+
+async function agentLaunchIntent(
+  params: AgentLaunchParams,
+  runtime: OrcaRuntimeService
+): Promise<AgentLaunchIntent> {
+  return {
+    agent: params.agent,
+    target: await agentLaunchTarget(params, runtime),
+    ...(params.prompt ? { prompt: params.prompt } : {}),
+    ...(params.sessionOptions ? { sessionOptions: params.sessionOptions } : {}),
+    ...(params.reuseTerminal ? { reuseTerminal: params.reuseTerminal } : {})
+  }
+}
+
+async function validateReusedTerminal(
+  intent: AgentLaunchIntent,
+  runtime: Pick<OrcaRuntimeService, 'showTerminal' | 'isTerminalRunningAgent'>
+): Promise<void> {
+  if (!intent.reuseTerminal) {
+    return
+  }
+  if (intent.target.kind !== 'existing') {
+    throw new Error('agent_launch_reuse_requires_existing_workspace')
+  }
+  const terminal = await runtime.showTerminal(intent.reuseTerminal.handle)
+  if (terminal.worktreeId !== intent.target.worktree) {
+    throw new Error('agent_launch_terminal_worktree_mismatch')
+  }
+  if (!(await runtime.isTerminalRunningAgent(intent.reuseTerminal.handle))) {
+    throw new Error('agent_launch_terminal_not_running_agent')
+  }
+}
+
+export const AGENT_LAUNCH_METHODS = [
+  defineMethod({
+    name: 'agent.launch',
+    params: AgentLaunch,
+    handler: async (params, context) => {
+      if (!supportsAgentLaunch(context)) {
+        throw new Error('agent_launch_unsupported')
+      }
+      const intent = await agentLaunchIntent(params, context.runtime)
+      await validateReusedTerminal(intent, context.runtime)
+      const execute = () =>
+        executeAgentLaunch({
+          runtime: context.runtime,
+          intent,
+          surfaces: agentLaunchSurfaceFactory(context),
+          workspaces: agentLaunchWorkspaceFactory(context, intent.agent)
+        })
+      if (params.target.kind === 'create-worktree' && params.target.create.clientMutationId) {
+        return context.runtime.dedupeWorktreeCreate(
+          params.target.create.repo,
+          `agent.launch:${params.target.create.clientMutationId}`,
+          execute
+        )
+      }
+      return execute()
+    }
+  })
+]