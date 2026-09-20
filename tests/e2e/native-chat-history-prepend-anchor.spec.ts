paidaxingyo666/Manta

Native chat keeps scrolling to bottom (#20898)
* fix(native-chat): prevent auto-scroll when transcript is hidden

Stop following new messages to bottom when the chat view is not visible
(e.g., in an inactive tab). Restore scroll position when the transcript
becomes visible again.

* fix(native-chat): preserve reader scroll offset when transcript is revea

When a reader scrolls away from the latest messages and the chat tab becomes hidden, save their scroll position. If messages arrive while the tab is hidden, don't auto-scroll. When the tab is revealed, restore the saved offset instead of jumping to latest, preserving their reading context across hide/reveal cycles.

* refactor(native-chat): extract growth-append tests and status component

Move transcript growth/append test suite to dedicated growth-windowing.test.tsx file for better organization. Extract status rendering logic (errors, retry, background tasks) from NativeChatStructuredSession into NativeChatStructuredSessionStatus. Fix scroll offset preservation in test harness when transcript visibility toggles.

* refactor(native-chat): remove retry UI

Remove unused retry functionality for failed message delivery from the native chat status component. The retryableOutboxEntry state is no longer managed, so the retry button and associated handling can be safely removed.

Mirror-Of: 15cac68802361f3b9075630335d3e6d379c2a909

tests/e2e/native-chat-history-prepend-anchor.spec.ts
@@ -55,6 +55,45 @@ async function toggleTerminalTabToChatView(
   }, args)
 }
 
+async function activateNewTerminalTab(page: Page, worktreeId: string): Promise<void> {
+  await page.evaluate((id) => {
+    const store = window.__store
+    if (!store) {
+      throw new Error('Store unavailable')
+    }
+    const state = store.getState()
+    const tab = state.createTab(id, undefined, undefined, { activate: true })
+    state.setActiveTab(tab.id)
+    state.setActiveTabType('terminal')
+  }, worktreeId)
+}
+
+async function activateTerminalTab(page: Page, tabId: string): Promise<void> {
+  await page.evaluate((id) => {
+    const store = window.__store
+    if (!store) {
+      throw new Error('Store unavailable')
+    }
+    const state = store.getState()
+    state.setActiveTab(id)
+    state.setActiveTabType('terminal')
+  }, tabId)
+}
+
+async function publishHiddenLaunchMessage(
+  page: Page,
+  args: { tabId: string; text: string }
+): Promise<void> {
+  await page.evaluate(({ tabId, text }) => {
+    window.__store?.getState().seedNativeChatLaunchPrompt({
+      tabId,
+      agent: 'claude',
+      text,
+      createdAt: Date.now()
+    })
+  }, args)
+}
+
 function claudeTranscript(rowCount: number, sessionId: string): string {
   const startedAt = Date.now() - rowCount * 1_000
   return `${Array.from({ length: rowCount }, (_, index) => {
@@ -77,7 +116,7 @@ function claudeTranscript(rowCount: number, sessionId: string): string {
   }).join('\n')}\n`
 }
 
-test.describe('Native chat history prepend anchoring', () => {
+test.describe('Native chat transcript anchoring', () => {
   test('keeps the visible transcript row at the same viewport offset', async ({ mantaPage }) => {
     await waitForSessionReady(mantaPage)
     await waitForActiveWorktree(mantaPage)
@@ -197,6 +236,78 @@ test.describe('Native chat history prepend anchoring', () => {
         `content grew ${contentGrowth}px while scrollTop adjusted ${scrollAdjustment}px`
       ).toBeLessThanOrEqual(2)
       expect(Math.abs((after?.viewportOffset ?? 0) - anchor.viewportOffset)).toBeLessThanOrEqual(3)
+    } finally {
+      rmSync(scratchDir, { recursive: true, force: true })
+    }
+  })
+
+  test('keeps a detached transcript in place across a hidden update', async ({ mantaPage }) => {
+    await waitForSessionReady(mantaPage)
+    await waitForActiveWorktree(mantaPage)
+    await ensureTerminalVisible(mantaPage)
+    await waitForActiveTerminalManager(mantaPage, 30_000)
+
+    const descriptor = await waitForActivePaneHookDescriptor(mantaPage)
+    const [tabId] = descriptor.paneKey.split(':')
+    const sessionId = `e2e-hidden-scroll-${randomUUID()}`
+    const scratchDir = mkdtempSync(path.join(os.tmpdir(), 'orca-e2e-native-chat-hidden-'))
+    const transcriptPath = path.join(scratchDir, `${sessionId}.jsonl`)
+    writeFileSync(transcriptPath, claudeTranscript(TRANSCRIPT_ROWS, sessionId))
+
+    try {
+      await enableNativeChatSetting(mantaPage)
+      await seedClaudeProviderSession(mantaPage, {
+        paneKey: descriptor.paneKey,
+        worktreeId: descriptor.worktreeId,
+        sessionId,
+        transcriptPath
+      })
+      await toggleTerminalTabToChatView(mantaPage, {
+        tabId,
+        worktreeId: descriptor.worktreeId
+      })
+
+      const root = mantaPage.locator('[data-native-chat-root="true"]')
+      const scroll = mantaPage.locator('[data-native-chat-scroll]')
+      const jump = mantaPage.getByRole('button', { name: 'Jump to latest' })
+      await expect(root).toBeVisible({ timeout: 15_000 })
+      await expect(mantaPage.getByText('E2E transcript row 0649', { exact: true })).toBeAttached({
+        timeout: 30_000
+      })
+      await scroll.hover()
+      await mantaPage.mouse.wheel(0, -2_000)
+      await expect
+        .poll(async () =>
+          scroll.evaluate(
+            (element) => element.scrollHeight - element.clientHeight - element.scrollTop
+          )
+        )
+        .toBeGreaterThan(1_000)
+      const readingAt = await scroll.evaluate((element) => element.scrollTop)
+      await expect(jump).toBeVisible()
+
+      await activateNewTerminalTab(mantaPage, descriptor.worktreeId)
+      await expect(root).toBeHidden()
+      await publishHiddenLaunchMessage(mantaPage, {
+        tabId,
+        text: 'E2E update received while the transcript is hidden'
+      })
+      await activateTerminalTab(mantaPage, tabId)
+
+      await expect(root).toBeVisible({ timeout: 15_000 })
+      await expect(
+        mantaPage.getByText('E2E update received while the transcript is hidden', { exact: true })
+      ).toBeAttached()
+      await expect
+        .poll(async () =>
+          Math.abs((await scroll.evaluate((element) => element.scrollTop)) - readingAt)
+        )
+        .toBeLessThanOrEqual(2)
+      await mantaPage.waitForTimeout(500)
+      expect(
+        Math.abs((await scroll.evaluate((element) => element.scrollTop)) - readingAt)
+      ).toBeLessThanOrEqual(2)
+      await expect(jump).toBeVisible()
     } finally {
       rmSync(scratchDir, { recursive: true, force: true })
     }

ivanvykopal/orca

tests/e2e/native-chat-history-prepend-anchor.spec.ts
@@ -55,6 +55,45 @@ async function toggleTerminalTabToChatView(
   }, args)
 }
 
+async function activateNewTerminalTab(page: Page, worktreeId: string): Promise<void> {
+  await page.evaluate((id) => {
+    const store = window.__store
+    if (!store) {
+      throw new Error('Store unavailable')
+    }
+    const state = store.getState()
+    const tab = state.createTab(id, undefined, undefined, { activate: true })
+    state.setActiveTab(tab.id)
+    state.setActiveTabType('terminal')
+  }, worktreeId)
+}
+
+async function activateTerminalTab(page: Page, tabId: string): Promise<void> {
+  await page.evaluate((id) => {
+    const store = window.__store
+    if (!store) {
+      throw new Error('Store unavailable')
+    }
+    const state = store.getState()
+    state.setActiveTab(id)
+    state.setActiveTabType('terminal')
+  }, tabId)
+}
+
+async function publishHiddenLaunchMessage(
+  page: Page,
+  args: { tabId: string; text: string }
+): Promise<void> {
+  await page.evaluate(({ tabId, text }) => {
+    window.__store?.getState().seedNativeChatLaunchPrompt({
+      tabId,
+      agent: 'claude',
+      text,
+      createdAt: Date.now()
+    })
+  }, args)
+}
+
 function claudeTranscript(rowCount: number, sessionId: string): string {
   const startedAt = Date.now() - rowCount * 1_000
   return `${Array.from({ length: rowCount }, (_, index) => {
@@ -77,7 +116,7 @@ function claudeTranscript(rowCount: number, sessionId: string): string {
   }).join('\n')}\n`
 }
 
-test.describe('Native chat history prepend anchoring', () => {
+test.describe('Native chat transcript anchoring', () => {
   test('keeps the visible transcript row at the same viewport offset', async ({ orcaPage }) => {
     await waitForSessionReady(orcaPage)
     await waitForActiveWorktree(orcaPage)
@@ -197,6 +236,78 @@ test.describe('Native chat history prepend anchoring', () => {
         `content grew ${contentGrowth}px while scrollTop adjusted ${scrollAdjustment}px`
       ).toBeLessThanOrEqual(2)
       expect(Math.abs((after?.viewportOffset ?? 0) - anchor.viewportOffset)).toBeLessThanOrEqual(3)
+    } finally {
+      rmSync(scratchDir, { recursive: true, force: true })
+    }
+  })
+
+  test('keeps a detached transcript in place across a hidden update', async ({ orcaPage }) => {
+    await waitForSessionReady(orcaPage)
+    await waitForActiveWorktree(orcaPage)
+    await ensureTerminalVisible(orcaPage)
+    await waitForActiveTerminalManager(orcaPage, 30_000)
+
+    const descriptor = await waitForActivePaneHookDescriptor(orcaPage)
+    const [tabId] = descriptor.paneKey.split(':')
+    const sessionId = `e2e-hidden-scroll-${randomUUID()}`
+    const scratchDir = mkdtempSync(path.join(os.tmpdir(), 'orca-e2e-native-chat-hidden-'))
+    const transcriptPath = path.join(scratchDir, `${sessionId}.jsonl`)
+    writeFileSync(transcriptPath, claudeTranscript(TRANSCRIPT_ROWS, sessionId))
+
+    try {
+      await enableNativeChatSetting(orcaPage)
+      await seedClaudeProviderSession(orcaPage, {
+        paneKey: descriptor.paneKey,
+        worktreeId: descriptor.worktreeId,
+        sessionId,
+        transcriptPath
+      })
+      await toggleTerminalTabToChatView(orcaPage, {
+        tabId,
+        worktreeId: descriptor.worktreeId
+      })
+
+      const root = orcaPage.locator('[data-native-chat-root="true"]')
+      const scroll = orcaPage.locator('[data-native-chat-scroll]')
+      const jump = orcaPage.getByRole('button', { name: 'Jump to latest' })
+      await expect(root).toBeVisible({ timeout: 15_000 })
+      await expect(orcaPage.getByText('E2E transcript row 0649', { exact: true })).toBeAttached({
+        timeout: 30_000
+      })
+      await scroll.hover()
+      await orcaPage.mouse.wheel(0, -2_000)
+      await expect
+        .poll(async () =>
+          scroll.evaluate(
+            (element) => element.scrollHeight - element.clientHeight - element.scrollTop
+          )
+        )
+        .toBeGreaterThan(1_000)
+      const readingAt = await scroll.evaluate((element) => element.scrollTop)
+      await expect(jump).toBeVisible()
+
+      await activateNewTerminalTab(orcaPage, descriptor.worktreeId)
+      await expect(root).toBeHidden()
+      await publishHiddenLaunchMessage(orcaPage, {
+        tabId,
+        text: 'E2E update received while the transcript is hidden'
+      })
+      await activateTerminalTab(orcaPage, tabId)
+
+      await expect(root).toBeVisible({ timeout: 15_000 })
+      await expect(
+        orcaPage.getByText('E2E update received while the transcript is hidden', { exact: true })
+      ).toBeAttached()
+      await expect
+        .poll(async () =>
+          Math.abs((await scroll.evaluate((element) => element.scrollTop)) - readingAt)
+        )
+        .toBeLessThanOrEqual(2)
+      await orcaPage.waitForTimeout(500)
+      expect(
+        Math.abs((await scroll.evaluate((element) => element.scrollTop)) - readingAt)
+      ).toBeLessThanOrEqual(2)
+      await expect(jump).toBeVisible()
     } finally {
       rmSync(scratchDir, { recursive: true, force: true })
     }