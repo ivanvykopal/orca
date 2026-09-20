paidaxingyo666/Manta

test(mobile): typecheck the test files on a ratchet, and pin the reply enums where tsc looks (#21298)
* fix(mobile): move the last six reply-enum pins where tsc looks

mobile/tsconfig.json excludes *.test.ts, so a `Record<HostUnion, true>`
coverage record in a schema test is never typechecked: the two that existed
(SshConnectionStatus, GitHubProjectOwnerType) checked nothing, and the four
closed enums beside them had only a doc citation of the host type.

Each arm list moves into its schema module as hostUnionArms<Union>(), which
#21269 introduced for the same reason, and each test iterates the exported
list instead of holding its own copy:

- SSH_CONNECTION_STATUS to SshConnectionStatus
- PROJECT_OWNER_TYPE to GitHubProjectOwnerType
- DETAIL_FILE_STATUS to GitHubPRFile['status']
- PUSH_TEST_REFUSAL_REASONS and PUSH_REGISTER_REFUSAL_REASONS to the refusal
  arms of MobilePushTestResult and MobilePushRegisterResult
- SETUP_RUN_POLICIES to SetupRunPolicy

openEnum's parameter widens from a non-empty tuple to `readonly string[]` so
a hostUnionArms list can feed it. z.enum already accepts the same, so the
tuple constraint only excluded callers zod itself takes; behaviour unchanged.

Twelve mutations prove the pins: dropping one arm and adding a bogus one
each fail mobile tsc in all six places. Zero goldens move, the schemas'
behaviour being unchanged, and the 21 recording suites pass at the existing
baseline.

Claude-Session: https://claude.ai/code/session_01JNnE9qzUZMMnqpZWCqM3nb

* test(mobile): fix the type errors in eighteen test files

Found by typechecking the tests for the first time (see the config that
follows). All mechanical, none weakens a product type:

- 67 `act(() => vi.advanceTimersByTime(...))` callbacks return VitestUtils
  where act wants void, so each becomes a block. The async ones await only a
  genuinely promise-returning call, so no extra microtask tick is introduced.
- Four fixtures were stale against a product type that gained a required
  memb

mobile/src/components/new-workspace-reply-schema.ts
@@ -1,9 +1,17 @@
 import { z } from 'zod'
-import { salvagedOptional, salvagingRecord } from '../../../src/shared/zod-salvage'
+import type { SetupRunPolicy } from '../../../src/shared/manta-yaml-hook-types'
+import { hostUnionArms, salvagedOptional, salvagingRecord } from '../../../src/shared/zod-salvage'
 
 // The New Workspace drawer's own two reads. Checked against `repo.hooks`
 // (src/main/runtime/rpc/methods/repo.ts:190 → runtime.getRepoHooks) and `ui.get`
 // (src/main/runtime/rpc/methods/client-ui.ts:60), which answers `{ ui }` and nothing else.
+
+// Pinned to the host's own union through hostUnionArms: an arm added or dropped host-side fails tsc.
+export const SETUP_RUN_POLICIES = hostUnionArms<SetupRunPolicy>({
+  ask: true,
+  'run-by-default': true,
+  'skip-by-default': true
+})
 
 /**
  * The repo's setup hook, as the drawer decorates its advanced section with it.
@@ -19,7 +27,8 @@ import { salvagedOptional, salvagingRecord } from '../../../src/shared/zod-salva
  * site's `?? 'run-by-default'` still doing the defaulting: the only two comparisons against it are
  * `!== 'skip-by-default'` and `=== 'ask'`, so an arm this build does not know behaves exactly as
  * main's unrecognised string did. The arms are the host's `SetupRunPolicy`
- * (src/shared/manta-yaml-hook-types.ts:1), which is what `getEffectiveSetupRunPolicy` answers. `setupTrust` is nullable as well as optional because the
+ * (src/shared/manta-yaml-hook-types.ts:1), which is what `getEffectiveSetupRunPolicy` answers, and
+ * they are pinned to it above. `setupTrust` is nullable as well as optional because the
  * `components-setup-ask` fixture sends an explicit `null` — salvaging that as a drop would move a
  * `normal` golden for a reply the host really sends.
  */
@@ -36,10 +45,7 @@ export const newWorkspaceRepoHooksSchema = z.looseObject({
       .nullable()
   ),
   source: z.string().nullable(),
-  setupRunPolicy: salvagedOptional(
-    'setupRunPolicy',
-    z.enum(['ask', 'run-by-default', 'skip-by-default'])
-  ),
+  setupRunPolicy: salvagedOptional('setupRunPolicy', z.enum(SETUP_RUN_POLICIES)),
   setupTrust: salvagedOptional(
     'setupTrust',
     z.looseObject({ contentHash: z.string(), scriptContent: z.string() }).nullable()

mobile/src/notifications/android-foreground-push.test.ts
@@ -1,5 +1,9 @@
 import { beforeEach, expect, it, vi } from 'vitest'
-import type { Notification } from 'expo-notifications'
+import type {
+  FirebaseRemoteMessageNotification,
+  Notification,
+  NotificationTrigger
+} from 'expo-notifications'
 import { startAndroidForegroundPushPresentation } from './android-foreground-push'
 
 const mocks = vi.hoisted(() => ({
@@ -19,26 +23,84 @@ vi.mock('expo-notifications', () => ({
   scheduleNotificationAsync: mocks.schedule
 }))
 
-function notification(trigger: unknown = { type: 'push', remoteMessage: { notification: null } }) {
+// Expo's remote-message types are wide and fully required; the two builders below fill them once
+// so the fixtures below can be plain `Notification` values rather than assertions.
+const REMOTE_NOTIFICATION: FirebaseRemoteMessageNotification = {
+  body: null,
+  bodyLocalizationArgs: null,
+  bodyLocalizationKey: null,
+  channelId: null,
+  clickAction: null,
+  color: null,
+  eventTime: null,
+  icon: null,
+  imageUrl: null,
+  lightSettings: null,
+  link: null,
+  localOnly: false,
+  notificationCount: null,
+  notificationPriority: null,
+  sound: null,
+  sticky: false,
+  tag: null,
+  ticker: null,
+  title: null,
+  titleLocalizationArgs: null,
+  titleLocalizationKey: null,
+  usesDefaultLightSettings: false,
+  usesDefaultSound: false,
+  usesDefaultVibrateSettings: false,
+  vibrateTimings: null,
+  visibility: null
+}
+
+function pushTrigger(remote: FirebaseRemoteMessageNotification | null): NotificationTrigger {
   return {
+    type: 'push',
+    remoteMessage: {
+      collapseKey: null,
+      data: {},
+      from: null,
+      messageId: 'message-1',
+      messageType: null,
+      notification: remote,
+      originalPriority: 1,
+      priority: 1,
+      sentTime: 0,
+      to: null,
+      ttl: 0
+    }
+  }
+}
+
+const ORCA_PUSH_DATA: Record<string, unknown> = {
+  hostFingerprint: 'host',
+  notificationId: 'event',
+  notificationEpoch: 'epoch',
+  notificationSeq: '3',
+  paneKey: 'pane',
+  channelId: 'manta-desktop'
+}
+
+function notification(
+  trigger: NotificationTrigger = pushTrigger(null),
+  data: Record<string, unknown> = ORCA_PUSH_DATA
+): Notification {
+  return {
+    date: 0,
     request: {
       identifier: 'message-1',
       trigger,
       content: {
         title: 'Test notification',
+        subtitle: null,
         body: '',
+        categoryIdentifier: null,
         sound: 'default',
-        data: {
-          hostFingerprint: 'host',
-          notificationId: 'event',
-          notificationEpoch: 'epoch',
-          notificationSeq: '3',
-          paneKey: 'pane',
-          channelId: 'manta-desktop'
-        }
+        data
       }
     }
-  } as unknown as Notification
+  }
 }
 
 beforeEach(() => {
@@ -52,9 +114,16 @@ it('presents a title-only data push with its original identity, routing and chan
   const incoming = notification()
   mocks.receive(incoming)
   await vi.waitFor(() => expect(mocks.schedule).toHaveBeenCalledOnce())
+  // The four members the presenter forwards, named: it rebuilds content rather than passing the
+  // arriving object through, so comparing against the whole fixture would only hold by accident.
   expect(mocks.schedule).toHaveBeenCalledWith({
     identifier: incoming.request.identifier,
-    content: incoming.request.content,
+    content: {
+      title: incoming.request.content.title,
+      body: incoming.request.content.body,
+      data: incoming.request.content.data,
+      sound: 'default'
+    },
     trigger: { channelId: 'manta-desktop' }
   })
   stop()
@@ -64,18 +133,15 @@ it('presents a title-only data push with its original identity, routing and chan
 it('does not reschedule its own local notification or normal provider notifications', () => {
   startAndroidForegroundPushPresentation()
   mocks.receive(notification(null))
-  mocks.receive(notification({ type: 'channel', channelId: 'manta-desktop' }))
-  mocks.receive(notification({ type: 'push', remoteMessage: { notification: { title: 'Test' } } }))
+  mocks.receive(notification({ channelId: 'manta-desktop' }))
+  mocks.receive(notification(pushTrigger({ ...REMOTE_NOTIFICATION, title: 'Test' })))
   expect(mocks.schedule).not.toHaveBeenCalled()
 })
 
 it('leaves silent dismissals and unrelated messages alone', () => {
   startAndroidForegroundPushPresentation()
-  const incoming = notification()
-  incoming.request.content.data.kind = 'dismiss'
-  mocks.receive(incoming)
-  incoming.request.content.data = {}
-  mocks.receive(incoming)
+  mocks.receive(notification(undefined, { ...ORCA_PUSH_DATA, kind: 'dismiss' }))
+  mocks.receive(notification(undefined, {}))
   expect(mocks.schedule).not.toHaveBeenCalled()
 })
 

ivanvykopal/orca

mobile/src/components/new-workspace-reply-schema.ts
@@ -1,9 +1,17 @@
 import { z } from 'zod'
-import { salvagedOptional, salvagingRecord } from '../../../src/shared/zod-salvage'
+import type { SetupRunPolicy } from '../../../src/shared/orca-yaml-hook-types'
+import { hostUnionArms, salvagedOptional, salvagingRecord } from '../../../src/shared/zod-salvage'
 
 // The New Workspace drawer's own two reads. Checked against `repo.hooks`
 // (src/main/runtime/rpc/methods/repo.ts:190 → runtime.getRepoHooks) and `ui.get`
 // (src/main/runtime/rpc/methods/client-ui.ts:60), which answers `{ ui }` and nothing else.
+
+// Pinned to the host's own union through hostUnionArms: an arm added or dropped host-side fails tsc.
+export const SETUP_RUN_POLICIES = hostUnionArms<SetupRunPolicy>({
+  ask: true,
+  'run-by-default': true,
+  'skip-by-default': true
+})
 
 /**
  * The repo's setup hook, as the drawer decorates its advanced section with it.
@@ -19,7 +27,8 @@ import { salvagedOptional, salvagingRecord } from '../../../src/shared/zod-salva
  * site's `?? 'run-by-default'` still doing the defaulting: the only two comparisons against it are
  * `!== 'skip-by-default'` and `=== 'ask'`, so an arm this build does not know behaves exactly as
  * main's unrecognised string did. The arms are the host's `SetupRunPolicy`
- * (src/shared/orca-yaml-hook-types.ts:1), which is what `getEffectiveSetupRunPolicy` answers. `setupTrust` is nullable as well as optional because the
+ * (src/shared/orca-yaml-hook-types.ts:1), which is what `getEffectiveSetupRunPolicy` answers, and
+ * they are pinned to it above. `setupTrust` is nullable as well as optional because the
  * `components-setup-ask` fixture sends an explicit `null` — salvaging that as a drop would move a
  * `normal` golden for a reply the host really sends.
  */
@@ -36,10 +45,7 @@ export const newWorkspaceRepoHooksSchema = z.looseObject({
       .nullable()
   ),
   source: z.string().nullable(),
-  setupRunPolicy: salvagedOptional(
-    'setupRunPolicy',
-    z.enum(['ask', 'run-by-default', 'skip-by-default'])
-  ),
+  setupRunPolicy: salvagedOptional('setupRunPolicy', z.enum(SETUP_RUN_POLICIES)),
   setupTrust: salvagedOptional(
     'setupTrust',
     z.looseObject({ contentHash: z.string(), scriptContent: z.string() }).nullable()

mobile/src/notifications/android-foreground-push.test.ts
@@ -1,5 +1,9 @@
 import { beforeEach, expect, it, vi } from 'vitest'
-import type { Notification } from 'expo-notifications'
+import type {
+  FirebaseRemoteMessageNotification,
+  Notification,
+  NotificationTrigger
+} from 'expo-notifications'
 import { startAndroidForegroundPushPresentation } from './android-foreground-push'
 
 const mocks = vi.hoisted(() => ({
@@ -19,26 +23,84 @@ vi.mock('expo-notifications', () => ({
   scheduleNotificationAsync: mocks.schedule
 }))
 
-function notification(trigger: unknown = { type: 'push', remoteMessage: { notification: null } }) {
+// Expo's remote-message types are wide and fully required; the two builders below fill them once
+// so the fixtures below can be plain `Notification` values rather than assertions.
+const REMOTE_NOTIFICATION: FirebaseRemoteMessageNotification = {
+  body: null,
+  bodyLocalizationArgs: null,
+  bodyLocalizationKey: null,
+  channelId: null,
+  clickAction: null,
+  color: null,
+  eventTime: null,
+  icon: null,
+  imageUrl: null,
+  lightSettings: null,
+  link: null,
+  localOnly: false,
+  notificationCount: null,
+  notificationPriority: null,
+  sound: null,
+  sticky: false,
+  tag: null,
+  ticker: null,
+  title: null,
+  titleLocalizationArgs: null,
+  titleLocalizationKey: null,
+  usesDefaultLightSettings: false,
+  usesDefaultSound: false,
+  usesDefaultVibrateSettings: false,
+  vibrateTimings: null,
+  visibility: null
+}
+
+function pushTrigger(remote: FirebaseRemoteMessageNotification | null): NotificationTrigger {
   return {
+    type: 'push',
+    remoteMessage: {
+      collapseKey: null,
+      data: {},
+      from: null,
+      messageId: 'message-1',
+      messageType: null,
+      notification: remote,
+      originalPriority: 1,
+      priority: 1,
+      sentTime: 0,
+      to: null,
+      ttl: 0
+    }
+  }
+}
+
+const ORCA_PUSH_DATA: Record<string, unknown> = {
+  hostFingerprint: 'host',
+  notificationId: 'event',
+  notificationEpoch: 'epoch',
+  notificationSeq: '3',
+  paneKey: 'pane',
+  channelId: 'orca-desktop'
+}
+
+function notification(
+  trigger: NotificationTrigger = pushTrigger(null),
+  data: Record<string, unknown> = ORCA_PUSH_DATA
+): Notification {
+  return {
+    date: 0,
     request: {
       identifier: 'message-1',
       trigger,
       content: {
         title: 'Test notification',
+        subtitle: null,
         body: '',
+        categoryIdentifier: null,
         sound: 'default',
-        data: {
-          hostFingerprint: 'host',
-          notificationId: 'event',
-          notificationEpoch: 'epoch',
-          notificationSeq: '3',
-          paneKey: 'pane',
-          channelId: 'orca-desktop'
-        }
+        data
       }
     }
-  } as unknown as Notification
+  }
 }
 
 beforeEach(() => {
@@ -52,9 +114,16 @@ it('presents a title-only data push with its original identity, routing and chan
   const incoming = notification()
   mocks.receive(incoming)
   await vi.waitFor(() => expect(mocks.schedule).toHaveBeenCalledOnce())
+  // The four members the presenter forwards, named: it rebuilds content rather than passing the
+  // arriving object through, so comparing against the whole fixture would only hold by accident.
   expect(mocks.schedule).toHaveBeenCalledWith({
     identifier: incoming.request.identifier,
-    content: incoming.request.content,
+    content: {
+      title: incoming.request.content.title,
+      body: incoming.request.content.body,
+      data: incoming.request.content.data,
+      sound: 'default'
+    },
     trigger: { channelId: 'orca-desktop' }
   })
   stop()
@@ -64,18 +133,15 @@ it('presents a title-only data push with its original identity, routing and chan
 it('does not reschedule its own local notification or normal provider notifications', () => {
   startAndroidForegroundPushPresentation()
   mocks.receive(notification(null))
-  mocks.receive(notification({ type: 'channel', channelId: 'orca-desktop' }))
-  mocks.receive(notification({ type: 'push', remoteMessage: { notification: { title: 'Test' } } }))
+  mocks.receive(notification({ channelId: 'orca-desktop' }))
+  mocks.receive(notification(pushTrigger({ ...REMOTE_NOTIFICATION, title: 'Test' })))
   expect(mocks.schedule).not.toHaveBeenCalled()
 })
 
 it('leaves silent dismissals and unrelated messages alone', () => {
   startAndroidForegroundPushPresentation()
-  const incoming = notification()
-  incoming.request.content.data.kind = 'dismiss'
-  mocks.receive(incoming)
-  incoming.request.content.data = {}
-  mocks.receive(incoming)
+  mocks.receive(notification(undefined, { ...ORCA_PUSH_DATA, kind: 'dismiss' }))
+  mocks.receive(notification(undefined, {}))
   expect(mocks.schedule).not.toHaveBeenCalled()
 })
 