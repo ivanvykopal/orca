paidaxingyo666/Manta

feat(cli): show orca search now the settings toggle ships (#20677)
* feat(cli): orca search over the agent session index

`orca search <query>` calls PR 5's `aiVault.searchSessions` over the CLI's
existing runtime RPC, against the host `--environment` / `--pairing-code`
selects and no other. `orca search --index-status` calls `aiVault.searchStatus`.
It is the proof the contract works with no panel.

Every flag maps onto a contract field and nothing else: `--scope`, `--fresh`,
`--limit`, `--cursor`, repeatable `--agent` and `--path`, `--since`, `--sort`,
`--debug`, `--json`. No fan-out, no merged output, no `--host`.

One command rather than a `search status` subcommand: the query is a bare
positional, so `orca search status` could not be told apart from searching for
the word "status". `--status` is unavailable because `orchestration task-list
--status <state>` already owns the name as a valued flag.

No new runtime capability. PR 5 decided an explicit `method_not_found` refusal
maps to `unavailable/no-service`, so reusing `createSessionSearchClient` gives
an old host a plain "this host runs no session search service" answer at exit 0
instead of a raw JSON-RPC error.

`CommandSpec.repeatableFlags` scopes repeatability per command, because
`--agent` must repeat for search and stay single-valued for `worktree create`.
`help.ts` sat exactly at max-lines, so `skills-command-flag-help.ts` becomes
`command-scoped-flag-help.ts` carrying both tables at the same call-site size.

* refactor(cli): drop the search type assertions main's casting gate now rejects

Main gained a `consistent-type-assertions: never` scan in the changed-code gate
after this branch was cut, and it reported twelve assertions in the new files.

The four in the argument parser were avoidable. `readEnum` now keeps the value
`find` returns, which already carries the narrow type, and the agent filter goes
through an `isAiVaultAgent` predicate over a `Set<string>` instead of widening
the agent tuple.

The te

src/cli/root-help-text-primary.ts
@@ -14,6 +14,9 @@ export const ROOT_HELP_TEXT_PRIMARY = [
   'Agent Discovery:',
   '  agent-context             Print the machine-readable command schema for agents',
   '',
+  'Agent Sessions:',
+  '  search                    Search the full text of agent sessions on one Manta host',
+  '',
   'Accounts:',
   '  account add               Add a managed Claude or Codex account on this Manta host',
   '  account list              List managed Claude and Codex accounts on this Manta host',

src/cli/root-help-text-secondary.ts
@@ -40,6 +40,8 @@ export const ROOT_HELP_TEXT_SECONDARY = [
   '  manta status [--json]',
   '  manta diagnostics memory [--json]',
   '  manta agent-context [--json]',
+  '  manta search <query> [--scope conversation|all] [--fresh] [--limit <n>] [--cursor <c>] [--agent <id>] [--path <p>] [--since <iso>] [--sort relevance|newest] [--debug] [--json]',
+  '  manta search --index-status [--json]',
   '  manta account add [--agent claude|codex] [--json]',
   '  manta account list [--json]',
   '  manta host list [--json]',

src/cli/specs/search.test.ts
@@ -89,17 +89,15 @@ describe('manta search command spec', () => {
 })
 
 describe('manta search discovery surfaces', () => {
-  it('is hidden until the settings toggle ships', () => {
-    expect(searchSpec.hidden).toBe(true)
+  it('is listed in the root help', () => {
+    expect(ROOT_HELP_TEXT_PRIMARY).toContain('Agent Sessions:')
+    expect(ROOT_HELP_TEXT_PRIMARY).toContain(
+      '  search                    Search the full text of agent sessions on one Manta host'
+    )
+    expect(ROOT_HELP_TEXT_SECONDARY).toContain('  manta search --index-status [--json]')
   })
 
-  it('is absent from the root help', () => {
-    expect(ROOT_HELP_TEXT_PRIMARY).not.toContain('Agent Sessions:')
-    expect(ROOT_HELP_TEXT_PRIMARY).not.toContain('  search  ')
-    expect(ROOT_HELP_TEXT_SECONDARY).not.toContain('manta search')
-  })
-
-  it('still prints its own help for `manta search --help`', () => {
+  it('prints its own help for `manta search --help`', () => {
     const lines: string[] = []
     const restore = console.log
     console.log = (value: unknown) => void lines.push(String(value))
@@ -111,18 +109,20 @@ describe('manta search discovery surfaces', () => {
     expect(lines.join('\n')).toContain('Usage: manta search <query>')
   })
 
-  it('still resolves for dispatch even though it is hidden', () => {
+  it('resolves for dispatch', () => {
     expect(findCommandSpec(COMMAND_SPECS, ['search'])).toBe(searchSpec)
   })
 
-  it('is withheld from agent discovery', () => {
+  it('exposes the command to agent discovery with its positional and flags', () => {
     const command = buildAgentContext(COMMAND_SPECS).commands.find(
       (entry) => entry.command === 'search'
     )
-    expect(command).toBeUndefined()
+    expect(command?.positionalArgs).toEqual(['query'])
+    expect(command?.flags).toContain('index-status')
+    expect(command?.flags).not.toContain('page')
   })
 
-  it('is not offered as a suggestion for an unknown command', () => {
-    expect(suggestCommands(COMMAND_SPECS, ['serch'])).not.toContain('search')
+  it('is offered as a suggestion for a near-miss command', () => {
+    expect(suggestCommands(COMMAND_SPECS, ['serch'])).toContain('search')
   })
 })

ivanvykopal/orca

src/cli/root-help-text-primary.ts
@@ -14,6 +14,9 @@ export const ROOT_HELP_TEXT_PRIMARY = [
   'Agent Discovery:',
   '  agent-context             Print the machine-readable command schema for agents',
   '',
+  'Agent Sessions:',
+  '  search                    Search the full text of agent sessions on one Orca host',
+  '',
   'Accounts:',
   '  account add               Add a managed Claude or Codex account on this Orca host',
   '  account list              List managed Claude and Codex accounts on this Orca host',

src/cli/root-help-text-secondary.ts
@@ -40,6 +40,8 @@ export const ROOT_HELP_TEXT_SECONDARY = [
   '  orca status [--json]',
   '  orca diagnostics memory [--json]',
   '  orca agent-context [--json]',
+  '  orca search <query> [--scope conversation|all] [--fresh] [--limit <n>] [--cursor <c>] [--agent <id>] [--path <p>] [--since <iso>] [--sort relevance|newest] [--debug] [--json]',
+  '  orca search --index-status [--json]',
   '  orca account add [--agent claude|codex] [--json]',
   '  orca account list [--json]',
   '  orca host list [--json]',

src/cli/specs/search.test.ts
@@ -89,17 +89,15 @@ describe('orca search command spec', () => {
 })
 
 describe('orca search discovery surfaces', () => {
-  it('is hidden until the settings toggle ships', () => {
-    expect(searchSpec.hidden).toBe(true)
+  it('is listed in the root help', () => {
+    expect(ROOT_HELP_TEXT_PRIMARY).toContain('Agent Sessions:')
+    expect(ROOT_HELP_TEXT_PRIMARY).toContain(
+      '  search                    Search the full text of agent sessions on one Orca host'
+    )
+    expect(ROOT_HELP_TEXT_SECONDARY).toContain('  orca search --index-status [--json]')
   })
 
-  it('is absent from the root help', () => {
-    expect(ROOT_HELP_TEXT_PRIMARY).not.toContain('Agent Sessions:')
-    expect(ROOT_HELP_TEXT_PRIMARY).not.toContain('  search  ')
-    expect(ROOT_HELP_TEXT_SECONDARY).not.toContain('orca search')
-  })
-
-  it('still prints its own help for `orca search --help`', () => {
+  it('prints its own help for `orca search --help`', () => {
     const lines: string[] = []
     const restore = console.log
     console.log = (value: unknown) => void lines.push(String(value))
@@ -111,18 +109,20 @@ describe('orca search discovery surfaces', () => {
     expect(lines.join('\n')).toContain('Usage: orca search <query>')
   })
 
-  it('still resolves for dispatch even though it is hidden', () => {
+  it('resolves for dispatch', () => {
     expect(findCommandSpec(COMMAND_SPECS, ['search'])).toBe(searchSpec)
   })
 
-  it('is withheld from agent discovery', () => {
+  it('exposes the command to agent discovery with its positional and flags', () => {
     const command = buildAgentContext(COMMAND_SPECS).commands.find(
       (entry) => entry.command === 'search'
     )
-    expect(command).toBeUndefined()
+    expect(command?.positionalArgs).toEqual(['query'])
+    expect(command?.flags).toContain('index-status')
+    expect(command?.flags).not.toContain('page')
   })
 
-  it('is not offered as a suggestion for an unknown command', () => {
-    expect(suggestCommands(COMMAND_SPECS, ['serch'])).not.toContain('search')
+  it('is offered as a suggestion for a near-miss command', () => {
+    expect(suggestCommands(COMMAND_SPECS, ['serch'])).toContain('search')
   })
 })