paidaxingyo666/Manta

test: add accumulated-workspace terminal typing reproduction (#20934)
* test: reproduce accumulated-workspace typing latency through real PTYs

* test: make the bench harness self-checks falsifiable

Review found four assertions that could not fail and one fixture gap:

- `missingPtyArrivalCount`/`missingEchoCount` were hardcoded `0` and
  `validateExpectedSeqs` throws before them, so every assertion on them
  was vacuous and every report read `0`. The throw is the real guard and
  is already covered; drop the vestigial fields.
- An absent status controller returned an all-zero result, which satisfied
  its own accepted-equals-generated equality. Assert presence first.
- The byte-pacing control had only an upper bound, so a generator emitting
  no stream bytes passed. Add the lower bound.
- `lineageEvery: 1` built zero lineage: no ordinal satisfies
  `% 1 === 1`. Offset the interval and cover the densest setting.
- The documented control command never set ORCA_TYPING_BENCH, so it
  skipped instead of running.

Mirror-Of: bdb18003e039e754d55437ed71a40b4d84c42b15

config/scripts/run-multi-workspace-typing-bench.mjs
@@ -4,7 +4,8 @@
  *
  * Usage:
  *   pnpm bench:multi-workspace-typing [-- --panes 8 --rate-kbps 512 \
- *     --keys 48 --cadence-ms 250 --cpu-workers 4 --label before-fix]
+ *     --keys 48 --cadence-ms 250 --cpu-workers 4 --worktrees 870 \
+ *     --repositories 27 --terminal-tabs 1410 --unified-tabs 2000 --label before-fix]
  *
  * Results land in tests/tools/benchmarks/results/multi-workspace-typing-*.json.
  * Run once per build/config with distinct --label values, then diff the
@@ -15,15 +16,34 @@ import { spawn } from 'node:child_process'
 const npxCommand = process.platform === 'win32' ? 'npx.cmd' : 'npx'
 
 const knobByFlag = {
+  '--visited-workspaces': 'ORCA_TYPING_BENCH_VISITED_WORKSPACES',
+  '--load-workspaces': 'ORCA_TYPING_BENCH_LOAD_WORKSPACES',
   '--panes': 'MANTA_TYPING_BENCH_LOAD_PANES',
   '--rate-kbps': 'MANTA_TYPING_BENCH_RATE_KBPS',
   '--keys': 'MANTA_TYPING_BENCH_KEYS',
   '--cadence-ms': 'MANTA_TYPING_BENCH_KEY_CADENCE_MS',
   '--cpu-workers': 'MANTA_TYPING_BENCH_CPU_WORKERS',
+  '--worktrees': 'ORCA_TYPING_BENCH_METADATA_WORKTREES',
+  '--repositories': 'ORCA_TYPING_BENCH_METADATA_REPOSITORIES',
+  '--terminal-tabs': 'ORCA_TYPING_BENCH_METADATA_TERMINAL_TABS',
+  '--unified-tabs': 'ORCA_TYPING_BENCH_METADATA_UNIFIED_TABS',
+  '--sleepers': 'ORCA_TYPING_BENCH_METADATA_SLEEPERS',
+  '--live-statuses': 'ORCA_TYPING_BENCH_METADATA_LIVE_STATUSES',
+  '--panes-per-tab': 'ORCA_TYPING_BENCH_METADATA_PANES',
+  '--status-history': 'ORCA_TYPING_BENCH_METADATA_STATUS_HISTORY',
+  '--status-interval-ms': 'ORCA_TYPING_BENCH_METADATA_STATUS_INTERVAL_MS',
+  '--agent-rows': 'ORCA_TYPING_BENCH_AGENT_ROWS',
+  '--title-change-ms': 'ORCA_TYPING_BENCH_TITLE_CHANGE_MS',
+  '--lifecycle-ms': 'ORCA_TYPING_BENCH_LIFECYCLE_MS',
+  '--cpu-profile': 'ORCA_TYPING_BENCH_CPU_PROFILE',
+  '--pty-metadata': 'ORCA_TYPING_BENCH_PTY_METADATA',
+  '--metadata-status': 'ORCA_TYPING_BENCH_METADATA_STATUS',
+  '--metadata-titles': 'ORCA_TYPING_BENCH_METADATA_TITLES',
+  '--instrumentation': 'ORCA_TYPING_BENCH_INSTRUMENTATION',
   '--label': 'MANTA_TYPING_BENCH_LABEL'
 }
 
-const env = { ...process.env, MANTA_TYPING_BENCH: '1' }
+const env = { ...process.env, MANTA_BACKGROUND_LAUNCH: '1', MANTA_TYPING_BENCH: '1' }
 const passthroughArgs = []
 const argv = process.argv.slice(2)
 for (let i = 0; i < argv.length; i++) {

tests/e2e/terminal-typing-measurement-control.spec.ts
@@ -1,0 +1,98 @@
+import { randomUUID } from 'node:crypto'
+import { mkdirSync, rmSync, writeFileSync } from 'node:fs'
+import path from 'node:path'
+import { test, expect } from './helpers/manta-app'
+import { ensureTerminalVisible, waitForActiveWorktree, waitForSessionReady } from './helpers/store'
+import {
+  sendToTerminal,
+  waitForActivePanePtyId,
+  waitForActiveTerminalManager
+} from './helpers/terminal'
+import { waitForTerminalOutputForPtyId } from './artificial-opencode-pane-interactions'
+import { measurePacedTyping, type PacedTypingMeasurement } from './paced-terminal-typing'
+import {
+  typingProbeReadyMarker,
+  writeTypingEchoProbeScript
+} from './sustained-agent-typing-load-scripts'
+
+// File scope, not the test body: a body-level skip still builds the Electron fixtures.
+test.skip(process.env.MANTA_TYPING_BENCH !== '1', 'Opt-in benchmark calibration')
+
+test('typing measurement charges known renderer stalls to the planned schedule', async ({
+  mantaPage,
+  testRepoPath
+}, testInfo) => {
+  test.setTimeout(120_000)
+  await waitForSessionReady(mantaPage)
+  await waitForActiveWorktree(mantaPage)
+  await ensureTerminalVisible(mantaPage)
+  await waitForActiveTerminalManager(mantaPage, 30_000)
+  const ptyId = await waitForActivePanePtyId(mantaPage)
+  const reports: {
+    injectStall: boolean
+    counts: { keys: number; stalls: number }
+    measurement: PacedTypingMeasurement
+  }[] = []
+  for (const injectStall of [false, true]) {
+    const runId = randomUUID()
+    const script = path.join(testRepoPath, `typing-control-${runId}.mjs`)
+    const sidecar = path.join(testRepoPath, `typing-control-${runId}.jsonl`)
+    writeTypingEchoProbeScript(script, runId, sidecar)
+    await sendToTerminal(mantaPage, ptyId, `node ${JSON.stringify(script)}\r`)
+    await waitForTerminalOutputForPtyId(mantaPage, ptyId, typingProbeReadyMarker(runId), 15_000)
+    const control = await mantaPage.evaluateHandle((enabled) => {
+      let keys = 0
+      let stalls = 0
+      const handler = (event: KeyboardEvent): void => {
+        if (event.key.length !== 1) {
+          return
+        }
+        keys += 1
+        if (!enabled || keys % 4 !== 0) {
+          return
+        }
+        stalls += 1
+        const until = performance.now() + 120
+        while (performance.now() < until) {
+          /* Known blocking positive control. */
+        }
+      }
+      window.addEventListener('keydown', handler, true)
+      return {
+        stop: () => {
+          window.removeEventListener('keydown', handler, true)
+          return { keys, stalls }
+        }
+      }
+    }, injectStall)
+    try {
+      const measurement = await measurePacedTyping(mantaPage, runId, sidecar, {
+        keyCount: 32,
+        keyCadenceMs: 25
+      })
+      const counts = await control.evaluate((value) => value.stop())
+      reports.push({ injectStall, counts, measurement })
+      expect(counts.keys).toBe(32)
+      if (injectStall) {
+        expect(counts.stalls).toBe(8)
+        expect(measurement.inputHalfMs?.p90).toBeGreaterThanOrEqual(110)
+        expect(measurement.dispatchDelayMs?.max).toBeGreaterThanOrEqual(70)
+        expect(measurement.plannedToPtyArrivalMs?.max).toBeGreaterThanOrEqual(120)
+      }
+    } finally {
+      await control.evaluate((value) => value.stop())
+      await control.dispose()
+      await sendToTerminal(mantaPage, ptyId, '\x03')
+      rmSync(script, { force: true })
+      rmSync(sidecar, { force: true })
+    }
+  }
+  const directory = path.resolve(__dirname, '..', 'tools', 'benchmarks', 'results')
+  mkdirSync(directory, { recursive: true })
+  const reportPath = path.join(directory, `typing-measurement-control-${Date.now()}.json`)
+  writeFileSync(reportPath, JSON.stringify(reports, null, 2))
+  await testInfo.attach('measurement-control', {
+    path: reportPath,
+    contentType: 'application/json'
+  })
+})

tests/e2e/typing-scale-census.ts
@@ -1,0 +1,15 @@
+import type { Page } from '@stablyai/playwright-test'
+import type {
+  TypingDiagnosticBridge,
+  TypingLatencyReport
+} from '../../src/renderer/src/lib/typing-latency/diagnostic'
+
+/** Reads the existing census without enabling per-keystroke diagnostic instrumentation. */
+export async function readTypingScaleCensus(
+  page: Page
+): Promise<TypingLatencyReport['census'] | null> {
+  return page.evaluate(() => {
+    const target: Window & { __mantaTypingDiagnostic?: TypingDiagnosticBridge } = window
+    return target.__mantaTypingDiagnostic?.report().census ?? null
+  })
+}

ivanvykopal/orca

config/scripts/run-multi-workspace-typing-bench.mjs
@@ -4,7 +4,8 @@
  *
  * Usage:
  *   pnpm bench:multi-workspace-typing [-- --panes 8 --rate-kbps 512 \
- *     --keys 48 --cadence-ms 250 --cpu-workers 4 --label before-fix]
+ *     --keys 48 --cadence-ms 250 --cpu-workers 4 --worktrees 870 \
+ *     --repositories 27 --terminal-tabs 1410 --unified-tabs 2000 --label before-fix]
  *
  * Results land in tests/tools/benchmarks/results/multi-workspace-typing-*.json.
  * Run once per build/config with distinct --label values, then diff the
@@ -15,15 +16,34 @@ import { spawn } from 'node:child_process'
 const npxCommand = process.platform === 'win32' ? 'npx.cmd' : 'npx'
 
 const knobByFlag = {
+  '--visited-workspaces': 'ORCA_TYPING_BENCH_VISITED_WORKSPACES',
+  '--load-workspaces': 'ORCA_TYPING_BENCH_LOAD_WORKSPACES',
   '--panes': 'ORCA_TYPING_BENCH_LOAD_PANES',
   '--rate-kbps': 'ORCA_TYPING_BENCH_RATE_KBPS',
   '--keys': 'ORCA_TYPING_BENCH_KEYS',
   '--cadence-ms': 'ORCA_TYPING_BENCH_KEY_CADENCE_MS',
   '--cpu-workers': 'ORCA_TYPING_BENCH_CPU_WORKERS',
+  '--worktrees': 'ORCA_TYPING_BENCH_METADATA_WORKTREES',
+  '--repositories': 'ORCA_TYPING_BENCH_METADATA_REPOSITORIES',
+  '--terminal-tabs': 'ORCA_TYPING_BENCH_METADATA_TERMINAL_TABS',
+  '--unified-tabs': 'ORCA_TYPING_BENCH_METADATA_UNIFIED_TABS',
+  '--sleepers': 'ORCA_TYPING_BENCH_METADATA_SLEEPERS',
+  '--live-statuses': 'ORCA_TYPING_BENCH_METADATA_LIVE_STATUSES',
+  '--panes-per-tab': 'ORCA_TYPING_BENCH_METADATA_PANES',
+  '--status-history': 'ORCA_TYPING_BENCH_METADATA_STATUS_HISTORY',
+  '--status-interval-ms': 'ORCA_TYPING_BENCH_METADATA_STATUS_INTERVAL_MS',
+  '--agent-rows': 'ORCA_TYPING_BENCH_AGENT_ROWS',
+  '--title-change-ms': 'ORCA_TYPING_BENCH_TITLE_CHANGE_MS',
+  '--lifecycle-ms': 'ORCA_TYPING_BENCH_LIFECYCLE_MS',
+  '--cpu-profile': 'ORCA_TYPING_BENCH_CPU_PROFILE',
+  '--pty-metadata': 'ORCA_TYPING_BENCH_PTY_METADATA',
+  '--metadata-status': 'ORCA_TYPING_BENCH_METADATA_STATUS',
+  '--metadata-titles': 'ORCA_TYPING_BENCH_METADATA_TITLES',
+  '--instrumentation': 'ORCA_TYPING_BENCH_INSTRUMENTATION',
   '--label': 'ORCA_TYPING_BENCH_LABEL'
 }
 
-const env = { ...process.env, ORCA_TYPING_BENCH: '1' }
+const env = { ...process.env, ORCA_BACKGROUND_LAUNCH: '1', ORCA_TYPING_BENCH: '1' }
 const passthroughArgs = []
 const argv = process.argv.slice(2)
 for (let i = 0; i < argv.length; i++) {

tests/e2e/terminal-typing-measurement-control.spec.ts
@@ -1,0 +1,98 @@
+import { randomUUID } from 'node:crypto'
+import { mkdirSync, rmSync, writeFileSync } from 'node:fs'
+import path from 'node:path'
+import { test, expect } from './helpers/orca-app'
+import { ensureTerminalVisible, waitForActiveWorktree, waitForSessionReady } from './helpers/store'
+import {
+  sendToTerminal,
+  waitForActivePanePtyId,
+  waitForActiveTerminalManager
+} from './helpers/terminal'
+import { waitForTerminalOutputForPtyId } from './artificial-opencode-pane-interactions'
+import { measurePacedTyping, type PacedTypingMeasurement } from './paced-terminal-typing'
+import {
+  typingProbeReadyMarker,
+  writeTypingEchoProbeScript
+} from './sustained-agent-typing-load-scripts'
+
+// File scope, not the test body: a body-level skip still builds the Electron fixtures.
+test.skip(process.env.ORCA_TYPING_BENCH !== '1', 'Opt-in benchmark calibration')
+
+test('typing measurement charges known renderer stalls to the planned schedule', async ({
+  orcaPage,
+  testRepoPath
+}, testInfo) => {
+  test.setTimeout(120_000)
+  await waitForSessionReady(orcaPage)
+  await waitForActiveWorktree(orcaPage)
+  await ensureTerminalVisible(orcaPage)
+  await waitForActiveTerminalManager(orcaPage, 30_000)
+  const ptyId = await waitForActivePanePtyId(orcaPage)
+  const reports: {
+    injectStall: boolean
+    counts: { keys: number; stalls: number }
+    measurement: PacedTypingMeasurement
+  }[] = []
+  for (const injectStall of [false, true]) {
+    const runId = randomUUID()
+    const script = path.join(testRepoPath, `typing-control-${runId}.mjs`)
+    const sidecar = path.join(testRepoPath, `typing-control-${runId}.jsonl`)
+    writeTypingEchoProbeScript(script, runId, sidecar)
+    await sendToTerminal(orcaPage, ptyId, `node ${JSON.stringify(script)}\r`)
+    await waitForTerminalOutputForPtyId(orcaPage, ptyId, typingProbeReadyMarker(runId), 15_000)
+    const control = await orcaPage.evaluateHandle((enabled) => {
+      let keys = 0
+      let stalls = 0
+      const handler = (event: KeyboardEvent): void => {
+        if (event.key.length !== 1) {
+          return
+        }
+        keys += 1
+        if (!enabled || keys % 4 !== 0) {
+          return
+        }
+        stalls += 1
+        const until = performance.now() + 120
+        while (performance.now() < until) {
+          /* Known blocking positive control. */
+        }
+      }
+      window.addEventListener('keydown', handler, true)
+      return {
+        stop: () => {
+          window.removeEventListener('keydown', handler, true)
+          return { keys, stalls }
+        }
+      }
+    }, injectStall)
+    try {
+      const measurement = await measurePacedTyping(orcaPage, runId, sidecar, {
+        keyCount: 32,
+        keyCadenceMs: 25
+      })
+      const counts = await control.evaluate((value) => value.stop())
+      reports.push({ injectStall, counts, measurement })
+      expect(counts.keys).toBe(32)
+      if (injectStall) {
+        expect(counts.stalls).toBe(8)
+        expect(measurement.inputHalfMs?.p90).toBeGreaterThanOrEqual(110)
+        expect(measurement.dispatchDelayMs?.max).toBeGreaterThanOrEqual(70)
+        expect(measurement.plannedToPtyArrivalMs?.max).toBeGreaterThanOrEqual(120)
+      }
+    } finally {
+      await control.evaluate((value) => value.stop())
+      await control.dispose()
+      await sendToTerminal(orcaPage, ptyId, '\x03')
+      rmSync(script, { force: true })
+      rmSync(sidecar, { force: true })
+    }
+  }
+  const directory = path.resolve(__dirname, '..', 'tools', 'benchmarks', 'results')
+  mkdirSync(directory, { recursive: true })
+  const reportPath = path.join(directory, `typing-measurement-control-${Date.now()}.json`)
+  writeFileSync(reportPath, JSON.stringify(reports, null, 2))
+  await testInfo.attach('measurement-control', {
+    path: reportPath,
+    contentType: 'application/json'
+  })
+})

tests/e2e/typing-scale-census.ts
@@ -1,0 +1,15 @@
+import type { Page } from '@stablyai/playwright-test'
+import type {
+  TypingDiagnosticBridge,
+  TypingLatencyReport
+} from '../../src/renderer/src/lib/typing-latency/diagnostic'
+
+/** Reads the existing census without enabling per-keystroke diagnostic instrumentation. */
+export async function readTypingScaleCensus(
+  page: Page
+): Promise<TypingLatencyReport['census'] | null> {
+  return page.evaluate(() => {
+    const target: Window & { __orcaTypingDiagnostic?: TypingDiagnosticBridge } = window
+    return target.__orcaTypingDiagnostic?.report().census ?? null
+  })
+}