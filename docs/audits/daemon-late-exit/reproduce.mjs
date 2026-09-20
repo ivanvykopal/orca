paidaxingyo666/Manta

fix(pty): reconcile daemon exits after synthetic notifications (#21000)
Co-authored-by: m4air <m4air@Mac.localdomain>

Mirror-Of: 41059f65b25d9ea1f67a3ed92ca795d98450a667

docs/audits/daemon-late-exit/reproduce.mjs
@@ -1,0 +1,120 @@
+import assert from 'node:assert/strict'
+import { createHash } from 'node:crypto'
+import { mkdtemp, readFile, rm, writeFile } from 'node:fs/promises'
+import { tmpdir } from 'node:os'
+import { join, resolve } from 'node:path'
+import { fileURLToPath, pathToFileURL } from 'node:url'
+import { startVitest } from 'vitest/node'
+
+if (process.env.MANTA_BACKGROUND_LAUNCH !== '1') {
+  throw new Error('Run with MANTA_BACKGROUND_LAUNCH=1.')
+}
+const root = fileURLToPath(new URL('../../../', import.meta.url))
+const sourcePath = join(root, 'src/main/ipc/pty/provider/bind-listeners.ts')
+const source = await readFile(sourcePath, 'utf8')
+const declaration =
+  '      const syntheticExit = session.consumeSyntheticKillExit(payload.id, payload.incarnationId)'
+const notificationFence =
+  '      // The control reply can overtake stream data; the physical exit must retire that late output.\n' +
+  '      if (syntheticExit) {\n        return\n      }'
+const restoreIntent =
+  '        if (syntheticExit) {\n          session.runtime?.markPtyStopRequested(payload.id)\n        }\n'
+for (const boundary of [declaration, notificationFence, restoreIntent]) {
+  assert(source.includes(boundary), 'Source changed: review the baseline transform.')
+}
+const before = source
+  .replace(notificationFence, '')
+  .replace(restoreIntent, '')
+  .replace(declaration, `${declaration}\n      if (syntheticExit) {\n        return\n      }`)
+const fixturePath = join(root, 'src/main/ipc/pty/daemon-late-exit-test-fixture.ts')
+const scratch = await mkdtemp(join(tmpdir(), 'orca-daemon-late-exit-proof-'))
+const phases = []
+try {
+  for (const phase of ['before', 'after']) {
+    const resultPath = join(scratch, `${phase}.json`)
+    const testPath = join(scratch, `${phase}.test.ts`)
+    const configPath = join(scratch, `${phase}.config.mjs`)
+    await writeFile(
+      testPath,
+      `
+import { afterAll, it } from ${JSON.stringify(join(root, 'node_modules/vitest/dist/index.js'))}
+import { writeFileSync } from 'node:fs'
+import { startLateExitHarness } from ${JSON.stringify(fixturePath)}
+const rows = []
+for (const scenario of ['queued-data', 'verified-stop', 'no-queued-data', 'natural-exit']) {
+  it(scenario, async () => {
+    const harness = await startLateExitHarness()
+    try {
+      if (scenario !== 'natural-exit') harness.pauseStream()
+      if (scenario !== 'no-queued-data') harness.subprocess._simulateData('final output\\r\\n')
+      if (scenario === 'natural-exit') harness.subprocess._simulateExit(0)
+      else if (scenario === 'verified-stop') { if (!await harness.stopAndWait()) throw new Error('Stop was not verified') }
+      else await harness.kill()
+      const beforeDrain = harness.runtime.captureState()
+      harness.resumeStream()
+      await harness.waitForExit()
+      const result = await harness.capture()
+      delete result.incarnationId
+      delete beforeDrain.incarnationId
+      rows.push({ scenario, beforeDrain, afterDrain: result })
+    } finally { await harness.dispose() }
+  })
+}
+afterAll(() => writeFileSync(${JSON.stringify(resultPath)}, JSON.stringify(rows)))
+`
+    )
+    await writeFile(
+      configPath,
+      `
+import base from ${JSON.stringify(pathToFileURL(join(root, 'config/vitest.config.ts')).href)}
+export default {
+  ...base,
+  plugins: [{ name: 'late-exit-baseline', enforce: 'pre', transform(code, id) {
+    if (${JSON.stringify(phase)} === 'before' && id.replaceAll('\\\\', '/').endsWith('/src/main/ipc/pty/provider/bind-listeners.ts')) return ${JSON.stringify(before)}
+  } }],
+  test: { ...base.test, include: [${JSON.stringify(testPath)}], maxWorkers: 1, fileParallelism: false }
+}
+`
+    )
+    const runner = await startVitest('test', [], {
+      root,
+      config: configPath,
+      watch: false,
+      reporters: ['dot']
+    })
+    assert(runner, 'Vitest did not start')
+    await runner.close()
+    const samples = JSON.parse(await readFile(resultPath, 'utf8'))
+    assert.equal(samples.length, 4)
+    for (const sample of samples) {
+      const leaked =
+        phase === 'before' && ['queued-data', 'verified-stop'].includes(sample.scenario)
+      assert.equal(sample.afterDrain.connected, leaked)
+      assert.equal(sample.afterDrain.headlessModelRetained, leaked)
+      assert.equal(sample.afterDrain.titleTrackerRetained, leaked)
+      assert.equal(sample.afterDrain.providerHasPty, false)
+      assert.equal(sample.afterDrain.hostInventoryCount, 0)
+      assert.equal(sample.afterDrain.rendererExitCount, 1)
+      assert.equal(sample.afterDrain.providerExitCount, 1)
+      assert.equal(sample.afterDrain.exitListenerCalls, 1)
+      assert.deepEqual(
+        sample.afterDrain.deliveredData,
+        sample.scenario === 'no-queued-data' ? [] : ['final output\r\n']
+      )
+    }
+    phases.push({ phase, samples })
+  }
+  const results = {
+    sourceSha256: createHash('sha256').update(source).digest('hex'),
+    baselineTransform:
+      'Restore duplicate suppression before provider/runtime cleanup only; keep current incarnation-fenced markers.',
+    phases
+  }
+  const output = `${JSON.stringify(results, null, 2)}\n`
+  if (process.argv[2]) {
+    await writeFile(resolve(process.argv[2]), output)
+  }
+  process.stdout.write(output)
+} finally {
+  await rm(scratch, { recursive: true, force: true })
+}

src/main/ipc/pty/daemon-late-exit-test-fixture.ts
@@ -1,0 +1,211 @@
+import type { BrowserWindow } from 'electron'
+import { Socket } from 'node:net'
+import { rmSync } from 'node:fs'
+import {
+  createMockSubprocess,
+  startDaemonAdapterHarness,
+  waitFor
+} from '../../daemon/daemon-pty-adapter-test-harness'
+import { MantaRuntimeService } from '../../runtime/manta-runtime'
+import { setPtyHostBindings, type PtyIpcSurface } from '../pty-host-bindings'
+import { consumeSyntheticKillExit, rememberSyntheticKillExit } from './delivery/exit'
+import { installPtyKillIpcHandler } from './ipc/renderer-kill'
+import { bindProviderListeners } from './provider/bind-listeners'
+import { ptyIncarnationById, ptyOwnership } from './provider/ownership-state'
+import { getLocalPtyProvider, setLocalPtyProvider } from './provider/registry'
+import { unbindLocalProviderListeners } from './provider/listener-lifecycle'
+import { shutdownProviderAndDetectExit } from './provider/shutdown-detect'
+import { finishPtyShutdown } from './provider/liveness'
+import { stopAndWaitPtyFromRuntimeController } from './runtime/kill'
+import type { PtyRuntimeControllerDeps } from './runtime/controller-deps'
+import { createPtyIpcSession } from './session'
+import type { DaemonPtyAdapter } from '../../daemon/daemon-pty-adapter'
+
+const PTY_ID = 'repo::/tmp/late-exit-audit@@terminal'
+const TAB_ID = '00000000-0000-4000-8000-000000000001'
+const LEAF_ID = '00000000-0000-4000-8000-000000000002'
+
+class LateExitRuntime extends MantaRuntimeService {
+  observeExit(listener: () => void): void {
+    this.ptyExitListenersByPtyId.set(PTY_ID, new Set([listener]))
+  }
+
+  captureState() {
+    const pty = this.ptysById.get(PTY_ID)
+    return {
+      connected: pty?.connected,
+      exitCause: pty?.lastExitCause,
+      incarnationId: pty?.incarnationId,
+      headlessModelRetained: this.headlessTerminals.has(PTY_ID),
+      titleTrackerRetained: this.ptyTitleTrackersByPtyId.has(PTY_ID),
+      liveness: this.ptyLivenessVerdictByPtyId.get(PTY_ID)?.verdict.status ?? null
+    }
+  }
+}
+
+function adapterStreamSocket(adapter: DaemonPtyAdapter): Socket {
+  const socket = adapter['client']['streamSocket']
+  if (!(socket instanceof Socket)) {
+    throw new Error('Daemon stream socket missing')
+  }
+  return socket
+}
+
+export async function startLateExitHarness() {
+  let subprocess = createMockSubprocess()
+  const harness = await startDaemonAdapterHarness(() => {
+    subprocess = createMockSubprocess()
+    return subprocess
+  })
+  const runtime = new LateExitRuntime()
+  const priorProvider = getLocalPtyProvider()
+  const deliveredData: string[] = []
+  const rendererExits: { id: string; code: number; incarnationId?: string }[] = []
+  const providerExits: { id: string; code: number; incarnationId?: string }[] = []
+  let exitListenerCalls = 0
+  const windowStub = { isDestroyed: () => false, webContents: { send() {} } }
+  const session = createPtyIpcSession({
+    // oxlint-disable-next-line typescript/consistent-type-assertions -- SAFETY: provider listeners only inspect isDestroyed and webContents.send on this headless fixture.
+    mainWindow: windowStub as unknown as BrowserWindow,
+    runtime
+  })
+  session.acceptPtyDataForRenderer = (event) => {
+    deliveredData.push(event.data)
+  }
+  session.sendPtyExitToRenderer = (event) => {
+    rendererExits.push(event)
+  }
+  session.consumeSyntheticKillExit = (id, incarnationId) =>
+    consumeSyntheticKillExit(session, id, incarnationId)
+  session.rememberSyntheticKillExit = (id, incarnationId) =>
+    rememberSyntheticKillExit(session, id, incarnationId)
+  session.sendModelRestoreNeededMarker = () => false
+  let killHandler: Parameters<PtyIpcSurface['handle']>[1] | undefined
+  setLocalPtyProvider(harness.adapter)
+  bindProviderListeners(session)
+  setPtyHostBindings({
+    ipc: {
+      handle: (channel, handler) => {
+        if (channel === 'pty:kill') {
+          killHandler = handler
+        }
+      },
+      on() {},
+      removeHandler() {},
+      removeAllListeners() {}
+    }
+  })
+  installPtyKillIpcHandler({
+    runtime,
+    getLocalPtyProviderStartupPromise: () => undefined,
+    shutdownProviderAndDetectExit,
+    rememberSyntheticKillExit: session.rememberSyntheticKillExit,
+    sendPtyExitToRenderer: session.sendPtyExitToRenderer
+  })
+  const result = await harness.adapter.spawn({ cols: 80, rows: 24, sessionId: PTY_ID })
+  ptyOwnership.set(PTY_ID, null)
+  if (result.incarnationId) {
+    ptyIncarnationById.set(PTY_ID, result.incarnationId)
+  }
+  runtime.registerPty(PTY_ID, 'repo::/tmp/late-exit-audit', null, {
+    tabId: TAB_ID,
+    leafId: LEAF_ID,
+    incarnationId: result.incarnationId
+  })
+  runtime.observeExit(() => {
+    exitListenerCalls++
+  })
+  harness.adapter.onExit((event) => {
+    providerExits.push(event)
+  })
+  const streamSocket = adapterStreamSocket(harness.adapter)
+  return {
+    ...harness,
+    runtime,
+    session,
+    result,
+    deliveredData,
+    rendererExits,
+    providerExits,
+    get subprocess() {
+      return subprocess
+    },
+    respawn: async () => {
+      harness.adapter.clearTombstone(PTY_ID)
+      const replacement = await harness.adapter.spawn({ cols: 80, rows: 24, sessionId: PTY_ID })
+      ptyOwnership.set(PTY_ID, null)
+      if (replacement.incarnationId) {
+        ptyIncarnationById.set(PTY_ID, replacement.incarnationId)
+      }
+      runtime.registerPty(PTY_ID, 'repo::/tmp/late-exit-audit', null, {
+        tabId: TAB_ID,
+        leafId: LEAF_ID,
+        incarnationId: replacement.incarnationId
+      })
+      runtime.observeExit(() => {
+        exitListenerCalls++
+      })
+      return replacement
+    },
+    id: PTY_ID,
+    deliverProviderExit: (event: { id: string; code: number; incarnationId?: string }) => {
+      // oxlint-disable-next-line unicorn/no-useless-spread -- copy-safe: exit listeners may unsubscribe while receiving the event.
+      for (const listener of [...harness.adapter['exitListeners']]) {
+        listener(event)
+      }
+    },
+    pauseStream: () => {
+      streamSocket.pause()
+    },
+    resumeStream: () => {
+      streamSocket.resume()
+    },
+    kill: async () => {
+      if (!killHandler) {
+        throw new Error('PTY kill handler missing')
+      }
+      // oxlint-disable-next-line typescript/consistent-type-assertions -- SAFETY: the installed renderer-kill handler ignores its Electron event argument.
+      await killHandler({} as never, { id: PTY_ID })
+    },
+    stopAndWait: () => {
+      const deps = {
+        runtime,
+        getLocalPtyProviderStartupPromise: () => undefined,
+        shutdownProviderAndDetectExit,
+        rememberSyntheticKillExit: session.rememberSyntheticKillExit,
+        sendPtyExitToRenderer: session.sendPtyExitToRenderer,
+        finishPtyShutdown
+      }
+      return stopAndWaitPtyFromRuntimeController(
+        // oxlint-disable-next-line typescript/consistent-type-assertions -- SAFETY: exact-stop reads only these seven controller ports and optional store; unrelated spawn ports are unused.
+        deps as unknown as PtyRuntimeControllerDeps,
+        PTY_ID
+      )
+    },
+    waitForExit: () => waitFor(() => providerExits.length > 0),
+    capture: async () => ({
+      ...runtime.captureState(),
+      providerHasPty: harness.adapter.hasPty(PTY_ID),
+      hostInventoryCount: (await harness.adapter.listProcesses()).length,
+      deliveredData: [...deliveredData],
+      rendererExitCount: rendererExits.length,
+      providerExitCount: providerExits.length,
+      exitListenerCalls
+    }),
+    dispose: async () => {
+      for (const pending of session.syntheticKillExitPtyIds.values()) {
+        clearTimeout(pending.cleanupTimer)
+      }
+      session.syntheticKillExitPtyIds.clear()
+      runtime.onPtyExit(PTY_ID, 0, runtime.captureState().incarnationId ?? undefined)
+      unbindLocalProviderListeners()
+      harness.adapter.dispose()
+      await harness.server.shutdown()
+      setLocalPtyProvider(priorProvider)
+      setPtyHostBindings({})
+      ptyOwnership.delete(PTY_ID)
+      ptyIncarnationById.delete(PTY_ID)
+      rmSync(harness.dir, { recursive: true, force: true })
+    }
+  }
+}

ivanvykopal/orca

docs/audits/daemon-late-exit/reproduce.mjs
@@ -1,0 +1,120 @@
+import assert from 'node:assert/strict'
+import { createHash } from 'node:crypto'
+import { mkdtemp, readFile, rm, writeFile } from 'node:fs/promises'
+import { tmpdir } from 'node:os'
+import { join, resolve } from 'node:path'
+import { fileURLToPath, pathToFileURL } from 'node:url'
+import { startVitest } from 'vitest/node'
+
+if (process.env.ORCA_BACKGROUND_LAUNCH !== '1') {
+  throw new Error('Run with ORCA_BACKGROUND_LAUNCH=1.')
+}
+const root = fileURLToPath(new URL('../../../', import.meta.url))
+const sourcePath = join(root, 'src/main/ipc/pty/provider/bind-listeners.ts')
+const source = await readFile(sourcePath, 'utf8')
+const declaration =
+  '      const syntheticExit = session.consumeSyntheticKillExit(payload.id, payload.incarnationId)'
+const notificationFence =
+  '      // The control reply can overtake stream data; the physical exit must retire that late output.\n' +
+  '      if (syntheticExit) {\n        return\n      }'
+const restoreIntent =
+  '        if (syntheticExit) {\n          session.runtime?.markPtyStopRequested(payload.id)\n        }\n'
+for (const boundary of [declaration, notificationFence, restoreIntent]) {
+  assert(source.includes(boundary), 'Source changed: review the baseline transform.')
+}
+const before = source
+  .replace(notificationFence, '')
+  .replace(restoreIntent, '')
+  .replace(declaration, `${declaration}\n      if (syntheticExit) {\n        return\n      }`)
+const fixturePath = join(root, 'src/main/ipc/pty/daemon-late-exit-test-fixture.ts')
+const scratch = await mkdtemp(join(tmpdir(), 'orca-daemon-late-exit-proof-'))
+const phases = []
+try {
+  for (const phase of ['before', 'after']) {
+    const resultPath = join(scratch, `${phase}.json`)
+    const testPath = join(scratch, `${phase}.test.ts`)
+    const configPath = join(scratch, `${phase}.config.mjs`)
+    await writeFile(
+      testPath,
+      `
+import { afterAll, it } from ${JSON.stringify(join(root, 'node_modules/vitest/dist/index.js'))}
+import { writeFileSync } from 'node:fs'
+import { startLateExitHarness } from ${JSON.stringify(fixturePath)}
+const rows = []
+for (const scenario of ['queued-data', 'verified-stop', 'no-queued-data', 'natural-exit']) {
+  it(scenario, async () => {
+    const harness = await startLateExitHarness()
+    try {
+      if (scenario !== 'natural-exit') harness.pauseStream()
+      if (scenario !== 'no-queued-data') harness.subprocess._simulateData('final output\\r\\n')
+      if (scenario === 'natural-exit') harness.subprocess._simulateExit(0)
+      else if (scenario === 'verified-stop') { if (!await harness.stopAndWait()) throw new Error('Stop was not verified') }
+      else await harness.kill()
+      const beforeDrain = harness.runtime.captureState()
+      harness.resumeStream()
+      await harness.waitForExit()
+      const result = await harness.capture()
+      delete result.incarnationId
+      delete beforeDrain.incarnationId
+      rows.push({ scenario, beforeDrain, afterDrain: result })
+    } finally { await harness.dispose() }
+  })
+}
+afterAll(() => writeFileSync(${JSON.stringify(resultPath)}, JSON.stringify(rows)))
+`
+    )
+    await writeFile(
+      configPath,
+      `
+import base from ${JSON.stringify(pathToFileURL(join(root, 'config/vitest.config.ts')).href)}
+export default {
+  ...base,
+  plugins: [{ name: 'late-exit-baseline', enforce: 'pre', transform(code, id) {
+    if (${JSON.stringify(phase)} === 'before' && id.replaceAll('\\\\', '/').endsWith('/src/main/ipc/pty/provider/bind-listeners.ts')) return ${JSON.stringify(before)}
+  } }],
+  test: { ...base.test, include: [${JSON.stringify(testPath)}], maxWorkers: 1, fileParallelism: false }
+}
+`
+    )
+    const runner = await startVitest('test', [], {
+      root,
+      config: configPath,
+      watch: false,
+      reporters: ['dot']
+    })
+    assert(runner, 'Vitest did not start')
+    await runner.close()
+    const samples = JSON.parse(await readFile(resultPath, 'utf8'))
+    assert.equal(samples.length, 4)
+    for (const sample of samples) {
+      const leaked =
+        phase === 'before' && ['queued-data', 'verified-stop'].includes(sample.scenario)
+      assert.equal(sample.afterDrain.connected, leaked)
+      assert.equal(sample.afterDrain.headlessModelRetained, leaked)
+      assert.equal(sample.afterDrain.titleTrackerRetained, leaked)
+      assert.equal(sample.afterDrain.providerHasPty, false)
+      assert.equal(sample.afterDrain.hostInventoryCount, 0)
+      assert.equal(sample.afterDrain.rendererExitCount, 1)
+      assert.equal(sample.afterDrain.providerExitCount, 1)
+      assert.equal(sample.afterDrain.exitListenerCalls, 1)
+      assert.deepEqual(
+        sample.afterDrain.deliveredData,
+        sample.scenario === 'no-queued-data' ? [] : ['final output\r\n']
+      )
+    }
+    phases.push({ phase, samples })
+  }
+  const results = {
+    sourceSha256: createHash('sha256').update(source).digest('hex'),
+    baselineTransform:
+      'Restore duplicate suppression before provider/runtime cleanup only; keep current incarnation-fenced markers.',
+    phases
+  }
+  const output = `${JSON.stringify(results, null, 2)}\n`
+  if (process.argv[2]) {
+    await writeFile(resolve(process.argv[2]), output)
+  }
+  process.stdout.write(output)
+} finally {
+  await rm(scratch, { recursive: true, force: true })
+}

src/main/ipc/pty/daemon-late-exit-test-fixture.ts
@@ -1,0 +1,211 @@
+import type { BrowserWindow } from 'electron'
+import { Socket } from 'node:net'
+import { rmSync } from 'node:fs'
+import {
+  createMockSubprocess,
+  startDaemonAdapterHarness,
+  waitFor
+} from '../../daemon/daemon-pty-adapter-test-harness'
+import { OrcaRuntimeService } from '../../runtime/orca-runtime'
+import { setPtyHostBindings, type PtyIpcSurface } from '../pty-host-bindings'
+import { consumeSyntheticKillExit, rememberSyntheticKillExit } from './delivery/exit'
+import { installPtyKillIpcHandler } from './ipc/renderer-kill'
+import { bindProviderListeners } from './provider/bind-listeners'
+import { ptyIncarnationById, ptyOwnership } from './provider/ownership-state'
+import { getLocalPtyProvider, setLocalPtyProvider } from './provider/registry'
+import { unbindLocalProviderListeners } from './provider/listener-lifecycle'
+import { shutdownProviderAndDetectExit } from './provider/shutdown-detect'
+import { finishPtyShutdown } from './provider/liveness'
+import { stopAndWaitPtyFromRuntimeController } from './runtime/kill'
+import type { PtyRuntimeControllerDeps } from './runtime/controller-deps'
+import { createPtyIpcSession } from './session'
+import type { DaemonPtyAdapter } from '../../daemon/daemon-pty-adapter'
+
+const PTY_ID = 'repo::/tmp/late-exit-audit@@terminal'
+const TAB_ID = '00000000-0000-4000-8000-000000000001'
+const LEAF_ID = '00000000-0000-4000-8000-000000000002'
+
+class LateExitRuntime extends OrcaRuntimeService {
+  observeExit(listener: () => void): void {
+    this.ptyExitListenersByPtyId.set(PTY_ID, new Set([listener]))
+  }
+
+  captureState() {
+    const pty = this.ptysById.get(PTY_ID)
+    return {
+      connected: pty?.connected,
+      exitCause: pty?.lastExitCause,
+      incarnationId: pty?.incarnationId,
+      headlessModelRetained: this.headlessTerminals.has(PTY_ID),
+      titleTrackerRetained: this.ptyTitleTrackersByPtyId.has(PTY_ID),
+      liveness: this.ptyLivenessVerdictByPtyId.get(PTY_ID)?.verdict.status ?? null
+    }
+  }
+}
+
+function adapterStreamSocket(adapter: DaemonPtyAdapter): Socket {
+  const socket = adapter['client']['streamSocket']
+  if (!(socket instanceof Socket)) {
+    throw new Error('Daemon stream socket missing')
+  }
+  return socket
+}
+
+export async function startLateExitHarness() {
+  let subprocess = createMockSubprocess()
+  const harness = await startDaemonAdapterHarness(() => {
+    subprocess = createMockSubprocess()
+    return subprocess
+  })
+  const runtime = new LateExitRuntime()
+  const priorProvider = getLocalPtyProvider()
+  const deliveredData: string[] = []
+  const rendererExits: { id: string; code: number; incarnationId?: string }[] = []
+  const providerExits: { id: string; code: number; incarnationId?: string }[] = []
+  let exitListenerCalls = 0
+  const windowStub = { isDestroyed: () => false, webContents: { send() {} } }
+  const session = createPtyIpcSession({
+    // oxlint-disable-next-line typescript/consistent-type-assertions -- SAFETY: provider listeners only inspect isDestroyed and webContents.send on this headless fixture.
+    mainWindow: windowStub as unknown as BrowserWindow,
+    runtime
+  })
+  session.acceptPtyDataForRenderer = (event) => {
+    deliveredData.push(event.data)
+  }
+  session.sendPtyExitToRenderer = (event) => {
+    rendererExits.push(event)
+  }
+  session.consumeSyntheticKillExit = (id, incarnationId) =>
+    consumeSyntheticKillExit(session, id, incarnationId)
+  session.rememberSyntheticKillExit = (id, incarnationId) =>
+    rememberSyntheticKillExit(session, id, incarnationId)
+  session.sendModelRestoreNeededMarker = () => false
+  let killHandler: Parameters<PtyIpcSurface['handle']>[1] | undefined
+  setLocalPtyProvider(harness.adapter)
+  bindProviderListeners(session)
+  setPtyHostBindings({
+    ipc: {
+      handle: (channel, handler) => {
+        if (channel === 'pty:kill') {
+          killHandler = handler
+        }
+      },
+      on() {},
+      removeHandler() {},
+      removeAllListeners() {}
+    }
+  })
+  installPtyKillIpcHandler({
+    runtime,
+    getLocalPtyProviderStartupPromise: () => undefined,
+    shutdownProviderAndDetectExit,
+    rememberSyntheticKillExit: session.rememberSyntheticKillExit,
+    sendPtyExitToRenderer: session.sendPtyExitToRenderer
+  })
+  const result = await harness.adapter.spawn({ cols: 80, rows: 24, sessionId: PTY_ID })
+  ptyOwnership.set(PTY_ID, null)
+  if (result.incarnationId) {
+    ptyIncarnationById.set(PTY_ID, result.incarnationId)
+  }
+  runtime.registerPty(PTY_ID, 'repo::/tmp/late-exit-audit', null, {
+    tabId: TAB_ID,
+    leafId: LEAF_ID,
+    incarnationId: result.incarnationId
+  })
+  runtime.observeExit(() => {
+    exitListenerCalls++
+  })
+  harness.adapter.onExit((event) => {
+    providerExits.push(event)
+  })
+  const streamSocket = adapterStreamSocket(harness.adapter)
+  return {
+    ...harness,
+    runtime,
+    session,
+    result,
+    deliveredData,
+    rendererExits,
+    providerExits,
+    get subprocess() {
+      return subprocess
+    },
+    respawn: async () => {
+      harness.adapter.clearTombstone(PTY_ID)
+      const replacement = await harness.adapter.spawn({ cols: 80, rows: 24, sessionId: PTY_ID })
+      ptyOwnership.set(PTY_ID, null)
+      if (replacement.incarnationId) {
+        ptyIncarnationById.set(PTY_ID, replacement.incarnationId)
+      }
+      runtime.registerPty(PTY_ID, 'repo::/tmp/late-exit-audit', null, {
+        tabId: TAB_ID,
+        leafId: LEAF_ID,
+        incarnationId: replacement.incarnationId
+      })
+      runtime.observeExit(() => {
+        exitListenerCalls++
+      })
+      return replacement
+    },
+    id: PTY_ID,
+    deliverProviderExit: (event: { id: string; code: number; incarnationId?: string }) => {
+      // oxlint-disable-next-line unicorn/no-useless-spread -- copy-safe: exit listeners may unsubscribe while receiving the event.
+      for (const listener of [...harness.adapter['exitListeners']]) {
+        listener(event)
+      }
+    },
+    pauseStream: () => {
+      streamSocket.pause()
+    },
+    resumeStream: () => {
+      streamSocket.resume()
+    },
+    kill: async () => {
+      if (!killHandler) {
+        throw new Error('PTY kill handler missing')
+      }
+      // oxlint-disable-next-line typescript/consistent-type-assertions -- SAFETY: the installed renderer-kill handler ignores its Electron event argument.
+      await killHandler({} as never, { id: PTY_ID })
+    },
+    stopAndWait: () => {
+      const deps = {
+        runtime,
+        getLocalPtyProviderStartupPromise: () => undefined,
+        shutdownProviderAndDetectExit,
+        rememberSyntheticKillExit: session.rememberSyntheticKillExit,
+        sendPtyExitToRenderer: session.sendPtyExitToRenderer,
+        finishPtyShutdown
+      }
+      return stopAndWaitPtyFromRuntimeController(
+        // oxlint-disable-next-line typescript/consistent-type-assertions -- SAFETY: exact-stop reads only these seven controller ports and optional store; unrelated spawn ports are unused.
+        deps as unknown as PtyRuntimeControllerDeps,
+        PTY_ID
+      )
+    },
+    waitForExit: () => waitFor(() => providerExits.length > 0),
+    capture: async () => ({
+      ...runtime.captureState(),
+      providerHasPty: harness.adapter.hasPty(PTY_ID),
+      hostInventoryCount: (await harness.adapter.listProcesses()).length,
+      deliveredData: [...deliveredData],
+      rendererExitCount: rendererExits.length,
+      providerExitCount: providerExits.length,
+      exitListenerCalls
+    }),
+    dispose: async () => {
+      for (const pending of session.syntheticKillExitPtyIds.values()) {
+        clearTimeout(pending.cleanupTimer)
+      }
+      session.syntheticKillExitPtyIds.clear()
+      runtime.onPtyExit(PTY_ID, 0, runtime.captureState().incarnationId ?? undefined)
+      unbindLocalProviderListeners()
+      harness.adapter.dispose()
+      await harness.server.shutdown()
+      setLocalPtyProvider(priorProvider)
+      setPtyHostBindings({})
+      ptyOwnership.delete(PTY_ID)
+      ptyIncarnationById.delete(PTY_ID)
+      rmSync(harness.dir, { recursive: true, force: true })
+    }
+  }
+}