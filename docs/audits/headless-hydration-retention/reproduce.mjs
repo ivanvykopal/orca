paidaxingyo666/Manta

fix(runtime): fence terminal snapshot completion by owner (#20996)
Co-authored-by: m4air <m4air@m4airs-MacBook-Air.local>

Mirror-Of: 9ed2f743a4dd7aab1d35906c049c11d143a92aaf

docs/audits/headless-hydration-retention/reproduce.mjs
@@ -1,0 +1,130 @@
+import { createHash } from 'node:crypto'
+import { mkdtemp, readFile, rm, writeFile } from 'node:fs/promises'
+import { createRequire } from 'node:module'
+import { tmpdir } from 'node:os'
+import { join, resolve } from 'node:path'
+import { fileURLToPath, pathToFileURL } from 'node:url'
+import { applyPatch, parsePatch, reversePatch } from 'diff'
+import { build } from 'esbuild'
+
+if (process.env.MANTA_BACKGROUND_LAUNCH !== '1') {
+  throw new Error('Run with MANTA_BACKGROUND_LAUNCH=1.')
+}
+
+const root = fileURLToPath(new URL('../../../', import.meta.url))
+const patch = await readFile(new URL('./fix.patch', import.meta.url), 'utf8')
+const beforeSources = {}
+const sourceHashes = {}
+for (const parsed of parsePatch(patch)) {
+  const path = parsed.newFileName.replace(/^b\//, '')
+  const absolute = resolve(root, path)
+  const current = await readFile(absolute, 'utf8')
+  const before = applyPatch(current, reversePatch(parsed))
+  if (before === false) {
+    throw new Error(`Source changed; review the proof patch: ${path}`)
+  }
+  beforeSources[absolute.replaceAll('\\', '/')] = before
+  sourceHashes[path] = {
+    before: createHash('sha256').update(before).digest('hex'),
+    after: createHash('sha256').update(current).digest('hex')
+  }
+}
+
+const scratch = await mkdtemp(join(tmpdir(), 'orca-hydration-retention-'))
+const require = createRequire(import.meta.url)
+let runnerModuleId
+try {
+  const runnerPath = join(scratch, 'run-process.cjs')
+  await build({
+    absWorkingDir: root,
+    entryPoints: [resolve(root, 'src/shared/child-process/run-process.ts')],
+    outfile: runnerPath,
+    bundle: true,
+    platform: 'node',
+    format: 'cjs',
+    logLevel: 'silent'
+  })
+  runnerModuleId = require.resolve(runnerPath)
+  const { runProcess } = require(runnerModuleId)
+  const baselineConfig = join(scratch, 'before.config.mjs')
+  const configImport = JSON.stringify(pathToFileURL(resolve(root, 'config/vitest.config.ts')).href)
+  await writeFile(
+    baselineConfig,
+    `import base from ${configImport};
+const beforeSources = ${JSON.stringify(beforeSources)};
+export default {...base, plugins: [{
+  name: 'hydrate-before-ownership-fix', enforce: 'pre',
+  transform(_code, id) {
+    const before = beforeSources[id.replaceAll('\\\\', '/').split('?')[0]];
+    return before === undefined ? null : {code: before, map: null};
+  }
+}]};\n`
+  )
+
+  async function run(label, config) {
+    const report = join(scratch, `${label}.json`)
+    const result = await runProcess({
+      program: process.execPath,
+      args: [
+        resolve(root, 'node_modules/vitest/vitest.mjs'),
+        'run',
+        '--config',
+        config,
+        'src/main/runtime/headless-hydration-ownership.test.ts',
+        'src/main/runtime/headless-seed-ownership.test.ts',
+        '--reporter=json',
+        `--outputFile=${report}`
+      ],
+      cwd: root,
+      env: process.env,
+      timeoutMs: 90_000,
+      maxOutputBytes: 4 * 1024 * 1024
+    })
+    let parsed
+    try {
+      parsed = JSON.parse(await readFile(report, 'utf8'))
+    } catch (error) {
+      throw new Error(`${label} runner failed: ${result.stderr || result.stdout}`, { cause: error })
+    }
+    return {
+      exitCode: result.code,
+      passed: parsed.numPassedTests,
+      failed: parsed.numFailedTests,
+      failedCases: parsed.testResults.flatMap((suite) =>
+        suite.assertionResults
+          .filter((test) => test.status === 'failed')
+          .map((test) => test.fullName)
+      )
+    }
+  }
+
+  const before = await run('before', baselineConfig)
+  const after = await run('after', resolve(root, 'config/vitest.config.ts'))
+  const passed =
+    before.failed > 0 &&
+    before.passed + before.failed === 22 &&
+    after.passed === 22 &&
+    after.failed === 0
+  console.log(
+    JSON.stringify(
+      {
+        comparison:
+          'Actual runtime tests; before reverses only fix.patch in a temporary Vite transform',
+        sourceHashes,
+        before,
+        after,
+        passed
+      },
+      null,
+      2
+    )
+  )
+  if (!passed) {
+    process.exitCode = 1
+  }
+} finally {
+  if (runnerModuleId) {
+    delete require.cache[runnerModuleId]
+  }
+  await rm(scratch, { recursive: true, force: true })
+}

src/main/runtime/headless-hydration-ownership-test-fixture.ts
@@ -1,0 +1,82 @@
+import { afterEach, vi } from 'vitest'
+import './manta-runtime-test-lifecycle.spec'
+import { MantaRuntimeService } from './manta-runtime'
+import { store, syncSinglePty } from './manta-runtime-test-fixtures.spec'
+
+export const PTY_ID = 'pty-hydration-owner'
+export const SIZE = { cols: 80, rows: 24 }
+export const RETIRED_SNAPSHOT = {
+  data: '\x1b]7;file:///retired-context\x07RETIRED-SEED',
+  lastTitle: 'Codex working',
+  ...SIZE
+}
+
+export class HydrationRuntime extends MantaRuntimeService {
+  model() {
+    const state = this.headlessTerminals.get(PTY_ID)
+    if (!state) {
+      throw new Error('Expected headless model')
+    }
+    return state
+  }
+
+  retainedState() {
+    return {
+      model: this.headlessTerminals.has(PTY_ID),
+      hydration: this.headlessHydrationState.get(PTY_ID),
+      cwd: this.terminalCwdByPtyId.get(PTY_ID),
+      titleTracker: this.ptyTitleTrackersByPtyId.has(PTY_ID),
+      recentOutput: this.recentPtyOutputById.has(PTY_ID),
+      providerPreferred: this.providerSnapshotPreferredPtys.has(PTY_ID),
+      generation: this.ptyLifecycleGenerationById.get(PTY_ID),
+      snapshotScans: this.providerModeSnapshotScansByPtyId.get(PTY_ID)?.size ?? 0
+    }
+  }
+
+  preferProvider() {
+    this.providerSnapshotPreferredPtys.add(PTY_ID)
+  }
+
+  replaceExecutionContext() {
+    this.replaceHeadlessTerminalAfterExecutionContextChange(PTY_ID)
+  }
+
+  captureProvider() {
+    return this.captureProviderTerminalBuffer(PTY_ID, {}, this.getPtyLifecycleGeneration(PTY_ID))
+  }
+
+  providerTail(visibleScreenOnly: boolean) {
+    return this.readProviderTerminalTailLines(PTY_ID, 10, { visibleScreenOnly })
+  }
+}
+
+const runtimes: HydrationRuntime[] = []
+
+export function createHydrationRuntime(): HydrationRuntime {
+  const runtime = new HydrationRuntime(store)
+  syncSinglePty(runtime, PTY_ID)
+  runtimes.push(runtime)
+  return runtime
+}
+
+export function retire(runtime: HydrationRuntime): void {
+  runtime.onPtyExit(PTY_ID, 0, undefined, { providerExitObserved: true })
+}
+
+export const EMPTY_RETAINED_STATE = {
+  model: false,
+  hydration: undefined,
+  cwd: undefined,
+  titleTracker: false,
+  recentOutput: false,
+  providerPreferred: false,
+  generation: undefined,
+  snapshotScans: 0
+}
+
+afterEach(() => {
+  for (const runtime of runtimes.splice(0)) {
+    retire(runtime)
+  }
+  vi.restoreAllMocks()
+})

src/main/runtime/headless-hydration-ownership.test.ts
@@ -1,0 +1,125 @@
+import { expect, it, vi } from 'vitest'
+import { deferred, makeDeferred } from './manta-runtime-test-fixtures.spec'
+import {
+  createHydrationRuntime,
+  EMPTY_RETAINED_STATE,
+  PTY_ID,
+  RETIRED_SNAPSHOT,
+  retire,
+  SIZE
+} from './headless-hydration-ownership-test-fixture'
+
+type Snapshot = typeof RETIRED_SNAPSHOT | null
+
+function prepare() {
+  const runtime = createHydrationRuntime()
+  const snapshot = deferred<Snapshot>()
+  const serialize = vi.fn(() => snapshot.promise)
+  runtime.setPtyController({
+    write: () => true,
+    kill: () => true,
+    getForegroundProcess: async () => null,
+    getSize: () => SIZE,
+    hasRendererSerializer: () => true,
+    serializeBuffer: serialize
+  })
+  return { runtime, snapshot, serialize }
+}
+
+it('does not start renderer hydration after the model retires before its callback', async () => {
+  const { runtime, snapshot, serialize } = prepare()
+  runtime.onPtyData(PTY_ID, 'queued-live', 1)
+  const old = runtime.model()
+  const write = vi.spyOn(old.emulator, 'write')
+  retire(runtime)
+  snapshot.resolve(RETIRED_SNAPSHOT)
+  await old.writeChain
+  expect(serialize).not.toHaveBeenCalled()
+  expect(write).toHaveBeenCalledWith('queued-live', { forwardQueryReplies: false })
+  expect(runtime.retainedState()).toEqual(EMPTY_RETAINED_STATE)
+})
+
+it.each(['success', 'null', 'reject'] as const)(
+  'does not resurrect retired renderer-hydration state after %s',
+  async (outcome) => {
+    const { runtime, snapshot, serialize } = prepare()
+    runtime.onPtyData(PTY_ID, 'queued-live', 1)
+    const old = runtime.model()
+    await vi.waitFor(() => expect(serialize).toHaveBeenCalledOnce())
+    retire(runtime)
+    if (outcome === 'reject') {
+      snapshot.reject(new Error('Renderer unavailable'))
+    } else {
+      snapshot.resolve(outcome === 'success' ? RETIRED_SNAPSHOT : null)
+    }
+    await old.writeChain
+    expect(runtime.retainedState()).toEqual(EMPTY_RETAINED_STATE)
+  }
+)
+
+it.each(['success', 'null', 'reject'] as const)(
+  'settles current renderer hydration after %s and preserves queued live bytes',
+  async (outcome) => {
+    const { runtime, snapshot } = prepare()
+    runtime.onPtyData(PTY_ID, 'CURRENT-LIVE', 1)
+    const current = runtime.model()
+    if (outcome === 'reject') {
+      snapshot.reject(new Error('Renderer unavailable'))
+    } else {
+      snapshot.resolve(outcome === 'success' ? RETIRED_SNAPSHOT : null)
+    }
+    await current.writeChain
+    expect(runtime.retainedState().hydration).toBe('done')
+    expect(current.emulator.getVisibleLines().join('\n')).toContain('CURRENT-LIVE')
+    expect(current.emulator.getVisibleLines().join('\n').includes('RETIRED-SEED')).toBe(
+      outcome === 'success'
+    )
+  }
+)
+
+it('keeps a same-ID replacement pending when an old renderer snapshot arrives', async () => {
+  const { runtime, snapshot, serialize } = prepare()
+  runtime.onPtyData(PTY_ID, 'OLD-LIVE', 1)
+  const old = runtime.model()
+  await vi.waitFor(() => expect(serialize).toHaveBeenCalledOnce())
+  runtime.notePtyDataGap(PTY_ID)
+  const replacementSnapshot = deferred<Snapshot>()
+  serialize.mockImplementation(() => replacementSnapshot.promise)
+  runtime.onPtyData(PTY_ID, 'NEW-LIVE', 2)
+  const replacement = runtime.model()
+  runtime.preferProvider()
+  await vi.waitFor(() => expect(serialize).toHaveBeenCalledTimes(2))
+  snapshot.resolve(RETIRED_SNAPSHOT)
+  await old.writeChain
+  expect(runtime.model()).toBe(replacement)
+  expect(runtime.retainedState()).toMatchObject({ hydration: 'pending', providerPreferred: true })
+  expect(runtime.retainedState().cwd).toBeUndefined()
+  replacementSnapshot.resolve({ ...RETIRED_SNAPSHOT, data: 'NEW-SEED', lastTitle: 'New title' })
+  await replacement.writeChain
+  const text = replacement.emulator.getVisibleLines().join('\n')
+  expect(text).toContain('NEW-SEEDNEW-LIVE')
+  expect(text).not.toContain('OLD-LIVE')
+  expect(text).not.toContain('RETIRED-SEED')
+  expect(runtime.retainedState()).toMatchObject({ hydration: 'done', providerPreferred: false })
+})
+
+it('skips late title and completion bookkeeping after disposal during the seed write', async () => {
+  const { runtime, snapshot, serialize } = prepare()
+  runtime.onPtyData(PTY_ID, 'queued-live', 1)
+  const old = runtime.model()
+  const started = makeDeferred()
+  const release = makeDeferred()
+  const original = old.emulator.write.bind(old.emulator)
+  vi.spyOn(old.emulator, 'write').mockImplementationOnce(async (data) => {
+    started.resolve()
+    await release.promise
+    return original(data)
+  })
+  await vi.waitFor(() => expect(serialize).toHaveBeenCalledOnce())
+  snapshot.resolve(RETIRED_SNAPSHOT)
+  await started.promise
+  retire(runtime)
+  release.resolve()
+  await old.writeChain
+  expect(runtime.retainedState()).toEqual(EMPTY_RETAINED_STATE)
+})

ivanvykopal/orca

docs/audits/headless-hydration-retention/reproduce.mjs
@@ -1,0 +1,130 @@
+import { createHash } from 'node:crypto'
+import { mkdtemp, readFile, rm, writeFile } from 'node:fs/promises'
+import { createRequire } from 'node:module'
+import { tmpdir } from 'node:os'
+import { join, resolve } from 'node:path'
+import { fileURLToPath, pathToFileURL } from 'node:url'
+import { applyPatch, parsePatch, reversePatch } from 'diff'
+import { build } from 'esbuild'
+
+if (process.env.ORCA_BACKGROUND_LAUNCH !== '1') {
+  throw new Error('Run with ORCA_BACKGROUND_LAUNCH=1.')
+}
+
+const root = fileURLToPath(new URL('../../../', import.meta.url))
+const patch = await readFile(new URL('./fix.patch', import.meta.url), 'utf8')
+const beforeSources = {}
+const sourceHashes = {}
+for (const parsed of parsePatch(patch)) {
+  const path = parsed.newFileName.replace(/^b\//, '')
+  const absolute = resolve(root, path)
+  const current = await readFile(absolute, 'utf8')
+  const before = applyPatch(current, reversePatch(parsed))
+  if (before === false) {
+    throw new Error(`Source changed; review the proof patch: ${path}`)
+  }
+  beforeSources[absolute.replaceAll('\\', '/')] = before
+  sourceHashes[path] = {
+    before: createHash('sha256').update(before).digest('hex'),
+    after: createHash('sha256').update(current).digest('hex')
+  }
+}
+
+const scratch = await mkdtemp(join(tmpdir(), 'orca-hydration-retention-'))
+const require = createRequire(import.meta.url)
+let runnerModuleId
+try {
+  const runnerPath = join(scratch, 'run-process.cjs')
+  await build({
+    absWorkingDir: root,
+    entryPoints: [resolve(root, 'src/shared/child-process/run-process.ts')],
+    outfile: runnerPath,
+    bundle: true,
+    platform: 'node',
+    format: 'cjs',
+    logLevel: 'silent'
+  })
+  runnerModuleId = require.resolve(runnerPath)
+  const { runProcess } = require(runnerModuleId)
+  const baselineConfig = join(scratch, 'before.config.mjs')
+  const configImport = JSON.stringify(pathToFileURL(resolve(root, 'config/vitest.config.ts')).href)
+  await writeFile(
+    baselineConfig,
+    `import base from ${configImport};
+const beforeSources = ${JSON.stringify(beforeSources)};
+export default {...base, plugins: [{
+  name: 'hydrate-before-ownership-fix', enforce: 'pre',
+  transform(_code, id) {
+    const before = beforeSources[id.replaceAll('\\\\', '/').split('?')[0]];
+    return before === undefined ? null : {code: before, map: null};
+  }
+}]};\n`
+  )
+
+  async function run(label, config) {
+    const report = join(scratch, `${label}.json`)
+    const result = await runProcess({
+      program: process.execPath,
+      args: [
+        resolve(root, 'node_modules/vitest/vitest.mjs'),
+        'run',
+        '--config',
+        config,
+        'src/main/runtime/headless-hydration-ownership.test.ts',
+        'src/main/runtime/headless-seed-ownership.test.ts',
+        '--reporter=json',
+        `--outputFile=${report}`
+      ],
+      cwd: root,
+      env: process.env,
+      timeoutMs: 90_000,
+      maxOutputBytes: 4 * 1024 * 1024
+    })
+    let parsed
+    try {
+      parsed = JSON.parse(await readFile(report, 'utf8'))
+    } catch (error) {
+      throw new Error(`${label} runner failed: ${result.stderr || result.stdout}`, { cause: error })
+    }
+    return {
+      exitCode: result.code,
+      passed: parsed.numPassedTests,
+      failed: parsed.numFailedTests,
+      failedCases: parsed.testResults.flatMap((suite) =>
+        suite.assertionResults
+          .filter((test) => test.status === 'failed')
+          .map((test) => test.fullName)
+      )
+    }
+  }
+
+  const before = await run('before', baselineConfig)
+  const after = await run('after', resolve(root, 'config/vitest.config.ts'))
+  const passed =
+    before.failed > 0 &&
+    before.passed + before.failed === 22 &&
+    after.passed === 22 &&
+    after.failed === 0
+  console.log(
+    JSON.stringify(
+      {
+        comparison:
+          'Actual runtime tests; before reverses only fix.patch in a temporary Vite transform',
+        sourceHashes,
+        before,
+        after,
+        passed
+      },
+      null,
+      2
+    )
+  )
+  if (!passed) {
+    process.exitCode = 1
+  }
+} finally {
+  if (runnerModuleId) {
+    delete require.cache[runnerModuleId]
+  }
+  await rm(scratch, { recursive: true, force: true })
+}

src/main/runtime/headless-hydration-ownership-test-fixture.ts
@@ -1,0 +1,82 @@
+import { afterEach, vi } from 'vitest'
+import './orca-runtime-test-lifecycle.spec'
+import { OrcaRuntimeService } from './orca-runtime'
+import { store, syncSinglePty } from './orca-runtime-test-fixtures.spec'
+
+export const PTY_ID = 'pty-hydration-owner'
+export const SIZE = { cols: 80, rows: 24 }
+export const RETIRED_SNAPSHOT = {
+  data: '\x1b]7;file:///retired-context\x07RETIRED-SEED',
+  lastTitle: 'Codex working',
+  ...SIZE
+}
+
+export class HydrationRuntime extends OrcaRuntimeService {
+  model() {
+    const state = this.headlessTerminals.get(PTY_ID)
+    if (!state) {
+      throw new Error('Expected headless model')
+    }
+    return state
+  }
+
+  retainedState() {
+    return {
+      model: this.headlessTerminals.has(PTY_ID),
+      hydration: this.headlessHydrationState.get(PTY_ID),
+      cwd: this.terminalCwdByPtyId.get(PTY_ID),
+      titleTracker: this.ptyTitleTrackersByPtyId.has(PTY_ID),
+      recentOutput: this.recentPtyOutputById.has(PTY_ID),
+      providerPreferred: this.providerSnapshotPreferredPtys.has(PTY_ID),
+      generation: this.ptyLifecycleGenerationById.get(PTY_ID),
+      snapshotScans: this.providerModeSnapshotScansByPtyId.get(PTY_ID)?.size ?? 0
+    }
+  }
+
+  preferProvider() {
+    this.providerSnapshotPreferredPtys.add(PTY_ID)
+  }
+
+  replaceExecutionContext() {
+    this.replaceHeadlessTerminalAfterExecutionContextChange(PTY_ID)
+  }
+
+  captureProvider() {
+    return this.captureProviderTerminalBuffer(PTY_ID, {}, this.getPtyLifecycleGeneration(PTY_ID))
+  }
+
+  providerTail(visibleScreenOnly: boolean) {
+    return this.readProviderTerminalTailLines(PTY_ID, 10, { visibleScreenOnly })
+  }
+}
+
+const runtimes: HydrationRuntime[] = []
+
+export function createHydrationRuntime(): HydrationRuntime {
+  const runtime = new HydrationRuntime(store)
+  syncSinglePty(runtime, PTY_ID)
+  runtimes.push(runtime)
+  return runtime
+}
+
+export function retire(runtime: HydrationRuntime): void {
+  runtime.onPtyExit(PTY_ID, 0, undefined, { providerExitObserved: true })
+}
+
+export const EMPTY_RETAINED_STATE = {
+  model: false,
+  hydration: undefined,
+  cwd: undefined,
+  titleTracker: false,
+  recentOutput: false,
+  providerPreferred: false,
+  generation: undefined,
+  snapshotScans: 0
+}
+
+afterEach(() => {
+  for (const runtime of runtimes.splice(0)) {
+    retire(runtime)
+  }
+  vi.restoreAllMocks()
+})

src/main/runtime/headless-hydration-ownership.test.ts
@@ -1,0 +1,125 @@
+import { expect, it, vi } from 'vitest'
+import { deferred, makeDeferred } from './orca-runtime-test-fixtures.spec'
+import {
+  createHydrationRuntime,
+  EMPTY_RETAINED_STATE,
+  PTY_ID,
+  RETIRED_SNAPSHOT,
+  retire,
+  SIZE
+} from './headless-hydration-ownership-test-fixture'
+
+type Snapshot = typeof RETIRED_SNAPSHOT | null
+
+function prepare() {
+  const runtime = createHydrationRuntime()
+  const snapshot = deferred<Snapshot>()
+  const serialize = vi.fn(() => snapshot.promise)
+  runtime.setPtyController({
+    write: () => true,
+    kill: () => true,
+    getForegroundProcess: async () => null,
+    getSize: () => SIZE,
+    hasRendererSerializer: () => true,
+    serializeBuffer: serialize
+  })
+  return { runtime, snapshot, serialize }
+}
+
+it('does not start renderer hydration after the model retires before its callback', async () => {
+  const { runtime, snapshot, serialize } = prepare()
+  runtime.onPtyData(PTY_ID, 'queued-live', 1)
+  const old = runtime.model()
+  const write = vi.spyOn(old.emulator, 'write')
+  retire(runtime)
+  snapshot.resolve(RETIRED_SNAPSHOT)
+  await old.writeChain
+  expect(serialize).not.toHaveBeenCalled()
+  expect(write).toHaveBeenCalledWith('queued-live', { forwardQueryReplies: false })
+  expect(runtime.retainedState()).toEqual(EMPTY_RETAINED_STATE)
+})
+
+it.each(['success', 'null', 'reject'] as const)(
+  'does not resurrect retired renderer-hydration state after %s',
+  async (outcome) => {
+    const { runtime, snapshot, serialize } = prepare()
+    runtime.onPtyData(PTY_ID, 'queued-live', 1)
+    const old = runtime.model()
+    await vi.waitFor(() => expect(serialize).toHaveBeenCalledOnce())
+    retire(runtime)
+    if (outcome === 'reject') {
+      snapshot.reject(new Error('Renderer unavailable'))
+    } else {
+      snapshot.resolve(outcome === 'success' ? RETIRED_SNAPSHOT : null)
+    }
+    await old.writeChain
+    expect(runtime.retainedState()).toEqual(EMPTY_RETAINED_STATE)
+  }
+)
+
+it.each(['success', 'null', 'reject'] as const)(
+  'settles current renderer hydration after %s and preserves queued live bytes',
+  async (outcome) => {
+    const { runtime, snapshot } = prepare()
+    runtime.onPtyData(PTY_ID, 'CURRENT-LIVE', 1)
+    const current = runtime.model()
+    if (outcome === 'reject') {
+      snapshot.reject(new Error('Renderer unavailable'))
+    } else {
+      snapshot.resolve(outcome === 'success' ? RETIRED_SNAPSHOT : null)
+    }
+    await current.writeChain
+    expect(runtime.retainedState().hydration).toBe('done')
+    expect(current.emulator.getVisibleLines().join('\n')).toContain('CURRENT-LIVE')
+    expect(current.emulator.getVisibleLines().join('\n').includes('RETIRED-SEED')).toBe(
+      outcome === 'success'
+    )
+  }
+)
+
+it('keeps a same-ID replacement pending when an old renderer snapshot arrives', async () => {
+  const { runtime, snapshot, serialize } = prepare()
+  runtime.onPtyData(PTY_ID, 'OLD-LIVE', 1)
+  const old = runtime.model()
+  await vi.waitFor(() => expect(serialize).toHaveBeenCalledOnce())
+  runtime.notePtyDataGap(PTY_ID)
+  const replacementSnapshot = deferred<Snapshot>()
+  serialize.mockImplementation(() => replacementSnapshot.promise)
+  runtime.onPtyData(PTY_ID, 'NEW-LIVE', 2)
+  const replacement = runtime.model()
+  runtime.preferProvider()
+  await vi.waitFor(() => expect(serialize).toHaveBeenCalledTimes(2))
+  snapshot.resolve(RETIRED_SNAPSHOT)
+  await old.writeChain
+  expect(runtime.model()).toBe(replacement)
+  expect(runtime.retainedState()).toMatchObject({ hydration: 'pending', providerPreferred: true })
+  expect(runtime.retainedState().cwd).toBeUndefined()
+  replacementSnapshot.resolve({ ...RETIRED_SNAPSHOT, data: 'NEW-SEED', lastTitle: 'New title' })
+  await replacement.writeChain
+  const text = replacement.emulator.getVisibleLines().join('\n')
+  expect(text).toContain('NEW-SEEDNEW-LIVE')
+  expect(text).not.toContain('OLD-LIVE')
+  expect(text).not.toContain('RETIRED-SEED')
+  expect(runtime.retainedState()).toMatchObject({ hydration: 'done', providerPreferred: false })
+})
+
+it('skips late title and completion bookkeeping after disposal during the seed write', async () => {
+  const { runtime, snapshot, serialize } = prepare()
+  runtime.onPtyData(PTY_ID, 'queued-live', 1)
+  const old = runtime.model()
+  const started = makeDeferred()
+  const release = makeDeferred()
+  const original = old.emulator.write.bind(old.emulator)
+  vi.spyOn(old.emulator, 'write').mockImplementationOnce(async (data) => {
+    started.resolve()
+    await release.promise
+    return original(data)
+  })
+  await vi.waitFor(() => expect(serialize).toHaveBeenCalledOnce())
+  snapshot.resolve(RETIRED_SNAPSHOT)
+  await started.promise
+  retire(runtime)
+  release.resolve()
+  await old.writeChain
+  expect(runtime.retainedState()).toEqual(EMPTY_RETAINED_STATE)
+})