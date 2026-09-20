paidaxingyo666/Manta

feat(session-search): add panel search and opt-in consent (PR7) (#20580)
* feat(session-search): add ranked history panel search and consent

* test: wait for initial session indexing before refreshing results

* fix(lint): drop the type import #20898 left behind in the windowing test

main's tip fails `typecheck` and `static analysis` on
`NativeChatMessageList.windowing.test.tsx`: #20898 moved the growth/append
suite into its own file and took the last use of `NativeChatMessage` with
it, leaving the import. Every open PR reds both jobs through the merge ref,
so this rides the first branch that has to merge main in.

Mirror-Of: a7e34d5695fda152a9ca441f0a496a89359bae91

src/renderer/src/components/right-sidebar/AiVaultPanelSearch.tsx
@@ -1,0 +1,156 @@
+import { useState, type ReactNode } from 'react'
+import { Button } from '@/components/ui/button'
+import { translate } from '@/i18n/i18n'
+import { useAppStore } from '@/store'
+import { resolveAiVaultSearchSettings } from '../../../../shared/ai-vault-search-settings'
+import type { useAiVaultPanelSearch } from './use-ai-vault-search'
+
+export function AiVaultPanelSearch({
+  search,
+  noAgents,
+  onDismiss,
+  children
+}: {
+  search: ReturnType<typeof useAiVaultPanelSearch>
+  noAgents: boolean
+  onDismiss: () => void
+  children: ReactNode
+}) {
+  const { localConsent, response, error, loading, retry: onRetry } = search
+  const allHosts = !search.host
+  const [saving, setSaving] = useState(false)
+  const [saveError, setSaveError] = useState(false)
+  async function enable() {
+    setSaving(true)
+    setSaveError(false)
+    try {
+      const store = useAppStore.getState()
+      await store.updateSettingsOrThrow({
+        aiVaultSearch: { ...resolveAiVaultSearchSettings(store.settings), enabled: true }
+      })
+      onRetry()
+    } catch {
+      setSaveError(true)
+    } finally {
+      setSaving(false)
+    }
+  }
+  const unavailable = response?.kind === 'unavailable' ? response.reason : null
+  let message: string | null = null
+  if (allHosts) {
+    message = translate(
+      'sessionSearch.panel.chooseHost',
+      'Choose one computer to search its sessions.'
+    )
+  } else if (localConsent) {
+    message = translate(
+      'sessionSearch.panel.consent',
+      'Enable full-text search? Manta builds an index on this computer from local agent transcripts, including full conversations and up to 3,072 characters per tool output. Content is not redacted. Authenticated paired clients can search it.'
+    )
+  } else if (noAgents) {
+    message = translate(
+      'auto.components.right.sidebar.AiVaultPanel.noAgentsSelected',
+      'No agents selected'
+    )
+  } else if (unavailable === 'disabled') {
+    message = translate(
+      'sessionSearch.panel.remoteDisabled',
+      'Search is disabled on this computer. Enable transcript indexing on that computer to search its sessions.'
+    )
+  } else if (unavailable === 'not-ready') {
+    message = translate(
+      'sessionSearch.panel.notReady',
+      'The search index is not ready yet. Try again shortly.'
+    )
+  } else if (unavailable === 'no-service') {
+    message = translate(
+      'sessionSearch.panel.noService',
+      'Search is unavailable on this computer. It may need a Manta update or a runtime with search support.'
+    )
+  } else if (error) {
+    message = translate(
+      'sessionSearch.panel.failed',
+      'Could not search this computer. Check its connection and try again.'
+    )
+  } else if (response?.kind === 'stale-cursor' || response?.kind === 'malformed-cursor') {
+    message = translate(
+      'sessionSearch.panel.changed',
+      'The index changed while searching. Search again for current results.'
+    )
+  } else if (response?.kind === 'results') {
+    if (
+      response.truncated.candidates ||
+      response.truncated.query ||
+      response.truncated.snippets > 0
+    ) {
+      message = translate(
+        'sessionSearch.panel.truncated',
+        'Some results or matching text were limited. Narrow your search for more precise results.'
+      )
+    }
+  }
+  if (!search.searching) {
+    return children
+  }
+  if (response?.kind === 'results' && search.hits.length === 0) {
+    message = translate(
+      'sessionSearch.panel.noMatches',
+      'No matching sessions in the indexed history. Try another query or scope.'
+    )
+  }
+  return (
+    <>
+      {message && (
+        <div
+          className="space-y-2 border-b border-sidebar-border px-3 py-3 text-xs text-muted-foreground"
+          role="status"
+        >
+          <p>{message}</p>
+          {localConsent && !allHosts ? (
+            <>
+              {saveError && (
+                <p className="text-destructive">
+                  {translate(
+                    'sessionSearch.panel.enableFailed',
+                    'Could not enable search. Try again.'
+                  )}
+                </p>
+              )}
+              <div className="flex gap-2">
+                <Button size="xs" disabled={saving} onClick={() => void enable()}>
+                  {translate('sessionSearch.panel.enable', 'Enable')}
+                </Button>
+                <Button size="xs" variant="ghost" disabled={saving} onClick={onDismiss}>
+                  {translate('sessionSearch.panel.notNow', 'Not now')}
+                </Button>
+              </div>
+            </>
+          ) : !allHosts &&
+            !noAgents &&
+            (error ||
+              unavailable ||
+              response?.kind === 'stale-cursor' ||
+              response?.kind === 'malformed-cursor') ? (
+            <Button size="xs" variant="outline" disabled={loading} onClick={onRetry}>
+              {translate('sessionSearch.panel.retry', 'Try again')}
+            </Button>
+          ) : null}
+        </div>
+      )}
+      {children}
+      {response?.kind === 'results' && response.page.hasMore && (
+        <div className="border-t border-sidebar-border p-2">
+          <Button
+            className="w-full"
+            variant="ghost"
+            size="xs"
+            disabled={loading}
+            onClick={search.loadMore}
+          >
+            {translate('sessionSearch.panel.loadMore', 'Load more matches')}
+          </Button>
+        </div>
+      )}
+    </>
+  )
+}

src/renderer/src/components/right-sidebar/AiVaultVirtualRow.tsx
@@ -23,6 +23,8 @@ import {
 } from './ai-vault-session-path-actions'
 import { canContinueAiVaultSessionInNewSession } from './ai-vault-session-continuation'
 import type { AiVaultResumeInChatEligibility } from './ai-vault-session-resume-in-chat'
+import type { AiVaultSearchHit } from '../../../../shared/ai-vault-search-types'
+import { canResumeAiVaultSearchHit, hasAiVaultSearchHitPath } from './ai-vault-search-session'
 
 export type AiVaultListRow =
   | { type: 'group'; group: AiVaultSessionGroup }
@@ -57,7 +59,8 @@ export function AiVaultVirtualRow({
   onOpenLog,
   onRevealLog,
   onOpenCwd,
-  onRequestDelete
+  onRequestDelete,
+  searchHits
 }: {
   row: AiVaultListRow | undefined
   index: number
@@ -88,6 +91,7 @@ export function AiVaultVirtualRow({
   onRevealLog: (session: AiVaultSession) => void
   onOpenCwd: (session: AiVaultSession) => void
   onRequestDelete: (session: AiVaultSession) => void
+  searchHits?: ReadonlyMap<string, AiVaultSearchHit>
 }): React.JSX.Element | null {
   if (!row) {
     return null
@@ -124,6 +128,20 @@ export function AiVaultVirtualRow({
   // identities that have no single file to open, while Reveal/CWD stay on the
   // existing local-path gate.
   const canOpenLogInManta = row.type === 'session' && canOpenAiVaultSessionLogInManta(row.session)
+  const searchHit = row.type === 'session' ? searchHits?.get(row.session.id) : undefined
+  const searchResumeAllowed = searchHit ? canResumeAiVaultSearchHit(searchHit) : true
+  const searchPathAllowed = searchHit ? hasAiVaultSearchHitPath(searchHit) : true
+  const resumeStartup =
+    row.type === 'session' && searchResumeAllowed
+      ? buildResumeStartup(row.session, resumeState?.worktreeId)
+      : { command: '' }
+  const visibleResumeActions =
+    searchResumeAllowed && resumeActions
+      ? resumeActions
+      : {
+          worktree: { worktreeId: null, disabled: true },
+          newTab: { worktreeId: null, disabled: true }
+        }
 
   return (
     <div
@@ -144,23 +162,20 @@ export function AiVaultVirtualRow({
       ) : (
         <VaultSessionRow
           session={row.session}
+          searchHit={searchHit}
           liveState={getSessionLiveState(row.session)}
-          resumeStartup={buildResumeStartup(row.session, resumeState?.worktreeId)}
-          realHomeResumeStartup={buildResumeStartup(
-            { ...row.session, codexHome: null },
-            resumeState?.worktreeId
-          )}
+          resumeStartup={resumeStartup}
+          realHomeResumeStartup={
+            searchResumeAllowed
+              ? buildResumeStartup({ ...row.session, codexHome: null }, resumeState?.worktreeId)
+              : resumeStartup
+          }
           worktreeInfo={worktreeInfo}
           vaultScope={vaultScope}
           detailsExpanded={expandedSessionIds.has(row.session.id)}
-          resumeDisabled={resumeGating.resumeDisabled}
+          resumeDisabled={!searchResumeAllowed || resumeGating.resumeDisabled}
           resumeLabel={resumeLabel}
-          resumeActions={
-            resumeActions ?? {
-              worktree: { worktreeId: null, disabled: true },
-              newTab: { worktreeId: null, disabled: true }
-            }
-          }
+          resumeActions={visibleResumeActions}
           onToggleDetails={() => onToggleSessionDetails(row.session.id)}
           onJumpToOriginalPane={
             originalPaneTarget ? () => onJumpToOriginalPane(row.session) : undefined
@@ -174,38 +189,46 @@ export function AiVaultVirtualRow({
             }
           }}
           onContinueInNewSession={
-            continuationWorktreeId
+            searchResumeAllowed && continuationWorktreeId
               ? () => onContinueInNewSession(row.session, continuationWorktreeId)
               : undefined
           }
           onResumeInNewChat={
-            resumeInChat?.available
+            searchResumeAllowed && resumeInChat?.available
               ? () => onResumeInNewChat(row.session, resumeInChat.workspaceId)
               : undefined
           }
           onResumeInWorktree={() => {
-            if (resumeActions?.worktree.worktreeId) {
+            if (searchResumeAllowed && resumeActions?.worktree.worktreeId) {
               onResume(row.session, resumeActions.worktree.worktreeId)
             }
           }}
           onResumeInNewTab={() => {
-            if (resumeActions?.newTab.worktreeId) {
+            if (searchResumeAllowed && resumeActions?.newTab.worktreeId) {
               onResume(row.session, resumeActions.newTab.worktreeId)
             }
           }}
           onCopyResume={
-            resumeGating.canCopyResumeCommand
+            searchResumeAllowed && resumeGating.canCopyResumeCommand
               ? () => onCopyResume(row.session, resumeState?.worktreeId)
               : undefined
           }
           onCopyId={() => onCopyId(row.session)}
-          onCopyPath={() => onCopyPath(row.session)}
-          onOpenLog={canOpenLogInManta ? () => onOpenLog(row.session) : undefined}
-          onRevealLog={canOpenLocalSessionPaths ? () => onRevealLog(row.session) : undefined}
-          onOpenCwd={
-            canOpenLocalSessionPaths && row.session.cwd ? () => onOpenCwd(row.session) : undefined
+          onCopyPath={searchPathAllowed ? () => onCopyPath(row.session) : undefined}
+          onOpenLog={
+            searchPathAllowed && canOpenLogInManta ? () => onOpenLog(row.session) : undefined
           }
-          onRequestDelete={onRequestDelete}
+          onRevealLog={
+            searchPathAllowed && canOpenLocalSessionPaths
+              ? () => onRevealLog(row.session)
+              : undefined
+          }
+          onOpenCwd={
+            searchPathAllowed && canOpenLocalSessionPaths && row.session.cwd
+              ? () => onOpenCwd(row.session)
+              : undefined
+          }
+          onRequestDelete={searchPathAllowed ? onRequestDelete : undefined}
         />
       )}
     </div>

ivanvykopal/orca

src/renderer/src/components/right-sidebar/AiVaultPanelSearch.tsx
@@ -1,0 +1,156 @@
+import { useState, type ReactNode } from 'react'
+import { Button } from '@/components/ui/button'
+import { translate } from '@/i18n/i18n'
+import { useAppStore } from '@/store'
+import { resolveAiVaultSearchSettings } from '../../../../shared/ai-vault-search-settings'
+import type { useAiVaultPanelSearch } from './use-ai-vault-search'
+
+export function AiVaultPanelSearch({
+  search,
+  noAgents,
+  onDismiss,
+  children
+}: {
+  search: ReturnType<typeof useAiVaultPanelSearch>
+  noAgents: boolean
+  onDismiss: () => void
+  children: ReactNode
+}) {
+  const { localConsent, response, error, loading, retry: onRetry } = search
+  const allHosts = !search.host
+  const [saving, setSaving] = useState(false)
+  const [saveError, setSaveError] = useState(false)
+  async function enable() {
+    setSaving(true)
+    setSaveError(false)
+    try {
+      const store = useAppStore.getState()
+      await store.updateSettingsOrThrow({
+        aiVaultSearch: { ...resolveAiVaultSearchSettings(store.settings), enabled: true }
+      })
+      onRetry()
+    } catch {
+      setSaveError(true)
+    } finally {
+      setSaving(false)
+    }
+  }
+  const unavailable = response?.kind === 'unavailable' ? response.reason : null
+  let message: string | null = null
+  if (allHosts) {
+    message = translate(
+      'sessionSearch.panel.chooseHost',
+      'Choose one computer to search its sessions.'
+    )
+  } else if (localConsent) {
+    message = translate(
+      'sessionSearch.panel.consent',
+      'Enable full-text search? Orca builds an index on this computer from local agent transcripts, including full conversations and up to 3,072 characters per tool output. Content is not redacted. Authenticated paired clients can search it.'
+    )
+  } else if (noAgents) {
+    message = translate(
+      'auto.components.right.sidebar.AiVaultPanel.noAgentsSelected',
+      'No agents selected'
+    )
+  } else if (unavailable === 'disabled') {
+    message = translate(
+      'sessionSearch.panel.remoteDisabled',
+      'Search is disabled on this computer. Enable transcript indexing on that computer to search its sessions.'
+    )
+  } else if (unavailable === 'not-ready') {
+    message = translate(
+      'sessionSearch.panel.notReady',
+      'The search index is not ready yet. Try again shortly.'
+    )
+  } else if (unavailable === 'no-service') {
+    message = translate(
+      'sessionSearch.panel.noService',
+      'Search is unavailable on this computer. It may need an Orca update or a runtime with search support.'
+    )
+  } else if (error) {
+    message = translate(
+      'sessionSearch.panel.failed',
+      'Could not search this computer. Check its connection and try again.'
+    )
+  } else if (response?.kind === 'stale-cursor' || response?.kind === 'malformed-cursor') {
+    message = translate(
+      'sessionSearch.panel.changed',
+      'The index changed while searching. Search again for current results.'
+    )
+  } else if (response?.kind === 'results') {
+    if (
+      response.truncated.candidates ||
+      response.truncated.query ||
+      response.truncated.snippets > 0
+    ) {
+      message = translate(
+        'sessionSearch.panel.truncated',
+        'Some results or matching text were limited. Narrow your search for more precise results.'
+      )
+    }
+  }
+  if (!search.searching) {
+    return children
+  }
+  if (response?.kind === 'results' && search.hits.length === 0) {
+    message = translate(
+      'sessionSearch.panel.noMatches',
+      'No matching sessions in the indexed history. Try another query or scope.'
+    )
+  }
+  return (
+    <>
+      {message && (
+        <div
+          className="space-y-2 border-b border-sidebar-border px-3 py-3 text-xs text-muted-foreground"
+          role="status"
+        >
+          <p>{message}</p>
+          {localConsent && !allHosts ? (
+            <>
+              {saveError && (
+                <p className="text-destructive">
+                  {translate(
+                    'sessionSearch.panel.enableFailed',
+                    'Could not enable search. Try again.'
+                  )}
+                </p>
+              )}
+              <div className="flex gap-2">
+                <Button size="xs" disabled={saving} onClick={() => void enable()}>
+                  {translate('sessionSearch.panel.enable', 'Enable')}
+                </Button>
+                <Button size="xs" variant="ghost" disabled={saving} onClick={onDismiss}>
+                  {translate('sessionSearch.panel.notNow', 'Not now')}
+                </Button>
+              </div>
+            </>
+          ) : !allHosts &&
+            !noAgents &&
+            (error ||
+              unavailable ||
+              response?.kind === 'stale-cursor' ||
+              response?.kind === 'malformed-cursor') ? (
+            <Button size="xs" variant="outline" disabled={loading} onClick={onRetry}>
+              {translate('sessionSearch.panel.retry', 'Try again')}
+            </Button>
+          ) : null}
+        </div>
+      )}
+      {children}
+      {response?.kind === 'results' && response.page.hasMore && (
+        <div className="border-t border-sidebar-border p-2">
+          <Button
+            className="w-full"
+            variant="ghost"
+            size="xs"
+            disabled={loading}
+            onClick={search.loadMore}
+          >
+            {translate('sessionSearch.panel.loadMore', 'Load more matches')}
+          </Button>
+        </div>
+      )}
+    </>
+  )
+}

src/renderer/src/components/right-sidebar/AiVaultVirtualRow.tsx
@@ -23,6 +23,8 @@ import {
 } from './ai-vault-session-path-actions'
 import { canContinueAiVaultSessionInNewSession } from './ai-vault-session-continuation'
 import type { AiVaultResumeInChatEligibility } from './ai-vault-session-resume-in-chat'
+import type { AiVaultSearchHit } from '../../../../shared/ai-vault-search-types'
+import { canResumeAiVaultSearchHit, hasAiVaultSearchHitPath } from './ai-vault-search-session'
 
 export type AiVaultListRow =
   | { type: 'group'; group: AiVaultSessionGroup }
@@ -57,7 +59,8 @@ export function AiVaultVirtualRow({
   onOpenLog,
   onRevealLog,
   onOpenCwd,
-  onRequestDelete
+  onRequestDelete,
+  searchHits
 }: {
   row: AiVaultListRow | undefined
   index: number
@@ -88,6 +91,7 @@ export function AiVaultVirtualRow({
   onRevealLog: (session: AiVaultSession) => void
   onOpenCwd: (session: AiVaultSession) => void
   onRequestDelete: (session: AiVaultSession) => void
+  searchHits?: ReadonlyMap<string, AiVaultSearchHit>
 }): React.JSX.Element | null {
   if (!row) {
     return null
@@ -124,6 +128,20 @@ export function AiVaultVirtualRow({
   // identities that have no single file to open, while Reveal/CWD stay on the
   // existing local-path gate.
   const canOpenLogInOrca = row.type === 'session' && canOpenAiVaultSessionLogInOrca(row.session)
+  const searchHit = row.type === 'session' ? searchHits?.get(row.session.id) : undefined
+  const searchResumeAllowed = searchHit ? canResumeAiVaultSearchHit(searchHit) : true
+  const searchPathAllowed = searchHit ? hasAiVaultSearchHitPath(searchHit) : true
+  const resumeStartup =
+    row.type === 'session' && searchResumeAllowed
+      ? buildResumeStartup(row.session, resumeState?.worktreeId)
+      : { command: '' }
+  const visibleResumeActions =
+    searchResumeAllowed && resumeActions
+      ? resumeActions
+      : {
+          worktree: { worktreeId: null, disabled: true },
+          newTab: { worktreeId: null, disabled: true }
+        }
 
   return (
     <div
@@ -144,23 +162,20 @@ export function AiVaultVirtualRow({
       ) : (
         <VaultSessionRow
           session={row.session}
+          searchHit={searchHit}
           liveState={getSessionLiveState(row.session)}
-          resumeStartup={buildResumeStartup(row.session, resumeState?.worktreeId)}
-          realHomeResumeStartup={buildResumeStartup(
-            { ...row.session, codexHome: null },
-            resumeState?.worktreeId
-          )}
+          resumeStartup={resumeStartup}
+          realHomeResumeStartup={
+            searchResumeAllowed
+              ? buildResumeStartup({ ...row.session, codexHome: null }, resumeState?.worktreeId)
+              : resumeStartup
+          }
           worktreeInfo={worktreeInfo}
           vaultScope={vaultScope}
           detailsExpanded={expandedSessionIds.has(row.session.id)}
-          resumeDisabled={resumeGating.resumeDisabled}
+          resumeDisabled={!searchResumeAllowed || resumeGating.resumeDisabled}
           resumeLabel={resumeLabel}
-          resumeActions={
-            resumeActions ?? {
-              worktree: { worktreeId: null, disabled: true },
-              newTab: { worktreeId: null, disabled: true }
-            }
-          }
+          resumeActions={visibleResumeActions}
           onToggleDetails={() => onToggleSessionDetails(row.session.id)}
           onJumpToOriginalPane={
             originalPaneTarget ? () => onJumpToOriginalPane(row.session) : undefined
@@ -174,38 +189,46 @@ export function AiVaultVirtualRow({
             }
           }}
           onContinueInNewSession={
-            continuationWorktreeId
+            searchResumeAllowed && continuationWorktreeId
               ? () => onContinueInNewSession(row.session, continuationWorktreeId)
               : undefined
           }
           onResumeInNewChat={
-            resumeInChat?.available
+            searchResumeAllowed && resumeInChat?.available
               ? () => onResumeInNewChat(row.session, resumeInChat.workspaceId)
               : undefined
           }
           onResumeInWorktree={() => {
-            if (resumeActions?.worktree.worktreeId) {
+            if (searchResumeAllowed && resumeActions?.worktree.worktreeId) {
               onResume(row.session, resumeActions.worktree.worktreeId)
             }
           }}
           onResumeInNewTab={() => {
-            if (resumeActions?.newTab.worktreeId) {
+            if (searchResumeAllowed && resumeActions?.newTab.worktreeId) {
               onResume(row.session, resumeActions.newTab.worktreeId)
             }
           }}
           onCopyResume={
-            resumeGating.canCopyResumeCommand
+            searchResumeAllowed && resumeGating.canCopyResumeCommand
               ? () => onCopyResume(row.session, resumeState?.worktreeId)
               : undefined
           }
           onCopyId={() => onCopyId(row.session)}
-          onCopyPath={() => onCopyPath(row.session)}
-          onOpenLog={canOpenLogInOrca ? () => onOpenLog(row.session) : undefined}
-          onRevealLog={canOpenLocalSessionPaths ? () => onRevealLog(row.session) : undefined}
-          onOpenCwd={
-            canOpenLocalSessionPaths && row.session.cwd ? () => onOpenCwd(row.session) : undefined
+          onCopyPath={searchPathAllowed ? () => onCopyPath(row.session) : undefined}
+          onOpenLog={
+            searchPathAllowed && canOpenLogInOrca ? () => onOpenLog(row.session) : undefined
           }
-          onRequestDelete={onRequestDelete}
+          onRevealLog={
+            searchPathAllowed && canOpenLocalSessionPaths
+              ? () => onRevealLog(row.session)
+              : undefined
+          }
+          onOpenCwd={
+            searchPathAllowed && canOpenLocalSessionPaths && row.session.cwd
+              ? () => onOpenCwd(row.session)
+              : undefined
+          }
+          onRequestDelete={searchPathAllowed ? onRequestDelete : undefined}
         />
       )}
     </div>