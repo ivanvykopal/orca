paidaxingyo666/Manta

refactor(mobile): checked reply readers for notifications, components, terminal, transport, home, worktree and browser (step 7) (#21176)
* refactor(mobile): checked reply readers for notifications, components, terminal, transport, home, worktree and browser (step 7)

Twenty-one unchecked reply readers across thirteen files become checked zod
readers, so a malformed host reply surfaces as one `RpcIncompatibleReplyError`
naming the method instead of a downstream `TypeError`, a rendered `undefined`, or
a card left "proven" over a reply that carried no rows. Deliberately a behaviour
change on malformed replies only.

What each domain required, and why it required no more:

- notifications (5 readers). All four call sites read the payload through `?.`,
  so every schema is nullish at the top level and no member is required. The
  test-push `reason` and the register `reason` become closed enums, because the
  two comparisons against them are the whole of what they decide and an arm this
  build does not know took the generic copy on main too. The stream unsubscribe
  and the unregister read no body at all.

- components (4). `repo.hooks` requires `source` and nothing else: the drawer
  assigns it straight into `SetupHookDetails.source`, whose type is
  `string | null`, with no guard in between — nullable so the "no hooks file"
  answer keeps its explicit null. `setupTrust` is nullable as well as optional
  because the `components-setup-ask` fixture sends an explicit null, and
  salvaging that would move a `normal` golden. `ui.get`'s trust record salvages
  per repo, so one unreadable repo cannot cost the others their approvals. The
  Codex redeem reply stays `z.unknown()`: `decodeResetResult` is a real
  scope-and-snapshot validator and splitting it would give one reply two refusal
  rules.

- terminal (4). The send verdict and the viewport pair keep main's exact
  `=== true` projections. `terminalSendAcceptedSchema` moves here from the
  session domain, which now re-expo

mobile/src/components/new-workspace-reply-schema.test.ts
@@ -1,0 +1,99 @@
+import { describe, expect, it } from 'vitest'
+import type { z } from 'zod'
+import {
+  newWorkspaceRepoHooksSchema,
+  newWorkspaceUiTrustSchema
+} from './new-workspace-reply-schema'
+
+function reads<T>(schema: z.ZodType<T, unknown>, value: unknown): T {
+  const parsed = schema.safeParse(value)
+  if (!parsed.success) {
+    throw new Error(`expected a readable reply: ${parsed.error.message}`)
+  }
+  return parsed.data
+}
+
+function refuses(schema: z.ZodType<unknown, unknown>, value: unknown): boolean {
+  return !schema.safeParse(value).success
+}
+
+describe('the drawer requires only what it reads unguarded', () => {
+  it('requires the hook source the drawer assigns straight into its details', () => {
+    const reply = { hooks: { scripts: { setup: 'pnpm i' } }, source: 'repo', setupTrust: null }
+    expect(reads(newWorkspaceRepoHooksSchema, reply).source).toBe('repo')
+    expect(refuses(newWorkspaceRepoHooksSchema, { hooks: null })).toBe(true)
+    expect(refuses(newWorkspaceRepoHooksSchema, { error: 'refused' })).toBe(true)
+    expect(refuses(newWorkspaceRepoHooksSchema, null)).toBe(true)
+  })
+
+  it('preserves an explicit null source rather than collapsing it to absent', () => {
+    expect(reads(newWorkspaceRepoHooksSchema, { hooks: null, source: null }).source).toBe(null)
+  })
+
+  it('takes the recorded reply with an explicit null setupTrust without salvaging it', () => {
+    // The components-setup-ask fixture sends `setupTrust: null`; treating that as a malformed
+    // member would report a drop on a reply the host really sends.
+    const reply = { hooks: null, source: null, setupRunPolicy: 'ask', setupTrust: null }
+    expect(reads(newWorkspaceRepoHooksSchema, reply).setupTrust).toBe(null)
+  })
+
+  it('drops a setupTrust missing the members normalizeSetupHookTrust reads', () => {
+    const reply = { source: null, setupTrust: { contentHash: 7, scriptContent: 'x' } }
+    expect(reads(newWorkspaceRepoHooksSchema, reply).setupTrust).toBe(undefined)
+  })
+})
+
+describe('the setup run policy is a closed enum with the call site defaulting it', () => {
+  it('keeps each arm the drawer compares against', () => {
+    for (const policy of ['ask', 'run-by-default', 'skip-by-default'] as const) {
+      expect(
+        reads(newWorkspaceRepoHooksSchema, { source: null, setupRunPolicy: policy }).setupRunPolicy
+      ).toBe(policy)
+    }
+  })
+
+  it('drops an arm this build does not know, which behaves as run-by-default did', () => {
+    expect(
+      reads(newWorkspaceRepoHooksSchema, { source: null, setupRunPolicy: 'on-tuesdays' })
+        .setupRunPolicy
+    ).toBe(undefined)
+  })
+})
+
+describe('the trust record salvages per repo', () => {
+  it('keeps the approvals isSetupHookTrusted reads', () => {
+    const ui = { trustedMantaHooks: { 'repo-1': { setup: { contentHash: 'h', approvedAt: 1 } } } }
+    expect(reads(newWorkspaceUiTrustSchema, { ui })?.trustedMantaHooks?.['repo-1']?.setup).toEqual({
+      contentHash: 'h',
+      approvedAt: 1
+    })
+  })
+
+  it('drops one unreadable repo without costing the others their trust', () => {
+    const ui = {
+      trustedMantaHooks: {
+        'repo-1': { all: { approvedAt: 1 } },
+        'repo-2': 'not-a-record'
+      }
+    }
+    const trust = reads(newWorkspaceUiTrustSchema, { ui })?.trustedMantaHooks
+    expect(trust?.['repo-1']?.all).toEqual({ approvedAt: 1 })
+    expect(trust?.['repo-2']).toBe(undefined)
+  })
+
+  it('reads a null or absent ui the way the drawer always has', () => {
+    expect(reads(newWorkspaceUiTrustSchema, null)).toBe(undefined)
+    expect(reads(newWorkspaceUiTrustSchema, {})).toBe(undefined)
+    expect(reads(newWorkspaceUiTrustSchema, { ui: null })).toBe(null)
+  })
+
+  // The call site interprets this reply inside an unawaited `void (async () => {})()` with no
+  // catch, so a refusal here is an unhandled rejection that also skips the provider computation
+  // below it. Main cast the value and read `?.trustedMantaHooks ?? {}`, which tolerated any shape.
+  it('reads a non-object ui.get result as absent rather than refusing it', () => {
+    for (const value of ['not-an-object', 42, true, [], [{ ui: {} }]]) {
+      expect(refuses(newWorkspaceUiTrustSchema, value)).toBe(false)
+      expect(reads(newWorkspaceUiTrustSchema, value)).toBe(undefined)
+    }
+  })
+})

mobile/src/components/new-workspace-reply-schema.ts
@@ -1,0 +1,96 @@
+import { z } from 'zod'
+import { salvagedOptional, salvagingRecord } from '../../../src/shared/zod-salvage'
+
+// The New Workspace drawer's own two reads. Checked against `repo.hooks`
+// (src/main/runtime/rpc/methods/repo.ts:190 → runtime.getRepoHooks) and `ui.get`
+// (src/main/runtime/rpc/methods/client-ui.ts:60), which answers `{ ui }` and nothing else.
+
+/**
+ * The repo's setup hook, as the drawer decorates its advanced section with it.
+ *
+ * `source` is the one required member: use-new-workspace-setup-script.ts:51 assigns it straight
+ * into `SetupHookDetails.source`, whose type is `string | null`, with no guard in between — and the
+ * handler always answers it, `null` when no manta.yaml parsed. Nullable rather than optional so the
+ * "no hooks file" answer keeps its explicit `null` instead of collapsing to absent.
+ *
+ * Everything else keeps main's own guards behind it. `hooks` is read through
+ * `result.hooks?.scripts?.setup?.trim()` and stays nullable, because the handler returns `null`
+ * there for a binary or unparsable manta.yaml. `setupRunPolicy` is a closed enum with the call
+ * site's `?? 'run-by-default'` still doing the defaulting: the only two comparisons against it are
+ * `!== 'skip-by-default'` and `=== 'ask'`, so an arm this build does not know behaves exactly as
+ * main's unrecognised string did. The arms are the host's `SetupRunPolicy`
+ * (src/shared/manta-yaml-hook-types.ts:1), which is what `getEffectiveSetupRunPolicy` answers. `setupTrust` is nullable as well as optional because the
+ * `components-setup-ask` fixture sends an explicit `null` — salvaging that as a drop would move a
+ * `normal` golden for a reply the host really sends.
+ */
+export const newWorkspaceRepoHooksSchema = z.looseObject({
+  hooks: salvagedOptional(
+    'hooks',
+    z
+      .looseObject({
+        scripts: salvagedOptional(
+          'scripts',
+          z.looseObject({ setup: salvagedOptional('setup', z.string()) })
+        )
+      })
+      .nullable()
+  ),
+  source: z.string().nullable(),
+  setupRunPolicy: salvagedOptional(
+    'setupRunPolicy',
+    z.enum(['ask', 'run-by-default', 'skip-by-default'])
+  ),
+  setupTrust: salvagedOptional(
+    'setupTrust',
+    z.looseObject({ contentHash: z.string(), scriptContent: z.string() }).nullable()
+  )
+})
+
+// One approval inside the persisted trust record. Both members are required *inside* a
+// `salvagedOptional`, so a malformed approval drops to absent rather than failing the reply — and an
+// approval that cannot be read is not an approval, which is the fail-closed direction for a trust
+// record. Entries this build does not know pass through, because `ui.set` writes the blob back.
+const trustedOrcaHookApprovalSchema = z.looseObject({
+  contentHash: z.string(),
+  approvedAt: z.number()
+})
+
+const trustedOrcaHookRepoSchema = z.looseObject({
+  all: salvagedOptional('all', z.looseObject({ approvedAt: z.number() })),
+  setup: salvagedOptional('setup', trustedOrcaHookApprovalSchema),
+  archive: salvagedOptional('archive', trustedOrcaHookApprovalSchema),
+  issueCommand: salvagedOptional('issueCommand', trustedOrcaHookApprovalSchema),
+  vmRecipe: salvagedOptional('vmRecipe', trustedOrcaHookApprovalSchema)
+})
+
+/**
+ * The persisted UI state, read for the trusted-hooks record alone.
+ *
+ * Total, and that is load-bearing rather than defensive. The call site interprets this reply inside
+ * an unawaited `void (async () => {})()` with no catch (use-new-workspace-runtime-context.ts:53),
+ * so a refusal would be an unhandled rejection that also skipped `setAvailableProviders` at :97.
+ * Main cast the result and read `?.trustedMantaHooks ?? {}`, which tolerated a reply of any shape,
+ * so anything that is not an object decodes as absent and reaches that `?? {}` exactly as before —
+ * use-new-workspace-runtime-context.ts:80 takes `ui.value?.trustedMantaHooks ?? {}`. The record
+ * salvages per repo, so one unreadable repo's approvals cannot cost every other repo its trust.
+ *
+ * The matrix cannot pin this: its mutation vocabulary is absent, null, inner-envelope and refusal,
+ * and none of those makes a fulfilled result a non-object. The unit case beside it is the pin.
+ */
+export const newWorkspaceUiTrustSchema = z
+  .looseObject({
+    ui: salvagedOptional(
+      'ui',
+      z
+        .looseObject({
+          trustedMantaHooks: salvagedOptional(
+            'trustedMantaHooks',
+            salvagingRecord(z.string(), trustedOrcaHookRepoSchema)
+          )
+        })
+        .nullable()
+    )
+  })
+  .nullish()
+  .catch(undefined)
+  .transform((reply) => reply?.ui)

mobile/src/components/use-new-workspace-runtime-context.ts
@@ -77,9 +77,7 @@ export function useNewWorkspaceRuntimeContext(
       if (uiRes.status === 'fulfilled') {
         const ui = newWorkspaceUiStateRead.interpret(uiRes.value)
         if (ui.accepted) {
-          // oxlint-disable-next-line typescript/consistent-type-assertions -- SAFETY: Preserve the established response shape at this boundary; a missing result reads as untrusted.
-          const trust = ui.value as { trustedMantaHooks?: PersistedTrustedMantaHooks } | undefined
-          setTrustedMantaHooks(trust?.trustedMantaHooks ?? {})
+          setTrustedMantaHooks(ui.value?.trustedMantaHooks ?? {})
         }
       }
 

ivanvykopal/orca

mobile/src/components/new-workspace-reply-schema.test.ts
@@ -1,0 +1,99 @@
+import { describe, expect, it } from 'vitest'
+import type { z } from 'zod'
+import {
+  newWorkspaceRepoHooksSchema,
+  newWorkspaceUiTrustSchema
+} from './new-workspace-reply-schema'
+
+function reads<T>(schema: z.ZodType<T, unknown>, value: unknown): T {
+  const parsed = schema.safeParse(value)
+  if (!parsed.success) {
+    throw new Error(`expected a readable reply: ${parsed.error.message}`)
+  }
+  return parsed.data
+}
+
+function refuses(schema: z.ZodType<unknown, unknown>, value: unknown): boolean {
+  return !schema.safeParse(value).success
+}
+
+describe('the drawer requires only what it reads unguarded', () => {
+  it('requires the hook source the drawer assigns straight into its details', () => {
+    const reply = { hooks: { scripts: { setup: 'pnpm i' } }, source: 'repo', setupTrust: null }
+    expect(reads(newWorkspaceRepoHooksSchema, reply).source).toBe('repo')
+    expect(refuses(newWorkspaceRepoHooksSchema, { hooks: null })).toBe(true)
+    expect(refuses(newWorkspaceRepoHooksSchema, { error: 'refused' })).toBe(true)
+    expect(refuses(newWorkspaceRepoHooksSchema, null)).toBe(true)
+  })
+
+  it('preserves an explicit null source rather than collapsing it to absent', () => {
+    expect(reads(newWorkspaceRepoHooksSchema, { hooks: null, source: null }).source).toBe(null)
+  })
+
+  it('takes the recorded reply with an explicit null setupTrust without salvaging it', () => {
+    // The components-setup-ask fixture sends `setupTrust: null`; treating that as a malformed
+    // member would report a drop on a reply the host really sends.
+    const reply = { hooks: null, source: null, setupRunPolicy: 'ask', setupTrust: null }
+    expect(reads(newWorkspaceRepoHooksSchema, reply).setupTrust).toBe(null)
+  })
+
+  it('drops a setupTrust missing the members normalizeSetupHookTrust reads', () => {
+    const reply = { source: null, setupTrust: { contentHash: 7, scriptContent: 'x' } }
+    expect(reads(newWorkspaceRepoHooksSchema, reply).setupTrust).toBe(undefined)
+  })
+})
+
+describe('the setup run policy is a closed enum with the call site defaulting it', () => {
+  it('keeps each arm the drawer compares against', () => {
+    for (const policy of ['ask', 'run-by-default', 'skip-by-default'] as const) {
+      expect(
+        reads(newWorkspaceRepoHooksSchema, { source: null, setupRunPolicy: policy }).setupRunPolicy
+      ).toBe(policy)
+    }
+  })
+
+  it('drops an arm this build does not know, which behaves as run-by-default did', () => {
+    expect(
+      reads(newWorkspaceRepoHooksSchema, { source: null, setupRunPolicy: 'on-tuesdays' })
+        .setupRunPolicy
+    ).toBe(undefined)
+  })
+})
+
+describe('the trust record salvages per repo', () => {
+  it('keeps the approvals isSetupHookTrusted reads', () => {
+    const ui = { trustedOrcaHooks: { 'repo-1': { setup: { contentHash: 'h', approvedAt: 1 } } } }
+    expect(reads(newWorkspaceUiTrustSchema, { ui })?.trustedOrcaHooks?.['repo-1']?.setup).toEqual({
+      contentHash: 'h',
+      approvedAt: 1
+    })
+  })
+
+  it('drops one unreadable repo without costing the others their trust', () => {
+    const ui = {
+      trustedOrcaHooks: {
+        'repo-1': { all: { approvedAt: 1 } },
+        'repo-2': 'not-a-record'
+      }
+    }
+    const trust = reads(newWorkspaceUiTrustSchema, { ui })?.trustedOrcaHooks
+    expect(trust?.['repo-1']?.all).toEqual({ approvedAt: 1 })
+    expect(trust?.['repo-2']).toBe(undefined)
+  })
+
+  it('reads a null or absent ui the way the drawer always has', () => {
+    expect(reads(newWorkspaceUiTrustSchema, null)).toBe(undefined)
+    expect(reads(newWorkspaceUiTrustSchema, {})).toBe(undefined)
+    expect(reads(newWorkspaceUiTrustSchema, { ui: null })).toBe(null)
+  })
+
+  // The call site interprets this reply inside an unawaited `void (async () => {})()` with no
+  // catch, so a refusal here is an unhandled rejection that also skips the provider computation
+  // below it. Main cast the value and read `?.trustedOrcaHooks ?? {}`, which tolerated any shape.
+  it('reads a non-object ui.get result as absent rather than refusing it', () => {
+    for (const value of ['not-an-object', 42, true, [], [{ ui: {} }]]) {
+      expect(refuses(newWorkspaceUiTrustSchema, value)).toBe(false)
+      expect(reads(newWorkspaceUiTrustSchema, value)).toBe(undefined)
+    }
+  })
+})

mobile/src/components/new-workspace-reply-schema.ts
@@ -1,0 +1,96 @@
+import { z } from 'zod'
+import { salvagedOptional, salvagingRecord } from '../../../src/shared/zod-salvage'
+
+// The New Workspace drawer's own two reads. Checked against `repo.hooks`
+// (src/main/runtime/rpc/methods/repo.ts:190 → runtime.getRepoHooks) and `ui.get`
+// (src/main/runtime/rpc/methods/client-ui.ts:60), which answers `{ ui }` and nothing else.
+
+/**
+ * The repo's setup hook, as the drawer decorates its advanced section with it.
+ *
+ * `source` is the one required member: use-new-workspace-setup-script.ts:51 assigns it straight
+ * into `SetupHookDetails.source`, whose type is `string | null`, with no guard in between — and the
+ * handler always answers it, `null` when no orca.yaml parsed. Nullable rather than optional so the
+ * "no hooks file" answer keeps its explicit `null` instead of collapsing to absent.
+ *
+ * Everything else keeps main's own guards behind it. `hooks` is read through
+ * `result.hooks?.scripts?.setup?.trim()` and stays nullable, because the handler returns `null`
+ * there for a binary or unparsable orca.yaml. `setupRunPolicy` is a closed enum with the call
+ * site's `?? 'run-by-default'` still doing the defaulting: the only two comparisons against it are
+ * `!== 'skip-by-default'` and `=== 'ask'`, so an arm this build does not know behaves exactly as
+ * main's unrecognised string did. The arms are the host's `SetupRunPolicy`
+ * (src/shared/orca-yaml-hook-types.ts:1), which is what `getEffectiveSetupRunPolicy` answers. `setupTrust` is nullable as well as optional because the
+ * `components-setup-ask` fixture sends an explicit `null` — salvaging that as a drop would move a
+ * `normal` golden for a reply the host really sends.
+ */
+export const newWorkspaceRepoHooksSchema = z.looseObject({
+  hooks: salvagedOptional(
+    'hooks',
+    z
+      .looseObject({
+        scripts: salvagedOptional(
+          'scripts',
+          z.looseObject({ setup: salvagedOptional('setup', z.string()) })
+        )
+      })
+      .nullable()
+  ),
+  source: z.string().nullable(),
+  setupRunPolicy: salvagedOptional(
+    'setupRunPolicy',
+    z.enum(['ask', 'run-by-default', 'skip-by-default'])
+  ),
+  setupTrust: salvagedOptional(
+    'setupTrust',
+    z.looseObject({ contentHash: z.string(), scriptContent: z.string() }).nullable()
+  )
+})
+
+// One approval inside the persisted trust record. Both members are required *inside* a
+// `salvagedOptional`, so a malformed approval drops to absent rather than failing the reply — and an
+// approval that cannot be read is not an approval, which is the fail-closed direction for a trust
+// record. Entries this build does not know pass through, because `ui.set` writes the blob back.
+const trustedOrcaHookApprovalSchema = z.looseObject({
+  contentHash: z.string(),
+  approvedAt: z.number()
+})
+
+const trustedOrcaHookRepoSchema = z.looseObject({
+  all: salvagedOptional('all', z.looseObject({ approvedAt: z.number() })),
+  setup: salvagedOptional('setup', trustedOrcaHookApprovalSchema),
+  archive: salvagedOptional('archive', trustedOrcaHookApprovalSchema),
+  issueCommand: salvagedOptional('issueCommand', trustedOrcaHookApprovalSchema),
+  vmRecipe: salvagedOptional('vmRecipe', trustedOrcaHookApprovalSchema)
+})
+
+/**
+ * The persisted UI state, read for the trusted-hooks record alone.
+ *
+ * Total, and that is load-bearing rather than defensive. The call site interprets this reply inside
+ * an unawaited `void (async () => {})()` with no catch (use-new-workspace-runtime-context.ts:53),
+ * so a refusal would be an unhandled rejection that also skipped `setAvailableProviders` at :97.
+ * Main cast the result and read `?.trustedOrcaHooks ?? {}`, which tolerated a reply of any shape,
+ * so anything that is not an object decodes as absent and reaches that `?? {}` exactly as before —
+ * use-new-workspace-runtime-context.ts:80 takes `ui.value?.trustedOrcaHooks ?? {}`. The record
+ * salvages per repo, so one unreadable repo's approvals cannot cost every other repo its trust.
+ *
+ * The matrix cannot pin this: its mutation vocabulary is absent, null, inner-envelope and refusal,
+ * and none of those makes a fulfilled result a non-object. The unit case beside it is the pin.
+ */
+export const newWorkspaceUiTrustSchema = z
+  .looseObject({
+    ui: salvagedOptional(
+      'ui',
+      z
+        .looseObject({
+          trustedOrcaHooks: salvagedOptional(
+            'trustedOrcaHooks',
+            salvagingRecord(z.string(), trustedOrcaHookRepoSchema)
+          )
+        })
+        .nullable()
+    )
+  })
+  .nullish()
+  .catch(undefined)
+  .transform((reply) => reply?.ui)

mobile/src/components/use-new-workspace-runtime-context.ts
@@ -77,9 +77,7 @@ export function useNewWorkspaceRuntimeContext(
       if (uiRes.status === 'fulfilled') {
         const ui = newWorkspaceUiStateRead.interpret(uiRes.value)
         if (ui.accepted) {
-          // oxlint-disable-next-line typescript/consistent-type-assertions -- SAFETY: Preserve the established response shape at this boundary; a missing result reads as untrusted.
-          const trust = ui.value as { trustedOrcaHooks?: PersistedTrustedOrcaHooks } | undefined
-          setTrustedOrcaHooks(trust?.trustedOrcaHooks ?? {})
+          setTrustedOrcaHooks(ui.value?.trustedOrcaHooks ?? {})
         }
       }
 