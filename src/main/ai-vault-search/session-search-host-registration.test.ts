paidaxingyo666/Manta

feat(session-search): enable indexing on paired servers from a client (#20886)
* feat(session-search): add ranked history panel search and consent

* test: wait for initial session indexing before refreshing results

* feat(session-history): add local search settings and index controls

* Use shared local host identifier for session index status

* feat(session-search): merge all-computers search across hosts

The `all` scope on `aiVault:searchSessions` now fans out from the desktop
to every host the session list enumerates and merges the pages into one.
Legs run in parallel: the local index through the search service, SSH and
runtime hosts through the existing remote search client.

Two fixed orders, because relevance scores from independent indexes are
not comparable. `newest` asks every leg for recency and k-way merges on
`updatedAt`, nulls last, ties broken on execution host id. `relevance`
rotates hosts in host-id order by their own rank.

The merged cursor is an opaque base64url payload holding each host's
cursor, how many of its current page were already emitted, and the
generation that offset counts into, plus the page size and sort the
cursor belongs to. A host whose index moved is fenced to `stale` and
stops contributing; the rest keep paging. Per-host outcomes ride back on
one new optional `hosts` field on the results response.

`aiVault:searchStatus` with `all` stays refused, and neither the runtime
RPC nor the CLI gains the scope, so a fan-out is never two hops.

* fix(preload): let the search bridge address the all-computers scope

* feat(settings): live index status, enable confirm, advanced delete

* feat(session-search): search every computer from the history panel

The panel's "All computers" scope produced no request: the hook parsed the
scope into a single host id and stopped when that was null, so the panel
answered "Choose one computer to search its sessions." The desktop already
merges every enumerated host behind `aiVault:searchSessions`, so 

src/main/ai-vault-search/session-search-host-registration.test.ts
@@ -3,6 +3,7 @@ import { join } from 'node:path'
 import { afterEach, beforeEach, expect, it, vi } from 'vitest'
 import { resetSessionParseCacheForTests } from '../ai-vault/session-scanner-parse-cache'
 import { resetTranscriptConsumersForTests } from '../ai-vault/session-transcript-consumers'
+import type { AiVaultSearchSettings } from '../../shared/ai-vault-search-settings'
 import { installInProcessSessionSearchService } from './session-search-in-process-service'
 import {
   openSessionSearchIndexerHarness,
@@ -35,7 +36,7 @@ vi.mock('../ai-vault/cached-session-list', async (importOriginal) => ({
 const ROOT = join(import.meta.dirname, '..', '..', '..')
 
 let harness: SessionSearchIndexerHarness
-let installed: { dispose(): void } | null
+let installed: { apply?(settings: AiVaultSearchSettings): void; dispose(): void } | null
 
 beforeEach(async () => {
   resetSessionParseCacheForTests()
@@ -203,4 +204,38 @@ it('mantad resolves no roots while disabled and discovers late roots when enable
   if (response.kind === 'results') {
     expect(response.hits.map((hit) => hit.sessionId)).toEqual([id])
   }
+})
+
+// A host with no scanner child has nothing to forward a policy to, so the installed
+// service is itself how a settings write reaches the index.
+it('re-applies consent on an in-process host without reinstalling the service', async () => {
+  installed = installInProcessSessionSearchService({
+    dataRoot: harness.root,
+    roots: harness.roots,
+    settings: { enabled: false, historyDays: null }
+  })
+  expect(await searchSessionService({ query: 'ledger' }, 'relay')).toEqual({
+    kind: 'unavailable',
+    reason: 'disabled'
+  })
+
+  installed?.apply?.({ enabled: true, historyDays: null })
+  expect(await searchSessionService({ query: 'ledger' }, 'relay')).not.toMatchObject({
+    kind: 'unavailable',
+    reason: 'disabled'
+  })
+
+  installed?.apply?.({ enabled: false, historyDays: null })
+  expect(await searchSessionService({ query: 'ledger' }, 'relay')).toEqual({
+    kind: 'unavailable',
+    reason: 'disabled'
+  })
+})
+
+// mantad reaches the index through the deps hook the runtime RPC calls; the wiring is
+// what no unit of either module can show.
+it('wires mantad consent from the runtime hook to the installed service', () => {
+  const source = readFileSync(join(ROOT, 'src/main/mantad/mantad-entry.ts'), 'utf8')
+  expect(source).toContain('applySessionSearchSettings:')
+  expect(source).toContain('sessionSearch?.apply(next)')
 })

src/main/runtime/rpc/methods/ai-vault-search.test.ts
@@ -1,4 +1,4 @@
-import { afterEach, describe, expect, it } from 'vitest'
+import { afterEach, describe, expect, it, vi } from 'vitest'
 import { RpcDispatcher } from '../dispatcher'
 import { MantaRuntimeService } from '../../manta-runtime'
 import { AI_VAULT_METHODS } from './ai-vault'
@@ -74,5 +74,108 @@ describe('session search runtime RPC', () => {
     })
     const broken = createSessionSearchClient(async () => ({ kind: 'results', hits: [] }), 'runtime')
     await expect(broken.searchSessions({ query: 'needle' })).rejects.toThrow()
+  })
+})
+
+describe('session search consent over the runtime RPC', () => {
+  const enableRequest = (params: unknown) => ({
+    id: 'enable-1',
+    authToken: 'test',
+    method: 'aiVault.setSearchEnabled',
+    params
+  })
+
+  function consentDispatcher(runtime = new MantaRuntimeService()) {
+    const setSessionSearchEnabled = vi.fn(async () => {})
+    // Overrides the surface-installed method, which proves it is there to override.
+    Object.assign(runtime, { setSessionSearchEnabled })
+    const dispatcher = new RpcDispatcher({ runtime, methods: AI_VAULT_METHODS })
+    // Why the streaming entry point: `pairedDeviceId` only reaches a handler through it, and
+    // it is the one the WebSocket transport a paired client connects over actually calls.
+    const call = async (
+      params: unknown,
+      options?: { pairedDeviceId?: string; clientKind?: 'runtime' | 'mobile' }
+    ): Promise<{ ok: boolean; result?: unknown; error?: { code: string } }> => {
+      let raw = ''
+      await dispatcher.dispatchStreaming(
+        enableRequest(params),
+        (response) => {
+          raw = response
+        },
+        options
+      )
+      return JSON.parse(raw)
+    }
+    return { setSessionSearchEnabled, call }
+  }
+
+  it('refuses an in-process caller and never touches the setting', async () => {
+    const { call, setSessionSearchEnabled } = consentDispatcher()
+    expect(await call({ enabled: true })).toMatchObject({
+      ok: false,
+      error: { code: 'forbidden' }
+    })
+    expect(setSessionSearchEnabled).not.toHaveBeenCalled()
+  })
+
+  it('applies a paired change and answers with this host status', async () => {
+    setSessionSearchService(fakeSearchService())
+    const { call, setSessionSearchEnabled } = consentDispatcher()
+    const response = await call(
+      { enabled: true },
+      { pairedDeviceId: 'device-7', clientKind: 'runtime' }
+    )
+
+    expect(setSessionSearchEnabled).toHaveBeenCalledExactlyOnceWith(true)
+    expect(response).toMatchObject({ ok: true, result: { enabled: true, generation: 7 } })
+  })
+
+  it('withholds host roots from a paired client, as searchStatus does', async () => {
+    setSessionSearchService({
+      ...fakeSearchService(),
+      status: async () => ({
+        enabled: true,
+        phase: 'degraded' as const,
+        filesIndexed: 0,
+        filesDue: 0,
+        filesFailed: 1,
+        degradedRoots: [{ root: '/Users/someone/.claude', reason: 'unreadable' }],
+        lastReconcileAt: null,
+        lastSweepCompletedAt: null,
+        generation: 3
+      })
+    })
+    const { call } = consentDispatcher()
+    const response = await call(
+      { enabled: false },
+      { pairedDeviceId: 'device-7', clientKind: 'runtime' }
+    )
+
+    expect(JSON.stringify(response)).not.toContain('/Users/someone/.claude')
+  })
+
+  it('reports the host refusal when this runtime has no settings store', async () => {
+    const runtime = new MantaRuntimeService()
+    const dispatcher = new RpcDispatcher({ runtime, methods: AI_VAULT_METHODS })
+    let raw = ''
+    await dispatcher.dispatchStreaming(
+      enableRequest({ enabled: true }),
+      (response) => {
+        raw = response
+      },
+      { pairedDeviceId: 'device-7', clientKind: 'runtime' }
+    )
+    expect(JSON.parse(raw)).toMatchObject({
+      ok: false,
+      error: { code: 'runtime_unavailable' }
+    })
+  })
+
+  it('rejects a non-boolean before reaching the runtime', async () => {
+    const { call, setSessionSearchEnabled } = consentDispatcher()
+    expect(await call({ enabled: 'yes' }, { pairedDeviceId: 'device-7' })).toMatchObject({
+      ok: false
+    })
+    expect(setSessionSearchEnabled).not.toHaveBeenCalled()
   })
 })

ivanvykopal/orca

src/main/ai-vault-search/session-search-host-registration.test.ts
@@ -3,6 +3,7 @@ import { join } from 'node:path'
 import { afterEach, beforeEach, expect, it, vi } from 'vitest'
 import { resetSessionParseCacheForTests } from '../ai-vault/session-scanner-parse-cache'
 import { resetTranscriptConsumersForTests } from '../ai-vault/session-transcript-consumers'
+import type { AiVaultSearchSettings } from '../../shared/ai-vault-search-settings'
 import { installInProcessSessionSearchService } from './session-search-in-process-service'
 import {
   openSessionSearchIndexerHarness,
@@ -35,7 +36,7 @@ vi.mock('../ai-vault/cached-session-list', async (importOriginal) => ({
 const ROOT = join(import.meta.dirname, '..', '..', '..')
 
 let harness: SessionSearchIndexerHarness
-let installed: { dispose(): void } | null
+let installed: { apply?(settings: AiVaultSearchSettings): void; dispose(): void } | null
 
 beforeEach(async () => {
   resetSessionParseCacheForTests()
@@ -203,4 +204,38 @@ it('orcad resolves no roots while disabled and discovers late roots when enabled
   if (response.kind === 'results') {
     expect(response.hits.map((hit) => hit.sessionId)).toEqual([id])
   }
+})
+
+// A host with no scanner child has nothing to forward a policy to, so the installed
+// service is itself how a settings write reaches the index.
+it('re-applies consent on an in-process host without reinstalling the service', async () => {
+  installed = installInProcessSessionSearchService({
+    dataRoot: harness.root,
+    roots: harness.roots,
+    settings: { enabled: false, historyDays: null }
+  })
+  expect(await searchSessionService({ query: 'ledger' }, 'relay')).toEqual({
+    kind: 'unavailable',
+    reason: 'disabled'
+  })
+
+  installed?.apply?.({ enabled: true, historyDays: null })
+  expect(await searchSessionService({ query: 'ledger' }, 'relay')).not.toMatchObject({
+    kind: 'unavailable',
+    reason: 'disabled'
+  })
+
+  installed?.apply?.({ enabled: false, historyDays: null })
+  expect(await searchSessionService({ query: 'ledger' }, 'relay')).toEqual({
+    kind: 'unavailable',
+    reason: 'disabled'
+  })
+})
+
+// orcad reaches the index through the deps hook the runtime RPC calls; the wiring is
+// what no unit of either module can show.
+it('wires orcad consent from the runtime hook to the installed service', () => {
+  const source = readFileSync(join(ROOT, 'src/main/orcad/orcad-entry.ts'), 'utf8')
+  expect(source).toContain('applySessionSearchSettings:')
+  expect(source).toContain('sessionSearch?.apply(next)')
 })

src/main/runtime/rpc/methods/ai-vault-search.test.ts
@@ -1,4 +1,4 @@
-import { afterEach, describe, expect, it } from 'vitest'
+import { afterEach, describe, expect, it, vi } from 'vitest'
 import { RpcDispatcher } from '../dispatcher'
 import { OrcaRuntimeService } from '../../orca-runtime'
 import { AI_VAULT_METHODS } from './ai-vault'
@@ -74,5 +74,108 @@ describe('session search runtime RPC', () => {
     })
     const broken = createSessionSearchClient(async () => ({ kind: 'results', hits: [] }), 'runtime')
     await expect(broken.searchSessions({ query: 'needle' })).rejects.toThrow()
+  })
+})
+
+describe('session search consent over the runtime RPC', () => {
+  const enableRequest = (params: unknown) => ({
+    id: 'enable-1',
+    authToken: 'test',
+    method: 'aiVault.setSearchEnabled',
+    params
+  })
+
+  function consentDispatcher(runtime = new OrcaRuntimeService()) {
+    const setSessionSearchEnabled = vi.fn(async () => {})
+    // Overrides the surface-installed method, which proves it is there to override.
+    Object.assign(runtime, { setSessionSearchEnabled })
+    const dispatcher = new RpcDispatcher({ runtime, methods: AI_VAULT_METHODS })
+    // Why the streaming entry point: `pairedDeviceId` only reaches a handler through it, and
+    // it is the one the WebSocket transport a paired client connects over actually calls.
+    const call = async (
+      params: unknown,
+      options?: { pairedDeviceId?: string; clientKind?: 'runtime' | 'mobile' }
+    ): Promise<{ ok: boolean; result?: unknown; error?: { code: string } }> => {
+      let raw = ''
+      await dispatcher.dispatchStreaming(
+        enableRequest(params),
+        (response) => {
+          raw = response
+        },
+        options
+      )
+      return JSON.parse(raw)
+    }
+    return { setSessionSearchEnabled, call }
+  }
+
+  it('refuses an in-process caller and never touches the setting', async () => {
+    const { call, setSessionSearchEnabled } = consentDispatcher()
+    expect(await call({ enabled: true })).toMatchObject({
+      ok: false,
+      error: { code: 'forbidden' }
+    })
+    expect(setSessionSearchEnabled).not.toHaveBeenCalled()
+  })
+
+  it('applies a paired change and answers with this host status', async () => {
+    setSessionSearchService(fakeSearchService())
+    const { call, setSessionSearchEnabled } = consentDispatcher()
+    const response = await call(
+      { enabled: true },
+      { pairedDeviceId: 'device-7', clientKind: 'runtime' }
+    )
+
+    expect(setSessionSearchEnabled).toHaveBeenCalledExactlyOnceWith(true)
+    expect(response).toMatchObject({ ok: true, result: { enabled: true, generation: 7 } })
+  })
+
+  it('withholds host roots from a paired client, as searchStatus does', async () => {
+    setSessionSearchService({
+      ...fakeSearchService(),
+      status: async () => ({
+        enabled: true,
+        phase: 'degraded' as const,
+        filesIndexed: 0,
+        filesDue: 0,
+        filesFailed: 1,
+        degradedRoots: [{ root: '/Users/someone/.claude', reason: 'unreadable' }],
+        lastReconcileAt: null,
+        lastSweepCompletedAt: null,
+        generation: 3
+      })
+    })
+    const { call } = consentDispatcher()
+    const response = await call(
+      { enabled: false },
+      { pairedDeviceId: 'device-7', clientKind: 'runtime' }
+    )
+
+    expect(JSON.stringify(response)).not.toContain('/Users/someone/.claude')
+  })
+
+  it('reports the host refusal when this runtime has no settings store', async () => {
+    const runtime = new OrcaRuntimeService()
+    const dispatcher = new RpcDispatcher({ runtime, methods: AI_VAULT_METHODS })
+    let raw = ''
+    await dispatcher.dispatchStreaming(
+      enableRequest({ enabled: true }),
+      (response) => {
+        raw = response
+      },
+      { pairedDeviceId: 'device-7', clientKind: 'runtime' }
+    )
+    expect(JSON.parse(raw)).toMatchObject({
+      ok: false,
+      error: { code: 'runtime_unavailable' }
+    })
+  })
+
+  it('rejects a non-boolean before reaching the runtime', async () => {
+    const { call, setSessionSearchEnabled } = consentDispatcher()
+    expect(await call({ enabled: 'yes' }, { pairedDeviceId: 'device-7' })).toMatchObject({
+      ok: false
+    })
+    expect(setSessionSearchEnabled).not.toHaveBeenCalled()
   })
 })