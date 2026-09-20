paidaxingyo666/Manta

refactor(mobile): checked reply readers for the tasks domain's board, runtime, search and create (step 7) (#21246)
* test(mobile): record main's agent.launch create receipt before checking it

`agent.launch` is the one read site in the tasks domain's project-board,
runtime, source-search and workspace create/source files with no recording
family at all, so main's answer to a malformed launch receipt was undocumented
and a checked reader would have had nothing to move.

One family, one scenario, two goldens: `worktree.agent-launch-create` drives
`createWorktreeWithNameRetry` down the `agent.launch` arm instead of
`worktree.create`, which needs an `agentLaunch` argument on the existing
worktree-create-retry adapter. The agent is a constant there on purpose — which
agent is picked changes only the params, and the arm under test is which method
the create is issued on.

A separate family rather than an eighth `worktree.create-retry` scenario:
`familyGoldens` drives its reply matrix over the family's FIRST scenario, so
adding to that family would have recorded a pilot golden and left the launch
receipt with no partitions. As its own base it gets all eleven.

Recorded from a detached worktree at the pinned baseline with this branch's
`rpc-recording/` and manifest copied in, per the recipe in the recorder README:
`mobile/pnpm-lock.yaml` has drifted past `4b876758d3` on main, so `--record`
refuses on this branch's tree even though `mobile/src` and `src/shared` are
byte-identical to the pin.

Thirty-four existing goldens move on `adapterSha256` and nothing else — the six
families mounted through the edited adapter module. No body moves.

Claude-Session: https://claude.ai/code/session_01JNnE9qzUZMMnqpZWCqM3nb

* refactor(mobile): checked reply readers for the tasks domain's board, runtime, search and create

Forty-three unchecked reply readers across five files become checked zod readers,
so a malformed host reply surfaces as one readable error naming the method
instead of a 

mobile/src/tasks/task-runtime-reply-schema.test.ts
@@ -1,0 +1,121 @@
+import { describe, expect, it } from 'vitest'
+import {
+  taskLinearStatusSchema,
+  taskPreferenceWriteSchema,
+  taskPreflightSchema,
+  taskRuntimeStatusSchema,
+  taskUiStateSchema
+} from './task-runtime-reply-schema'
+
+// Pins what the hydration reads require and what they deliberately do not.
+
+describe('the runtime status', () => {
+  it('reads the recorded capability lists', () => {
+    for (const capabilities of [['mobile.tasks.v1'], ['files.mutation-ownership.v1']]) {
+      const parsed = taskRuntimeStatusSchema.safeParse({ capabilities })
+      expect(parsed.success && parsed.data).toMatchObject({ capabilities })
+    }
+  })
+
+  it('requires the container main read `.capabilities` off', () => {
+    expect(taskRuntimeStatusSchema.safeParse(null).success).toBe(false)
+    expect(taskRuntimeStatusSchema.safeParse('ok').success).toBe(false)
+    expect(taskRuntimeStatusSchema.safeParse({}).success).toBe(true)
+  })
+
+  it('drops a non-string capability, which `includes` could never have matched', () => {
+    const parsed = taskRuntimeStatusSchema.safeParse({ capabilities: ['push.v1', 7] })
+    expect(parsed.success && parsed.data).toMatchObject({ capabilities: ['push.v1'] })
+  })
+
+  it('leaves worktreeCreateIdempotency untouched in all four states the probe triages', () => {
+    for (const advertised of [undefined, null, 'nonsense', { dedupeTtlMs: 45_000 }]) {
+      const parsed = taskRuntimeStatusSchema.safeParse({ worktreeCreateIdempotency: advertised })
+      expect(parsed.success).toBe(true)
+      expect(parsed.success ? parsed.data.worktreeCreateIdempotency : 'unparsed').toEqual(
+        advertised
+      )
+    }
+  })
+})
+
+describe('the persisted UI state', () => {
+  it('answers the `ui` member, which is what the member reader it replaces did', () => {
+    const parsed = taskUiStateSchema.safeParse({
+      ui: { sortBy: 'name', trustedMantaHooks: { 'repo-1': { all: { approvedAt: 1 } } } }
+    })
+    expect(parsed.success && parsed.data).toMatchObject({
+      sortBy: 'name',
+      trustedMantaHooks: { 'repo-1': { all: { approvedAt: 1 } } }
+    })
+  })
+
+  it('reads the recorded empty ui as an empty record, not as absent', () => {
+    const parsed = taskUiStateSchema.safeParse({ ui: {} })
+    expect(parsed.success && parsed.data).toEqual({})
+  })
+
+  it('answers undefined for a payload with no ui, where main read undefined off the object', () => {
+    const parsed = taskUiStateSchema.safeParse({})
+    expect(parsed.success && parsed.data).toBeUndefined()
+  })
+
+  it.each([null, undefined, 'ok', 7, [], true])(
+    'answers undefined for %j, so the other hydration legs survive an unreadable ui reply',
+    (payload) => {
+      const parsed = taskUiStateSchema.safeParse(payload)
+      expect(parsed.success).toBe(true)
+      expect(parsed.success ? parsed.data : 'unparsed').toBeUndefined()
+    }
+  )
+})
+
+describe('the two advisory probes', () => {
+  it('reads the recorded preflight and linear replies', () => {
+    expect(taskPreflightSchema.safeParse({ glab: { installed: false } }).success).toBe(true)
+    expect(taskLinearStatusSchema.safeParse({ connected: false }).success).toBe(true)
+  })
+
+  it('requires nothing, because every consumer compares the leaf to `true`', () => {
+    expect(taskPreflightSchema.safeParse({}).success).toBe(true)
+    expect(taskLinearStatusSchema.safeParse({}).success).toBe(true)
+  })
+
+  it('drops a malformed glab rather than reading it as installed', () => {
+    const parsed = taskPreflightSchema.safeParse({ glab: 'yes' })
+    expect(parsed.success ? parsed.data.glab : 'unparsed').toBeUndefined()
+  })
+
+  it('passes a host member no mobile consumer reads straight through', () => {
+    const parsed = taskPreflightSchema.safeParse({ gh: { installed: true, authenticated: true } })
+    expect(parsed.success && parsed.data).toMatchObject({
+      gh: { installed: true, authenticated: true }
+    })
+  })
+
+  // `success-result-or-skip` accepts an envelope whose `result` is absent or null and then asks
+  // the reader to decode it, so a refusal here throws out of the caller's whole hydration. Main
+  // hydrated on both of these replies; the recorded settings.task-hydration and
+  // settings.workspace-context partitions are what caught the difference.
+  it.each([
+    ['absent', undefined],
+    ['null', null],
+    ['a bare string', 'preflight'],
+    ['an array', []]
+  ])('reads a %s payload as "nothing advertised" rather than refusing it', (_label, reply) => {
+    const preflight = taskPreflightSchema.safeParse(reply)
+    expect(preflight.success).toBe(true)
+    expect(preflight.success && preflight.data.glab?.installed).toBeUndefined()
+    const linear = taskLinearStatusSchema.safeParse(reply)
+    expect(linear.success).toBe(true)
+    expect(linear.success && linear.data.connected).toBeUndefined()
+  })
+})
+
+describe('the three preference writes', () => {
+  it('accept every payload, because no call site reads their body', () => {
+    for (const reply of [null, undefined, 0, 'written', { ok: true }, []]) {
+      expect(taskPreferenceWriteSchema.safeParse(reply).success).toBe(true)
+    }
+  })
+})

mobile/src/tasks/task-runtime-reply-schema.ts
@@ -1,0 +1,120 @@
+import { z } from 'zod'
+import { salvagedOptional, salvagingArray } from '../../../src/shared/zod-salvage'
+
+// What the Tasks screen reads once per host to hydrate, and the preferences it writes back.
+// Checked against src/main/runtime/rpc/methods/status.ts:6-16 (RuntimeStatus, declared in
+// src/shared/runtime-session-contracts.ts:64), client-ui.ts:22-74 (the `{ settings }` / `{ ui }`
+// envelopes), preflight.ts:17 (PreflightStatus) and linear.ts:35-42 (the connection status).
+
+/**
+ * The runtime status, read for a capability list and nothing else.
+ *
+ * `capabilities` is the only member any consumer here reaches for, and every one of them guards it
+ * — `status.capabilities?.includes(…)` in use-mobile-tasks-runtime-hydration.tsx:205 and in
+ * src/shared/file-mutation-ownership.ts:10, `result.capabilities ?? []` in
+ * worktree-create-capability.ts:51. So the requirement is the container: main read `.capabilities`
+ * off whatever the payload was, and a string or a number reply was a property read that answered
+ * `undefined` and silently downgraded the host to "Tasks unsupported".
+ *
+ * Elements are strings and a non-string element drops. `includes` over a mixed array already never
+ * matched a capability id, so the drop changes no verdict; it is what makes the drop visible in
+ * the salvage report instead of invisible in an `includes` that quietly answers false.
+ *
+ * `worktreeCreateIdempotency` is `unknown` on purpose. worktree-create-capability.ts:55-70 does its
+ * own `typeof`/`Array.isArray` triage over it and treats absent, null, non-object and object as
+ * four different answers; a schema that narrowed it would have to pick one of those apart here and
+ * change which branch a host lands in.
+ */
+export const taskRuntimeStatusSchema = z.looseObject({
+  capabilities: salvagedOptional('capabilities', salvagingArray(z.string())),
+  worktreeCreateIdempotency: z.unknown().optional(),
+  hostPlatform: salvagedOptional('hostPlatform', z.string())
+})
+
+/**
+ * Persisted client UI state, answered under a `ui` member.
+ *
+ * The reader yields `ui` itself, which is what the member reader it replaces did, and it is total
+ * like the two probes below it. Main's `rpcPayloadMember` boxed the payload and read `undefined`
+ * off a string, a number or an array, so the Tasks screen hydrated on any of those; a refusal here
+ * would instead throw out of `hydrateTaskState` and leave the whole screen unhydrated (the outer
+ * `.catch` at use-mobile-tasks-runtime-hydration.tsx:349). Main did throw on a null or absent
+ * payload, and that is the one place this reader is deliberately kinder: persisted UI state is
+ * optional at every read (:283 spells `uiState?.trustedMantaHooks ?? {}`, :284
+ * `uiState?.taskResumeState ?? {}`), so "no ui" hydrates with the defaults rather than failing the
+ * settings, preflight and Linear legs that arrived beside it. The
+ * `matrix-settings.task-hydration-ui.get-1` golden records that move on `result-absent` and
+ * `result-null`.
+ *
+ * Both members are `unknown`, and the call site keeps one narrowing cast over them. They are
+ * opaque forwards: `trustedMantaHooks` goes straight into state, and `taskResumeState` is the
+ * screen's whole persisted view state, re-read field by field with its own defaults at :286 and
+ * across use-mobile-tasks-client-settings-actions.tsx. Declaring either here would restate a
+ * twelve-member union that nothing in this reader reads.
+ */
+export const taskUiStateSchema = z
+  .looseObject({
+    ui: salvagedOptional(
+      'ui',
+      z.looseObject({
+        taskResumeState: z.unknown().optional(),
+        trustedMantaHooks: z.unknown().optional()
+      })
+    )
+  })
+  .nullish()
+  .catch(undefined)
+  .transform((reply) => reply?.ui)
+
+/**
+ * The provider preflight, read only for whether `glab` is installed.
+ *
+ * Three consumers and all three guard to the leaf: `preflight?.glab?.installed === true`
+ * (use-mobile-tasks-runtime-hydration.tsx:295, mobile-home-host-requests.ts:95) and the
+ * `readProbeMember` pair in use-new-workspace-runtime-context.ts:92. `git` and `gh` are declared
+ * non-optional by PreflightStatus but no mobile consumer reads them, so they pass through.
+ *
+ * Total on purpose. This read is advertised as advisory, but `success-result-or-skip` accepts an
+ * envelope whose `result` is absent or null and then asks the reader to decode it, and a refusal
+ * there throws out of the whole hydration -- the corpus records main hydrating the Tasks screen on
+ * that reply and this reader leaving it unhydrated. `.catch` restores main's answer exactly,
+ * because every consumer guards to the leaf and reads absence as "not installed".
+ */
+export const taskPreflightSchema = z
+  .looseObject({
+    glab: salvagedOptional(
+      'glab',
+      z.looseObject({ installed: salvagedOptional('installed', z.boolean()) })
+    )
+  })
+  .catch({ glab: undefined })
+
+/**
+ * Whether Linear is connected.
+ *
+ * `connected` is compared to `true` at use-mobile-tasks-runtime-hydration.tsx:293,
+ * mobile-home-host-requests.ts:96 and use-new-workspace-runtime-context.ts:96, so absence and
+ * `false` already mean the same thing and nothing is required. `workspaces` is not declared: the
+ * picker reads it through a different operation on this same method, and listing a member ahead of
+ * a reader is how a schema starts refusing replies no consumer here would have noticed.
+ *
+ * Total for the same reason as the preflight read above: absence and `false` already mean the same
+ * thing here, so a nullish payload must read as "not connected" rather than throw.
+ */
+export const taskLinearStatusSchema = z
+  .looseObject({
+    connected: salvagedOptional('connected', z.boolean())
+  })
+  .catch({ connected: undefined })
+
+/**
+ * The three writes whose reply body no call site reads.
+ *
+ * `ui.set` is interpreted for its verdict alone and the value discarded
+ * (use-mobile-tasks-client-settings-actions.tsx:201, setup-hook-trust.ts:49-51); `settings.update` is
+ * fire-and-forget at five sites and never interpreted; `linear.selectWorkspace` chains the context
+ * reload off the send without interpreting it. Declaring a member on any of them would be a
+ * requirement with no reader, and would make a refusal visible for the first time at a site whose
+ * whole documented behaviour is that it is not.
+ */
+export const taskPreferenceWriteSchema = z.unknown()

ivanvykopal/orca

mobile/src/tasks/task-runtime-reply-schema.test.ts
@@ -1,0 +1,121 @@
+import { describe, expect, it } from 'vitest'
+import {
+  taskLinearStatusSchema,
+  taskPreferenceWriteSchema,
+  taskPreflightSchema,
+  taskRuntimeStatusSchema,
+  taskUiStateSchema
+} from './task-runtime-reply-schema'
+
+// Pins what the hydration reads require and what they deliberately do not.
+
+describe('the runtime status', () => {
+  it('reads the recorded capability lists', () => {
+    for (const capabilities of [['mobile.tasks.v1'], ['files.mutation-ownership.v1']]) {
+      const parsed = taskRuntimeStatusSchema.safeParse({ capabilities })
+      expect(parsed.success && parsed.data).toMatchObject({ capabilities })
+    }
+  })
+
+  it('requires the container main read `.capabilities` off', () => {
+    expect(taskRuntimeStatusSchema.safeParse(null).success).toBe(false)
+    expect(taskRuntimeStatusSchema.safeParse('ok').success).toBe(false)
+    expect(taskRuntimeStatusSchema.safeParse({}).success).toBe(true)
+  })
+
+  it('drops a non-string capability, which `includes` could never have matched', () => {
+    const parsed = taskRuntimeStatusSchema.safeParse({ capabilities: ['push.v1', 7] })
+    expect(parsed.success && parsed.data).toMatchObject({ capabilities: ['push.v1'] })
+  })
+
+  it('leaves worktreeCreateIdempotency untouched in all four states the probe triages', () => {
+    for (const advertised of [undefined, null, 'nonsense', { dedupeTtlMs: 45_000 }]) {
+      const parsed = taskRuntimeStatusSchema.safeParse({ worktreeCreateIdempotency: advertised })
+      expect(parsed.success).toBe(true)
+      expect(parsed.success ? parsed.data.worktreeCreateIdempotency : 'unparsed').toEqual(
+        advertised
+      )
+    }
+  })
+})
+
+describe('the persisted UI state', () => {
+  it('answers the `ui` member, which is what the member reader it replaces did', () => {
+    const parsed = taskUiStateSchema.safeParse({
+      ui: { sortBy: 'name', trustedOrcaHooks: { 'repo-1': { all: { approvedAt: 1 } } } }
+    })
+    expect(parsed.success && parsed.data).toMatchObject({
+      sortBy: 'name',
+      trustedOrcaHooks: { 'repo-1': { all: { approvedAt: 1 } } }
+    })
+  })
+
+  it('reads the recorded empty ui as an empty record, not as absent', () => {
+    const parsed = taskUiStateSchema.safeParse({ ui: {} })
+    expect(parsed.success && parsed.data).toEqual({})
+  })
+
+  it('answers undefined for a payload with no ui, where main read undefined off the object', () => {
+    const parsed = taskUiStateSchema.safeParse({})
+    expect(parsed.success && parsed.data).toBeUndefined()
+  })
+
+  it.each([null, undefined, 'ok', 7, [], true])(
+    'answers undefined for %j, so the other hydration legs survive an unreadable ui reply',
+    (payload) => {
+      const parsed = taskUiStateSchema.safeParse(payload)
+      expect(parsed.success).toBe(true)
+      expect(parsed.success ? parsed.data : 'unparsed').toBeUndefined()
+    }
+  )
+})
+
+describe('the two advisory probes', () => {
+  it('reads the recorded preflight and linear replies', () => {
+    expect(taskPreflightSchema.safeParse({ glab: { installed: false } }).success).toBe(true)
+    expect(taskLinearStatusSchema.safeParse({ connected: false }).success).toBe(true)
+  })
+
+  it('requires nothing, because every consumer compares the leaf to `true`', () => {
+    expect(taskPreflightSchema.safeParse({}).success).toBe(true)
+    expect(taskLinearStatusSchema.safeParse({}).success).toBe(true)
+  })
+
+  it('drops a malformed glab rather than reading it as installed', () => {
+    const parsed = taskPreflightSchema.safeParse({ glab: 'yes' })
+    expect(parsed.success ? parsed.data.glab : 'unparsed').toBeUndefined()
+  })
+
+  it('passes a host member no mobile consumer reads straight through', () => {
+    const parsed = taskPreflightSchema.safeParse({ gh: { installed: true, authenticated: true } })
+    expect(parsed.success && parsed.data).toMatchObject({
+      gh: { installed: true, authenticated: true }
+    })
+  })
+
+  // `success-result-or-skip` accepts an envelope whose `result` is absent or null and then asks
+  // the reader to decode it, so a refusal here throws out of the caller's whole hydration. Main
+  // hydrated on both of these replies; the recorded settings.task-hydration and
+  // settings.workspace-context partitions are what caught the difference.
+  it.each([
+    ['absent', undefined],
+    ['null', null],
+    ['a bare string', 'preflight'],
+    ['an array', []]
+  ])('reads a %s payload as "nothing advertised" rather than refusing it', (_label, reply) => {
+    const preflight = taskPreflightSchema.safeParse(reply)
+    expect(preflight.success).toBe(true)
+    expect(preflight.success && preflight.data.glab?.installed).toBeUndefined()
+    const linear = taskLinearStatusSchema.safeParse(reply)
+    expect(linear.success).toBe(true)
+    expect(linear.success && linear.data.connected).toBeUndefined()
+  })
+})
+
+describe('the three preference writes', () => {
+  it('accept every payload, because no call site reads their body', () => {
+    for (const reply of [null, undefined, 0, 'written', { ok: true }, []]) {
+      expect(taskPreferenceWriteSchema.safeParse(reply).success).toBe(true)
+    }
+  })
+})

mobile/src/tasks/task-runtime-reply-schema.ts
@@ -1,0 +1,120 @@
+import { z } from 'zod'
+import { salvagedOptional, salvagingArray } from '../../../src/shared/zod-salvage'
+
+// What the Tasks screen reads once per host to hydrate, and the preferences it writes back.
+// Checked against src/main/runtime/rpc/methods/status.ts:6-16 (RuntimeStatus, declared in
+// src/shared/runtime-session-contracts.ts:64), client-ui.ts:22-74 (the `{ settings }` / `{ ui }`
+// envelopes), preflight.ts:17 (PreflightStatus) and linear.ts:35-42 (the connection status).
+
+/**
+ * The runtime status, read for a capability list and nothing else.
+ *
+ * `capabilities` is the only member any consumer here reaches for, and every one of them guards it
+ * — `status.capabilities?.includes(…)` in use-mobile-tasks-runtime-hydration.tsx:205 and in
+ * src/shared/file-mutation-ownership.ts:10, `result.capabilities ?? []` in
+ * worktree-create-capability.ts:51. So the requirement is the container: main read `.capabilities`
+ * off whatever the payload was, and a string or a number reply was a property read that answered
+ * `undefined` and silently downgraded the host to "Tasks unsupported".
+ *
+ * Elements are strings and a non-string element drops. `includes` over a mixed array already never
+ * matched a capability id, so the drop changes no verdict; it is what makes the drop visible in
+ * the salvage report instead of invisible in an `includes` that quietly answers false.
+ *
+ * `worktreeCreateIdempotency` is `unknown` on purpose. worktree-create-capability.ts:55-70 does its
+ * own `typeof`/`Array.isArray` triage over it and treats absent, null, non-object and object as
+ * four different answers; a schema that narrowed it would have to pick one of those apart here and
+ * change which branch a host lands in.
+ */
+export const taskRuntimeStatusSchema = z.looseObject({
+  capabilities: salvagedOptional('capabilities', salvagingArray(z.string())),
+  worktreeCreateIdempotency: z.unknown().optional(),
+  hostPlatform: salvagedOptional('hostPlatform', z.string())
+})
+
+/**
+ * Persisted client UI state, answered under a `ui` member.
+ *
+ * The reader yields `ui` itself, which is what the member reader it replaces did, and it is total
+ * like the two probes below it. Main's `rpcPayloadMember` boxed the payload and read `undefined`
+ * off a string, a number or an array, so the Tasks screen hydrated on any of those; a refusal here
+ * would instead throw out of `hydrateTaskState` and leave the whole screen unhydrated (the outer
+ * `.catch` at use-mobile-tasks-runtime-hydration.tsx:349). Main did throw on a null or absent
+ * payload, and that is the one place this reader is deliberately kinder: persisted UI state is
+ * optional at every read (:283 spells `uiState?.trustedOrcaHooks ?? {}`, :284
+ * `uiState?.taskResumeState ?? {}`), so "no ui" hydrates with the defaults rather than failing the
+ * settings, preflight and Linear legs that arrived beside it. The
+ * `matrix-settings.task-hydration-ui.get-1` golden records that move on `result-absent` and
+ * `result-null`.
+ *
+ * Both members are `unknown`, and the call site keeps one narrowing cast over them. They are
+ * opaque forwards: `trustedOrcaHooks` goes straight into state, and `taskResumeState` is the
+ * screen's whole persisted view state, re-read field by field with its own defaults at :286 and
+ * across use-mobile-tasks-client-settings-actions.tsx. Declaring either here would restate a
+ * twelve-member union that nothing in this reader reads.
+ */
+export const taskUiStateSchema = z
+  .looseObject({
+    ui: salvagedOptional(
+      'ui',
+      z.looseObject({
+        taskResumeState: z.unknown().optional(),
+        trustedOrcaHooks: z.unknown().optional()
+      })
+    )
+  })
+  .nullish()
+  .catch(undefined)
+  .transform((reply) => reply?.ui)
+
+/**
+ * The provider preflight, read only for whether `glab` is installed.
+ *
+ * Three consumers and all three guard to the leaf: `preflight?.glab?.installed === true`
+ * (use-mobile-tasks-runtime-hydration.tsx:295, mobile-home-host-requests.ts:95) and the
+ * `readProbeMember` pair in use-new-workspace-runtime-context.ts:92. `git` and `gh` are declared
+ * non-optional by PreflightStatus but no mobile consumer reads them, so they pass through.
+ *
+ * Total on purpose. This read is advertised as advisory, but `success-result-or-skip` accepts an
+ * envelope whose `result` is absent or null and then asks the reader to decode it, and a refusal
+ * there throws out of the whole hydration -- the corpus records main hydrating the Tasks screen on
+ * that reply and this reader leaving it unhydrated. `.catch` restores main's answer exactly,
+ * because every consumer guards to the leaf and reads absence as "not installed".
+ */
+export const taskPreflightSchema = z
+  .looseObject({
+    glab: salvagedOptional(
+      'glab',
+      z.looseObject({ installed: salvagedOptional('installed', z.boolean()) })
+    )
+  })
+  .catch({ glab: undefined })
+
+/**
+ * Whether Linear is connected.
+ *
+ * `connected` is compared to `true` at use-mobile-tasks-runtime-hydration.tsx:293,
+ * mobile-home-host-requests.ts:96 and use-new-workspace-runtime-context.ts:96, so absence and
+ * `false` already mean the same thing and nothing is required. `workspaces` is not declared: the
+ * picker reads it through a different operation on this same method, and listing a member ahead of
+ * a reader is how a schema starts refusing replies no consumer here would have noticed.
+ *
+ * Total for the same reason as the preflight read above: absence and `false` already mean the same
+ * thing here, so a nullish payload must read as "not connected" rather than throw.
+ */
+export const taskLinearStatusSchema = z
+  .looseObject({
+    connected: salvagedOptional('connected', z.boolean())
+  })
+  .catch({ connected: undefined })
+
+/**
+ * The three writes whose reply body no call site reads.
+ *
+ * `ui.set` is interpreted for its verdict alone and the value discarded
+ * (use-mobile-tasks-client-settings-actions.tsx:201, setup-hook-trust.ts:49-51); `settings.update` is
+ * fire-and-forget at five sites and never interpreted; `linear.selectWorkspace` chains the context
+ * reload off the send without interpreting it. Declaring a member on any of them would be a
+ * requirement with no reader, and would make a refusal visible for the first time at a site whose
+ * whole documented behaviour is that it is not.
+ */
+export const taskPreferenceWriteSchema = z.unknown()