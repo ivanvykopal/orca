paidaxingyo666/Manta

fix(agent-launch): resolve a launch scope, not a git worktree record (#21193)
* fix(agent-launch): resolve a launch scope, not a git worktree record

`agent.launch` asked the runtime for a managed worktree record and then read
exactly one field off it, `.id`. That record does not exist for every workspace
a launch can run in, so the request refused launches the method could otherwise
run: the floating workspace resolves to a scope with an id and a path but no
worktree row, and `showManagedTerminalWorkspace` throws `selector_not_found`
rather than hand back the id it had already resolved.

A folder workspace survived that only because the resolver fabricates a worktree
row for it. The scope is the answer that is real for all three kinds, so the
launch asks for that instead. `showManagedTerminalWorkspace` is unchanged -
callers that genuinely need the git record still get it, and still get the
refusal.

With floating now reaching the mode decision, the host must know which kind of
workspace it resolved. The kind is derived from the id it resolved itself,
never accepted from a caller, and the route module's existing `floating`
blocker does the rest: a workspace with nowhere to keep a session runs a
terminal agent.

Behaviour change, deliberate: a floating-workspace `agent.launch` used to fail
with `selector_not_found` and now succeeds as a terminal agent. That is what
lets the floating titlebar agent button move onto the shared launch command
instead of driving tab startup itself.

No wire change: `AgentLaunchTarget` is untouched.

* test(agent-launch): cover floating RPC workspace resolution

Mirror-Of: b66ef2e8a86d817773bec4d78d99b0a450ca30a9

src/main/runtime/rpc/methods/agent-launch-floating-workspace.test.ts
@@ -1,0 +1,79 @@
+import { homedir } from 'node:os'
+import { afterEach, describe, expect, it, vi } from 'vitest'
+import { FLOATING_TERMINAL_WORKTREE_ID } from '../../../../shared/constants'
+import { MantaRuntimeService } from '../../manta-runtime'
+import { AGENT_LAUNCH_METHODS } from './agent-launch'
+import { CAPABLE_CLIENT, methodNamed, STRUCTURED_PREFERENCE } from './agent-launch.test-fixture'
+
+vi.mock('electron', () => ({
+  BrowserWindow: { fromId: vi.fn(() => null) },
+  webContents: { fromId: vi.fn(() => null) },
+  ipcMain: { on: vi.fn(), removeListener: vi.fn() },
+  app: { getPath: vi.fn(() => '/tmp') }
+}))
+
+const launch = methodNamed(AGENT_LAUNCH_METHODS, 'agent.launch')
+const selectors = [FLOATING_TERMINAL_WORKTREE_ID, `id:${FLOATING_TERMINAL_WORKTREE_ID}`]
+
+afterEach(() => vi.restoreAllMocks())
+
+describe('agent.launch with the real floating workspace resolver', () => {
+  it.each(selectors)('resolves %s without a managed worktree record', async (selector) => {
+    const runtime = new MantaRuntimeService()
+
+    await expect(runtime.showManagedTerminalWorkspace(selector)).rejects.toThrow(
+      'selector_not_found'
+    )
+    await expect(runtime.showTerminalWorkspaceLaunchScope(selector)).resolves.toEqual({
+      id: FLOATING_TERMINAL_WORKTREE_ID,
+      path: homedir(),
+      connectionId: null,
+      repo: null,
+      folderWorkspace: null
+    })
+  })
+
+  describe.each([true, false])('structured preference %s', (structuredPreference) => {
+    it.each(selectors)('launches a terminal through %s', async (selector) => {
+      const runtime = new MantaRuntimeService()
+      vi.spyOn(runtime, 'getClientSettings').mockReturnValue(
+        // oxlint-disable-next-line typescript/consistent-type-assertions -- SAFETY: the launch reads only these preferences and optional agentCmdOverrides; no other settings consumer runs because terminal creation is stubbed.
+        {
+          ...STRUCTURED_PREFERENCE,
+          openAgentTabsInChatByDefault: structuredPreference
+        } as ReturnType<MantaRuntimeService['getClientSettings']>
+      )
+      const scope = vi.spyOn(runtime, 'showTerminalWorkspaceLaunchScope')
+      const createSupport = vi.spyOn(runtime, 'getStructuredAgentSessionCreateSupport')
+      const structuredHost = vi.spyOn(runtime, 'ensureStructuredAgentSessionHost')
+      const createTerminal = vi.spyOn(runtime, 'createTerminal').mockResolvedValue({
+        handle: 'term_floating',
+        tabId: 'tab_floating',
+        worktreeId: FLOATING_TERMINAL_WORKTREE_ID,
+        title: 'Claude',
+        surface: 'background'
+      })
+
+      const result = await launch.handler(
+        launch.params.parse({ agent: 'claude', target: { kind: 'existing', worktree: selector } }),
+        { runtime, ...CAPABLE_CLIENT }
+      )
+
+      expect(scope).toHaveBeenCalledExactlyOnceWith(selector)
+      expect(createSupport).not.toHaveBeenCalled()
+      expect(structuredHost).not.toHaveBeenCalled()
+      expect(createTerminal).toHaveBeenCalledExactlyOnceWith(
+        `id:${FLOATING_TERMINAL_WORKTREE_ID}`,
+        { startupAgent: 'claude' }
+      )
+      expect(result).toMatchObject({
+        worktreeId: FLOATING_TERMINAL_WORKTREE_ID,
+        outcome: { kind: 'terminal', handle: 'term_floating' },
+        receipt: {
+          mode: 'terminal',
+          reason: structuredPreference ? 'structured_unsupported_on_host' : 'user_default'
+        }
+      })
+    })
+  })
+})

src/main/runtime/rpc/methods/agent-launch.ts
@@ -59,17 +59,22 @@ export function supportsAgentLaunch(
 /**
  * A client addresses a workspace by selector, but the result's `worktreeId` is an id and every
  * step below the executor re-prefixes it as `id:<worktreeId>`. Resolving here is what keeps a
- * caller's `id:wt-7` from reaching the runtime as `id:id:wt-7`; the terminal-workspace resolver is
- * used rather than the git-worktree one so a folder workspace is addressable too.
+ * caller's `id:wt-7` from reaching the runtime as `id:id:wt-7`.
+ *
+ * The launch *scope* is what is asked for, because the id below is the only thing read off it. The
+ * git-worktree record is the narrower answer — it does not exist for the floating workspace, so
+ * asking for one refused a launch this method can perfectly well run, on a workspace whose id it
+ * had already resolved. A folder workspace survived that only because the resolver fabricates a
+ * worktree row for it; the scope is the answer that is real for all three kinds.
  */
 async function agentLaunchTarget(
   params: AgentLaunchParams,
-  runtime: Pick<MantaRuntimeService, 'showManagedTerminalWorkspace'>
+  runtime: Pick<MantaRuntimeService, 'showTerminalWorkspaceLaunchScope'>
 ): Promise<AgentLaunchTarget> {
   if (params.target.kind === 'create-worktree') {
     return { kind: 'create-worktree', create: { ...params.target.create } }
   }
-  const workspace = await runtime.showManagedTerminalWorkspace(params.target.worktree)
+  const workspace = await runtime.showTerminalWorkspaceLaunchScope(params.target.worktree)
   return { kind: 'existing', worktree: workspace.id }
 }
 

ivanvykopal/orca

src/main/runtime/rpc/methods/agent-launch-floating-workspace.test.ts
@@ -1,0 +1,79 @@
+import { homedir } from 'node:os'
+import { afterEach, describe, expect, it, vi } from 'vitest'
+import { FLOATING_TERMINAL_WORKTREE_ID } from '../../../../shared/constants'
+import { OrcaRuntimeService } from '../../orca-runtime'
+import { AGENT_LAUNCH_METHODS } from './agent-launch'
+import { CAPABLE_CLIENT, methodNamed, STRUCTURED_PREFERENCE } from './agent-launch.test-fixture'
+
+vi.mock('electron', () => ({
+  BrowserWindow: { fromId: vi.fn(() => null) },
+  webContents: { fromId: vi.fn(() => null) },
+  ipcMain: { on: vi.fn(), removeListener: vi.fn() },
+  app: { getPath: vi.fn(() => '/tmp') }
+}))
+
+const launch = methodNamed(AGENT_LAUNCH_METHODS, 'agent.launch')
+const selectors = [FLOATING_TERMINAL_WORKTREE_ID, `id:${FLOATING_TERMINAL_WORKTREE_ID}`]
+
+afterEach(() => vi.restoreAllMocks())
+
+describe('agent.launch with the real floating workspace resolver', () => {
+  it.each(selectors)('resolves %s without a managed worktree record', async (selector) => {
+    const runtime = new OrcaRuntimeService()
+
+    await expect(runtime.showManagedTerminalWorkspace(selector)).rejects.toThrow(
+      'selector_not_found'
+    )
+    await expect(runtime.showTerminalWorkspaceLaunchScope(selector)).resolves.toEqual({
+      id: FLOATING_TERMINAL_WORKTREE_ID,
+      path: homedir(),
+      connectionId: null,
+      repo: null,
+      folderWorkspace: null
+    })
+  })
+
+  describe.each([true, false])('structured preference %s', (structuredPreference) => {
+    it.each(selectors)('launches a terminal through %s', async (selector) => {
+      const runtime = new OrcaRuntimeService()
+      vi.spyOn(runtime, 'getClientSettings').mockReturnValue(
+        // oxlint-disable-next-line typescript/consistent-type-assertions -- SAFETY: the launch reads only these preferences and optional agentCmdOverrides; no other settings consumer runs because terminal creation is stubbed.
+        {
+          ...STRUCTURED_PREFERENCE,
+          openAgentTabsInChatByDefault: structuredPreference
+        } as ReturnType<OrcaRuntimeService['getClientSettings']>
+      )
+      const scope = vi.spyOn(runtime, 'showTerminalWorkspaceLaunchScope')
+      const createSupport = vi.spyOn(runtime, 'getStructuredAgentSessionCreateSupport')
+      const structuredHost = vi.spyOn(runtime, 'ensureStructuredAgentSessionHost')
+      const createTerminal = vi.spyOn(runtime, 'createTerminal').mockResolvedValue({
+        handle: 'term_floating',
+        tabId: 'tab_floating',
+        worktreeId: FLOATING_TERMINAL_WORKTREE_ID,
+        title: 'Claude',
+        surface: 'background'
+      })
+
+      const result = await launch.handler(
+        launch.params.parse({ agent: 'claude', target: { kind: 'existing', worktree: selector } }),
+        { runtime, ...CAPABLE_CLIENT }
+      )
+
+      expect(scope).toHaveBeenCalledExactlyOnceWith(selector)
+      expect(createSupport).not.toHaveBeenCalled()
+      expect(structuredHost).not.toHaveBeenCalled()
+      expect(createTerminal).toHaveBeenCalledExactlyOnceWith(
+        `id:${FLOATING_TERMINAL_WORKTREE_ID}`,
+        { startupAgent: 'claude' }
+      )
+      expect(result).toMatchObject({
+        worktreeId: FLOATING_TERMINAL_WORKTREE_ID,
+        outcome: { kind: 'terminal', handle: 'term_floating' },
+        receipt: {
+          mode: 'terminal',
+          reason: structuredPreference ? 'structured_unsupported_on_host' : 'user_default'
+        }
+      })
+    })
+  })
+})

src/main/runtime/rpc/methods/agent-launch.ts
@@ -59,17 +59,22 @@ export function supportsAgentLaunch(
 /**
  * A client addresses a workspace by selector, but the result's `worktreeId` is an id and every
  * step below the executor re-prefixes it as `id:<worktreeId>`. Resolving here is what keeps a
- * caller's `id:wt-7` from reaching the runtime as `id:id:wt-7`; the terminal-workspace resolver is
- * used rather than the git-worktree one so a folder workspace is addressable too.
+ * caller's `id:wt-7` from reaching the runtime as `id:id:wt-7`.
+ *
+ * The launch *scope* is what is asked for, because the id below is the only thing read off it. The
+ * git-worktree record is the narrower answer — it does not exist for the floating workspace, so
+ * asking for one refused a launch this method can perfectly well run, on a workspace whose id it
+ * had already resolved. A folder workspace survived that only because the resolver fabricates a
+ * worktree row for it; the scope is the answer that is real for all three kinds.
  */
 async function agentLaunchTarget(
   params: AgentLaunchParams,
-  runtime: Pick<OrcaRuntimeService, 'showManagedTerminalWorkspace'>
+  runtime: Pick<OrcaRuntimeService, 'showTerminalWorkspaceLaunchScope'>
 ): Promise<AgentLaunchTarget> {
   if (params.target.kind === 'create-worktree') {
     return { kind: 'create-worktree', create: { ...params.target.create } }
   }
-  const workspace = await runtime.showManagedTerminalWorkspace(params.target.worktree)
+  const workspace = await runtime.showTerminalWorkspaceLaunchScope(params.target.worktree)
   return { kind: 'existing', worktree: workspace.id }
 }
 