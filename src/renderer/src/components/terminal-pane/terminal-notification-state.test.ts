paidaxingyo666/Manta

fix: name the notification project by the worktree's own host (#20958)
* fix: name the notification project by the worktree's own host

STA-4343: a worktree id is `repoId::path` with no host component, so the
local host and an SSH host publish one id for two different workspaces.
The id-keyed worktree map is first-wins and the repo map is last-wins, so
a colliding workspace could be labelled with the other host's project and
branch. Resolve the owning host first and name nothing when hydrated
ownership cannot prove one; an omitted label beats a wrong one.

Only the collision branch changes: a single-row id still takes the same
map lookups it did before.

* fix: name the project by its host when a repo id spans hosts

A repo id is registered per host, so two hosts can hold one id at
different paths. Their worktree ids are then unique, so the single-row
path skipped host resolution and fell back to the id-keyed repo map,
which is last-wins — a local worktree got the ssh project's name.

Gate the repo lookup on the id actually being ambiguous: one owner keeps
the plain map lookup and its cost, and only a spanning id resolves the
owning host, naming nothing when ownership is unprovable.

Mirror-Of: 7c4325457c5220d1b80625427ccaf3cb12fde325

src/renderer/src/components/terminal-pane/terminal-notification-state.test.ts
@@ -3,6 +3,12 @@ import { describe, expect, it } from 'vitest'
 import { FLOATING_TERMINAL_WORKTREE_ID } from '../../../../shared/constants'
 import { useAppStore } from '@/store'
 import { makeFolderWorkspace, makeWorktree } from '@/store/slices/worktrees-slice-test-fixtures'
+import {
+  mergeWorktreesForHost,
+  worktreeHostMatchOptions
+} from '@/store/slices/worktrees/listing/worktree-host-ownership'
+import type { Repo } from '../../../../shared/repo-types'
+import type { Worktree } from '../../../../shared/worktree/types'
 import { getNotificationWorkspaceLabels } from './terminal-notification-state'
 
 function stateWithWorkspace() {
@@ -132,6 +138,75 @@ describe('notification workspace labels', () => {
     }
   )
 
+  // A repo id is registered per host, so two hosts can hold one id at different
+  // paths. Their worktree ids are then unique, so the collision lives entirely in
+  // the repo lookup — the id-keyed repo map is last-wins and names the wrong project.
+  function duplicateRepoIdAcrossHosts(): Repo[] {
+    return [
+      {
+        id: 'dup',
+        displayName: 'Dup Local',
+        path: '/laptop/dup',
+        connectionId: null,
+        badgeColor: 'blue',
+        addedAt: 0
+      },
+      {
+        id: 'dup',
+        displayName: 'Dup Remote',
+        path: '/remote/dup',
+        connectionId: 'ssh-1',
+        badgeColor: 'blue',
+        addedAt: 0
+      }
+    ]
+  }
+
+  it('names the local project for a unique worktree id whose repo id spans hosts', () => {
+    const state = {
+      ...useAppStore.getInitialState(),
+      // The ssh row is last, so a bare-id repo lookup answers "Dup Remote" here.
+      repos: duplicateRepoIdAcrossHosts(),
+      worktreesByRepo: {
+        dup: [
+          makeWorktree({
+            id: 'dup::/laptop/dup',
+            repoId: 'dup',
+            hostId: 'local',
+            path: '/laptop/dup',
+            displayName: 'Laptop main'
+          })
+        ]
+      }
+    }
+    expect(getNotificationWorkspaceLabels(state, 'dup::/laptop/dup', 'Terminal')).toEqual({
+      repoLabel: 'Dup Local',
+      worktreeLabel: 'Laptop main'
+    })
+  })
+
+  it('omits the project rather than guessing when a spanning repo id has no provable host', () => {
+    const state = {
+      ...useAppStore.getInitialState(),
+      repos: duplicateRepoIdAcrossHosts(),
+      worktreesByRepo: {
+        dup: [
+          makeWorktree({
+            id: 'dup::/laptop/dup',
+            repoId: 'dup',
+            path: '/laptop/dup',
+            displayName: 'Laptop main'
+          })
+        ]
+      }
+    }
+    // Last-wins on the bare id would answer "Dup Remote" for this local row.
+    expect(getNotificationWorkspaceLabels(state, 'dup::/laptop/dup', 'Terminal')).toEqual({
+      repoLabel: undefined,
+      worktreeLabel: 'Laptop main'
+    })
+  })
+
   it('does not pick an arbitrary folder when hosts have conflicting records', () => {
     const state = stateWithWorkspace()
     state.folderWorkspaces = (['ssh:a', 'ssh:b'] as const).map((executionHostId) =>
@@ -141,6 +216,86 @@ describe('notification workspace labels', () => {
       repoLabel: undefined,
       worktreeLabel: 'Terminal'
     })
+  })
+
+  describe('STA-4343 two-host worktree id collision', () => {
+    const COLLIDING_ID = 'repo1::/work/manta'
+    const HOSTS = {
+      // The laptop row is unqualified, which is how a local worktree listing publishes it.
+      local: {
+        row: { displayName: 'Laptop feature' },
+        repo: { displayName: 'Manta on laptop', connectionId: null }
+      },
+      'ssh:build-box': {
+        row: { hostId: 'ssh:build-box' as const, displayName: 'Build box feature' },
+        repo: { displayName: 'Manta on build box', connectionId: 'build-box' }
+      }
+    } as const
+    type CollidingHostId = keyof typeof HOSTS
+
+    /**
+     * Reachability: both rows land through the production per-host merge, which
+     * replaces only the refreshing host's rows and keeps every other host's.
+     *
+     * `naiveWinner` refreshes first and is listed last in `repos`, so it is the row
+     * a first-wins worktree map AND a last-wins repo map both answer with. Naming
+     * the other host therefore cannot pass by accident.
+     */
+    function stateWithCollidingHosts(naiveWinner: CollidingHostId) {
+      const rival: CollidingHostId = naiveWinner === 'local' ? 'ssh:build-box' : 'local'
+      const repos: Repo[] = [rival, naiveWinner].map((hostId) => ({
+        id: 'repo1',
+        path: '/work/manta',
+        badgeColor: 'blue',
+        addedAt: 0,
+        ...HOSTS[hostId].repo
+      }))
+      const merged = [naiveWinner, rival].reduce<Worktree[] | undefined>(
+        (current, hostId) =>
+          mergeWorktreesForHost(
+            current,
+            [
+              makeWorktree({
+                id: COLLIDING_ID,
+                repoId: 'repo1',
+                path: '/work/manta',
+                ...HOSTS[hostId].row
+              })
+            ],
+            hostId,
+            worktreeHostMatchOptions({ repos }, 'repo1', hostId)
+          ),
+        undefined
+      )
+      expect(merged).toHaveLength(2)
+      return { ...stateWithWorkspace(), repos, worktreesByRepo: { repo1: merged ?? [] } }
+    }
+
+    it.each(['local', 'ssh:build-box'] as const)(
+      'names the %s row, never the other host',
+      (hostId) => {
+        const state = stateWithCollidingHosts(hostId === 'local' ? 'ssh:build-box' : 'local')
+        state.activeWorktreeId = COLLIDING_ID
+        state.activeWorkspaceExecutionHostId = hostId
+        expect(getNotificationWorkspaceLabels(state, COLLIDING_ID, 'Terminal')).toEqual({
+          repoLabel: HOSTS[hostId].repo.displayName,
+          worktreeLabel: HOSTS[hostId].row.displayName
+        })
+      }
+    )
+
+    it.each(['local', 'ssh:build-box'] as const)(
+      'omits both names for a background workspace whose %s row would win a bare-id lookup',
+      (naiveWinner) => {
+        const state = stateWithCollidingHosts(naiveWinner)
+        state.activeWorktreeId = 'repo1::/work/other'
+        state.activeWorkspaceExecutionHostId = 'local'
+        expect(getNotificationWorkspaceLabels(state, COLLIDING_ID, 'Terminal')).toEqual({
+          repoLabel: undefined,
+          worktreeLabel: 'Terminal'
+        })
+      }
+    )
   })
 
   it.each(['folder:missing', 'missing-worktree', FLOATING_TERMINAL_WORKTREE_ID])(

ivanvykopal/orca

src/renderer/src/components/terminal-pane/terminal-notification-state.test.ts
@@ -3,6 +3,12 @@ import { describe, expect, it } from 'vitest'
 import { FLOATING_TERMINAL_WORKTREE_ID } from '../../../../shared/constants'
 import { useAppStore } from '@/store'
 import { makeFolderWorkspace, makeWorktree } from '@/store/slices/worktrees-slice-test-fixtures'
+import {
+  mergeWorktreesForHost,
+  worktreeHostMatchOptions
+} from '@/store/slices/worktrees/listing/worktree-host-ownership'
+import type { Repo } from '../../../../shared/repo-types'
+import type { Worktree } from '../../../../shared/worktree/types'
 import { getNotificationWorkspaceLabels } from './terminal-notification-state'
 
 function stateWithWorkspace() {
@@ -132,6 +138,75 @@ describe('notification workspace labels', () => {
     }
   )
 
+  // A repo id is registered per host, so two hosts can hold one id at different
+  // paths. Their worktree ids are then unique, so the collision lives entirely in
+  // the repo lookup — the id-keyed repo map is last-wins and names the wrong project.
+  function duplicateRepoIdAcrossHosts(): Repo[] {
+    return [
+      {
+        id: 'dup',
+        displayName: 'Dup Local',
+        path: '/laptop/dup',
+        connectionId: null,
+        badgeColor: 'blue',
+        addedAt: 0
+      },
+      {
+        id: 'dup',
+        displayName: 'Dup Remote',
+        path: '/remote/dup',
+        connectionId: 'ssh-1',
+        badgeColor: 'blue',
+        addedAt: 0
+      }
+    ]
+  }
+
+  it('names the local project for a unique worktree id whose repo id spans hosts', () => {
+    const state = {
+      ...useAppStore.getInitialState(),
+      // The ssh row is last, so a bare-id repo lookup answers "Dup Remote" here.
+      repos: duplicateRepoIdAcrossHosts(),
+      worktreesByRepo: {
+        dup: [
+          makeWorktree({
+            id: 'dup::/laptop/dup',
+            repoId: 'dup',
+            hostId: 'local',
+            path: '/laptop/dup',
+            displayName: 'Laptop main'
+          })
+        ]
+      }
+    }
+    expect(getNotificationWorkspaceLabels(state, 'dup::/laptop/dup', 'Terminal')).toEqual({
+      repoLabel: 'Dup Local',
+      worktreeLabel: 'Laptop main'
+    })
+  })
+
+  it('omits the project rather than guessing when a spanning repo id has no provable host', () => {
+    const state = {
+      ...useAppStore.getInitialState(),
+      repos: duplicateRepoIdAcrossHosts(),
+      worktreesByRepo: {
+        dup: [
+          makeWorktree({
+            id: 'dup::/laptop/dup',
+            repoId: 'dup',
+            path: '/laptop/dup',
+            displayName: 'Laptop main'
+          })
+        ]
+      }
+    }
+    // Last-wins on the bare id would answer "Dup Remote" for this local row.
+    expect(getNotificationWorkspaceLabels(state, 'dup::/laptop/dup', 'Terminal')).toEqual({
+      repoLabel: undefined,
+      worktreeLabel: 'Laptop main'
+    })
+  })
+
   it('does not pick an arbitrary folder when hosts have conflicting records', () => {
     const state = stateWithWorkspace()
     state.folderWorkspaces = (['ssh:a', 'ssh:b'] as const).map((executionHostId) =>
@@ -141,6 +216,86 @@ describe('notification workspace labels', () => {
       repoLabel: undefined,
       worktreeLabel: 'Terminal'
     })
+  })
+
+  describe('STA-4343 two-host worktree id collision', () => {
+    const COLLIDING_ID = 'repo1::/work/orca'
+    const HOSTS = {
+      // The laptop row is unqualified, which is how a local worktree listing publishes it.
+      local: {
+        row: { displayName: 'Laptop feature' },
+        repo: { displayName: 'Orca on laptop', connectionId: null }
+      },
+      'ssh:build-box': {
+        row: { hostId: 'ssh:build-box' as const, displayName: 'Build box feature' },
+        repo: { displayName: 'Orca on build box', connectionId: 'build-box' }
+      }
+    } as const
+    type CollidingHostId = keyof typeof HOSTS
+
+    /**
+     * Reachability: both rows land through the production per-host merge, which
+     * replaces only the refreshing host's rows and keeps every other host's.
+     *
+     * `naiveWinner` refreshes first and is listed last in `repos`, so it is the row
+     * a first-wins worktree map AND a last-wins repo map both answer with. Naming
+     * the other host therefore cannot pass by accident.
+     */
+    function stateWithCollidingHosts(naiveWinner: CollidingHostId) {
+      const rival: CollidingHostId = naiveWinner === 'local' ? 'ssh:build-box' : 'local'
+      const repos: Repo[] = [rival, naiveWinner].map((hostId) => ({
+        id: 'repo1',
+        path: '/work/orca',
+        badgeColor: 'blue',
+        addedAt: 0,
+        ...HOSTS[hostId].repo
+      }))
+      const merged = [naiveWinner, rival].reduce<Worktree[] | undefined>(
+        (current, hostId) =>
+          mergeWorktreesForHost(
+            current,
+            [
+              makeWorktree({
+                id: COLLIDING_ID,
+                repoId: 'repo1',
+                path: '/work/orca',
+                ...HOSTS[hostId].row
+              })
+            ],
+            hostId,
+            worktreeHostMatchOptions({ repos }, 'repo1', hostId)
+          ),
+        undefined
+      )
+      expect(merged).toHaveLength(2)
+      return { ...stateWithWorkspace(), repos, worktreesByRepo: { repo1: merged ?? [] } }
+    }
+
+    it.each(['local', 'ssh:build-box'] as const)(
+      'names the %s row, never the other host',
+      (hostId) => {
+        const state = stateWithCollidingHosts(hostId === 'local' ? 'ssh:build-box' : 'local')
+        state.activeWorktreeId = COLLIDING_ID
+        state.activeWorkspaceExecutionHostId = hostId
+        expect(getNotificationWorkspaceLabels(state, COLLIDING_ID, 'Terminal')).toEqual({
+          repoLabel: HOSTS[hostId].repo.displayName,
+          worktreeLabel: HOSTS[hostId].row.displayName
+        })
+      }
+    )
+
+    it.each(['local', 'ssh:build-box'] as const)(
+      'omits both names for a background workspace whose %s row would win a bare-id lookup',
+      (naiveWinner) => {
+        const state = stateWithCollidingHosts(naiveWinner)
+        state.activeWorktreeId = 'repo1::/work/other'
+        state.activeWorkspaceExecutionHostId = 'local'
+        expect(getNotificationWorkspaceLabels(state, COLLIDING_ID, 'Terminal')).toEqual({
+          repoLabel: undefined,
+          worktreeLabel: 'Terminal'
+        })
+      }
+    )
   })
 
   it.each(['folder:missing', 'missing-worktree', FLOATING_TERMINAL_WORKTREE_ID])(