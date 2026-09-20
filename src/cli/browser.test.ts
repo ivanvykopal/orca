paidaxingyo666/Manta

fix(browser): make the browser identity one process-wide choice (#13822) (#20767)
* feat(browser): process-wide browser identity, chosen before ready

Electron resolves worker identity from a single process-global default, so two
coherent identities cannot coexist in one process. This makes clean/native one
app-wide decision read before `ready`, instead of a per-profile one that leaves
documents on one identity and every worker request on the other.

Both identities are load-bearing, measured across four origins at five reps:
the cleaned identity clears an embedded Turnstile widget and WhatsApp's browser
check where native is refused; native clears a full-page Cloudflare interstitial
that the cleaned identity never clears.

Base commit only: removing the per-profile field, its settings surface, and the
migration notice follow.

* test(browser): cover cross-context UA wire identity

* refactor(browser): make user agent identity app-wide

* test(browser): repair process identity wire fixture

* Fix browser identity startup migration failures

* WIP: rescue in-flight reduced-design work from a dead worker

Worker ctx_cb5b1262d7fe stopped ~2h ago mid-implementation (last heartbeat
2026-09-14T22:48:06Z) leaving this uncommitted. Committed unverified to make it
recoverable; not reviewed, not necessarily green.

* fix(browser): repair the rescued identity work so it typechecks

Finishes the interrupted edits in 7db9c54b54:

- browser-user-agent-migration-notice.ts was truncated mid-write; close the
  then() callback so the file parses.
- Register browser.identity.get/set in the generated RPC params catalog so the
  params type-parity gate is satisfied.
- Retire the persistence assertions for the superseded design: a
  migratedNativeProfileIds event map, a notice-acknowledgement clear, and a
  global persistence-failure accessor. Legacy userAgentMode bytes are retained
  now, so these assert retention plus a failed notice write still hydrating.
- The in-memory fs fixture th

src/cli/browser.test.ts
@@ -314,6 +314,76 @@ describe('manta cli browser page targeting', () => {
   })
 })
 
+describe('manta cli browser identity', () => {
+  beforeEach(() => {
+    callMock.mockReset()
+  })
+
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
+
+  it('gets the host identity only after capability negotiation', async () => {
+    queueFixtures(
+      callMock,
+      okFixture('status', { capabilities: ['browser.identity.v1'] }),
+      okFixture('identity', {
+        identity: {
+          state: 'valid',
+          appliedMode: 'clean',
+          configuredMode: 'native',
+          explicitSelection: true,
+          migrationNoticePending: false,
+          restartRequired: true
+        },
+        migrationNotice: null
+      })
+    )
+    vi.spyOn(console, 'log').mockImplementation(() => {})
+
+    await main(['browser', 'identity', 'get', '--json'], '/tmp/not-an-manta-worktree')
+
+    expect(callMock).toHaveBeenNthCalledWith(1, 'status.get')
+    expect(callMock).toHaveBeenNthCalledWith(2, 'browser.identity.get')
+  })
+
+  it('sets the host identity through the runtime writer', async () => {
+    queueFixtures(
+      callMock,
+      okFixture('status', { capabilities: ['browser.identity.v1'] }),
+      okFixture('identity', {
+        ok: true,
+        identity: {
+          state: 'valid',
+          appliedMode: 'clean',
+          configuredMode: 'native',
+          explicitSelection: true,
+          migrationNoticePending: false,
+          restartRequired: true
+        }
+      })
+    )
+    vi.spyOn(console, 'log').mockImplementation(() => {})
+
+    await main(
+      ['browser', 'identity', 'set', '--mode', 'native', '--json'],
+      '/tmp/not-an-manta-worktree'
+    )
+
+    expect(callMock).toHaveBeenNthCalledWith(2, 'browser.identity.set', { mode: 'native' })
+  })
+
+  it('refuses an older runtime instead of guessing at identity support', async () => {
+    queueFixtures(callMock, okFixture('status', { capabilities: [] }))
+    const error = vi.spyOn(console, 'error').mockImplementation(() => {})
+
+    await main(['browser', 'identity', 'get'], '/tmp/not-an-manta-worktree')
+
+    expect(callMock).toHaveBeenCalledTimes(1)
+    expect(error).toHaveBeenCalledWith(expect.stringContaining('Update or restart Manta'))
+  })
+})
+
 describe('manta cli browser profile management', () => {
   beforeEach(() => {
     callMock.mockReset()
@@ -435,29 +505,6 @@ describe('manta cli browser tab profiles', () => {
     expect(logSpy).toHaveBeenCalledWith('No browser profiles found.')
   })
 
-  it('marks native-UA profiles in text output', async () => {
-    queueFixtures(
-      callMock,
-      okFixture('req_profiles_native_ua', {
-        profiles: [
-          {
-            id: 'google',
-            scope: 'isolated',
-            label: 'Google',
-            partition: 'persist:manta-browser-session-google',
-            source: null,
-            userAgentMode: 'native'
-          }
-        ]
-      })
-    )
-    const logSpy = vi.spyOn(console, 'log').mockImplementation(() => {})
-
-    await main(['tab', 'profile', 'list'], '/tmp/not-an-manta-worktree')
-
-    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining('ua:native'))
-  })
-
   it('creates isolated browser tab profiles by default', async () => {
     queueFixtures(
       callMock,
@@ -506,33 +553,6 @@ describe('manta cli browser tab profiles', () => {
     expect(callMock).toHaveBeenCalledWith('browser.profileCreate', {
       label: 'From Chrome',
       scope: 'imported'
-    })
-  })
-
-  it('creates a profile with the native user agent when requested', async () => {
-    queueFixtures(
-      callMock,
-      okFixture('req_profile_native_ua', {
-        profile: {
-          id: 'google',
-          scope: 'isolated',
-          label: 'Google',
-          partition: 'persist:manta-browser-session-google',
-          userAgentMode: 'native'
-        }
-      })
-    )
-    vi.spyOn(console, 'log').mockImplementation(() => {})
-
-    await main(
-      ['tab', 'profile', 'create', '--label', 'Google', '--no-ua-spoof', '--json'],
-      '/tmp/not-an-manta-worktree'
-    )
-
-    expect(callMock).toHaveBeenCalledWith('browser.profileCreate', {
-      label: 'Google',
-      scope: 'isolated',
-      userAgentMode: 'native'
     })
   })
 

src/cli/index.test.ts
@@ -381,6 +381,38 @@ describe('unknown help command surfaces a suggestion', () => {
   })
 })
 
+describe('nested command group help', () => {
+  it.each([
+    ['browser', ['browser'], ['identity get', 'identity set']],
+    ['browser identity', ['browser', 'identity'], ['get', 'set']]
+  ])(
+    'prints successful help for %s without constructing a runtime client',
+    async (_, path, commands) => {
+      const previousExitCode = process.exitCode
+      const logSpy = vi.spyOn(console, 'log').mockImplementation(() => {})
+      runtimeClientConstructorMock.mockClear()
+      process.exitCode = 0
+
+      try {
+        await main([...path, '--help'], '/tmp/repo')
+
+        expect(process.exitCode).toBe(0)
+        const output = logSpy.mock.calls.flat().join('\n')
+        expect(output).toContain(`manta ${path.join(' ')}`)
+        for (const command of commands) {
+          expect(output).toContain(command)
+        }
+        expect(output).not.toContain('Unknown command')
+        expect(runtimeClientConstructorMock).not.toHaveBeenCalled()
+        expect(callMock).not.toHaveBeenCalled()
+      } finally {
+        process.exitCode = previousExitCode
+        logSpy.mockRestore()
+      }
+    }
+  )
+})
+
 describe('manta root help', () => {
   it('advertises machine-readable agent discovery', async () => {
     const logSpy = vi.spyOn(console, 'log').mockImplementation(() => {})

src/main/server/serve-stdout-boundary.test.ts
@@ -1,5 +1,8 @@
 import { describe, expect, it, vi } from 'vitest'
-import { reserveServeStdoutForReadiness } from './serve-stdout-boundary'
+import {
+  emitServeBrowserIdentityActionLine,
+  reserveServeStdoutForReadiness
+} from './serve-stdout-boundary'
 
 describe('reserveServeStdoutForReadiness', () => {
   it('routes console diagnostics to stderr', () => {
@@ -16,5 +19,47 @@ describe('reserveServeStdoutForReadiness', () => {
     target.log('log')
 
     expect(target.error.mock.calls).toEqual([['debug'], ['info'], ['log']])
+  })
+})
+
+describe('emitServeBrowserIdentityActionLine', () => {
+  it.each([
+    {
+      state: 'valid' as const,
+      migrationNotice: { degraded: false },
+      expected: 'choose Cleaned or Native'
+    },
+    {
+      state: 'valid' as const,
+      migrationNotice: { degraded: true },
+      expected: 'old choice could not be inspected'
+    },
+    { state: 'corrupt' as const, migrationNotice: null, expected: 'reset it explicitly' },
+    { state: 'future' as const, migrationNotice: null, expected: 'update Manta' }
+  ])('writes one stderr action for $state', ({ state, migrationNotice, expected }) => {
+    const write = vi.fn()
+    const identity =
+      state === 'valid'
+        ? {
+            state,
+            appliedMode: 'clean' as const,
+            configuredMode: 'clean' as const,
+            explicitSelection: false,
+            migrationNoticePending: true,
+            restartRequired: false
+          }
+        : {
+            state,
+            appliedMode: 'clean' as const,
+            configuredMode: null,
+            explicitSelection: null,
+            migrationNoticePending: null,
+            restartRequired: false as const
+          }
+
+    emitServeBrowserIdentityActionLine({ identity, migrationNotice }, { write })
+
+    expect(write).toHaveBeenCalledTimes(1)
+    expect(write).toHaveBeenCalledWith(expect.stringContaining(expected))
   })
 })

src/main/server/serve-stdout-boundary.ts
@@ -1,4 +1,7 @@
+import type { BrowserIdentityModeStatus } from '../../shared/browser-user-agent-mode'
+
 type DiagnosticConsole = Pick<Console, 'debug' | 'error' | 'info' | 'log'>
+type StderrTarget = Pick<NodeJS.WriteStream, 'write'>
 
 export function reserveServeStdoutForReadiness(target: DiagnosticConsole = console): void {
   // Why: stdout is the serve readiness API; route incidental diagnostics to stderr so JSON stays parseable.
@@ -6,4 +9,26 @@ export function reserveServeStdoutForReadiness(target: DiagnosticConsole = conso
   target.debug = writeDiagnostic
   target.info = writeDiagnostic
   target.log = writeDiagnostic
+}
+
+export function emitServeBrowserIdentityActionLine(
+  status: BrowserIdentityModeStatus,
+  target: StderrTarget = process.stderr
+): void {
+  let action: string | null = null
+  if (status.identity.state === 'future') {
+    action = 'browser identity data is from a newer version; update Manta'
+  } else if (
+    status.identity.state === 'corrupt' ||
+    status.identity.state === 'unreadable'
+  ) {
+    action = `browser identity data is ${status.identity.state}; reset it explicitly`
+  } else if (status.migrationNotice?.degraded) {
+    action = 'an old choice could not be inspected; choose Cleaned or Native'
+  } else if (status.migrationNotice) {
+    action = 'browser identity changed to app-wide; choose Cleaned or Native'
+  }
+  if (action) {
+    target.write(`[browser-identity] action required: ${action}\n`)
+  }
 }

ivanvykopal/orca

src/cli/browser.test.ts
@@ -314,6 +314,76 @@ describe('orca cli browser page targeting', () => {
   })
 })
 
+describe('orca cli browser identity', () => {
+  beforeEach(() => {
+    callMock.mockReset()
+  })
+
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
+
+  it('gets the host identity only after capability negotiation', async () => {
+    queueFixtures(
+      callMock,
+      okFixture('status', { capabilities: ['browser.identity.v1'] }),
+      okFixture('identity', {
+        identity: {
+          state: 'valid',
+          appliedMode: 'clean',
+          configuredMode: 'native',
+          explicitSelection: true,
+          migrationNoticePending: false,
+          restartRequired: true
+        },
+        migrationNotice: null
+      })
+    )
+    vi.spyOn(console, 'log').mockImplementation(() => {})
+
+    await main(['browser', 'identity', 'get', '--json'], '/tmp/not-an-orca-worktree')
+
+    expect(callMock).toHaveBeenNthCalledWith(1, 'status.get')
+    expect(callMock).toHaveBeenNthCalledWith(2, 'browser.identity.get')
+  })
+
+  it('sets the host identity through the runtime writer', async () => {
+    queueFixtures(
+      callMock,
+      okFixture('status', { capabilities: ['browser.identity.v1'] }),
+      okFixture('identity', {
+        ok: true,
+        identity: {
+          state: 'valid',
+          appliedMode: 'clean',
+          configuredMode: 'native',
+          explicitSelection: true,
+          migrationNoticePending: false,
+          restartRequired: true
+        }
+      })
+    )
+    vi.spyOn(console, 'log').mockImplementation(() => {})
+
+    await main(
+      ['browser', 'identity', 'set', '--mode', 'native', '--json'],
+      '/tmp/not-an-orca-worktree'
+    )
+
+    expect(callMock).toHaveBeenNthCalledWith(2, 'browser.identity.set', { mode: 'native' })
+  })
+
+  it('refuses an older runtime instead of guessing at identity support', async () => {
+    queueFixtures(callMock, okFixture('status', { capabilities: [] }))
+    const error = vi.spyOn(console, 'error').mockImplementation(() => {})
+
+    await main(['browser', 'identity', 'get'], '/tmp/not-an-orca-worktree')
+
+    expect(callMock).toHaveBeenCalledTimes(1)
+    expect(error).toHaveBeenCalledWith(expect.stringContaining('Update or restart Orca'))
+  })
+})
+
 describe('orca cli browser profile management', () => {
   beforeEach(() => {
     callMock.mockReset()
@@ -435,29 +505,6 @@ describe('orca cli browser tab profiles', () => {
     expect(logSpy).toHaveBeenCalledWith('No browser profiles found.')
   })
 
-  it('marks native-UA profiles in text output', async () => {
-    queueFixtures(
-      callMock,
-      okFixture('req_profiles_native_ua', {
-        profiles: [
-          {
-            id: 'google',
-            scope: 'isolated',
-            label: 'Google',
-            partition: 'persist:orca-browser-session-google',
-            source: null,
-            userAgentMode: 'native'
-          }
-        ]
-      })
-    )
-    const logSpy = vi.spyOn(console, 'log').mockImplementation(() => {})
-
-    await main(['tab', 'profile', 'list'], '/tmp/not-an-orca-worktree')
-
-    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining('ua:native'))
-  })
-
   it('creates isolated browser tab profiles by default', async () => {
     queueFixtures(
       callMock,
@@ -506,33 +553,6 @@ describe('orca cli browser tab profiles', () => {
     expect(callMock).toHaveBeenCalledWith('browser.profileCreate', {
       label: 'From Chrome',
       scope: 'imported'
-    })
-  })
-
-  it('creates a profile with the native user agent when requested', async () => {
-    queueFixtures(
-      callMock,
-      okFixture('req_profile_native_ua', {
-        profile: {
-          id: 'google',
-          scope: 'isolated',
-          label: 'Google',
-          partition: 'persist:orca-browser-session-google',
-          userAgentMode: 'native'
-        }
-      })
-    )
-    vi.spyOn(console, 'log').mockImplementation(() => {})
-
-    await main(
-      ['tab', 'profile', 'create', '--label', 'Google', '--no-ua-spoof', '--json'],
-      '/tmp/not-an-orca-worktree'
-    )
-
-    expect(callMock).toHaveBeenCalledWith('browser.profileCreate', {
-      label: 'Google',
-      scope: 'isolated',
-      userAgentMode: 'native'
     })
   })
 

src/cli/index.test.ts
@@ -381,6 +381,38 @@ describe('unknown help command surfaces a suggestion', () => {
   })
 })
 
+describe('nested command group help', () => {
+  it.each([
+    ['browser', ['browser'], ['identity get', 'identity set']],
+    ['browser identity', ['browser', 'identity'], ['get', 'set']]
+  ])(
+    'prints successful help for %s without constructing a runtime client',
+    async (_, path, commands) => {
+      const previousExitCode = process.exitCode
+      const logSpy = vi.spyOn(console, 'log').mockImplementation(() => {})
+      runtimeClientConstructorMock.mockClear()
+      process.exitCode = 0
+
+      try {
+        await main([...path, '--help'], '/tmp/repo')
+
+        expect(process.exitCode).toBe(0)
+        const output = logSpy.mock.calls.flat().join('\n')
+        expect(output).toContain(`orca ${path.join(' ')}`)
+        for (const command of commands) {
+          expect(output).toContain(command)
+        }
+        expect(output).not.toContain('Unknown command')
+        expect(runtimeClientConstructorMock).not.toHaveBeenCalled()
+        expect(callMock).not.toHaveBeenCalled()
+      } finally {
+        process.exitCode = previousExitCode
+        logSpy.mockRestore()
+      }
+    }
+  )
+})
+
 describe('orca root help', () => {
   it('advertises machine-readable agent discovery', async () => {
     const logSpy = vi.spyOn(console, 'log').mockImplementation(() => {})

src/main/server/serve-stdout-boundary.test.ts
@@ -1,5 +1,8 @@
 import { describe, expect, it, vi } from 'vitest'
-import { reserveServeStdoutForReadiness } from './serve-stdout-boundary'
+import {
+  emitServeBrowserIdentityActionLine,
+  reserveServeStdoutForReadiness
+} from './serve-stdout-boundary'
 
 describe('reserveServeStdoutForReadiness', () => {
   it('routes console diagnostics to stderr', () => {
@@ -16,5 +19,47 @@ describe('reserveServeStdoutForReadiness', () => {
     target.log('log')
 
     expect(target.error.mock.calls).toEqual([['debug'], ['info'], ['log']])
+  })
+})
+
+describe('emitServeBrowserIdentityActionLine', () => {
+  it.each([
+    {
+      state: 'valid' as const,
+      migrationNotice: { degraded: false },
+      expected: 'choose Cleaned or Native'
+    },
+    {
+      state: 'valid' as const,
+      migrationNotice: { degraded: true },
+      expected: 'old choice could not be inspected'
+    },
+    { state: 'corrupt' as const, migrationNotice: null, expected: 'reset it explicitly' },
+    { state: 'future' as const, migrationNotice: null, expected: 'update Orca' }
+  ])('writes one stderr action for $state', ({ state, migrationNotice, expected }) => {
+    const write = vi.fn()
+    const identity =
+      state === 'valid'
+        ? {
+            state,
+            appliedMode: 'clean' as const,
+            configuredMode: 'clean' as const,
+            explicitSelection: false,
+            migrationNoticePending: true,
+            restartRequired: false
+          }
+        : {
+            state,
+            appliedMode: 'clean' as const,
+            configuredMode: null,
+            explicitSelection: null,
+            migrationNoticePending: null,
+            restartRequired: false as const
+          }
+
+    emitServeBrowserIdentityActionLine({ identity, migrationNotice }, { write })
+
+    expect(write).toHaveBeenCalledTimes(1)
+    expect(write).toHaveBeenCalledWith(expect.stringContaining(expected))
   })
 })

src/main/server/serve-stdout-boundary.ts
@@ -1,4 +1,7 @@
+import type { BrowserIdentityModeStatus } from '../../shared/browser-user-agent-mode'
+
 type DiagnosticConsole = Pick<Console, 'debug' | 'error' | 'info' | 'log'>
+type StderrTarget = Pick<NodeJS.WriteStream, 'write'>
 
 export function reserveServeStdoutForReadiness(target: DiagnosticConsole = console): void {
   // Why: stdout is the serve readiness API; route incidental diagnostics to stderr so JSON stays parseable.
@@ -6,4 +9,26 @@ export function reserveServeStdoutForReadiness(target: DiagnosticConsole = conso
   target.debug = writeDiagnostic
   target.info = writeDiagnostic
   target.log = writeDiagnostic
+}
+
+export function emitServeBrowserIdentityActionLine(
+  status: BrowserIdentityModeStatus,
+  target: StderrTarget = process.stderr
+): void {
+  let action: string | null = null
+  if (status.identity.state === 'future') {
+    action = 'browser identity data is from a newer version; update Orca'
+  } else if (
+    status.identity.state === 'corrupt' ||
+    status.identity.state === 'unreadable'
+  ) {
+    action = `browser identity data is ${status.identity.state}; reset it explicitly`
+  } else if (status.migrationNotice?.degraded) {
+    action = 'an old choice could not be inspected; choose Cleaned or Native'
+  } else if (status.migrationNotice) {
+    action = 'browser identity changed to app-wide; choose Cleaned or Native'
+  }
+  if (action) {
+    target.write(`[browser-identity] action required: ${action}\n`)
+  }
 }