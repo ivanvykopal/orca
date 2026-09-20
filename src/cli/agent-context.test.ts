paidaxingyo666/Manta

feat(cli): orca search over the agent session index (#20514)
* feat(cli): orca search over the agent session index

`orca search <query>` calls PR 5's `aiVault.searchSessions` over the CLI's
existing runtime RPC, against the host `--environment` / `--pairing-code`
selects and no other. `orca search --index-status` calls `aiVault.searchStatus`.
It is the proof the contract works with no panel.

Every flag maps onto a contract field and nothing else: `--scope`, `--fresh`,
`--limit`, `--cursor`, repeatable `--agent` and `--path`, `--since`, `--sort`,
`--debug`, `--json`. No fan-out, no merged output, no `--host`.

One command rather than a `search status` subcommand: the query is a bare
positional, so `orca search status` could not be told apart from searching for
the word "status". `--status` is unavailable because `orchestration task-list
--status <state>` already owns the name as a valued flag.

No new runtime capability. PR 5 decided an explicit `method_not_found` refusal
maps to `unavailable/no-service`, so reusing `createSessionSearchClient` gives
an old host a plain "this host runs no session search service" answer at exit 0
instead of a raw JSON-RPC error.

`CommandSpec.repeatableFlags` scopes repeatability per command, because
`--agent` must repeat for search and stay single-valued for `worktree create`.
`help.ts` sat exactly at max-lines, so `skills-command-flag-help.ts` becomes
`command-scoped-flag-help.ts` carrying both tables at the same call-site size.

* refactor(cli): drop the search type assertions main's casting gate now rejects

Main gained a `consistent-type-assertions: never` scan in the changed-code gate
after this branch was cut, and it reported twelve assertions in the new files.

The four in the argument parser were avoidable. `readEnum` now keeps the value
`find` returns, which already carries the narrow type, and the agent filter goes
through an `isAiVaultAgent` predicate over a `Set<string>` instead of widening
the agent tuple.

The test now

src/cli/agent-context.test.ts
@@ -57,6 +57,21 @@ describe('buildAgentContext', () => {
     ])
   })
 
+  it('omits hidden specs so agents do not discover an unadvertised command', () => {
+    const schema = buildAgentContext([
+      ...specs,
+      {
+        path: ['terminal', 'stop'],
+        summary: 'Deprecated',
+        usage: 'manta terminal stop',
+        allowedFlags: [],
+        hidden: true
+      }
+    ])
+    expect(schema.commandCount).toBe(2)
+    expect(schema.commands.map((command) => command.command)).not.toContain('terminal stop')
+  })
+
   it('defaults optional fields to empty arrays', () => {
     const schema = buildAgentContext(specs)
     const agentContext = schema.commands.find((command) => command.command === 'agent-context')

src/cli/specs/search.test.ts
@@ -1,0 +1,128 @@
+import { describe, expect, it } from 'vitest'
+import { SEARCH_COMMAND_SPECS } from './search'
+import { effectiveAllowedFlags, findCommandSpec, GLOBAL_FLAGS } from '../args'
+import { buildAgentContext } from '../agent-context'
+import { suggestCommands } from '../command-suggestion'
+import { HANDLER_COMMAND_KEYS } from '../dispatch'
+import { formatCommandHelp, printHelp } from '../help'
+import { ROOT_HELP_TEXT_PRIMARY } from '../root-help-text-primary'
+import { ROOT_HELP_TEXT_SECONDARY } from '../root-help-text-secondary'
+import { COMMAND_SPECS } from './index'
+import { CLI_COMMAND_NAMES } from '../../main/startup/cli-command-names'
+
+const searchSpec = SEARCH_COMMAND_SPECS[0]!
+const help = formatCommandHelp(searchSpec)
+
+describe('manta search command spec', () => {
+  it('is one command, not a group, because the query is a bare positional', () => {
+    expect(SEARCH_COMMAND_SPECS).toHaveLength(1)
+    expect(searchSpec.path).toEqual(['search'])
+    expect(searchSpec.positionalArgs).toEqual(['query'])
+  })
+
+  it('is registered in the live spec table, the dispatcher and the launch redirect', () => {
+    expect(COMMAND_SPECS).toContain(searchSpec)
+    expect(HANDLER_COMMAND_KEYS.has('search')).toBe(true)
+    expect(CLI_COMMAND_NAMES).toContain('search')
+  })
+
+  it('accepts exactly the flags that map onto the search contract', () => {
+    expect([...searchSpec.allowedFlags].sort()).toEqual([
+      'agent',
+      'cursor',
+      'debug',
+      'environment',
+      'fresh',
+      'help',
+      'index-status',
+      'json',
+      'limit',
+      'pairing-code',
+      'path',
+      'query',
+      'scope',
+      'since',
+      'sort'
+    ])
+  })
+
+  it('declares --agent and --path repeatable for this command only', () => {
+    expect(searchSpec.repeatableFlags).toEqual(['agent', 'path'])
+    for (const spec of COMMAND_SPECS) {
+      if (spec !== searchSpec) {
+        expect(spec.repeatableFlags).toBeUndefined()
+      }
+    }
+  })
+
+  it('does not accept or advertise browser page targeting', () => {
+    expect(effectiveAllowedFlags(searchSpec)).not.toContain('page')
+    expect(help).not.toContain('--page')
+  })
+
+  it('describes every search flag rather than falling back to the bare name', () => {
+    const searchOnly = searchSpec.allowedFlags.filter((flag) => !GLOBAL_FLAGS.includes(flag))
+    expect(searchOnly).toHaveLength(11)
+    for (const flag of searchOnly) {
+      expect(help).toContain(`--${flag}`)
+      expect(help.split('\n')).not.toContain(`  --${flag}`)
+    }
+  })
+
+  it('describes --agent as a search filter, not a terminal agent to launch', () => {
+    expect(help).toContain('Restrict to one agent; repeat for several')
+    expect(help).not.toContain('TUI agent')
+  })
+
+  it('tells the reader to quote a multi-word query', () => {
+    expect(searchSpec.notes?.join('\n')).toContain('Quote a multi-word query')
+  })
+
+  it('states that it searches one host and offers no all-computers search', () => {
+    expect(searchSpec.notes?.join('\n')).toContain('There is no all-computers search.')
+  })
+
+  it('shows both the query and the index report in its usage', () => {
+    expect(searchSpec.usage).toContain('manta search <query>')
+    expect(searchSpec.usage).toContain('manta search --index-status')
+  })
+})
+
+describe('manta search discovery surfaces', () => {
+  it('is hidden until the settings toggle ships', () => {
+    expect(searchSpec.hidden).toBe(true)
+  })
+
+  it('is absent from the root help', () => {
+    expect(ROOT_HELP_TEXT_PRIMARY).not.toContain('Agent Sessions:')
+    expect(ROOT_HELP_TEXT_PRIMARY).not.toContain('  search  ')
+    expect(ROOT_HELP_TEXT_SECONDARY).not.toContain('manta search')
+  })
+
+  it('still prints its own help for `manta search --help`', () => {
+    const lines: string[] = []
+    const restore = console.log
+    console.log = (value: unknown) => void lines.push(String(value))
+    try {
+      printHelp(COMMAND_SPECS, ['search'])
+    } finally {
+      console.log = restore
+    }
+    expect(lines.join('\n')).toContain('Usage: manta search <query>')
+  })
+
+  it('still resolves for dispatch even though it is hidden', () => {
+    expect(findCommandSpec(COMMAND_SPECS, ['search'])).toBe(searchSpec)
+  })
+
+  it('is withheld from agent discovery', () => {
+    const command = buildAgentContext(COMMAND_SPECS).commands.find(
+      (entry) => entry.command === 'search'
+    )
+    expect(command).toBeUndefined()
+  })
+
+  it('is not offered as a suggestion for an unknown command', () => {
+    expect(suggestCommands(COMMAND_SPECS, ['serch'])).not.toContain('search')
+  })
+})

src/cli/specs/search.ts
@@ -1,0 +1,53 @@
+import { GLOBAL_FLAGS, type CommandSpec } from '../args'
+
+// Why one command and not a `search status` subcommand: the query is a bare
+// positional, so `manta search status` would be indistinguishable from searching
+// for the word "status". The index report is a flag on the same command instead.
+export const SEARCH_COMMAND_SPECS: CommandSpec[] = [
+  {
+    path: ['search'],
+    // Why hidden: the command dispatches, but stays off every discovery surface
+    // until the settings PR ships the toggle that enables the feature.
+    hidden: true,
+    summary: 'Search the full text of agent sessions indexed on the selected Manta host',
+    usage:
+      'manta search <query> [--scope conversation|all] [--fresh] [--limit <n>] [--cursor <c>] [--agent <id>] [--path <p>] [--since <iso>] [--sort relevance|newest] [--debug] [--json]\n  manta search --index-status [--json]',
+    allowedFlags: [
+      ...GLOBAL_FLAGS,
+      'query',
+      'scope',
+      'fresh',
+      'limit',
+      'cursor',
+      'agent',
+      'path',
+      'since',
+      'sort',
+      'debug',
+      'index-status'
+    ],
+    repeatableFlags: ['agent', 'path'],
+    positionalArgs: ['query'],
+    notes: [
+      'Searches one host: this machine, or the paired Manta server named by --environment / --pairing-code. There is no all-computers search.',
+      'In a Manta SSH terminal, the forwarded CLI searches the controlling Manta runtime by default. Use --environment / --pairing-code to select a paired server; --path only filters results on the selected runtime.',
+      'Quote a multi-word query, or pass it as --query "<text>"; unquoted words are read as command names.',
+      '--scope conversation searches user and assistant turns only; --scope all (the default) also searches commands and tool output.',
+      '--fresh waits up to five seconds for the host to reconcile its index before searching, then searches anyway.',
+      '--agent and --path may be repeated. --path is a literal execution-host path and is not expanded or resolved against the current directory.',
+      '--since takes an ISO 8601 timestamp with an offset, for example 2026-08-01T00:00:00Z.',
+      '--limit is per page (default 20, maximum 100). Pass the printed cursor back with --cursor to read the next page.',
+      'A cursor belongs to one query on one host. Change the query, the filters, or the host and the cursor stops being valid.',
+      'Resume commands and source paths are printed only for a host on this machine; a paired server withholds them.',
+      '--json prints the runtime response envelope with the search contract answer under `result`.'
+    ],
+    examples: [
+      'manta search "strict mode violation getByRole"',
+      'manta search resolveTerminalPath --agent claude --sort newest',
+      'manta search "kernel panic" --path /Users/me/manta --since 2026-08-01T00:00:00Z --json',
+      'manta search "kernel panic" --limit 50 --cursor eyJ2IjoxfQ',
+      'manta search --index-status',
+      'manta search "flaky test" --environment build-server'
+    ]
+  }
+]

ivanvykopal/orca

src/cli/agent-context.test.ts
@@ -57,6 +57,21 @@ describe('buildAgentContext', () => {
     ])
   })
 
+  it('omits hidden specs so agents do not discover an unadvertised command', () => {
+    const schema = buildAgentContext([
+      ...specs,
+      {
+        path: ['terminal', 'stop'],
+        summary: 'Deprecated',
+        usage: 'orca terminal stop',
+        allowedFlags: [],
+        hidden: true
+      }
+    ])
+    expect(schema.commandCount).toBe(2)
+    expect(schema.commands.map((command) => command.command)).not.toContain('terminal stop')
+  })
+
   it('defaults optional fields to empty arrays', () => {
     const schema = buildAgentContext(specs)
     const agentContext = schema.commands.find((command) => command.command === 'agent-context')

src/cli/specs/search.test.ts
@@ -1,0 +1,128 @@
+import { describe, expect, it } from 'vitest'
+import { SEARCH_COMMAND_SPECS } from './search'
+import { effectiveAllowedFlags, findCommandSpec, GLOBAL_FLAGS } from '../args'
+import { buildAgentContext } from '../agent-context'
+import { suggestCommands } from '../command-suggestion'
+import { HANDLER_COMMAND_KEYS } from '../dispatch'
+import { formatCommandHelp, printHelp } from '../help'
+import { ROOT_HELP_TEXT_PRIMARY } from '../root-help-text-primary'
+import { ROOT_HELP_TEXT_SECONDARY } from '../root-help-text-secondary'
+import { COMMAND_SPECS } from './index'
+import { CLI_COMMAND_NAMES } from '../../main/startup/cli-command-names'
+
+const searchSpec = SEARCH_COMMAND_SPECS[0]!
+const help = formatCommandHelp(searchSpec)
+
+describe('orca search command spec', () => {
+  it('is one command, not a group, because the query is a bare positional', () => {
+    expect(SEARCH_COMMAND_SPECS).toHaveLength(1)
+    expect(searchSpec.path).toEqual(['search'])
+    expect(searchSpec.positionalArgs).toEqual(['query'])
+  })
+
+  it('is registered in the live spec table, the dispatcher and the launch redirect', () => {
+    expect(COMMAND_SPECS).toContain(searchSpec)
+    expect(HANDLER_COMMAND_KEYS.has('search')).toBe(true)
+    expect(CLI_COMMAND_NAMES).toContain('search')
+  })
+
+  it('accepts exactly the flags that map onto the search contract', () => {
+    expect([...searchSpec.allowedFlags].sort()).toEqual([
+      'agent',
+      'cursor',
+      'debug',
+      'environment',
+      'fresh',
+      'help',
+      'index-status',
+      'json',
+      'limit',
+      'pairing-code',
+      'path',
+      'query',
+      'scope',
+      'since',
+      'sort'
+    ])
+  })
+
+  it('declares --agent and --path repeatable for this command only', () => {
+    expect(searchSpec.repeatableFlags).toEqual(['agent', 'path'])
+    for (const spec of COMMAND_SPECS) {
+      if (spec !== searchSpec) {
+        expect(spec.repeatableFlags).toBeUndefined()
+      }
+    }
+  })
+
+  it('does not accept or advertise browser page targeting', () => {
+    expect(effectiveAllowedFlags(searchSpec)).not.toContain('page')
+    expect(help).not.toContain('--page')
+  })
+
+  it('describes every search flag rather than falling back to the bare name', () => {
+    const searchOnly = searchSpec.allowedFlags.filter((flag) => !GLOBAL_FLAGS.includes(flag))
+    expect(searchOnly).toHaveLength(11)
+    for (const flag of searchOnly) {
+      expect(help).toContain(`--${flag}`)
+      expect(help.split('\n')).not.toContain(`  --${flag}`)
+    }
+  })
+
+  it('describes --agent as a search filter, not a terminal agent to launch', () => {
+    expect(help).toContain('Restrict to one agent; repeat for several')
+    expect(help).not.toContain('TUI agent')
+  })
+
+  it('tells the reader to quote a multi-word query', () => {
+    expect(searchSpec.notes?.join('\n')).toContain('Quote a multi-word query')
+  })
+
+  it('states that it searches one host and offers no all-computers search', () => {
+    expect(searchSpec.notes?.join('\n')).toContain('There is no all-computers search.')
+  })
+
+  it('shows both the query and the index report in its usage', () => {
+    expect(searchSpec.usage).toContain('orca search <query>')
+    expect(searchSpec.usage).toContain('orca search --index-status')
+  })
+})
+
+describe('orca search discovery surfaces', () => {
+  it('is hidden until the settings toggle ships', () => {
+    expect(searchSpec.hidden).toBe(true)
+  })
+
+  it('is absent from the root help', () => {
+    expect(ROOT_HELP_TEXT_PRIMARY).not.toContain('Agent Sessions:')
+    expect(ROOT_HELP_TEXT_PRIMARY).not.toContain('  search  ')
+    expect(ROOT_HELP_TEXT_SECONDARY).not.toContain('orca search')
+  })
+
+  it('still prints its own help for `orca search --help`', () => {
+    const lines: string[] = []
+    const restore = console.log
+    console.log = (value: unknown) => void lines.push(String(value))
+    try {
+      printHelp(COMMAND_SPECS, ['search'])
+    } finally {
+      console.log = restore
+    }
+    expect(lines.join('\n')).toContain('Usage: orca search <query>')
+  })
+
+  it('still resolves for dispatch even though it is hidden', () => {
+    expect(findCommandSpec(COMMAND_SPECS, ['search'])).toBe(searchSpec)
+  })
+
+  it('is withheld from agent discovery', () => {
+    const command = buildAgentContext(COMMAND_SPECS).commands.find(
+      (entry) => entry.command === 'search'
+    )
+    expect(command).toBeUndefined()
+  })
+
+  it('is not offered as a suggestion for an unknown command', () => {
+    expect(suggestCommands(COMMAND_SPECS, ['serch'])).not.toContain('search')
+  })
+})

src/cli/specs/search.ts
@@ -1,0 +1,53 @@
+import { GLOBAL_FLAGS, type CommandSpec } from '../args'
+
+// Why one command and not a `search status` subcommand: the query is a bare
+// positional, so `orca search status` would be indistinguishable from searching
+// for the word "status". The index report is a flag on the same command instead.
+export const SEARCH_COMMAND_SPECS: CommandSpec[] = [
+  {
+    path: ['search'],
+    // Why hidden: the command dispatches, but stays off every discovery surface
+    // until the settings PR ships the toggle that enables the feature.
+    hidden: true,
+    summary: 'Search the full text of agent sessions indexed on the selected Orca host',
+    usage:
+      'orca search <query> [--scope conversation|all] [--fresh] [--limit <n>] [--cursor <c>] [--agent <id>] [--path <p>] [--since <iso>] [--sort relevance|newest] [--debug] [--json]\n  orca search --index-status [--json]',
+    allowedFlags: [
+      ...GLOBAL_FLAGS,
+      'query',
+      'scope',
+      'fresh',
+      'limit',
+      'cursor',
+      'agent',
+      'path',
+      'since',
+      'sort',
+      'debug',
+      'index-status'
+    ],
+    repeatableFlags: ['agent', 'path'],
+    positionalArgs: ['query'],
+    notes: [
+      'Searches one host: this machine, or the paired Orca server named by --environment / --pairing-code. There is no all-computers search.',
+      'In an Orca SSH terminal, the forwarded CLI searches the controlling Orca runtime by default. Use --environment / --pairing-code to select a paired server; --path only filters results on the selected runtime.',
+      'Quote a multi-word query, or pass it as --query "<text>"; unquoted words are read as command names.',
+      '--scope conversation searches user and assistant turns only; --scope all (the default) also searches commands and tool output.',
+      '--fresh waits up to five seconds for the host to reconcile its index before searching, then searches anyway.',
+      '--agent and --path may be repeated. --path is a literal execution-host path and is not expanded or resolved against the current directory.',
+      '--since takes an ISO 8601 timestamp with an offset, for example 2026-08-01T00:00:00Z.',
+      '--limit is per page (default 20, maximum 100). Pass the printed cursor back with --cursor to read the next page.',
+      'A cursor belongs to one query on one host. Change the query, the filters, or the host and the cursor stops being valid.',
+      'Resume commands and source paths are printed only for a host on this machine; a paired server withholds them.',
+      '--json prints the runtime response envelope with the search contract answer under `result`.'
+    ],
+    examples: [
+      'orca search "strict mode violation getByRole"',
+      'orca search resolveTerminalPath --agent claude --sort newest',
+      'orca search "kernel panic" --path /Users/me/orca --since 2026-08-01T00:00:00Z --json',
+      'orca search "kernel panic" --limit 50 --cursor eyJ2IjoxfQ',
+      'orca search --index-status',
+      'orca search "flaky test" --environment build-server'
+    ]
+  }
+]