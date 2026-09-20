paidaxingyo666/Manta

refactor(mobile): checked reply readers for files, dictation, host-screen and agent-history (step 7) (#21269)
* test(mobile): record main's file-preview and markdown-disk-fallback replies

Four of this branch's read sites had no malformed-reply coverage, so the reader
change would have had nothing to move at them. `familyGoldens` matrixes only the
first scenario of each family, and `files.preview-load`'s base is the grant-refresh
chain while `session.tab-documents`' is the served markdown tab — which left
`files.read` and `files.readPreview` on the worktree preview path, the artifact
image read, and the markdown tab's on-disk fallback recorded on their success path
only. This commit is the before picture, taken from main's own tree with no product
edit in it.

Three new families, five scenarios, ten goldens:

- `files.preview-worktree-text` / `files.preview-worktree-image` — `files.read` and
  `files.readPreview` as the preview screen asks them for a worktree file.
- `files.preview-artifact-image` — `files.readTerminalArtifactPreview`.
- `session.markdown-disk-fallback` — the `files.read` leg a headless host's
  `renderer_unavailable` sends the markdown tab down. It carries a second scenario
  that serves `markdown.readTab`, because a matrix site needs a fulfilled reply
  recorded somewhere in its own family to replay as the `normal` partition.

No existing scenario moved to a new family and no adapter changed, so every
pre-existing golden keeps its `adapterSha256` and `scenarioSha256`. Recorded in a
detached worktree at the manifest's pin (`4b876758d3`) with this manifest copied in;
the control is that all 748 pre-existing goldens came back byte-identical to
origin/main's, which `git diff c2962a765a -- mobile/rpc-foundation/goldens` confirms
as empty.

Claude-Session: https://claude.ai/code/session_01JNnE9qzUZMMnqpZWCqM3nb

* refactor(mobile): checked reply readers for files, dictation, host-screen and agent-history

Thirty-five unchecked reply readers across seven

mobile/src/dictation/dictation-reply-schema.test.ts
@@ -1,0 +1,103 @@
+import { describe, expect, it } from 'vitest'
+import {
+  dictationSetupSchema,
+  SPEECH_MODEL_PROVIDERS,
+  SPEECH_MODEL_STATUSES
+} from './dictation-reply-schema'
+
+const setup = (overrides: Record<string, unknown> = {}) => ({
+  enabled: true,
+  selectedModelId: 'm1',
+  dictationMode: 'toggle',
+  models: [],
+  ...overrides
+})
+
+describe('dictation setup reply schema', () => {
+  it('requires the model list the sheet maps unguarded', () => {
+    const { models: _models, ...withoutModels } = setup()
+    expect(dictationSetupSchema.safeParse(withoutModels).success).toBe(false)
+    expect(dictationSetupSchema.safeParse(setup({ models: 'none' })).success).toBe(false)
+  })
+
+  it('forwards a dictation mode this build has never heard of instead of substituting one', () => {
+    expect(dictationSetupSchema.parse(setup({ dictationMode: 'push-to-talk' })).dictationMode).toBe(
+      'push-to-talk'
+    )
+    expect(dictationSetupSchema.parse(setup({ dictationMode: 'hold' })).dictationMode).toBe('hold')
+  })
+
+  it('keeps a sheet whose mode is absent or unreadable, which main rendered', () => {
+    const { dictationMode: _mode, ...withoutMode } = setup()
+    expect(dictationSetupSchema.parse(withoutMode).dictationMode).toBeUndefined()
+    expect(dictationSetupSchema.parse(setup({ dictationMode: 3 })).dictationMode).toBeUndefined()
+  })
+
+  it('salvages enabled and selectedModelId onto the off-and-unselected sheet main drew', () => {
+    const parsed = dictationSetupSchema.parse(setup({ enabled: 'yes', selectedModelId: 4 }))
+    expect(parsed.enabled).toBeUndefined()
+    expect(parsed.selectedModelId).toBeUndefined()
+  })
+
+  it('drops a model row the sheet could not address or send back', () => {
+    const parsed = dictationSetupSchema.parse(
+      setup({ models: [{ id: 'a', label: 'A' }, { label: 'B' }, { id: 'c' }] })
+    )
+    expect(parsed.models.map((model) => model.id)).toEqual(['a', 'c'])
+  })
+
+  it('degrades an unknown provider or status arm to absent and keeps the row', () => {
+    const [model] = dictationSetupSchema.parse(
+      setup({ models: [{ id: 'a', provider: 'anthropic', status: 'verifying' }] })
+    ).models
+    expect(model?.id).toBe('a')
+    expect(model?.provider).toBeUndefined()
+    expect(model?.status).toBeUndefined()
+  })
+
+  it('keeps the known provider and status arms', () => {
+    const [model] = dictationSetupSchema.parse(
+      setup({ models: [{ id: 'a', provider: 'openai', status: 'extracting' }] })
+    ).models
+    expect(model?.provider).toBe('openai')
+    expect(model?.status).toBe('extracting')
+  })
+
+  it('salvages a row member behind main own guard and keeps the truthy recommended flag', () => {
+    const [model] = dictationSetupSchema.parse(
+      setup({ models: [{ id: 'a', sizeBytes: 'big', progress: 'half', recommended: 1 }] })
+    ).models
+    expect(model?.sizeBytes).toBeUndefined()
+    expect(model?.progress).toBeUndefined()
+    expect(model?.recommended).toBe(1)
+  })
+
+  it('keeps an explicit null size and progress, which the host sends for an API model', () => {
+    const [model] = dictationSetupSchema.parse(
+      setup({ models: [{ id: 'a', sizeBytes: null, progress: null }] })
+    ).models
+    expect(model?.sizeBytes).toBeNull()
+    expect(model?.progress).toBeNull()
+  })
+
+  it('passes a newer host member through', () => {
+    expect(dictationSetupSchema.parse(setup({ hotword: 'manta' }))).toMatchObject({
+      hotword: 'manta'
+    })
+  })
+})
+
+describe('provider and status are closed over the host union', () => {
+  // The arm lists are pinned to RuntimeSpeechModelSummary in the schema module, where tsc looks;
+  // this loop proves every pinned arm survives the parse, not just the two the tests above pick.
+  it('keeps every arm the host declares, so nothing it sends today degrades', () => {
+    for (const provider of SPEECH_MODEL_PROVIDERS) {
+      for (const status of SPEECH_MODEL_STATUSES) {
+        const [model] = dictationSetupSchema.parse(
+          setup({ models: [{ id: 'a', provider, status }] })
+        ).models
+        expect(model).toMatchObject({ provider, status })
+      }
+    }
+  })
+})

mobile/src/host-screen/host-screen-reply-schema.test.ts
@@ -1,0 +1,175 @@
+import { describe, expect, it } from 'vitest'
+import type { RepoIcon } from '../../../src/shared/repo-icon'
+import { NODE_PLATFORM_NAMES } from '../transport/mobile-runtime-host-platform'
+import {
+  hostPlatformSchema,
+  hostRepoCatalogSchema,
+  hostSshTargetSummariesSchema,
+  hostViewSettingsSchema,
+  WORKSPACE_GROUP_BY_ARMS,
+  WORKSPACE_SORT_BY_ARMS
+} from './host-screen-reply-schema'
+
+describe('host screen reply schemas', () => {
+  it('requires the repo list and drops a row no map could key', () => {
+    expect(hostRepoCatalogSchema.safeParse({}).success).toBe(false)
+    const parsed = hostRepoCatalogSchema.parse({
+      repos: [{ id: 'r1', displayName: 'manta' }, { id: 'r2' }, { displayName: 'ghost' }]
+    })
+    expect(parsed.map((repo) => repo.id)).toEqual(['r1'])
+  })
+
+  it('salvages badgeColor onto the generated swatch main fell back to', () => {
+    expect(
+      hostRepoCatalogSchema.parse({ repos: [{ id: 'r', displayName: 'o', badgeColor: 3 }] })[0]
+        ?.badgeColor
+    ).toBeUndefined()
+  })
+
+  it('keeps a known repo icon arm and degrades an unknown one to absent', () => {
+    const known = hostRepoCatalogSchema.parse({
+      repos: [{ id: 'r', displayName: 'o', repoIcon: { type: 'emoji', emoji: 'x' } }]
+    })
+    expect(known[0]?.repoIcon).toEqual({ type: 'emoji', emoji: 'x' })
+    const unknown = hostRepoCatalogSchema.parse({
+      repos: [{ id: 'r', displayName: 'o', repoIcon: { type: 'svg', markup: '<svg/>' } }]
+    })
+    // The row survives and draws the Folder default MobileRepoIcon already drew for an arm it
+    // could not match.
+    expect(unknown[0]?.id).toBe('r')
+    expect(unknown[0]?.repoIcon).toBeUndefined()
+  })
+
+  it('keeps an image icon whose source this build has never heard of', () => {
+    const [repo] = hostRepoCatalogSchema.parse({
+      repos: [
+        {
+          id: 'r',
+          displayName: 'o',
+          repoIcon: {
+            type: 'image',
+            src: 'https://example.invalid/a.png',
+            source: 'gitlab',
+            label: 'acme/manta'
+          }
+        }
+      ]
+    })
+    // MobileRepoIcon reads src and label and never source, so narrowing source would have drawn a
+    // Folder where main drew the image. The member itself still reaches the row.
+    expect(repo?.repoIcon).toEqual({
+      type: 'image',
+      src: 'https://example.invalid/a.png',
+      source: 'gitlab',
+      label: 'acme/manta'
+    })
+  })
+
+  it('passes a host-id spelling through for getRepoExecutionHostId to judge', () => {
+    const [repo] = hostRepoCatalogSchema.parse({
+      repos: [{ id: 'r', displayName: 'o', executionHostId: 'cloud:zone-a', connectionId: null }]
+    })
+    expect(repo?.executionHostId).toBe('cloud:zone-a')
+    expect(repo?.connectionId).toBeNull()
+  })
+
+  it('degrades an unreadable ssh target list to the empty one that falls back to host ids', () => {
+    expect(hostSshTargetSummariesSchema.parse({})).toEqual([])
+    expect(hostSshTargetSummariesSchema.parse({ targets: 'none' })).toEqual([])
+    expect(
+      hostSshTargetSummariesSchema.parse({
+        targets: [{ id: 't', label: 'T' }, { id: 'u' }, { label: 'V' }]
+      })
+    ).toEqual([{ id: 't', label: 'T' }])
+  })
+
+  it('answers the empty target list for a reply that is no object at all', () => {
+    // Why: the label write runs before the platform write in the same sequence, so a throw here
+    // would take the platform down with it. readSshTargets answered [] for every one of these.
+    for (const payload of [null, undefined, 'nope', 42, []]) {
+      expect(hostSshTargetSummariesSchema.parse(payload)).toEqual([])
+    }
+  })
+
+  it('reads only a platform Node could have reported', () => {
+    expect(hostPlatformSchema.parse({ platform: 'win32' })).toBe('win32')
+    expect(hostPlatformSchema.parse({ platform: 'plan9' })).toBeNull()
+    expect(hostPlatformSchema.parse({ platform: '' })).toBeNull()
+    expect(hostPlatformSchema.parse({})).toBeNull()
+    for (const payload of [null, undefined, 'nope', 42]) {
+      expect(hostPlatformSchema.parse(payload)).toBeNull()
+    }
+  })
+
+  it('requires the ui member main read with a bare property access', () => {
+    expect(hostViewSettingsSchema.safeParse({}).success).toBe(false)
+    expect(hostViewSettingsSchema.safeParse(null).success).toBe(false)
+    expect(hostViewSettingsSchema.parse({ ui: {} })).toEqual({})
+  })
+
+  it('degrades an unknown grouping or sort arm to absent so the local mode is kept', () => {
+    const parsed = hostViewSettingsSchema.parse({
+      ui: { groupBy: 'agent', sortBy: 'stars', hideSleepingWorkspaces: 'yes' }
+    })
+    expect(parsed.groupBy).toBeUndefined()
+    expect(parsed.sortBy).toBeUndefined()
+    expect(parsed.hideSleepingWorkspaces).toBeUndefined()
+  })
+
+  it('keeps the known view arms and the lists the screen adopts', () => {
+    const parsed = hostViewSettingsSchema.parse({
+      ui: {
+        groupBy: 'workspace-status',
+        sortBy: 'recent',
+        filterRepoIds: ['r1'],
+        collapsedGroups: [],
+        workspaceStatuses: [{ id: 'active', label: 'Active' }]
+      }
+    })
+    expect(parsed.groupBy).toBe('workspace-status')
+    expect(parsed.sortBy).toBe('recent')
+    expect(parsed.filterRepoIds).toEqual(['r1'])
+    expect(parsed.workspaceStatuses).toEqual([{ id: 'active', label: 'Active' }])
+  })
+
+  it('salvages a non-array status catalog rather than handing a string to the group lookups', () => {
+    expect(
+      hostViewSettingsSchema.parse({ ui: { workspaceStatuses: 'active' } }).workspaceStatuses
+    ).toBeUndefined()
+  })
+})
+
+describe('the closed arm sets are the desktop unions', () => {
+  // The arm lists are pinned to the desktop unions in the schema modules, where tsc looks; these
+  // loops prove every pinned arm survives the parse, not just the ones the tests above pick.
+  it('keeps every platform Node can report', () => {
+    for (const platform of NODE_PLATFORM_NAMES) {
+      expect(hostPlatformSchema.parse({ platform })).toBe(platform)
+    }
+  })
+
+  it('keeps every grouping and sort arm the desktop persists', () => {
+    for (const groupBy of WORKSPACE_GROUP_BY_ARMS) {
+      for (const sortBy of WORKSPACE_SORT_BY_ARMS) {
+        expect(hostViewSettingsSchema.parse({ ui: { groupBy, sortBy } })).toMatchObject({
+          groupBy,
+          sortBy
+        })
+      }
+    }
+  })
+
+  it('keeps every repo icon arm the desktop draws', () => {
+    const icons: Record<RepoIcon['type'], Record<string, string>> = {
+      lucide: { type: 'lucide', name: 'Folder' },
+      emoji: { type: 'emoji', emoji: '🐳' },
+      image: { type: 'image', src: 'data:,x' }
+    }
+    for (const repoIcon of Object.values(icons)) {
+      const [row] = hostRepoCatalogSchema.parse({
+        repos: [{ id: 'r', displayName: 'r', repoIcon }]
+      })
+      expect(row?.repoIcon).toMatchObject({ type: repoIcon.type })
+    }
+  })
+})

ivanvykopal/orca

mobile/src/dictation/dictation-reply-schema.test.ts
@@ -1,0 +1,103 @@
+import { describe, expect, it } from 'vitest'
+import {
+  dictationSetupSchema,
+  SPEECH_MODEL_PROVIDERS,
+  SPEECH_MODEL_STATUSES
+} from './dictation-reply-schema'
+
+const setup = (overrides: Record<string, unknown> = {}) => ({
+  enabled: true,
+  selectedModelId: 'm1',
+  dictationMode: 'toggle',
+  models: [],
+  ...overrides
+})
+
+describe('dictation setup reply schema', () => {
+  it('requires the model list the sheet maps unguarded', () => {
+    const { models: _models, ...withoutModels } = setup()
+    expect(dictationSetupSchema.safeParse(withoutModels).success).toBe(false)
+    expect(dictationSetupSchema.safeParse(setup({ models: 'none' })).success).toBe(false)
+  })
+
+  it('forwards a dictation mode this build has never heard of instead of substituting one', () => {
+    expect(dictationSetupSchema.parse(setup({ dictationMode: 'push-to-talk' })).dictationMode).toBe(
+      'push-to-talk'
+    )
+    expect(dictationSetupSchema.parse(setup({ dictationMode: 'hold' })).dictationMode).toBe('hold')
+  })
+
+  it('keeps a sheet whose mode is absent or unreadable, which main rendered', () => {
+    const { dictationMode: _mode, ...withoutMode } = setup()
+    expect(dictationSetupSchema.parse(withoutMode).dictationMode).toBeUndefined()
+    expect(dictationSetupSchema.parse(setup({ dictationMode: 3 })).dictationMode).toBeUndefined()
+  })
+
+  it('salvages enabled and selectedModelId onto the off-and-unselected sheet main drew', () => {
+    const parsed = dictationSetupSchema.parse(setup({ enabled: 'yes', selectedModelId: 4 }))
+    expect(parsed.enabled).toBeUndefined()
+    expect(parsed.selectedModelId).toBeUndefined()
+  })
+
+  it('drops a model row the sheet could not address or send back', () => {
+    const parsed = dictationSetupSchema.parse(
+      setup({ models: [{ id: 'a', label: 'A' }, { label: 'B' }, { id: 'c' }] })
+    )
+    expect(parsed.models.map((model) => model.id)).toEqual(['a', 'c'])
+  })
+
+  it('degrades an unknown provider or status arm to absent and keeps the row', () => {
+    const [model] = dictationSetupSchema.parse(
+      setup({ models: [{ id: 'a', provider: 'anthropic', status: 'verifying' }] })
+    ).models
+    expect(model?.id).toBe('a')
+    expect(model?.provider).toBeUndefined()
+    expect(model?.status).toBeUndefined()
+  })
+
+  it('keeps the known provider and status arms', () => {
+    const [model] = dictationSetupSchema.parse(
+      setup({ models: [{ id: 'a', provider: 'openai', status: 'extracting' }] })
+    ).models
+    expect(model?.provider).toBe('openai')
+    expect(model?.status).toBe('extracting')
+  })
+
+  it('salvages a row member behind main own guard and keeps the truthy recommended flag', () => {
+    const [model] = dictationSetupSchema.parse(
+      setup({ models: [{ id: 'a', sizeBytes: 'big', progress: 'half', recommended: 1 }] })
+    ).models
+    expect(model?.sizeBytes).toBeUndefined()
+    expect(model?.progress).toBeUndefined()
+    expect(model?.recommended).toBe(1)
+  })
+
+  it('keeps an explicit null size and progress, which the host sends for an API model', () => {
+    const [model] = dictationSetupSchema.parse(
+      setup({ models: [{ id: 'a', sizeBytes: null, progress: null }] })
+    ).models
+    expect(model?.sizeBytes).toBeNull()
+    expect(model?.progress).toBeNull()
+  })
+
+  it('passes a newer host member through', () => {
+    expect(dictationSetupSchema.parse(setup({ hotword: 'orca' }))).toMatchObject({
+      hotword: 'orca'
+    })
+  })
+})
+
+describe('provider and status are closed over the host union', () => {
+  // The arm lists are pinned to RuntimeSpeechModelSummary in the schema module, where tsc looks;
+  // this loop proves every pinned arm survives the parse, not just the two the tests above pick.
+  it('keeps every arm the host declares, so nothing it sends today degrades', () => {
+    for (const provider of SPEECH_MODEL_PROVIDERS) {
+      for (const status of SPEECH_MODEL_STATUSES) {
+        const [model] = dictationSetupSchema.parse(
+          setup({ models: [{ id: 'a', provider, status }] })
+        ).models
+        expect(model).toMatchObject({ provider, status })
+      }
+    }
+  })
+})

mobile/src/host-screen/host-screen-reply-schema.test.ts
@@ -1,0 +1,175 @@
+import { describe, expect, it } from 'vitest'
+import type { RepoIcon } from '../../../src/shared/repo-icon'
+import { NODE_PLATFORM_NAMES } from '../transport/mobile-runtime-host-platform'
+import {
+  hostPlatformSchema,
+  hostRepoCatalogSchema,
+  hostSshTargetSummariesSchema,
+  hostViewSettingsSchema,
+  WORKSPACE_GROUP_BY_ARMS,
+  WORKSPACE_SORT_BY_ARMS
+} from './host-screen-reply-schema'
+
+describe('host screen reply schemas', () => {
+  it('requires the repo list and drops a row no map could key', () => {
+    expect(hostRepoCatalogSchema.safeParse({}).success).toBe(false)
+    const parsed = hostRepoCatalogSchema.parse({
+      repos: [{ id: 'r1', displayName: 'orca' }, { id: 'r2' }, { displayName: 'ghost' }]
+    })
+    expect(parsed.map((repo) => repo.id)).toEqual(['r1'])
+  })
+
+  it('salvages badgeColor onto the generated swatch main fell back to', () => {
+    expect(
+      hostRepoCatalogSchema.parse({ repos: [{ id: 'r', displayName: 'o', badgeColor: 3 }] })[0]
+        ?.badgeColor
+    ).toBeUndefined()
+  })
+
+  it('keeps a known repo icon arm and degrades an unknown one to absent', () => {
+    const known = hostRepoCatalogSchema.parse({
+      repos: [{ id: 'r', displayName: 'o', repoIcon: { type: 'emoji', emoji: 'x' } }]
+    })
+    expect(known[0]?.repoIcon).toEqual({ type: 'emoji', emoji: 'x' })
+    const unknown = hostRepoCatalogSchema.parse({
+      repos: [{ id: 'r', displayName: 'o', repoIcon: { type: 'svg', markup: '<svg/>' } }]
+    })
+    // The row survives and draws the Folder default MobileRepoIcon already drew for an arm it
+    // could not match.
+    expect(unknown[0]?.id).toBe('r')
+    expect(unknown[0]?.repoIcon).toBeUndefined()
+  })
+
+  it('keeps an image icon whose source this build has never heard of', () => {
+    const [repo] = hostRepoCatalogSchema.parse({
+      repos: [
+        {
+          id: 'r',
+          displayName: 'o',
+          repoIcon: {
+            type: 'image',
+            src: 'https://example.invalid/a.png',
+            source: 'gitlab',
+            label: 'acme/orca'
+          }
+        }
+      ]
+    })
+    // MobileRepoIcon reads src and label and never source, so narrowing source would have drawn a
+    // Folder where main drew the image. The member itself still reaches the row.
+    expect(repo?.repoIcon).toEqual({
+      type: 'image',
+      src: 'https://example.invalid/a.png',
+      source: 'gitlab',
+      label: 'acme/orca'
+    })
+  })
+
+  it('passes a host-id spelling through for getRepoExecutionHostId to judge', () => {
+    const [repo] = hostRepoCatalogSchema.parse({
+      repos: [{ id: 'r', displayName: 'o', executionHostId: 'cloud:zone-a', connectionId: null }]
+    })
+    expect(repo?.executionHostId).toBe('cloud:zone-a')
+    expect(repo?.connectionId).toBeNull()
+  })
+
+  it('degrades an unreadable ssh target list to the empty one that falls back to host ids', () => {
+    expect(hostSshTargetSummariesSchema.parse({})).toEqual([])
+    expect(hostSshTargetSummariesSchema.parse({ targets: 'none' })).toEqual([])
+    expect(
+      hostSshTargetSummariesSchema.parse({
+        targets: [{ id: 't', label: 'T' }, { id: 'u' }, { label: 'V' }]
+      })
+    ).toEqual([{ id: 't', label: 'T' }])
+  })
+
+  it('answers the empty target list for a reply that is no object at all', () => {
+    // Why: the label write runs before the platform write in the same sequence, so a throw here
+    // would take the platform down with it. readSshTargets answered [] for every one of these.
+    for (const payload of [null, undefined, 'nope', 42, []]) {
+      expect(hostSshTargetSummariesSchema.parse(payload)).toEqual([])
+    }
+  })
+
+  it('reads only a platform Node could have reported', () => {
+    expect(hostPlatformSchema.parse({ platform: 'win32' })).toBe('win32')
+    expect(hostPlatformSchema.parse({ platform: 'plan9' })).toBeNull()
+    expect(hostPlatformSchema.parse({ platform: '' })).toBeNull()
+    expect(hostPlatformSchema.parse({})).toBeNull()
+    for (const payload of [null, undefined, 'nope', 42]) {
+      expect(hostPlatformSchema.parse(payload)).toBeNull()
+    }
+  })
+
+  it('requires the ui member main read with a bare property access', () => {
+    expect(hostViewSettingsSchema.safeParse({}).success).toBe(false)
+    expect(hostViewSettingsSchema.safeParse(null).success).toBe(false)
+    expect(hostViewSettingsSchema.parse({ ui: {} })).toEqual({})
+  })
+
+  it('degrades an unknown grouping or sort arm to absent so the local mode is kept', () => {
+    const parsed = hostViewSettingsSchema.parse({
+      ui: { groupBy: 'agent', sortBy: 'stars', hideSleepingWorkspaces: 'yes' }
+    })
+    expect(parsed.groupBy).toBeUndefined()
+    expect(parsed.sortBy).toBeUndefined()
+    expect(parsed.hideSleepingWorkspaces).toBeUndefined()
+  })
+
+  it('keeps the known view arms and the lists the screen adopts', () => {
+    const parsed = hostViewSettingsSchema.parse({
+      ui: {
+        groupBy: 'workspace-status',
+        sortBy: 'recent',
+        filterRepoIds: ['r1'],
+        collapsedGroups: [],
+        workspaceStatuses: [{ id: 'active', label: 'Active' }]
+      }
+    })
+    expect(parsed.groupBy).toBe('workspace-status')
+    expect(parsed.sortBy).toBe('recent')
+    expect(parsed.filterRepoIds).toEqual(['r1'])
+    expect(parsed.workspaceStatuses).toEqual([{ id: 'active', label: 'Active' }])
+  })
+
+  it('salvages a non-array status catalog rather than handing a string to the group lookups', () => {
+    expect(
+      hostViewSettingsSchema.parse({ ui: { workspaceStatuses: 'active' } }).workspaceStatuses
+    ).toBeUndefined()
+  })
+})
+
+describe('the closed arm sets are the desktop unions', () => {
+  // The arm lists are pinned to the desktop unions in the schema modules, where tsc looks; these
+  // loops prove every pinned arm survives the parse, not just the ones the tests above pick.
+  it('keeps every platform Node can report', () => {
+    for (const platform of NODE_PLATFORM_NAMES) {
+      expect(hostPlatformSchema.parse({ platform })).toBe(platform)
+    }
+  })
+
+  it('keeps every grouping and sort arm the desktop persists', () => {
+    for (const groupBy of WORKSPACE_GROUP_BY_ARMS) {
+      for (const sortBy of WORKSPACE_SORT_BY_ARMS) {
+        expect(hostViewSettingsSchema.parse({ ui: { groupBy, sortBy } })).toMatchObject({
+          groupBy,
+          sortBy
+        })
+      }
+    }
+  })
+
+  it('keeps every repo icon arm the desktop draws', () => {
+    const icons: Record<RepoIcon['type'], Record<string, string>> = {
+      lucide: { type: 'lucide', name: 'Folder' },
+      emoji: { type: 'emoji', emoji: '🐳' },
+      image: { type: 'image', src: 'data:,x' }
+    }
+    for (const repoIcon of Object.values(icons)) {
+      const [row] = hostRepoCatalogSchema.parse({
+        repos: [{ id: 'r', displayName: 'r', repoIcon }]
+      })
+      expect(row?.repoIcon).toMatchObject({ type: repoIcon.type })
+    }
+  })
+})