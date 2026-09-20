paidaxingyo666/Manta

fix(runtime): preserve observed exit during explicit terminal close (#21019)
* fix(pty): reconcile daemon exits after synthetic notifications

* fix(runtime): preserve observed exit during explicit terminal close

---------

Co-authored-by: m4air <m4air@Mac.localdomain>

Mirror-Of: 1aadf9115346b28aa9acee93430e1fd47703b303

docs/audits/terminal-close-observed-exit/reproduce.mjs
@@ -1,0 +1,132 @@
+import assert from 'node:assert/strict'
+import { createHash } from 'node:crypto'
+import { mkdtemp, readFile, rm, writeFile } from 'node:fs/promises'
+import { tmpdir } from 'node:os'
+import { join, resolve } from 'node:path'
+import { fileURLToPath, pathToFileURL } from 'node:url'
+import { startVitest } from 'vitest/node'
+
+if (process.env.MANTA_BACKGROUND_LAUNCH !== '1') {
+  throw new Error('Run with MANTA_BACKGROUND_LAUNCH=1.')
+}
+const root = fileURLToPath(new URL('../../../', import.meta.url))
+const sourcePath = 'src/main/runtime/manta-runtime-stop-explicitly-closed-tab-ptys.ts'
+const fixturePath = 'src/main/runtime/terminal-close-observed-exit-test-fixture.ts'
+const source = await readFile(join(root, sourcePath), 'utf8')
+const capture = '      const expectedIncarnationId = this.ptysById.get(ptyId)?.incarnationId\n'
+const guard = `        // Preserve an observed exit when a broader inventory check could not finish.
+        if (
+          !stopped &&
+          expectedIncarnationId &&
+          this.ptysById.get(ptyId)?.incarnationId === expectedIncarnationId &&
+          this.getPtyLivenessVerdict(ptyId)?.status === 'exited'
+        ) {
+          stopped = true
+        }
+`
+assert(source.includes(capture) && source.includes(guard), 'Review the baseline transform.')
+const baseline = source.replace(capture, '').replace(guard, '')
+const scratch = await mkdtemp(join(tmpdir(), 'orca-observed-exit-proof-'))
+const phases = []
+try {
+  for (const phase of ['before', 'after']) {
+    const testPath = join(scratch, `${phase}.test.ts`)
+    const outputPath = join(scratch, `${phase}.json`)
+    const configPath = join(scratch, `${phase}.config.mjs`)
+    await writeFile(
+      testPath,
+      `
+import { afterAll, it } from ${JSON.stringify(join(root, 'node_modules/vitest/dist/index.js'))}
+import { writeFileSync } from 'node:fs'
+import { runObservedExitSocketScenario, runObservedExitControl } from ${JSON.stringify(join(root, fixturePath))}
+const sockets = []
+const controls = []
+for (const scenario of ['healthy', 'unrelated-endpoint-gone', 'physical-exit-observed']) {
+  it(scenario, async () => sockets.push(await runObservedExitSocketScenario(scenario)))
+}
+for (const control of ['same-incarnation', 'replacement', 'unverified', 'legacy-unstamped', 'throw-after-exit']) {
+  it(control, async () => controls.push(await runObservedExitControl(control)))
+}
+afterAll(() => writeFileSync(${JSON.stringify(outputPath)}, JSON.stringify({ sockets, controls })))
+`
+    )
+    await writeFile(
+      configPath,
+      `
+import base from ${JSON.stringify(pathToFileURL(join(root, 'config/vitest.config.ts')).href)}
+export default {
+  ...base,
+  plugins: [{ name: 'observed-exit-baseline', enforce: 'pre', transform(code, id) {
+    if (id.replaceAll('\\\\', '/').endsWith(${JSON.stringify(`/${sourcePath}`)})) return ${JSON.stringify(phase === 'before' ? baseline : source)}
+  } }],
+  test: { ...base.test, include: [${JSON.stringify(testPath)}], maxWorkers: 1, fileParallelism: false }
+}
+`
+    )
+    const runner = await startVitest('test', [], {
+      root,
+      config: configPath,
+      watch: false,
+      reporters: ['dot']
+    })
+    assert(runner, 'Vitest did not start')
+    await runner.close()
+    const result = JSON.parse(await readFile(outputPath, 'utf8'))
+    assert.equal(result.sockets.length, 3)
+    assert.equal(result.controls.length, 5)
+    for (const row of result.sockets) {
+      const observed = row.scenario === 'physical-exit-observed'
+      const healthy = row.scenario === 'healthy'
+      assert.equal(row.close.ptyKilled, healthy || (observed && phase === 'after'))
+      assert.equal(row.fallbackKills, healthy || (observed && phase === 'after') ? 0 : 1)
+      assert.equal(row.targetInventoryCount, 0)
+      assert.equal(row.targetProbe, false)
+      assert.equal(row.routerProbe, healthy ? false : null)
+      assert.equal(row.settled.connected, false)
+      assert.equal(row.settled.headlessModelRetained, false)
+      assert.equal(row.settled.providerExitCount, 1)
+      assert.equal(row.settled.exitListenerCalls, 1)
+      if (observed) {
+        assert.deepEqual(
+          row.settled.exitCause,
+          phase === 'after'
+            ? { kind: 'operator_close' }
+            : { kind: 'unknown', reason: 'stop_unverified' }
+        )
+        assert.equal(row.settled.rendererExitCount, phase === 'after' ? 1 : 2)
+      }
+      if (!healthy && !observed) {
+        assert.equal(row.close.ptyStopVerdict, 'unverifiable')
+        assert.equal(row.beforeStreamResume.providerExitCount, 0)
+      }
+      delete row.beforeStreamResume.incarnationId
+      delete row.settled.incarnationId
+    }
+    for (const row of result.controls) {
+      const accepts = phase === 'after' && row.scenario === 'same-incarnation'
+      assert.equal(row.stopped, accepts)
+      assert.equal(row.fallbackKills, accepts ? 0 : 1)
+    }
+    phases.push({ phase, ...result })
+  }
+  const output = `${JSON.stringify(
+    {
+      sourceHashes: {
+        before: createHash('sha256').update(baseline).digest('hex'),
+        after: createHash('sha256').update(source).digest('hex'),
+        fixture: createHash('sha256')
+          .update(await readFile(join(root, fixturePath)))
+          .digest('hex')
+      },
+      phases
+    },
+    null,
+    2
+  )}\n`
+  if (process.argv[2]) {
+    await writeFile(resolve(process.argv[2]), output)
+  }
+  process.stdout.write(output)
+} finally {
+  await rm(scratch, { recursive: true, force: true })
+}

src/main/runtime/terminal-close-observed-exit-test-fixture.ts
@@ -1,0 +1,158 @@
+import { rmSync } from 'node:fs'
+import { DaemonPtyRouter } from '../daemon/daemon-pty-router'
+import {
+  createMockSubprocess,
+  startDaemonAdapterHarness
+} from '../daemon/daemon-pty-adapter-test-harness'
+import { startLateExitHarness } from '../ipc/pty/daemon-late-exit-test-fixture'
+import { bindProviderListeners } from '../ipc/pty/provider/bind-listeners'
+import { finishPtyShutdown } from '../ipc/pty/provider/liveness'
+import { setLocalPtyProvider } from '../ipc/pty/provider/registry'
+import { shutdownProviderAndDetectExit } from '../ipc/pty/provider/shutdown-detect'
+import type { PtyRuntimeControllerDeps } from '../ipc/pty/runtime/controller-deps'
+import {
+  killPtyFromRuntimeController,
+  stopAndWaitPtyFromRuntimeController
+} from '../ipc/pty/runtime/kill'
+import { MantaRuntimeService } from './manta-runtime'
+
+export type ObservedExitSocketScenario =
+  | 'healthy'
+  | 'unrelated-endpoint-gone'
+  | 'physical-exit-observed'
+
+export async function runObservedExitSocketScenario(scenario: ObservedExitSocketScenario) {
+  const harness = await startLateExitHarness()
+  const legacy = await startDaemonAdapterHarness(() => createMockSubprocess())
+  const router = new DaemonPtyRouter({ current: harness.adapter, legacy: [legacy.adapter] })
+  let fallbackKills = 0
+  try {
+    await router.discoverLegacySessions()
+    setLocalPtyProvider(router)
+    bindProviderListeners(harness.session)
+    const ports = {
+      runtime: harness.runtime,
+      getLocalPtyProviderStartupPromise: () => undefined,
+      shutdownProviderAndDetectExit,
+      rememberSyntheticKillExit: harness.session.rememberSyntheticKillExit,
+      sendPtyExitToRenderer: harness.session.sendPtyExitToRenderer,
+      finishPtyShutdown,
+      retiredRejectedPtyIds: new Map<string, NodeJS.Timeout>(),
+      reversibleStopOwnersByPtyId: new Map<string, number>()
+    }
+    // oxlint-disable-next-line typescript/consistent-type-assertions -- SAFETY: stop/kill read only these controller ports and optional store; spawn ports are unused.
+    const deps = ports as unknown as PtyRuntimeControllerDeps
+    harness.runtime.setPtyController({
+      write: () => true,
+      getForegroundProcess: async () => null,
+      kill: (id) => {
+        fallbackKills++
+        return killPtyFromRuntimeController(deps, id)
+      },
+      stopAndWait: (id) =>
+        stopAndWaitPtyFromRuntimeController(deps, id, { deadlineMs: Date.now() + 1_500 })
+    })
+    const list = await harness.runtime.listTerminals()
+    const terminal = list.terminals.find((entry) => entry.ptyId === harness.id)
+    if (!terminal) {
+      throw new Error('Fixture terminal missing')
+    }
+    if (scenario !== 'healthy') {
+      await legacy.server.shutdown()
+    }
+    if (scenario !== 'physical-exit-observed') {
+      harness.pauseStream()
+    }
+    const close = await harness.runtime.closeTerminal(terminal.handle)
+    const targetInventory = await harness.adapter.listProcesses()
+    const targetProbe = await harness.adapter.probePtyLiveness(harness.id)
+    const routerProbe = await router.probePtyLiveness(harness.id)
+    const beforeStreamResume = await harness.capture()
+    harness.resumeStream()
+    await harness.waitForExit()
+    const settled = await harness.capture()
+    return {
+      scenario,
+      close: {
+        ptyKilled: close.ptyKilled,
+        ptyStopVerdict: close.ptyStopVerdict ?? null
+      },
+      fallbackKills,
+      targetInventoryCount: targetInventory.length,
+      targetProbe,
+      routerProbe,
+      beforeStreamResume,
+      settled
+    }
+  } finally {
+    router.disposeRouterOnly()
+    await harness.dispose()
+    legacy.adapter.dispose()
+    await legacy.server.shutdown()
+    rmSync(legacy.dir, { recursive: true, force: true })
+  }
+}
+
+const CONTROL_PTY_ID = 'repo::/tmp/observed-exit-control@@pty'
+const WORKTREE_ID = 'repo::/tmp/observed-exit-control'
+const FIRST_INCARNATION = '10000000-0000-4000-8000-000000000001'
+const NEXT_INCARNATION = '10000000-0000-4000-8000-000000000002'
+const BINDING = {
+  tabId: 'control-tab',
+  leafId: '10000000-0000-4000-8000-000000000004'
+}
+
+class ObservedExitRuntime extends MantaRuntimeService {
+  closeControl(): Promise<boolean> {
+    return this.stopExplicitlyClosedTabPtys([CONTROL_PTY_ID], CONTROL_PTY_ID)
+  }
+}
+
+export type ObservedExitControl =
+  | 'same-incarnation'
+  | 'replacement'
+  | 'unverified'
+  | 'legacy-unstamped'
+  | 'throw-after-exit'
+
+export async function runObservedExitControl(control: ObservedExitControl) {
+  const runtime = new ObservedExitRuntime()
+  const original = control === 'legacy-unstamped' ? undefined : FIRST_INCARNATION
+  let fallbackKills = 0
+  runtime.registerPty(CONTROL_PTY_ID, WORKTREE_ID, null, {
+    ...BINDING,
+    ...(original ? { incarnationId: original } : {})
+  })
+  runtime.setPtyController({
+    write: () => true,
+    kill: () => {
+      fallbackKills++
+      return true
+    },
+    getForegroundProcess: async () => null,
+    stopAndWait: async () => {
+      runtime.onPtyExit(CONTROL_PTY_ID, control === 'unverified' ? -1 : 0, original)
+      if (control === 'replacement') {
+        runtime.registerPty(CONTROL_PTY_ID, WORKTREE_ID, null, {
+          ...BINDING,
+          incarnationId: NEXT_INCARNATION
+        })
+      }
+      if (control === 'throw-after-exit') {
+        throw new Error('unverified transport failure')
+      }
+      return false
+    }
+  })
+  try {
+    const stopped = await runtime.closeControl()
+    return {
+      scenario: control,
+      stopped,
+      fallbackKills,
+      verdict: runtime.getPtyLivenessVerdict(CONTROL_PTY_ID)
+    }
+  } finally {
+    runtime.onPtyExit(CONTROL_PTY_ID, 0, control === 'replacement' ? NEXT_INCARNATION : original)
+  }
+}

ivanvykopal/orca

docs/audits/terminal-close-observed-exit/reproduce.mjs
@@ -1,0 +1,132 @@
+import assert from 'node:assert/strict'
+import { createHash } from 'node:crypto'
+import { mkdtemp, readFile, rm, writeFile } from 'node:fs/promises'
+import { tmpdir } from 'node:os'
+import { join, resolve } from 'node:path'
+import { fileURLToPath, pathToFileURL } from 'node:url'
+import { startVitest } from 'vitest/node'
+
+if (process.env.ORCA_BACKGROUND_LAUNCH !== '1') {
+  throw new Error('Run with ORCA_BACKGROUND_LAUNCH=1.')
+}
+const root = fileURLToPath(new URL('../../../', import.meta.url))
+const sourcePath = 'src/main/runtime/orca-runtime-stop-explicitly-closed-tab-ptys.ts'
+const fixturePath = 'src/main/runtime/terminal-close-observed-exit-test-fixture.ts'
+const source = await readFile(join(root, sourcePath), 'utf8')
+const capture = '      const expectedIncarnationId = this.ptysById.get(ptyId)?.incarnationId\n'
+const guard = `        // Preserve an observed exit when a broader inventory check could not finish.
+        if (
+          !stopped &&
+          expectedIncarnationId &&
+          this.ptysById.get(ptyId)?.incarnationId === expectedIncarnationId &&
+          this.getPtyLivenessVerdict(ptyId)?.status === 'exited'
+        ) {
+          stopped = true
+        }
+`
+assert(source.includes(capture) && source.includes(guard), 'Review the baseline transform.')
+const baseline = source.replace(capture, '').replace(guard, '')
+const scratch = await mkdtemp(join(tmpdir(), 'orca-observed-exit-proof-'))
+const phases = []
+try {
+  for (const phase of ['before', 'after']) {
+    const testPath = join(scratch, `${phase}.test.ts`)
+    const outputPath = join(scratch, `${phase}.json`)
+    const configPath = join(scratch, `${phase}.config.mjs`)
+    await writeFile(
+      testPath,
+      `
+import { afterAll, it } from ${JSON.stringify(join(root, 'node_modules/vitest/dist/index.js'))}
+import { writeFileSync } from 'node:fs'
+import { runObservedExitSocketScenario, runObservedExitControl } from ${JSON.stringify(join(root, fixturePath))}
+const sockets = []
+const controls = []
+for (const scenario of ['healthy', 'unrelated-endpoint-gone', 'physical-exit-observed']) {
+  it(scenario, async () => sockets.push(await runObservedExitSocketScenario(scenario)))
+}
+for (const control of ['same-incarnation', 'replacement', 'unverified', 'legacy-unstamped', 'throw-after-exit']) {
+  it(control, async () => controls.push(await runObservedExitControl(control)))
+}
+afterAll(() => writeFileSync(${JSON.stringify(outputPath)}, JSON.stringify({ sockets, controls })))
+`
+    )
+    await writeFile(
+      configPath,
+      `
+import base from ${JSON.stringify(pathToFileURL(join(root, 'config/vitest.config.ts')).href)}
+export default {
+  ...base,
+  plugins: [{ name: 'observed-exit-baseline', enforce: 'pre', transform(code, id) {
+    if (id.replaceAll('\\\\', '/').endsWith(${JSON.stringify(`/${sourcePath}`)})) return ${JSON.stringify(phase === 'before' ? baseline : source)}
+  } }],
+  test: { ...base.test, include: [${JSON.stringify(testPath)}], maxWorkers: 1, fileParallelism: false }
+}
+`
+    )
+    const runner = await startVitest('test', [], {
+      root,
+      config: configPath,
+      watch: false,
+      reporters: ['dot']
+    })
+    assert(runner, 'Vitest did not start')
+    await runner.close()
+    const result = JSON.parse(await readFile(outputPath, 'utf8'))
+    assert.equal(result.sockets.length, 3)
+    assert.equal(result.controls.length, 5)
+    for (const row of result.sockets) {
+      const observed = row.scenario === 'physical-exit-observed'
+      const healthy = row.scenario === 'healthy'
+      assert.equal(row.close.ptyKilled, healthy || (observed && phase === 'after'))
+      assert.equal(row.fallbackKills, healthy || (observed && phase === 'after') ? 0 : 1)
+      assert.equal(row.targetInventoryCount, 0)
+      assert.equal(row.targetProbe, false)
+      assert.equal(row.routerProbe, healthy ? false : null)
+      assert.equal(row.settled.connected, false)
+      assert.equal(row.settled.headlessModelRetained, false)
+      assert.equal(row.settled.providerExitCount, 1)
+      assert.equal(row.settled.exitListenerCalls, 1)
+      if (observed) {
+        assert.deepEqual(
+          row.settled.exitCause,
+          phase === 'after'
+            ? { kind: 'operator_close' }
+            : { kind: 'unknown', reason: 'stop_unverified' }
+        )
+        assert.equal(row.settled.rendererExitCount, phase === 'after' ? 1 : 2)
+      }
+      if (!healthy && !observed) {
+        assert.equal(row.close.ptyStopVerdict, 'unverifiable')
+        assert.equal(row.beforeStreamResume.providerExitCount, 0)
+      }
+      delete row.beforeStreamResume.incarnationId
+      delete row.settled.incarnationId
+    }
+    for (const row of result.controls) {
+      const accepts = phase === 'after' && row.scenario === 'same-incarnation'
+      assert.equal(row.stopped, accepts)
+      assert.equal(row.fallbackKills, accepts ? 0 : 1)
+    }
+    phases.push({ phase, ...result })
+  }
+  const output = `${JSON.stringify(
+    {
+      sourceHashes: {
+        before: createHash('sha256').update(baseline).digest('hex'),
+        after: createHash('sha256').update(source).digest('hex'),
+        fixture: createHash('sha256')
+          .update(await readFile(join(root, fixturePath)))
+          .digest('hex')
+      },
+      phases
+    },
+    null,
+    2
+  )}\n`
+  if (process.argv[2]) {
+    await writeFile(resolve(process.argv[2]), output)
+  }
+  process.stdout.write(output)
+} finally {
+  await rm(scratch, { recursive: true, force: true })
+}

src/main/runtime/terminal-close-observed-exit-test-fixture.ts
@@ -1,0 +1,158 @@
+import { rmSync } from 'node:fs'
+import { DaemonPtyRouter } from '../daemon/daemon-pty-router'
+import {
+  createMockSubprocess,
+  startDaemonAdapterHarness
+} from '../daemon/daemon-pty-adapter-test-harness'
+import { startLateExitHarness } from '../ipc/pty/daemon-late-exit-test-fixture'
+import { bindProviderListeners } from '../ipc/pty/provider/bind-listeners'
+import { finishPtyShutdown } from '../ipc/pty/provider/liveness'
+import { setLocalPtyProvider } from '../ipc/pty/provider/registry'
+import { shutdownProviderAndDetectExit } from '../ipc/pty/provider/shutdown-detect'
+import type { PtyRuntimeControllerDeps } from '../ipc/pty/runtime/controller-deps'
+import {
+  killPtyFromRuntimeController,
+  stopAndWaitPtyFromRuntimeController
+} from '../ipc/pty/runtime/kill'
+import { OrcaRuntimeService } from './orca-runtime'
+
+export type ObservedExitSocketScenario =
+  | 'healthy'
+  | 'unrelated-endpoint-gone'
+  | 'physical-exit-observed'
+
+export async function runObservedExitSocketScenario(scenario: ObservedExitSocketScenario) {
+  const harness = await startLateExitHarness()
+  const legacy = await startDaemonAdapterHarness(() => createMockSubprocess())
+  const router = new DaemonPtyRouter({ current: harness.adapter, legacy: [legacy.adapter] })
+  let fallbackKills = 0
+  try {
+    await router.discoverLegacySessions()
+    setLocalPtyProvider(router)
+    bindProviderListeners(harness.session)
+    const ports = {
+      runtime: harness.runtime,
+      getLocalPtyProviderStartupPromise: () => undefined,
+      shutdownProviderAndDetectExit,
+      rememberSyntheticKillExit: harness.session.rememberSyntheticKillExit,
+      sendPtyExitToRenderer: harness.session.sendPtyExitToRenderer,
+      finishPtyShutdown,
+      retiredRejectedPtyIds: new Map<string, NodeJS.Timeout>(),
+      reversibleStopOwnersByPtyId: new Map<string, number>()
+    }
+    // oxlint-disable-next-line typescript/consistent-type-assertions -- SAFETY: stop/kill read only these controller ports and optional store; spawn ports are unused.
+    const deps = ports as unknown as PtyRuntimeControllerDeps
+    harness.runtime.setPtyController({
+      write: () => true,
+      getForegroundProcess: async () => null,
+      kill: (id) => {
+        fallbackKills++
+        return killPtyFromRuntimeController(deps, id)
+      },
+      stopAndWait: (id) =>
+        stopAndWaitPtyFromRuntimeController(deps, id, { deadlineMs: Date.now() + 1_500 })
+    })
+    const list = await harness.runtime.listTerminals()
+    const terminal = list.terminals.find((entry) => entry.ptyId === harness.id)
+    if (!terminal) {
+      throw new Error('Fixture terminal missing')
+    }
+    if (scenario !== 'healthy') {
+      await legacy.server.shutdown()
+    }
+    if (scenario !== 'physical-exit-observed') {
+      harness.pauseStream()
+    }
+    const close = await harness.runtime.closeTerminal(terminal.handle)
+    const targetInventory = await harness.adapter.listProcesses()
+    const targetProbe = await harness.adapter.probePtyLiveness(harness.id)
+    const routerProbe = await router.probePtyLiveness(harness.id)
+    const beforeStreamResume = await harness.capture()
+    harness.resumeStream()
+    await harness.waitForExit()
+    const settled = await harness.capture()
+    return {
+      scenario,
+      close: {
+        ptyKilled: close.ptyKilled,
+        ptyStopVerdict: close.ptyStopVerdict ?? null
+      },
+      fallbackKills,
+      targetInventoryCount: targetInventory.length,
+      targetProbe,
+      routerProbe,
+      beforeStreamResume,
+      settled
+    }
+  } finally {
+    router.disposeRouterOnly()
+    await harness.dispose()
+    legacy.adapter.dispose()
+    await legacy.server.shutdown()
+    rmSync(legacy.dir, { recursive: true, force: true })
+  }
+}
+
+const CONTROL_PTY_ID = 'repo::/tmp/observed-exit-control@@pty'
+const WORKTREE_ID = 'repo::/tmp/observed-exit-control'
+const FIRST_INCARNATION = '10000000-0000-4000-8000-000000000001'
+const NEXT_INCARNATION = '10000000-0000-4000-8000-000000000002'
+const BINDING = {
+  tabId: 'control-tab',
+  leafId: '10000000-0000-4000-8000-000000000004'
+}
+
+class ObservedExitRuntime extends OrcaRuntimeService {
+  closeControl(): Promise<boolean> {
+    return this.stopExplicitlyClosedTabPtys([CONTROL_PTY_ID], CONTROL_PTY_ID)
+  }
+}
+
+export type ObservedExitControl =
+  | 'same-incarnation'
+  | 'replacement'
+  | 'unverified'
+  | 'legacy-unstamped'
+  | 'throw-after-exit'
+
+export async function runObservedExitControl(control: ObservedExitControl) {
+  const runtime = new ObservedExitRuntime()
+  const original = control === 'legacy-unstamped' ? undefined : FIRST_INCARNATION
+  let fallbackKills = 0
+  runtime.registerPty(CONTROL_PTY_ID, WORKTREE_ID, null, {
+    ...BINDING,
+    ...(original ? { incarnationId: original } : {})
+  })
+  runtime.setPtyController({
+    write: () => true,
+    kill: () => {
+      fallbackKills++
+      return true
+    },
+    getForegroundProcess: async () => null,
+    stopAndWait: async () => {
+      runtime.onPtyExit(CONTROL_PTY_ID, control === 'unverified' ? -1 : 0, original)
+      if (control === 'replacement') {
+        runtime.registerPty(CONTROL_PTY_ID, WORKTREE_ID, null, {
+          ...BINDING,
+          incarnationId: NEXT_INCARNATION
+        })
+      }
+      if (control === 'throw-after-exit') {
+        throw new Error('unverified transport failure')
+      }
+      return false
+    }
+  })
+  try {
+    const stopped = await runtime.closeControl()
+    return {
+      scenario: control,
+      stopped,
+      fallbackKills,
+      verdict: runtime.getPtyLivenessVerdict(CONTROL_PTY_ID)
+    }
+  } finally {
+    runtime.onPtyExit(CONTROL_PTY_ID, 0, control === 'replacement' ? NEXT_INCARNATION : original)
+  }
+}