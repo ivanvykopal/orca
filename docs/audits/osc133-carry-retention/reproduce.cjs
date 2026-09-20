paidaxingyo666/Manta

Detach retained CI and terminal tails from oversized strings (#20960)
* fix(memory): detach retained CI and terminal tails from oversized strings

* fix(terminal): detach retained error and reattach string slices

* fix(terminal): release oversized recent-output backing strings

* fix(terminal): release backing strings held by PTY detectors

* fix(memory): own bounded Claude background task labels

* fix: detach retained terminal mode scan tails

* fix: own retained plugin worker output strings

* fix: own incomplete OSC 133 carry strings

---------

Co-authored-by: m4air <m4air@Mac.localdomain>
Co-authored-by: m4air <m4air@m4airs-MacBook-Air.local>

Mirror-Of: d04b05b5c8f0b8074a7e316c8cd6ccdbbddf7e2c

docs/audits/osc133-carry-retention/reproduce.cjs
@@ -1,0 +1,142 @@
+const assert = require('node:assert/strict')
+const fs = require('node:fs')
+const path = require('node:path')
+const { load, loadSources, readText, sha, versions: sourceVersions } = require('./sources.cjs')
+const { inputs, heap, makeOwner, behavior } = require('./scenario.cjs')
+
+assert.equal(process.env.MANTA_BACKGROUND_LAUNCH, '1')
+assert.equal(typeof global.gc, 'function', 'Run with --expose-gc')
+
+async function main() {
+  const reports = []
+  const versions = []
+  for (const variant of ['baseline', 'candidate-buffer', 'candidate-fallback']) {
+    const fixed = variant !== 'baseline'
+    const loaded = await load(fixed)
+    loaded.api.resetOwnRetainedStringCopier()
+    const originalBuffer = globalThis.Buffer
+    try {
+      if (variant === 'candidate-fallback') {
+        globalThis.Buffer = undefined
+      }
+      assert.equal(
+        loaded.api.ownRetainedString('prefix-\ud800a\udfff\u0000漢-suffix'),
+        'prefix-\ud800a\udfff\u0000漢-suffix'
+      )
+    } finally {
+      globalThis.Buffer = originalBuffer
+    }
+    const controls = behavior(loaded.api)
+    versions.push({
+      variant,
+      fixed,
+      sourceSha256: loaded.sourceSha256,
+      bundleSha256: loaded.bundleSha256,
+      evaluatedSources: loaded.evaluatedSources,
+      callerSourceHashes: loaded.callerSourceHashes,
+      controls
+    })
+    for (const ownerKind of ['scanner', 'title-tracker', 'background-relay']) {
+      for (const input of inputs) {
+        for (const [chars, count] of [
+          [64 * 1024, 32],
+          [1024 * 1024, 8]
+        ]) {
+          const before = await heap()
+          const owners = Array.from({ length: count }, (_, index) =>
+            makeOwner(loaded.api, ownerKind, input, chars, index)
+          )
+          const retainedDelta = (await heap()) - before
+          const expectParent = !fixed && input.retained
+          assert.ok(
+            expectParent ? retainedDelta > chars * count * 0.75 : retainedDelta < 1024 * 1024,
+            JSON.stringify({ fixed, ownerKind, input: input.name, retainedDelta })
+          )
+          for (const owner of owners) {
+            owner.complete()
+          }
+          const completedDelta = (await heap()) - before
+          assert.ok(
+            completedDelta < 1024 * 1024,
+            JSON.stringify({ fixed, ownerKind, input: input.name, completedDelta })
+          )
+          for (const owner of owners) {
+            owner.release()
+          }
+          reports.push({
+            variant,
+            fixed,
+            ownerKind,
+            input: input.name,
+            inputCodeUnits: chars,
+            owners: count,
+            inputSuffixCodeUnits: input.suffix.length,
+            retainedDelta,
+            completedDelta
+          })
+        }
+      }
+      const input = inputs[0]
+      const before = await heap()
+      const owners = Array.from({ length: 8 }, (_, index) =>
+        makeOwner(loaded.api, ownerKind, input, 1024 * 1024, index)
+      )
+      for (const owner of owners) {
+        owner.release()
+      }
+      const resetDelta = (await heap()) - before
+      assert.ok(resetDelta < 1024 * 1024, JSON.stringify({ fixed, ownerKind, resetDelta }))
+      reports.push({ variant, fixed, ownerKind, input: 'reset-without-completion', resetDelta })
+    }
+  }
+  assert.deepEqual(versions[0].controls, versions[1].controls)
+  assert.deepEqual(versions[0].controls, versions[2].controls)
+  let crlfReads = 0
+  const crlfSources = loadSources((file) => {
+    crlfReads += 1
+    return readText(file).replaceAll('\n', '\r\n')
+  })
+  assert.deepEqual(crlfSources, loadSources())
+  assert.equal(crlfReads, 2)
+  const artifacts = [
+    'sources.cjs',
+    'scenario.cjs',
+    'reproduce.cjs',
+    'source-versions.json',
+    'fix.patch',
+    'before.config.mjs'
+  ]
+  const artifactHashes = Object.fromEntries(
+    artifacts.map((file) => [file, sha(readText(path.join(__dirname, file)))])
+  )
+  const result = {
+    scope:
+      'Actual-source bounded diagnostic; captured fish sequence syntax with synthetic large-prefix placement and chunk split. No affected-host, RSS, native PTY, or whole-release claim.',
+    node: process.version,
+    electron: process.versions.electron ?? null,
+    v8: process.versions.v8,
+    sourcePath: sourceVersions.sourcePath,
+    crlfReads,
+    artifactHashes,
+    versions,
+    reports
+  }
+  const resultPath = process.argv[2]
+    ? path.resolve(process.argv[2])
+    : path.join(
+        __dirname,
+        process.versions.electron ? 'electron-results.json' : 'node-results.json'
+      )
+  fs.writeFileSync(resultPath, `${JSON.stringify(result, null, 2)}\n`)
+  console.log(
+    JSON.stringify({ resultPath, cases: reports.length, variants: versions.map((x) => x.variant) })
+  )
+}
+main().catch((error) => {
+  console.error(error)
+  process.exitCode = 1
+})
+setTimeout(() => {
+  console.error('fixture deadline')
+  process.exit(2)
+}, 60000).unref()

docs/audits/pty-detector-retention/reproduce.mjs
@@ -1,0 +1,144 @@
+import assert from 'node:assert/strict'
+import { createHash } from 'node:crypto'
+import { readFile } from 'node:fs/promises'
+import { fileURLToPath } from 'node:url'
+import { build } from 'esbuild'
+
+if (process.env.MANTA_BACKGROUND_LAUNCH !== '1' || typeof global.gc !== 'function') {
+  throw new Error('Run with MANTA_BACKGROUND_LAUNCH=1 node --expose-gc')
+}
+const root = fileURLToPath(new URL('../../../', import.meta.url))
+const replacements = {
+  'command-code-output-status.ts': [
+    'ownRetainedString(data.slice(-RECENT_TEXT_LIMIT))',
+    'data.slice(-RECENT_TEXT_LIMIT)'
+  ],
+  'advertised-url-parsing.ts': [
+    'ownRetainedString(chunk.slice(-PER_PTY_BUFFER_LIMIT))',
+    'chunk.slice(-PER_PTY_BUFFER_LIMIT)'
+  ],
+  'advertised-url-watcher.ts': [
+    'ownRetainedString(combined.slice(-PENDING_PRE_BIND_LIMIT))',
+    'combined.slice(-PENDING_PRE_BIND_LIMIT)'
+  ]
+}
+const results = []
+const bundles = {}
+
+function heapAfterGc() {
+  global.gc()
+  global.gc()
+  return process.memoryUsage().heapUsed
+}
+
+function measure(makeOwner, validate, inputChars, count) {
+  const before = heapAfterGc()
+  const owners = Array.from({ length: count }, (_, index) => {
+    const prefix = `${index}:`
+    const suffix = `\nhttp://localhost:${4100 + index}`
+    return makeOwner(
+      `${prefix}${'x'.repeat(inputChars - prefix.length - suffix.length)}${suffix}`,
+      index
+    )
+  })
+  const heapDelta = heapAfterGc() - before
+  owners.forEach(validate)
+  return { inputChars, count, heapDelta }
+}
+
+for (const fixed of [false, true]) {
+  const result = await build({
+    stdin: {
+      contents: `
+        export { createCommandCodeOutputStatusDetector } from './src/shared/command-code-output-status'
+        export { AdvertisedUrlWatcher } from './src/main/ports/advertised-url-watcher'
+      `,
+      resolveDir: root,
+      loader: 'ts'
+    },
+    bundle: true,
+    platform: 'node',
+    format: 'esm',
+    write: false,
+    plugins: fixed
+      ? []
+      : [
+          {
+            name: 'baseline-without-detector-tail-copy',
+            setup(builder) {
+              builder.onLoad(
+                {
+                  filter:
+                    /(?:command-code-output-status|advertised-url-parsing|advertised-url-watcher)\.ts$/
+                },
+                async ({ path }) => {
+                  const source = await readFile(path, 'utf8')
+                  const replacement = Object.entries(replacements).find(([name]) =>
+                    path.endsWith(name)
+                  )?.[1]
+                  if (!replacement || !source.includes(replacement[0])) {
+                    throw new Error('The copy boundary changed; update the baseline transform')
+                  }
+                  return { contents: source.replaceAll(...replacement), loader: 'ts' }
+                }
+              )
+            }
+          }
+        ]
+  })
+  const bundle = result.outputFiles[0].text
+  bundles[fixed ? 'after' : 'before'] = createHash('sha256').update(bundle).digest('hex')
+  const { createCommandCodeOutputStatusDetector, AdvertisedUrlWatcher } = await import(
+    `data:text/javascript;base64,${Buffer.from(bundle).toString('base64')}`
+  )
+  for (const [inputChars, count] of [
+    [64 * 1024, 32],
+    [4 * 1024 * 1024, 8]
+  ]) {
+    results.push({
+      kind: 'command-code-detector',
+      fixed,
+      ...measure(
+        (data) => {
+          const detector = createCommandCodeOutputStatusDetector({ onWorking: () => {} })
+          detector.observe(data)
+          return detector
+        },
+        (detector) => assert.equal(detector.observe('\nordinary output\n'), false),
+        inputChars,
+        count
+      )
+    })
+    for (const bound of [true, false]) {
+      results.push({
+        kind: bound ? 'url-bound-pty' : 'url-before-binding',
+        fixed,
+        ...measure(
+          (data) => {
+            const watcher = new AdvertisedUrlWatcher()
+            if (bound) {
+              watcher.bindPty('pty', 'workspace')
+            }
+            watcher.ingest('pty', data)
+            return watcher
+          },
+          (watcher, index) => {
+            watcher.bindPty('pty', 'workspace')
+            watcher.ingest('pty', '/\n')
+            assert.equal(
+              watcher.lookup('workspace', 4100 + index)?.origin,
+              `http://localhost:${4100 + index}`
+            )
+            watcher.unbindPty('pty')
+            assert.equal(watcher.lookup('workspace', 4100 + index), undefined)
+          },
+          inputChars,
+          count
+        )
+      })
+    }
+  }
+}
+console.log(
+  JSON.stringify({ node: process.version, platform: process.platform, bundles, results }, null, 2)
+)

ivanvykopal/orca

docs/audits/osc133-carry-retention/reproduce.cjs
@@ -1,0 +1,142 @@
+const assert = require('node:assert/strict')
+const fs = require('node:fs')
+const path = require('node:path')
+const { load, loadSources, readText, sha, versions: sourceVersions } = require('./sources.cjs')
+const { inputs, heap, makeOwner, behavior } = require('./scenario.cjs')
+
+assert.equal(process.env.ORCA_BACKGROUND_LAUNCH, '1')
+assert.equal(typeof global.gc, 'function', 'Run with --expose-gc')
+
+async function main() {
+  const reports = []
+  const versions = []
+  for (const variant of ['baseline', 'candidate-buffer', 'candidate-fallback']) {
+    const fixed = variant !== 'baseline'
+    const loaded = await load(fixed)
+    loaded.api.resetOwnRetainedStringCopier()
+    const originalBuffer = globalThis.Buffer
+    try {
+      if (variant === 'candidate-fallback') {
+        globalThis.Buffer = undefined
+      }
+      assert.equal(
+        loaded.api.ownRetainedString('prefix-\ud800a\udfff\u0000漢-suffix'),
+        'prefix-\ud800a\udfff\u0000漢-suffix'
+      )
+    } finally {
+      globalThis.Buffer = originalBuffer
+    }
+    const controls = behavior(loaded.api)
+    versions.push({
+      variant,
+      fixed,
+      sourceSha256: loaded.sourceSha256,
+      bundleSha256: loaded.bundleSha256,
+      evaluatedSources: loaded.evaluatedSources,
+      callerSourceHashes: loaded.callerSourceHashes,
+      controls
+    })
+    for (const ownerKind of ['scanner', 'title-tracker', 'background-relay']) {
+      for (const input of inputs) {
+        for (const [chars, count] of [
+          [64 * 1024, 32],
+          [1024 * 1024, 8]
+        ]) {
+          const before = await heap()
+          const owners = Array.from({ length: count }, (_, index) =>
+            makeOwner(loaded.api, ownerKind, input, chars, index)
+          )
+          const retainedDelta = (await heap()) - before
+          const expectParent = !fixed && input.retained
+          assert.ok(
+            expectParent ? retainedDelta > chars * count * 0.75 : retainedDelta < 1024 * 1024,
+            JSON.stringify({ fixed, ownerKind, input: input.name, retainedDelta })
+          )
+          for (const owner of owners) {
+            owner.complete()
+          }
+          const completedDelta = (await heap()) - before
+          assert.ok(
+            completedDelta < 1024 * 1024,
+            JSON.stringify({ fixed, ownerKind, input: input.name, completedDelta })
+          )
+          for (const owner of owners) {
+            owner.release()
+          }
+          reports.push({
+            variant,
+            fixed,
+            ownerKind,
+            input: input.name,
+            inputCodeUnits: chars,
+            owners: count,
+            inputSuffixCodeUnits: input.suffix.length,
+            retainedDelta,
+            completedDelta
+          })
+        }
+      }
+      const input = inputs[0]
+      const before = await heap()
+      const owners = Array.from({ length: 8 }, (_, index) =>
+        makeOwner(loaded.api, ownerKind, input, 1024 * 1024, index)
+      )
+      for (const owner of owners) {
+        owner.release()
+      }
+      const resetDelta = (await heap()) - before
+      assert.ok(resetDelta < 1024 * 1024, JSON.stringify({ fixed, ownerKind, resetDelta }))
+      reports.push({ variant, fixed, ownerKind, input: 'reset-without-completion', resetDelta })
+    }
+  }
+  assert.deepEqual(versions[0].controls, versions[1].controls)
+  assert.deepEqual(versions[0].controls, versions[2].controls)
+  let crlfReads = 0
+  const crlfSources = loadSources((file) => {
+    crlfReads += 1
+    return readText(file).replaceAll('\n', '\r\n')
+  })
+  assert.deepEqual(crlfSources, loadSources())
+  assert.equal(crlfReads, 2)
+  const artifacts = [
+    'sources.cjs',
+    'scenario.cjs',
+    'reproduce.cjs',
+    'source-versions.json',
+    'fix.patch',
+    'before.config.mjs'
+  ]
+  const artifactHashes = Object.fromEntries(
+    artifacts.map((file) => [file, sha(readText(path.join(__dirname, file)))])
+  )
+  const result = {
+    scope:
+      'Actual-source bounded diagnostic; captured fish sequence syntax with synthetic large-prefix placement and chunk split. No affected-host, RSS, native PTY, or whole-release claim.',
+    node: process.version,
+    electron: process.versions.electron ?? null,
+    v8: process.versions.v8,
+    sourcePath: sourceVersions.sourcePath,
+    crlfReads,
+    artifactHashes,
+    versions,
+    reports
+  }
+  const resultPath = process.argv[2]
+    ? path.resolve(process.argv[2])
+    : path.join(
+        __dirname,
+        process.versions.electron ? 'electron-results.json' : 'node-results.json'
+      )
+  fs.writeFileSync(resultPath, `${JSON.stringify(result, null, 2)}\n`)
+  console.log(
+    JSON.stringify({ resultPath, cases: reports.length, variants: versions.map((x) => x.variant) })
+  )
+}
+main().catch((error) => {
+  console.error(error)
+  process.exitCode = 1
+})
+setTimeout(() => {
+  console.error('fixture deadline')
+  process.exit(2)
+}, 60000).unref()

docs/audits/pty-detector-retention/reproduce.mjs
@@ -1,0 +1,144 @@
+import assert from 'node:assert/strict'
+import { createHash } from 'node:crypto'
+import { readFile } from 'node:fs/promises'
+import { fileURLToPath } from 'node:url'
+import { build } from 'esbuild'
+
+if (process.env.ORCA_BACKGROUND_LAUNCH !== '1' || typeof global.gc !== 'function') {
+  throw new Error('Run with ORCA_BACKGROUND_LAUNCH=1 node --expose-gc')
+}
+const root = fileURLToPath(new URL('../../../', import.meta.url))
+const replacements = {
+  'command-code-output-status.ts': [
+    'ownRetainedString(data.slice(-RECENT_TEXT_LIMIT))',
+    'data.slice(-RECENT_TEXT_LIMIT)'
+  ],
+  'advertised-url-parsing.ts': [
+    'ownRetainedString(chunk.slice(-PER_PTY_BUFFER_LIMIT))',
+    'chunk.slice(-PER_PTY_BUFFER_LIMIT)'
+  ],
+  'advertised-url-watcher.ts': [
+    'ownRetainedString(combined.slice(-PENDING_PRE_BIND_LIMIT))',
+    'combined.slice(-PENDING_PRE_BIND_LIMIT)'
+  ]
+}
+const results = []
+const bundles = {}
+
+function heapAfterGc() {
+  global.gc()
+  global.gc()
+  return process.memoryUsage().heapUsed
+}
+
+function measure(makeOwner, validate, inputChars, count) {
+  const before = heapAfterGc()
+  const owners = Array.from({ length: count }, (_, index) => {
+    const prefix = `${index}:`
+    const suffix = `\nhttp://localhost:${4100 + index}`
+    return makeOwner(
+      `${prefix}${'x'.repeat(inputChars - prefix.length - suffix.length)}${suffix}`,
+      index
+    )
+  })
+  const heapDelta = heapAfterGc() - before
+  owners.forEach(validate)
+  return { inputChars, count, heapDelta }
+}
+
+for (const fixed of [false, true]) {
+  const result = await build({
+    stdin: {
+      contents: `
+        export { createCommandCodeOutputStatusDetector } from './src/shared/command-code-output-status'
+        export { AdvertisedUrlWatcher } from './src/main/ports/advertised-url-watcher'
+      `,
+      resolveDir: root,
+      loader: 'ts'
+    },
+    bundle: true,
+    platform: 'node',
+    format: 'esm',
+    write: false,
+    plugins: fixed
+      ? []
+      : [
+          {
+            name: 'baseline-without-detector-tail-copy',
+            setup(builder) {
+              builder.onLoad(
+                {
+                  filter:
+                    /(?:command-code-output-status|advertised-url-parsing|advertised-url-watcher)\.ts$/
+                },
+                async ({ path }) => {
+                  const source = await readFile(path, 'utf8')
+                  const replacement = Object.entries(replacements).find(([name]) =>
+                    path.endsWith(name)
+                  )?.[1]
+                  if (!replacement || !source.includes(replacement[0])) {
+                    throw new Error('The copy boundary changed; update the baseline transform')
+                  }
+                  return { contents: source.replaceAll(...replacement), loader: 'ts' }
+                }
+              )
+            }
+          }
+        ]
+  })
+  const bundle = result.outputFiles[0].text
+  bundles[fixed ? 'after' : 'before'] = createHash('sha256').update(bundle).digest('hex')
+  const { createCommandCodeOutputStatusDetector, AdvertisedUrlWatcher } = await import(
+    `data:text/javascript;base64,${Buffer.from(bundle).toString('base64')}`
+  )
+  for (const [inputChars, count] of [
+    [64 * 1024, 32],
+    [4 * 1024 * 1024, 8]
+  ]) {
+    results.push({
+      kind: 'command-code-detector',
+      fixed,
+      ...measure(
+        (data) => {
+          const detector = createCommandCodeOutputStatusDetector({ onWorking: () => {} })
+          detector.observe(data)
+          return detector
+        },
+        (detector) => assert.equal(detector.observe('\nordinary output\n'), false),
+        inputChars,
+        count
+      )
+    })
+    for (const bound of [true, false]) {
+      results.push({
+        kind: bound ? 'url-bound-pty' : 'url-before-binding',
+        fixed,
+        ...measure(
+          (data) => {
+            const watcher = new AdvertisedUrlWatcher()
+            if (bound) {
+              watcher.bindPty('pty', 'workspace')
+            }
+            watcher.ingest('pty', data)
+            return watcher
+          },
+          (watcher, index) => {
+            watcher.bindPty('pty', 'workspace')
+            watcher.ingest('pty', '/\n')
+            assert.equal(
+              watcher.lookup('workspace', 4100 + index)?.origin,
+              `http://localhost:${4100 + index}`
+            )
+            watcher.unbindPty('pty')
+            assert.equal(watcher.lookup('workspace', 4100 + index), undefined)
+          },
+          inputChars,
+          count
+        )
+      })
+    }
+  }
+}
+console.log(
+  JSON.stringify({ node: process.version, platform: process.platform, bundles, results }, null, 2)
+)