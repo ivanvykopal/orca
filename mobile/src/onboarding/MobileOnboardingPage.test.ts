paidaxingyo666/Manta

feat(mobile): clarify the notification opt-in screen (#20930)
* feat(mobile): clarify the notification opt-in screen

Replace the generic enable-notifications prompt with copy and a looping
banner preview that show background alerts when an agent finishes or is
waiting, even if the app is closed.

* fix(mobile): share reduced-motion hook and wait before animating

Extract the duplicated onboarding reduced-motion probe and hold the
banner loop until the OS preference is known, so Reduce Motion users
do not see the first cycle.

Mirror-Of: c33a446190bdfa21286a373e097c50a2b3e0a4d4

mobile/src/onboarding/MobileOnboardingPage.test.ts
@@ -5,8 +5,29 @@ import { MobileOnboardingPage } from './MobileOnboardingPage'
 
 vi.mock('react-native', async () => {
   const React = await import('react')
+  class AnimatedValue {
+    interpolate() {
+      return 0
+    }
+    setValue() {}
+  }
   return {
+    AccessibilityInfo: {
+      addEventListener: vi.fn(() => ({ remove: vi.fn() })),
+      isReduceMotionEnabled: vi.fn(() => Promise.resolve(true))
+    },
     ActivityIndicator: 'ActivityIndicator',
+    Animated: {
+      Value: AnimatedValue,
+      View: 'AnimatedView',
+      delay: vi.fn(() => ({ start: vi.fn(), stop: vi.fn() })),
+      loop: vi.fn(() => ({ start: vi.fn(), stop: vi.fn() })),
+      parallel: vi.fn(() => ({ start: vi.fn(), stop: vi.fn() })),
+      sequence: vi.fn(() => ({ start: vi.fn(), stop: vi.fn() })),
+      timing: vi.fn(() => ({ start: vi.fn(), stop: vi.fn() }))
+    },
+    Easing: { cubic: (t: number) => t, in: (e: unknown) => e, out: (e: unknown) => e },
+    Image: 'Image',
     Pressable: 'Pressable',
     ScrollView: ({ children, ...props }: { children?: unknown }) =>
       React.createElement('ScrollView', props, children),
@@ -17,9 +38,10 @@ vi.mock('react-native', async () => {
 })
 
 vi.mock('lucide-react-native', () => ({
-  BellRing: 'BellRing',
   MessageSquare: 'MessageSquare'
 }))
+
+vi.mock('../components/MantaLogo', () => ({ MantaLogo: 'MantaLogo' }))
 
 describe('MobileOnboardingPage', () => {
   let renderer: ReactTestRenderer | null = null
@@ -64,6 +86,14 @@ describe('MobileOnboardingPage', () => {
     )
   }
 
+  function collectText(): string {
+    return renderer!.root
+      .findAllByType('Text')
+      .map((node) => node.props.children)
+      .flat()
+      .join(' ')
+  }
+
   it('renders the session choices and sends exactly one selected view', async () => {
     const callbacks = await renderPage('session-view')
 
@@ -78,6 +108,19 @@ describe('MobileOnboardingPage', () => {
 
     expect(callbacks.onNotificationChoice).toHaveBeenCalledWith('skip')
     expect(callbacks.onSessionChoice).not.toHaveBeenCalled()
+  })
+
+  it('explains that alerts cover finished work and waiting agents, even if the app is closed', async () => {
+    await renderPage('notifications')
+    const copy = collectText()
+
+    expect(copy).toContain('Don’t miss when an agent needs you')
+    expect(copy).toContain('finishes or is waiting')
+    expect(copy).toContain('using the app')
+    expect(copy).toContain('Enable notifications')
+    expect(copy).toContain('Codex finished')
+    expect(copy).toContain('Claude needs input')
+    expect(renderer!.root.findByProps({ testID: 'notification-onboarding-preview' })).toBeTruthy()
   })
 
   it('disables both notification choices while permission is pending', async () => {

mobile/src/onboarding/MobileOnboardingPage.tsx
@@ -1,7 +1,8 @@
 import { ActivityIndicator, Pressable, ScrollView, Text, View } from 'react-native'
-import { BellRing, MessageSquare } from 'lucide-react-native'
+import { MessageSquare } from 'lucide-react-native'
 import type { MobileOnboardingStep } from './mobile-onboarding-plan'
 import { mobileOnboardingStyles as styles } from './mobile-onboarding-styles'
+import { NotificationOnboardingPreview } from './NotificationOnboardingPreview'
 import type { MobileSessionView } from '../storage/session-view-preferences'
 import { colors } from '../theme/mobile-theme'
 
@@ -38,33 +39,29 @@ export function MobileOnboardingPage({
       accessibilityElementsHidden={!active}
       importantForAccessibility={active ? 'auto' : 'no-hide-descendants'}
     >
-      <View style={styles.content}>
-        <View style={styles.iconSurface}>
-          {isSessionView ? (
+      <View style={[styles.content, !isSessionView && styles.notificationContent]}>
+        {isSessionView ? (
+          <View style={styles.iconSurface}>
             <MessageSquare size={30} color={colors.textPrimary} />
-          ) : (
-            <BellRing size={30} color={colors.textPrimary} />
-          )}
-        </View>
+          </View>
+        ) : (
+          <NotificationOnboardingPreview active={active} />
+        )}
         <Text style={styles.title}>
-          {isSessionView ? 'How should sessions open?' : 'Enable notifications'}
+          {isSessionView ? 'How should sessions open?' : 'Don’t miss when an agent needs you'}
         </Text>
         <Text style={styles.body}>
           {isSessionView
             ? 'Choose whether supported agent sessions open in the terminal or Chat UI on this device. Press and hold a session tab to switch its view, or change the default later in Settings.'
-            : 'Get notified when an agent finishes a task or needs your input.'}
+            : 'Get a notification on this phone when an agent finishes or is waiting — even if you aren’t using the app.'}
         </Text>
-        {!isSessionView ? (
-          <Text style={styles.body}>
-            By default, notifications arrive after your desktop has been idle for 3 minutes.
-          </Text>
-        ) : null}
       </View>
 
       <View style={styles.footer}>
         {!isSessionView ? (
           <Text style={styles.disclosure}>
-            Delivered through Manta’s push service. Change this anytime in Settings.
+            Delivered through Manta’s push service after your desktop has been idle for 3 minutes.
+            Change this anytime in Settings.
           </Text>
         ) : null}
         {error ? (

mobile/src/onboarding/NotificationOnboardingPreview.test.ts
@@ -1,0 +1,116 @@
+import { createElement } from 'react'
+import { act, create, type ReactTestRenderer } from 'react-test-renderer'
+import { afterEach, describe, expect, it, vi } from 'vitest'
+import { NotificationOnboardingPreview } from './NotificationOnboardingPreview'
+
+const mocks = vi.hoisted(() => {
+  const anim = () => ({ start: vi.fn(), stop: vi.fn() })
+  return {
+    reducedMotion: true,
+    reducedMotionResult: null as Promise<boolean> | null,
+    timing: vi.fn(anim),
+    loop: vi.fn(anim)
+  }
+})
+
+vi.mock('react-native', async () => {
+  const React = await import('react')
+  class AnimatedValue {
+    interpolate() {
+      return 0
+    }
+    setValue() {}
+  }
+  return {
+    AccessibilityInfo: {
+      addEventListener: vi.fn(() => ({ remove: vi.fn() })),
+      isReduceMotionEnabled: vi.fn(
+        () => mocks.reducedMotionResult ?? Promise.resolve(mocks.reducedMotion)
+      )
+    },
+    Animated: {
+      Value: AnimatedValue,
+      View: ({ children, ...props }: { children?: unknown }) =>
+        React.createElement('AnimatedView', props, children),
+      delay: vi.fn(() => ({ start: vi.fn(), stop: vi.fn() })),
+      loop: mocks.loop,
+      parallel: vi.fn(() => ({ start: vi.fn(), stop: vi.fn() })),
+      sequence: vi.fn(() => ({ start: vi.fn(), stop: vi.fn() })),
+      timing: mocks.timing
+    },
+    Easing: { cubic: (t: number) => t, in: (e: unknown) => e, out: (e: unknown) => e },
+    StyleSheet: { create: (styles: unknown) => styles },
+    Text: 'Text',
+    View: 'View'
+  }
+})
+
+vi.mock('../components/MantaLogo', () => ({ MantaLogo: 'MantaLogo' }))
+
+describe('NotificationOnboardingPreview', () => {
+  let renderer: ReactTestRenderer | null = null
+
+  afterEach(() => {
+    act(() => renderer?.unmount())
+    renderer = null
+    mocks.reducedMotion = true
+    mocks.reducedMotionResult = null
+    mocks.timing.mockClear()
+    mocks.loop.mockClear()
+    vi.restoreAllMocks()
+  })
+
+  async function renderPreview(active = true) {
+    const consoleError = vi.spyOn(console, 'error').mockImplementation((...args) => {
+      if (typeof args[0] !== 'string' || !args[0].includes('react-test-renderer is deprecated')) {
+        throw new Error(String(args[0]))
+      }
+    })
+    await act(async () => {
+      renderer = create(createElement(NotificationOnboardingPreview, { active }))
+    })
+    consoleError.mockRestore()
+  }
+
+  it('shows sample agent-done and needs-input banners as a decorative mock', async () => {
+    await renderPreview()
+    const preview = renderer!.root.findByProps({ testID: 'notification-onboarding-preview' })
+    const copy = renderer!.root
+      .findAllByType('Text')
+      .map((node) => node.props.children)
+      .flat()
+      .join(' ')
+
+    expect(preview.props.accessibilityElementsHidden).toBe(true)
+    expect(preview.props.importantForAccessibility).toBe('no-hide-descendants')
+    expect(copy).toContain('Codex finished')
+    expect(copy).toContain('Claude needs input')
+  })
+
+  it('does not animate the banners when the page is off-screen', async () => {
+    await renderPreview(false)
+    expect(mocks.loop).not.toHaveBeenCalled()
+  })
+
+  it('loops the arriving banners while the page is active', async () => {
+    mocks.reducedMotion = false
+    await renderPreview(true)
+    expect(mocks.loop).toHaveBeenCalledOnce()
+    expect(mocks.loop.mock.results[0]?.value.start).toHaveBeenCalledOnce()
+  })
+
+  it('does not start the loop until reduced-motion is known', async () => {
+    let resolvePreference: (enabled: boolean) => void = () => {}
+    mocks.reducedMotionResult = new Promise((resolve) => {
+      resolvePreference = resolve
+    })
+
+    await renderPreview(true)
+    expect(mocks.loop).not.toHaveBeenCalled()
+
+    await act(async () => {
+      resolvePreference(false)
+    })
+    expect(mocks.loop).toHaveBeenCalledOnce()
+  })
+})

mobile/src/onboarding/NotificationOnboardingPreview.tsx
@@ -1,0 +1,179 @@
+import { useEffect, useRef } from 'react'
+import { Animated, Easing, StyleSheet, Text, View } from 'react-native'
+import { MantaLogo } from '../components/MantaLogo'
+import { colors, radii, spacing, typography } from '../theme/mobile-theme'
+import { useReducedMotionEnabled } from './use-reduced-motion'
+
+const SAMPLE_NOTIFICATIONS = [
+  { title: 'Codex finished', body: 'Tests are passing.' },
+  { title: 'Claude needs input', body: 'Waiting on you.' }
+] as const
+
+const ENTER_MS = 676
+const EXIT_MS = 416
+const STAGGER_MS = 546
+const HOLD_MS = 2860
+const GAP_MS = 624
+const SLIDE_FROM_Y = -22
+
+type Props = {
+  active: boolean
+}
+
+/** Decorative banners; the surrounding copy is the accessible explanation. */
+export function NotificationOnboardingPreview({ active }: Props) {
+  const reduceMotion = useReducedMotionEnabled()
+  const first = useRef(new Animated.Value(0)).current
+  const second = useRef(new Animated.Value(0)).current
+
+  useEffect(() => {
+    if (!active || reduceMotion === null) {
+      first.setValue(0)
+      second.setValue(0)
+      return
+    }
+    if (reduceMotion) {
+      first.setValue(1)
+      second.setValue(1)
+      return
+    }
+
+    const enter = (value: Animated.Value) =>
+      Animated.timing(value, {
+        toValue: 1,
+        duration: ENTER_MS,
+        easing: Easing.out(Easing.cubic),
+        useNativeDriver: true
+      })
+    const leave = (value: Animated.Value) =>
+      Animated.timing(value, {
+        toValue: 0,
+        duration: EXIT_MS,
+        easing: Easing.in(Easing.cubic),
+        useNativeDriver: true
+      })
+    const loop = Animated.loop(
+      Animated.sequence([
+        enter(first),
+        Animated.delay(STAGGER_MS),
+        enter(second),
+        Animated.delay(HOLD_MS),
+        Animated.parallel([leave(first), leave(second)]),
+        Animated.delay(GAP_MS)
+      ])
+    )
+    loop.start()
+    return () => loop.stop()
+  }, [active, first, reduceMotion, second])
+
+  return (
+    <View
+      accessible={false}
+      accessibilityElementsHidden
+      importantForAccessibility="no-hide-descendants"
+      pointerEvents="none"
+      testID="notification-onboarding-preview"
+      style={styles.stack}
+    >
+      <Animated.View style={bannerMotion(first)}>
+        <SampleBanner notification={SAMPLE_NOTIFICATIONS[0]} />
+      </Animated.View>
+      <Animated.View style={bannerMotion(second)}>
+        <SampleBanner notification={SAMPLE_NOTIFICATIONS[1]} />
+      </Animated.View>
+    </View>
+  )
+}
+
+function SampleBanner({ notification }: { notification: (typeof SAMPLE_NOTIFICATIONS)[number] }) {
+  return (
+    <View style={styles.card}>
+      <View style={styles.appIcon}>
+        <MantaLogo size={14} />
+      </View>
+      <View style={styles.cardCopy}>
+        <View style={styles.cardMeta}>
+          <Text style={styles.appName}>Manta</Text>
+          <Text style={styles.now}>now</Text>
+        </View>
+        <Text style={styles.cardTitle} numberOfLines={1}>
+          {notification.title}
+        </Text>
+        <Text style={styles.cardBody} numberOfLines={1}>
+          {notification.body}
+        </Text>
+      </View>
+    </View>
+  )
+}
+
+function bannerMotion(progress: Animated.Value) {
+  return {
+    opacity: progress,
+    transform: [
+      {
+        translateY: progress.interpolate({
+          inputRange: [0, 1],
+          outputRange: [SLIDE_FROM_Y, 0]
+        })
+      }
+    ]
+  }
+}
+
+const styles = StyleSheet.create({
+  stack: {
+    width: '100%',
+    maxWidth: 320,
+    gap: spacing.sm,
+    marginBottom: spacing.xl + spacing.lg
+  },
+  card: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: spacing.md,
+    backgroundColor: colors.bgPanel,
+    borderColor: colors.borderSubtle,
+    borderWidth: 1,
+    borderRadius: radii.card,
+    paddingHorizontal: spacing.md,
+    paddingVertical: spacing.md
+  },
+  appIcon: {
+    width: 32,
+    height: 32,
+    borderRadius: radii.camera,
+    backgroundColor: colors.bgRaised,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  cardCopy: {
+    flex: 1,
+    minWidth: 0
+  },
+  cardMeta: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 2
+  },
+  appName: {
+    color: colors.textMuted,
+    fontSize: typography.metaSize,
+    fontWeight: '600'
+  },
+  now: {
+    color: colors.textMuted,
+    fontSize: typography.metaSize
+  },
+  cardTitle: {
+    color: colors.textPrimary,
+    fontSize: typography.bodySize,
+    fontWeight: '600'
+  },
+  cardBody: {
+    color: colors.textSecondary,
+    fontSize: typography.metaSize,
+    marginTop: 1
+  }
+})

ivanvykopal/orca

mobile/src/onboarding/MobileOnboardingPage.test.ts
@@ -5,8 +5,29 @@ import { MobileOnboardingPage } from './MobileOnboardingPage'
 
 vi.mock('react-native', async () => {
   const React = await import('react')
+  class AnimatedValue {
+    interpolate() {
+      return 0
+    }
+    setValue() {}
+  }
   return {
+    AccessibilityInfo: {
+      addEventListener: vi.fn(() => ({ remove: vi.fn() })),
+      isReduceMotionEnabled: vi.fn(() => Promise.resolve(true))
+    },
     ActivityIndicator: 'ActivityIndicator',
+    Animated: {
+      Value: AnimatedValue,
+      View: 'AnimatedView',
+      delay: vi.fn(() => ({ start: vi.fn(), stop: vi.fn() })),
+      loop: vi.fn(() => ({ start: vi.fn(), stop: vi.fn() })),
+      parallel: vi.fn(() => ({ start: vi.fn(), stop: vi.fn() })),
+      sequence: vi.fn(() => ({ start: vi.fn(), stop: vi.fn() })),
+      timing: vi.fn(() => ({ start: vi.fn(), stop: vi.fn() }))
+    },
+    Easing: { cubic: (t: number) => t, in: (e: unknown) => e, out: (e: unknown) => e },
+    Image: 'Image',
     Pressable: 'Pressable',
     ScrollView: ({ children, ...props }: { children?: unknown }) =>
       React.createElement('ScrollView', props, children),
@@ -17,9 +38,10 @@ vi.mock('react-native', async () => {
 })
 
 vi.mock('lucide-react-native', () => ({
-  BellRing: 'BellRing',
   MessageSquare: 'MessageSquare'
 }))
+
+vi.mock('../components/OrcaLogo', () => ({ OrcaLogo: 'OrcaLogo' }))
 
 describe('MobileOnboardingPage', () => {
   let renderer: ReactTestRenderer | null = null
@@ -64,6 +86,14 @@ describe('MobileOnboardingPage', () => {
     )
   }
 
+  function collectText(): string {
+    return renderer!.root
+      .findAllByType('Text')
+      .map((node) => node.props.children)
+      .flat()
+      .join(' ')
+  }
+
   it('renders the session choices and sends exactly one selected view', async () => {
     const callbacks = await renderPage('session-view')
 
@@ -78,6 +108,19 @@ describe('MobileOnboardingPage', () => {
 
     expect(callbacks.onNotificationChoice).toHaveBeenCalledWith('skip')
     expect(callbacks.onSessionChoice).not.toHaveBeenCalled()
+  })
+
+  it('explains that alerts cover finished work and waiting agents, even if the app is closed', async () => {
+    await renderPage('notifications')
+    const copy = collectText()
+
+    expect(copy).toContain('Don’t miss when an agent needs you')
+    expect(copy).toContain('finishes or is waiting')
+    expect(copy).toContain('using the app')
+    expect(copy).toContain('Enable notifications')
+    expect(copy).toContain('Codex finished')
+    expect(copy).toContain('Claude needs input')
+    expect(renderer!.root.findByProps({ testID: 'notification-onboarding-preview' })).toBeTruthy()
   })
 
   it('disables both notification choices while permission is pending', async () => {

mobile/src/onboarding/MobileOnboardingPage.tsx
@@ -1,7 +1,8 @@
 import { ActivityIndicator, Pressable, ScrollView, Text, View } from 'react-native'
-import { BellRing, MessageSquare } from 'lucide-react-native'
+import { MessageSquare } from 'lucide-react-native'
 import type { MobileOnboardingStep } from './mobile-onboarding-plan'
 import { mobileOnboardingStyles as styles } from './mobile-onboarding-styles'
+import { NotificationOnboardingPreview } from './NotificationOnboardingPreview'
 import type { MobileSessionView } from '../storage/session-view-preferences'
 import { colors } from '../theme/mobile-theme'
 
@@ -38,33 +39,29 @@ export function MobileOnboardingPage({
       accessibilityElementsHidden={!active}
       importantForAccessibility={active ? 'auto' : 'no-hide-descendants'}
     >
-      <View style={styles.content}>
-        <View style={styles.iconSurface}>
-          {isSessionView ? (
+      <View style={[styles.content, !isSessionView && styles.notificationContent]}>
+        {isSessionView ? (
+          <View style={styles.iconSurface}>
             <MessageSquare size={30} color={colors.textPrimary} />
-          ) : (
-            <BellRing size={30} color={colors.textPrimary} />
-          )}
-        </View>
+          </View>
+        ) : (
+          <NotificationOnboardingPreview active={active} />
+        )}
         <Text style={styles.title}>
-          {isSessionView ? 'How should sessions open?' : 'Enable notifications'}
+          {isSessionView ? 'How should sessions open?' : 'Don’t miss when an agent needs you'}
         </Text>
         <Text style={styles.body}>
           {isSessionView
             ? 'Choose whether supported agent sessions open in the terminal or Chat UI on this device. Press and hold a session tab to switch its view, or change the default later in Settings.'
-            : 'Get notified when an agent finishes a task or needs your input.'}
+            : 'Get a notification on this phone when an agent finishes or is waiting — even if you aren’t using the app.'}
         </Text>
-        {!isSessionView ? (
-          <Text style={styles.body}>
-            By default, notifications arrive after your desktop has been idle for 3 minutes.
-          </Text>
-        ) : null}
       </View>
 
       <View style={styles.footer}>
         {!isSessionView ? (
           <Text style={styles.disclosure}>
-            Delivered through Orca’s push service. Change this anytime in Settings.
+            Delivered through Orca’s push service after your desktop has been idle for 3 minutes.
+            Change this anytime in Settings.
           </Text>
         ) : null}
         {error ? (

mobile/src/onboarding/NotificationOnboardingPreview.test.ts
@@ -1,0 +1,116 @@
+import { createElement } from 'react'
+import { act, create, type ReactTestRenderer } from 'react-test-renderer'
+import { afterEach, describe, expect, it, vi } from 'vitest'
+import { NotificationOnboardingPreview } from './NotificationOnboardingPreview'
+
+const mocks = vi.hoisted(() => {
+  const anim = () => ({ start: vi.fn(), stop: vi.fn() })
+  return {
+    reducedMotion: true,
+    reducedMotionResult: null as Promise<boolean> | null,
+    timing: vi.fn(anim),
+    loop: vi.fn(anim)
+  }
+})
+
+vi.mock('react-native', async () => {
+  const React = await import('react')
+  class AnimatedValue {
+    interpolate() {
+      return 0
+    }
+    setValue() {}
+  }
+  return {
+    AccessibilityInfo: {
+      addEventListener: vi.fn(() => ({ remove: vi.fn() })),
+      isReduceMotionEnabled: vi.fn(
+        () => mocks.reducedMotionResult ?? Promise.resolve(mocks.reducedMotion)
+      )
+    },
+    Animated: {
+      Value: AnimatedValue,
+      View: ({ children, ...props }: { children?: unknown }) =>
+        React.createElement('AnimatedView', props, children),
+      delay: vi.fn(() => ({ start: vi.fn(), stop: vi.fn() })),
+      loop: mocks.loop,
+      parallel: vi.fn(() => ({ start: vi.fn(), stop: vi.fn() })),
+      sequence: vi.fn(() => ({ start: vi.fn(), stop: vi.fn() })),
+      timing: mocks.timing
+    },
+    Easing: { cubic: (t: number) => t, in: (e: unknown) => e, out: (e: unknown) => e },
+    StyleSheet: { create: (styles: unknown) => styles },
+    Text: 'Text',
+    View: 'View'
+  }
+})
+
+vi.mock('../components/OrcaLogo', () => ({ OrcaLogo: 'OrcaLogo' }))
+
+describe('NotificationOnboardingPreview', () => {
+  let renderer: ReactTestRenderer | null = null
+
+  afterEach(() => {
+    act(() => renderer?.unmount())
+    renderer = null
+    mocks.reducedMotion = true
+    mocks.reducedMotionResult = null
+    mocks.timing.mockClear()
+    mocks.loop.mockClear()
+    vi.restoreAllMocks()
+  })
+
+  async function renderPreview(active = true) {
+    const consoleError = vi.spyOn(console, 'error').mockImplementation((...args) => {
+      if (typeof args[0] !== 'string' || !args[0].includes('react-test-renderer is deprecated')) {
+        throw new Error(String(args[0]))
+      }
+    })
+    await act(async () => {
+      renderer = create(createElement(NotificationOnboardingPreview, { active }))
+    })
+    consoleError.mockRestore()
+  }
+
+  it('shows sample agent-done and needs-input banners as a decorative mock', async () => {
+    await renderPreview()
+    const preview = renderer!.root.findByProps({ testID: 'notification-onboarding-preview' })
+    const copy = renderer!.root
+      .findAllByType('Text')
+      .map((node) => node.props.children)
+      .flat()
+      .join(' ')
+
+    expect(preview.props.accessibilityElementsHidden).toBe(true)
+    expect(preview.props.importantForAccessibility).toBe('no-hide-descendants')
+    expect(copy).toContain('Codex finished')
+    expect(copy).toContain('Claude needs input')
+  })
+
+  it('does not animate the banners when the page is off-screen', async () => {
+    await renderPreview(false)
+    expect(mocks.loop).not.toHaveBeenCalled()
+  })
+
+  it('loops the arriving banners while the page is active', async () => {
+    mocks.reducedMotion = false
+    await renderPreview(true)
+    expect(mocks.loop).toHaveBeenCalledOnce()
+    expect(mocks.loop.mock.results[0]?.value.start).toHaveBeenCalledOnce()
+  })
+
+  it('does not start the loop until reduced-motion is known', async () => {
+    let resolvePreference: (enabled: boolean) => void = () => {}
+    mocks.reducedMotionResult = new Promise((resolve) => {
+      resolvePreference = resolve
+    })
+
+    await renderPreview(true)
+    expect(mocks.loop).not.toHaveBeenCalled()
+
+    await act(async () => {
+      resolvePreference(false)
+    })
+    expect(mocks.loop).toHaveBeenCalledOnce()
+  })
+})

mobile/src/onboarding/NotificationOnboardingPreview.tsx
@@ -1,0 +1,179 @@
+import { useEffect, useRef } from 'react'
+import { Animated, Easing, StyleSheet, Text, View } from 'react-native'
+import { OrcaLogo } from '../components/OrcaLogo'
+import { colors, radii, spacing, typography } from '../theme/mobile-theme'
+import { useReducedMotionEnabled } from './use-reduced-motion'
+
+const SAMPLE_NOTIFICATIONS = [
+  { title: 'Codex finished', body: 'Tests are passing.' },
+  { title: 'Claude needs input', body: 'Waiting on you.' }
+] as const
+
+const ENTER_MS = 676
+const EXIT_MS = 416
+const STAGGER_MS = 546
+const HOLD_MS = 2860
+const GAP_MS = 624
+const SLIDE_FROM_Y = -22
+
+type Props = {
+  active: boolean
+}
+
+/** Decorative banners; the surrounding copy is the accessible explanation. */
+export function NotificationOnboardingPreview({ active }: Props) {
+  const reduceMotion = useReducedMotionEnabled()
+  const first = useRef(new Animated.Value(0)).current
+  const second = useRef(new Animated.Value(0)).current
+
+  useEffect(() => {
+    if (!active || reduceMotion === null) {
+      first.setValue(0)
+      second.setValue(0)
+      return
+    }
+    if (reduceMotion) {
+      first.setValue(1)
+      second.setValue(1)
+      return
+    }
+
+    const enter = (value: Animated.Value) =>
+      Animated.timing(value, {
+        toValue: 1,
+        duration: ENTER_MS,
+        easing: Easing.out(Easing.cubic),
+        useNativeDriver: true
+      })
+    const leave = (value: Animated.Value) =>
+      Animated.timing(value, {
+        toValue: 0,
+        duration: EXIT_MS,
+        easing: Easing.in(Easing.cubic),
+        useNativeDriver: true
+      })
+    const loop = Animated.loop(
+      Animated.sequence([
+        enter(first),
+        Animated.delay(STAGGER_MS),
+        enter(second),
+        Animated.delay(HOLD_MS),
+        Animated.parallel([leave(first), leave(second)]),
+        Animated.delay(GAP_MS)
+      ])
+    )
+    loop.start()
+    return () => loop.stop()
+  }, [active, first, reduceMotion, second])
+
+  return (
+    <View
+      accessible={false}
+      accessibilityElementsHidden
+      importantForAccessibility="no-hide-descendants"
+      pointerEvents="none"
+      testID="notification-onboarding-preview"
+      style={styles.stack}
+    >
+      <Animated.View style={bannerMotion(first)}>
+        <SampleBanner notification={SAMPLE_NOTIFICATIONS[0]} />
+      </Animated.View>
+      <Animated.View style={bannerMotion(second)}>
+        <SampleBanner notification={SAMPLE_NOTIFICATIONS[1]} />
+      </Animated.View>
+    </View>
+  )
+}
+
+function SampleBanner({ notification }: { notification: (typeof SAMPLE_NOTIFICATIONS)[number] }) {
+  return (
+    <View style={styles.card}>
+      <View style={styles.appIcon}>
+        <OrcaLogo size={14} />
+      </View>
+      <View style={styles.cardCopy}>
+        <View style={styles.cardMeta}>
+          <Text style={styles.appName}>Orca</Text>
+          <Text style={styles.now}>now</Text>
+        </View>
+        <Text style={styles.cardTitle} numberOfLines={1}>
+          {notification.title}
+        </Text>
+        <Text style={styles.cardBody} numberOfLines={1}>
+          {notification.body}
+        </Text>
+      </View>
+    </View>
+  )
+}
+
+function bannerMotion(progress: Animated.Value) {
+  return {
+    opacity: progress,
+    transform: [
+      {
+        translateY: progress.interpolate({
+          inputRange: [0, 1],
+          outputRange: [SLIDE_FROM_Y, 0]
+        })
+      }
+    ]
+  }
+}
+
+const styles = StyleSheet.create({
+  stack: {
+    width: '100%',
+    maxWidth: 320,
+    gap: spacing.sm,
+    marginBottom: spacing.xl + spacing.lg
+  },
+  card: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: spacing.md,
+    backgroundColor: colors.bgPanel,
+    borderColor: colors.borderSubtle,
+    borderWidth: 1,
+    borderRadius: radii.card,
+    paddingHorizontal: spacing.md,
+    paddingVertical: spacing.md
+  },
+  appIcon: {
+    width: 32,
+    height: 32,
+    borderRadius: radii.camera,
+    backgroundColor: colors.bgRaised,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  cardCopy: {
+    flex: 1,
+    minWidth: 0
+  },
+  cardMeta: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 2
+  },
+  appName: {
+    color: colors.textMuted,
+    fontSize: typography.metaSize,
+    fontWeight: '600'
+  },
+  now: {
+    color: colors.textMuted,
+    fontSize: typography.metaSize
+  },
+  cardTitle: {
+    color: colors.textPrimary,
+    fontSize: typography.bodySize,
+    fontWeight: '600'
+  },
+  cardBody: {
+    color: colors.textSecondary,
+    fontSize: typography.metaSize,
+    marginTop: 1
+  }
+})