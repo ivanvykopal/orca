paidaxingyo666/Manta

feat(settings): connected computers rows for session history indexing (#20887)
* feat(session-search): add ranked history panel search and consent

* test: wait for initial session indexing before refreshing results

* feat(session-history): add local search settings and index controls

* Use shared local host identifier for session index status

* feat(session-search): merge all-computers search across hosts

The `all` scope on `aiVault:searchSessions` now fans out from the desktop
to every host the session list enumerates and merges the pages into one.
Legs run in parallel: the local index through the search service, SSH and
runtime hosts through the existing remote search client.

Two fixed orders, because relevance scores from independent indexes are
not comparable. `newest` asks every leg for recency and k-way merges on
`updatedAt`, nulls last, ties broken on execution host id. `relevance`
rotates hosts in host-id order by their own rank.

The merged cursor is an opaque base64url payload holding each host's
cursor, how many of its current page were already emitted, and the
generation that offset counts into, plus the page size and sort the
cursor belongs to. A host whose index moved is fenced to `stale` and
stops contributing; the rest keep paging. Per-host outcomes ride back on
one new optional `hosts` field on the results response.

`aiVault:searchStatus` with `all` stays refused, and neither the runtime
RPC nor the CLI gains the scope, so a fan-out is never two hops.

* fix(preload): let the search bridge address the all-computers scope

* feat(settings): live index status, enable confirm, advanced delete

* feat(session-search): search every computer from the history panel

The panel's "All computers" scope produced no request: the hook parsed the
scope into a single host id and stopped when that was null, so the panel
answered "Choose one computer to search its sessions." The desktop already
merges every enumerated host behind `aiVault:searchSessions`, so 

src/renderer/src/components/settings/SessionHistoryComputerRow.tsx
@@ -1,0 +1,90 @@
+import { Laptop, Server } from 'lucide-react'
+import { Button } from '@/components/ui/button'
+import { cn } from '@/lib/utils'
+import { translate } from '@/i18n/i18n'
+import { SettingsSwitch } from './SettingsFormControls'
+
+const NO_DETAILS: readonly string[] = []
+
+export type SessionHistoryComputerRowProps = {
+  kind: 'local' | 'server'
+  name: string
+  /** Paired servers only; a host that never answered has no version to claim. */
+  version?: string | null
+  /** Empty when this client cannot read the host's index at all, as in the browser. */
+  status?: string
+  details?: readonly string[]
+  /** An offline or too-old computer is still listed, just visibly out of play. */
+  dimmed?: boolean
+  checked: boolean
+  disabled?: boolean
+  onToggle: () => void
+  action?: { label: string; onClick: () => void } | null
+}
+
+export function SessionHistoryComputerRow({
+  kind,
+  name,
+  version,
+  status,
+  details = NO_DETAILS,
+  dimmed = false,
+  checked,
+  disabled = false,
+  onToggle,
+  action = null
+}: SessionHistoryComputerRowProps): React.JSX.Element {
+  const Icon = kind === 'local' ? Laptop : Server
+  return (
+    <div
+      className={cn('flex items-center gap-3 border-t border-border py-3', dimmed && 'opacity-60')}
+    >
+      <Icon className="size-4 shrink-0 text-muted-foreground" />
+      <div className="min-w-0 flex-1 space-y-1">
+        <div className="flex min-w-0 items-baseline gap-2">
+          <span className="truncate text-sm font-medium">{name}</span>
+          {version ? (
+            <span className="shrink-0 text-[11px] text-muted-foreground">
+              {translate('sessionHistory.settings.serverVersion', 'Manta v{{version}}', { version })}
+            </span>
+          ) : null}
+        </div>
+        {status ? (
+          <p role="status" className="text-xs text-muted-foreground">
+            {status}
+            {action ? (
+              <Button
+                type="button"
+                variant="link"
+                size="xs"
+                className="ml-1 h-auto align-baseline"
+                onClick={action.onClick}
+              >
+                {action.label}
+              </Button>
+            ) : null}
+          </p>
+        ) : null}
+        {details.map((line) => (
+          <p key={line} className="text-xs text-muted-foreground">
+            {line}
+          </p>
+        ))}
+      </div>
+      <div className="shrink-0">
+        <SettingsSwitch
+          checked={checked}
+          disabled={disabled}
+          onChange={onToggle}
+          ariaLabel={translate(
+            'sessionHistory.settings.rowSwitchLabel',
+            'Search sessions on {{host}}',
+            {
+              host: name
+            }
+          )}
+        />
+      </div>
+    </div>
+  )
+}

src/renderer/src/components/settings/SessionHistoryServerRow.tsx
@@ -1,0 +1,180 @@
+import { useEffect, useState } from 'react'
+import type { PublicKnownRuntimeEnvironment } from '../../../../shared/runtime-environments'
+import { toRuntimeExecutionHostId } from '../../../../shared/execution-host'
+import { useMountedRef } from '@/hooks/useMountedRef'
+import { translate } from '@/i18n/i18n'
+import { useAppStore } from '@/store'
+import {
+  getRuntimeServerConnectionState,
+  isRuntimeServerTransportConnected,
+  type RuntimeHostDetails
+} from './runtime-environment-host-details'
+import { SessionHistoryComputerRow } from './SessionHistoryComputerRow'
+import type { SessionSearchComputerState } from './session-search-computer-rollup'
+import {
+  isHostTooOldError,
+  sessionSearchCheckingMessage,
+  sessionSearchReadErrorMessage,
+  sessionSearchStatusDetails,
+  sessionSearchStatusMessage
+} from './session-history-status-copy'
+import { useSessionSearchStatus } from './use-session-search-status'
+
+export function SessionHistoryServerRow({
+  environment,
+  details,
+  refresh = 0,
+  onError,
+  onStateChange
+}: {
+  environment: PublicKnownRuntimeEnvironment
+  details: RuntimeHostDetails | undefined
+  /** Bumped by the pane after it changes this host from outside the row. */
+  refresh?: number
+  onError: (message: string | null) => void
+  /** Lets the pane count and order computers it does not itself poll. */
+  onStateChange?: (environmentId: string, state: SessionSearchComputerState) => void
+}): React.JSX.Element {
+  const hostId = toRuntimeExecutionHostId(environment.id)
+  const mounted = useMountedRef()
+  const openSettingsPage = useAppStore((state) => state.openSettingsPage)
+  const openSettingsTarget = useAppStore((state) => state.openSettingsTarget)
+  const [tooOldOnSet, setTooOldOnSet] = useState(false)
+  const [busy, setBusy] = useState(false)
+  const connectionState = getRuntimeServerConnectionState(details)
+  const connected = isRuntimeServerTransportConnected(connectionState)
+  const { status, failed, hostTooOld, adopt } = useSessionSearchStatus({
+    executionHostId: hostId,
+    active: connected && !tooOldOnSet,
+    refresh
+  })
+  // A status read or a set call can each prove the server predates session search.
+  const tooOld = tooOldOnSet || hostTooOld
+  const enabled = status?.enabled === true
+  const state = resolveServerState({
+    tooOld,
+    connected,
+    checking: connectionState === 'checking',
+    enabled,
+    answered: Boolean(status)
+  })
+
+  useEffect(() => {
+    onStateChange?.(environment.id, state)
+  }, [environment.id, onStateChange, state])
+
+  async function setEnabled(next: boolean): Promise<void> {
+    setBusy(true)
+    onError(null)
+    try {
+      adopt(await window.api.aiVault.setSearchEnabled(hostId, next))
+    } catch (error) {
+      if (!mounted.current) {
+        return
+      }
+      if (isHostTooOldError(error)) {
+        setTooOldOnSet(true)
+        return
+      }
+      onError(
+        translate(
+          'sessionHistory.settings.serverToggleError',
+          'Could not change session search on {{host}}. Try again.',
+          { host: environment.name }
+        )
+      )
+    } finally {
+      if (mounted.current) {
+        setBusy(false)
+      }
+    }
+  }
+
+  function toggle(): Promise<void> {
+    return setEnabled(!enabled)
+  }
+
+  function openServerSettings(): void {
+    openSettingsPage()
+    openSettingsTarget({ pane: 'servers', repoId: null, sectionId: environment.id })
+  }
+
+  const row = {
+    kind: 'server' as const,
+    name: environment.name,
+    version: details?.runtimeStatus?.appVersion ?? null,
+    onToggle: () => void toggle()
+  }
+  if (tooOld) {
+    return (
+      <SessionHistoryComputerRow
+        {...row}
+        dimmed
+        checked={false}
+        disabled
+        status={translate('sessionHistory.settings.serverTooOld', 'Needs a newer version of Manta.')}
+        action={{
+          label: translate('sessionHistory.settings.updateServer', 'Update server'),
+          onClick: openServerSettings
+        }}
+      />
+    )
+  }
+  if (!connected) {
+    // Checking is not yet evidence of an unreachable host, so it does not claim the index was left behind.
+    const checking = connectionState === 'checking'
+    return (
+      <SessionHistoryComputerRow
+        {...row}
+        dimmed={!checking}
+        checked={enabled}
+        disabled
+        status={
+          checking
+            ? sessionSearchCheckingMessage()
+            : translate('sessionHistory.settings.serverOffline', 'Offline')
+        }
+      />
+    )
+  }
+  // An off computer says so with its switch; a sentence repeating it is noise.
+  let statusText: string | undefined = sessionSearchCheckingMessage()
+  if (failed) {
+    statusText = sessionSearchReadErrorMessage()
+  } else if (status) {
+    statusText = enabled ? sessionSearchStatusMessage(status) : undefined
+  }
+  return (
+    <SessionHistoryComputerRow
+      {...row}
+      checked={enabled}
+      disabled={busy}
+      {...(statusText === undefined ? {} : { status: statusText })}
+      details={sessionSearchStatusDetails(status)}
+    />
+  )
+}
+
+/** What the pane needs to count and order this row, from what the row already knows. */
+function resolveServerState(args: {
+  tooOld: boolean
+  connected: boolean
+  checking: boolean
+  enabled: boolean
+  answered: boolean
+}): SessionSearchComputerState {
+  if (args.tooOld) {
+    return 'needs-update'
+  }
+  // A probe still in flight is not evidence of an unreachable host.
+  if (args.checking) {
+    return 'checking'
+  }
+  if (!args.connected) {
+    return 'offline'
+  }
+  if (!args.answered) {
+    return 'checking'
+  }
+  return args.enabled ? 'on' : 'off'
+}

src/renderer/src/components/settings/SessionSearchComputerList.tsx
@@ -1,0 +1,69 @@
+import { useState } from 'react'
+import { ChevronDown, ChevronUp } from 'lucide-react'
+import { Button } from '@/components/ui/button'
+import { translate } from '@/i18n/i18n'
+
+/** Local computer plus five servers. Past that the list stops being a list and becomes a wall. */
+const VISIBLE_COMPUTER_LIMIT = 6
+
+export type SessionSearchServerRowEntry = { id: string; node: React.ReactNode }
+
+/**
+ * Presentation only: the caller has already decided which servers exist and in
+ * what order, so the list owns nothing but the two subheads and the fold.
+ */
+export function SessionSearchComputerList({
+  local,
+  servers
+}: {
+  local: React.ReactNode
+  servers: readonly SessionSearchServerRowEntry[]
+}): React.JSX.Element {
+  const [expanded, setExpanded] = useState(false)
+  const visibleServerCount = VISIBLE_COMPUTER_LIMIT - 1
+  const hiddenCount = Math.max(0, servers.length - visibleServerCount)
+  const shownServers = expanded ? servers : servers.slice(0, visibleServerCount)
+  // One row needs no heading to tell it apart from the rest; the pair of subheads appears together or not at all.
+  const showSubheads = servers.length > 0
+  return (
+    <div>
+      {showSubheads ? (
+        <p className="pt-4 text-[11px] font-semibold uppercase tracking-[0.05em] text-muted-foreground">
+          {translate('sessionHistory.settings.thisComputer', 'This computer')}
+        </p>
+      ) : null}
+      {local}
+      {showSubheads ? (
+        <p className="pt-4 text-[11px] font-semibold uppercase tracking-[0.05em] text-muted-foreground">
+          {translate('sessionHistory.settings.remoteServers', 'Manta remote servers')}
+        </p>
+      ) : null}
+      {shownServers.map((server) => (
+        <div key={server.id}>{server.node}</div>
+      ))}
+      {hiddenCount > 0 ? (
+        <Button
+          type="button"
+          variant="ghost"
+          size="sm"
+          className="w-full"
+          onClick={() => setExpanded((value) => !value)}
+        >
+          {expanded ? (
+            <>
+              {translate('sessionHistory.settings.showFewer', 'Show fewer')}
+              <ChevronUp />
+            </>
+          ) : (
+            <>
+              {translate('sessionHistory.settings.showMore', 'Show {{count}} more', {
+                count: hiddenCount
+              })}
+              <ChevronDown />
+            </>
+          )}
+        </Button>
+      ) : null}
+    </div>
+  )
+}

src/renderer/src/components/settings/settings-setup-workflow-section-renderers.tsx
@@ -185,16 +185,15 @@ export function renderSessionHistorySettingsSection(
   return (
     <SettingsSection
       id="session-history"
-      title={translate('sessionHistory.settings.title', 'Agent Session History')}
+      title={translate('sessionHistory.settings.title', 'Agent Session Search')}
       description={translate(
         'sessionHistory.settings.description',
-        'Manage session search on this computer. These settings do not enable indexing on SSH or paired hosts.'
+        'Search everything your agents have said and done, on this computer and on any paired Manta server.'
       )}
       searchEntries={navigation.getSectionSearchEntries('session-history')}
     >
       {view.isSectionMounted('session-history') ? (
         <SessionHistorySettingsPane
-          key={JSON.stringify(model.settings.aiVaultSearch)}
           settings={model.settings}
           updateSettings={model.updateSettingsOrThrow}
         />

src/renderer/src/hooks/settings-navigation-workflow-sections.ts
@@ -71,18 +71,18 @@ export function buildWorkflowSettingsSections(
     },
     {
       id: 'session-history',
-      title: translate('sessionHistory.settings.title', 'Agent Session History'),
+      title: translate('sessionHistory.settings.title', 'Agent Session Search'),
       description: translate(
         'sessionHistory.settings.description',
-        'Manage session search on this computer. These settings do not enable indexing on SSH or paired hosts.'
+        'Search everything your agents have said and done, on this computer and on any paired Manta server.'
       ),
       icon: History,
       searchEntries: [
         {
-          title: translate('sessionHistory.settings.enable', 'Enable session history search'),
+          title: translate('sessionHistory.settings.indexComputers', 'Search inside sessions'),
           description: translate(
             'sessionHistory.settings.searchDescription',
-            'Transcript indexing, index status and delete index.'
+            'Turn on session search for this computer and paired servers, or clear search data.'
           )
         }
       ],

ivanvykopal/orca

src/renderer/src/components/settings/SessionHistoryComputerRow.tsx
@@ -1,0 +1,90 @@
+import { Laptop, Server } from 'lucide-react'
+import { Button } from '@/components/ui/button'
+import { cn } from '@/lib/utils'
+import { translate } from '@/i18n/i18n'
+import { SettingsSwitch } from './SettingsFormControls'
+
+const NO_DETAILS: readonly string[] = []
+
+export type SessionHistoryComputerRowProps = {
+  kind: 'local' | 'server'
+  name: string
+  /** Paired servers only; a host that never answered has no version to claim. */
+  version?: string | null
+  /** Empty when this client cannot read the host's index at all, as in the browser. */
+  status?: string
+  details?: readonly string[]
+  /** An offline or too-old computer is still listed, just visibly out of play. */
+  dimmed?: boolean
+  checked: boolean
+  disabled?: boolean
+  onToggle: () => void
+  action?: { label: string; onClick: () => void } | null
+}
+
+export function SessionHistoryComputerRow({
+  kind,
+  name,
+  version,
+  status,
+  details = NO_DETAILS,
+  dimmed = false,
+  checked,
+  disabled = false,
+  onToggle,
+  action = null
+}: SessionHistoryComputerRowProps): React.JSX.Element {
+  const Icon = kind === 'local' ? Laptop : Server
+  return (
+    <div
+      className={cn('flex items-center gap-3 border-t border-border py-3', dimmed && 'opacity-60')}
+    >
+      <Icon className="size-4 shrink-0 text-muted-foreground" />
+      <div className="min-w-0 flex-1 space-y-1">
+        <div className="flex min-w-0 items-baseline gap-2">
+          <span className="truncate text-sm font-medium">{name}</span>
+          {version ? (
+            <span className="shrink-0 text-[11px] text-muted-foreground">
+              {translate('sessionHistory.settings.serverVersion', 'Orca v{{version}}', { version })}
+            </span>
+          ) : null}
+        </div>
+        {status ? (
+          <p role="status" className="text-xs text-muted-foreground">
+            {status}
+            {action ? (
+              <Button
+                type="button"
+                variant="link"
+                size="xs"
+                className="ml-1 h-auto align-baseline"
+                onClick={action.onClick}
+              >
+                {action.label}
+              </Button>
+            ) : null}
+          </p>
+        ) : null}
+        {details.map((line) => (
+          <p key={line} className="text-xs text-muted-foreground">
+            {line}
+          </p>
+        ))}
+      </div>
+      <div className="shrink-0">
+        <SettingsSwitch
+          checked={checked}
+          disabled={disabled}
+          onChange={onToggle}
+          ariaLabel={translate(
+            'sessionHistory.settings.rowSwitchLabel',
+            'Search sessions on {{host}}',
+            {
+              host: name
+            }
+          )}
+        />
+      </div>
+    </div>
+  )
+}

src/renderer/src/components/settings/SessionHistoryServerRow.tsx
@@ -1,0 +1,180 @@
+import { useEffect, useState } from 'react'
+import type { PublicKnownRuntimeEnvironment } from '../../../../shared/runtime-environments'
+import { toRuntimeExecutionHostId } from '../../../../shared/execution-host'
+import { useMountedRef } from '@/hooks/useMountedRef'
+import { translate } from '@/i18n/i18n'
+import { useAppStore } from '@/store'
+import {
+  getRuntimeServerConnectionState,
+  isRuntimeServerTransportConnected,
+  type RuntimeHostDetails
+} from './runtime-environment-host-details'
+import { SessionHistoryComputerRow } from './SessionHistoryComputerRow'
+import type { SessionSearchComputerState } from './session-search-computer-rollup'
+import {
+  isHostTooOldError,
+  sessionSearchCheckingMessage,
+  sessionSearchReadErrorMessage,
+  sessionSearchStatusDetails,
+  sessionSearchStatusMessage
+} from './session-history-status-copy'
+import { useSessionSearchStatus } from './use-session-search-status'
+
+export function SessionHistoryServerRow({
+  environment,
+  details,
+  refresh = 0,
+  onError,
+  onStateChange
+}: {
+  environment: PublicKnownRuntimeEnvironment
+  details: RuntimeHostDetails | undefined
+  /** Bumped by the pane after it changes this host from outside the row. */
+  refresh?: number
+  onError: (message: string | null) => void
+  /** Lets the pane count and order computers it does not itself poll. */
+  onStateChange?: (environmentId: string, state: SessionSearchComputerState) => void
+}): React.JSX.Element {
+  const hostId = toRuntimeExecutionHostId(environment.id)
+  const mounted = useMountedRef()
+  const openSettingsPage = useAppStore((state) => state.openSettingsPage)
+  const openSettingsTarget = useAppStore((state) => state.openSettingsTarget)
+  const [tooOldOnSet, setTooOldOnSet] = useState(false)
+  const [busy, setBusy] = useState(false)
+  const connectionState = getRuntimeServerConnectionState(details)
+  const connected = isRuntimeServerTransportConnected(connectionState)
+  const { status, failed, hostTooOld, adopt } = useSessionSearchStatus({
+    executionHostId: hostId,
+    active: connected && !tooOldOnSet,
+    refresh
+  })
+  // A status read or a set call can each prove the server predates session search.
+  const tooOld = tooOldOnSet || hostTooOld
+  const enabled = status?.enabled === true
+  const state = resolveServerState({
+    tooOld,
+    connected,
+    checking: connectionState === 'checking',
+    enabled,
+    answered: Boolean(status)
+  })
+
+  useEffect(() => {
+    onStateChange?.(environment.id, state)
+  }, [environment.id, onStateChange, state])
+
+  async function setEnabled(next: boolean): Promise<void> {
+    setBusy(true)
+    onError(null)
+    try {
+      adopt(await window.api.aiVault.setSearchEnabled(hostId, next))
+    } catch (error) {
+      if (!mounted.current) {
+        return
+      }
+      if (isHostTooOldError(error)) {
+        setTooOldOnSet(true)
+        return
+      }
+      onError(
+        translate(
+          'sessionHistory.settings.serverToggleError',
+          'Could not change session search on {{host}}. Try again.',
+          { host: environment.name }
+        )
+      )
+    } finally {
+      if (mounted.current) {
+        setBusy(false)
+      }
+    }
+  }
+
+  function toggle(): Promise<void> {
+    return setEnabled(!enabled)
+  }
+
+  function openServerSettings(): void {
+    openSettingsPage()
+    openSettingsTarget({ pane: 'servers', repoId: null, sectionId: environment.id })
+  }
+
+  const row = {
+    kind: 'server' as const,
+    name: environment.name,
+    version: details?.runtimeStatus?.appVersion ?? null,
+    onToggle: () => void toggle()
+  }
+  if (tooOld) {
+    return (
+      <SessionHistoryComputerRow
+        {...row}
+        dimmed
+        checked={false}
+        disabled
+        status={translate('sessionHistory.settings.serverTooOld', 'Needs a newer version of Orca.')}
+        action={{
+          label: translate('sessionHistory.settings.updateServer', 'Update server'),
+          onClick: openServerSettings
+        }}
+      />
+    )
+  }
+  if (!connected) {
+    // Checking is not yet evidence of an unreachable host, so it does not claim the index was left behind.
+    const checking = connectionState === 'checking'
+    return (
+      <SessionHistoryComputerRow
+        {...row}
+        dimmed={!checking}
+        checked={enabled}
+        disabled
+        status={
+          checking
+            ? sessionSearchCheckingMessage()
+            : translate('sessionHistory.settings.serverOffline', 'Offline')
+        }
+      />
+    )
+  }
+  // An off computer says so with its switch; a sentence repeating it is noise.
+  let statusText: string | undefined = sessionSearchCheckingMessage()
+  if (failed) {
+    statusText = sessionSearchReadErrorMessage()
+  } else if (status) {
+    statusText = enabled ? sessionSearchStatusMessage(status) : undefined
+  }
+  return (
+    <SessionHistoryComputerRow
+      {...row}
+      checked={enabled}
+      disabled={busy}
+      {...(statusText === undefined ? {} : { status: statusText })}
+      details={sessionSearchStatusDetails(status)}
+    />
+  )
+}
+
+/** What the pane needs to count and order this row, from what the row already knows. */
+function resolveServerState(args: {
+  tooOld: boolean
+  connected: boolean
+  checking: boolean
+  enabled: boolean
+  answered: boolean
+}): SessionSearchComputerState {
+  if (args.tooOld) {
+    return 'needs-update'
+  }
+  // A probe still in flight is not evidence of an unreachable host.
+  if (args.checking) {
+    return 'checking'
+  }
+  if (!args.connected) {
+    return 'offline'
+  }
+  if (!args.answered) {
+    return 'checking'
+  }
+  return args.enabled ? 'on' : 'off'
+}

src/renderer/src/components/settings/SessionSearchComputerList.tsx
@@ -1,0 +1,69 @@
+import { useState } from 'react'
+import { ChevronDown, ChevronUp } from 'lucide-react'
+import { Button } from '@/components/ui/button'
+import { translate } from '@/i18n/i18n'
+
+/** Local computer plus five servers. Past that the list stops being a list and becomes a wall. */
+const VISIBLE_COMPUTER_LIMIT = 6
+
+export type SessionSearchServerRowEntry = { id: string; node: React.ReactNode }
+
+/**
+ * Presentation only: the caller has already decided which servers exist and in
+ * what order, so the list owns nothing but the two subheads and the fold.
+ */
+export function SessionSearchComputerList({
+  local,
+  servers
+}: {
+  local: React.ReactNode
+  servers: readonly SessionSearchServerRowEntry[]
+}): React.JSX.Element {
+  const [expanded, setExpanded] = useState(false)
+  const visibleServerCount = VISIBLE_COMPUTER_LIMIT - 1
+  const hiddenCount = Math.max(0, servers.length - visibleServerCount)
+  const shownServers = expanded ? servers : servers.slice(0, visibleServerCount)
+  // One row needs no heading to tell it apart from the rest; the pair of subheads appears together or not at all.
+  const showSubheads = servers.length > 0
+  return (
+    <div>
+      {showSubheads ? (
+        <p className="pt-4 text-[11px] font-semibold uppercase tracking-[0.05em] text-muted-foreground">
+          {translate('sessionHistory.settings.thisComputer', 'This computer')}
+        </p>
+      ) : null}
+      {local}
+      {showSubheads ? (
+        <p className="pt-4 text-[11px] font-semibold uppercase tracking-[0.05em] text-muted-foreground">
+          {translate('sessionHistory.settings.remoteServers', 'Orca remote servers')}
+        </p>
+      ) : null}
+      {shownServers.map((server) => (
+        <div key={server.id}>{server.node}</div>
+      ))}
+      {hiddenCount > 0 ? (
+        <Button
+          type="button"
+          variant="ghost"
+          size="sm"
+          className="w-full"
+          onClick={() => setExpanded((value) => !value)}
+        >
+          {expanded ? (
+            <>
+              {translate('sessionHistory.settings.showFewer', 'Show fewer')}
+              <ChevronUp />
+            </>
+          ) : (
+            <>
+              {translate('sessionHistory.settings.showMore', 'Show {{count}} more', {
+                count: hiddenCount
+              })}
+              <ChevronDown />
+            </>
+          )}
+        </Button>
+      ) : null}
+    </div>
+  )
+}

src/renderer/src/components/settings/settings-setup-workflow-section-renderers.tsx
@@ -185,16 +185,15 @@ export function renderSessionHistorySettingsSection(
   return (
     <SettingsSection
       id="session-history"
-      title={translate('sessionHistory.settings.title', 'Agent Session History')}
+      title={translate('sessionHistory.settings.title', 'Agent Session Search')}
       description={translate(
         'sessionHistory.settings.description',
-        'Manage session search on this computer. These settings do not enable indexing on SSH or paired hosts.'
+        'Search everything your agents have said and done, on this computer and on any paired Orca server.'
       )}
       searchEntries={navigation.getSectionSearchEntries('session-history')}
     >
       {view.isSectionMounted('session-history') ? (
         <SessionHistorySettingsPane
-          key={JSON.stringify(model.settings.aiVaultSearch)}
           settings={model.settings}
           updateSettings={model.updateSettingsOrThrow}
         />

src/renderer/src/hooks/settings-navigation-workflow-sections.ts
@@ -71,18 +71,18 @@ export function buildWorkflowSettingsSections(
     },
     {
       id: 'session-history',
-      title: translate('sessionHistory.settings.title', 'Agent Session History'),
+      title: translate('sessionHistory.settings.title', 'Agent Session Search'),
       description: translate(
         'sessionHistory.settings.description',
-        'Manage session search on this computer. These settings do not enable indexing on SSH or paired hosts.'
+        'Search everything your agents have said and done, on this computer and on any paired Orca server.'
       ),
       icon: History,
       searchEntries: [
         {
-          title: translate('sessionHistory.settings.enable', 'Enable session history search'),
+          title: translate('sessionHistory.settings.indexComputers', 'Search inside sessions'),
           description: translate(
             'sessionHistory.settings.searchDescription',
-            'Transcript indexing, index status and delete index.'
+            'Turn on session search for this computer and paired servers, or clear search data.'
           )
         }
       ],