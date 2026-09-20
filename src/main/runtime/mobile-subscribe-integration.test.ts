paidaxingyo666/Manta

fix(lint): enable anti-slop/no-reflect-get (#20786)
`anti-slop/no-reflect-get` rejects every call to `Reflect.get`. The
reflective read bypasses ordinary property access and throws away the
type evidence the compiler would otherwise give you: the result is
`any`/`unknown` with no narrowing, so a typo in the key or a shape drift
in the source object is invisible until runtime. The rule's remedy is to
parse dynamic input into a named domain type (or narrow it with `in`)
and then read the field normally.

Baseline: 86 violations across 67 files. Now zero unsuppressed
violations under
`npx oxlint --config config/oxlint-anti-slop.json --ignore-pattern 'config/oxlint-plugins/anti-slop/**' src config tests mobile`.

Fix pattern
-----------
44 of the 86 were rewritten. The dominant shape was an `unknown` value
read through `Reflect.get` right after a `typeof === 'object'` guard;
those became `in`-narrowed property access, which TypeScript checks:

  - Reflect.get(value, 'agents')
  + 'agents' in value ? value.agents : null

Two further shapes:
- `Reflect.get(Object(x), 'k')` on a possibly-primitive envelope became a
  small named reader that boxes once and indexes a
  `Record<string, unknown>` (`settingsField` in
  mobile/src/transport/settings-read-operations.ts).
- Tests reaching into private state moved to TypeScript's checked
  bracket-index escape hatch (`runtime['layoutQueues']`), or to a
  documented read-only accessor on the owning class
  (`SearchSubprocessLineAccumulator.retainedCapacityBytes()`,
  `CodexSubagentExecutions.retentionSizes()`).

No type assertion was added anywhere: the diff contains zero net-new
`as` casts, `as any`, `as unknown as`, `@ts-ignore`, or
`@ts-expect-error`, so nothing was laundered into the sibling
assertion rules.

Suppressions
------------
42x `// oxlint-disable-next-line anti-slop/no-reflect-get` across 38
files. Every one is the default-forward branch of a `Proxy` `get` trap:

    get(target, property, receiver) {
      ...
      r

src/main/runtime/mobile-subscribe-integration.test.ts
@@ -87,8 +87,24 @@ const store = {
   }
 }
 
+/** Reclaim clears protected retention maps that no public reader exposes. */
+class ObservableRuntime extends MantaRuntimeService {
+  get restoreTimers(): typeof this.pendingRestoreTimers {
+    return this.pendingRestoreTimers
+  }
+  get softLeavers(): typeof this.pendingSoftLeavers {
+    return this.pendingSoftLeavers
+  }
+  get fitOverrides(): typeof this.terminalFitOverrides {
+    return this.terminalFitOverrides
+  }
+  get drivers(): typeof this.terminalDrivers {
+    return this.terminalDrivers
+  }
+}
+
 function createRuntime() {
-  const runtime = new MantaRuntimeService(store)
+  const runtime = new ObservableRuntime(store)
   const ptySizes = new Map<string, { cols: number; rows: number }>()
   ptySizes.set('pty-1', { cols: 150, rows: 40 })
   ptySizes.set('pty-2', { cols: 120, rows: 35 })
@@ -865,9 +881,9 @@ describe('mobile subscribe integration', () => {
       runtime.handleMobileUnsubscribe('pty-1', 'client-a')
       await runtime.handleMobileSubscribe('pty-1', 'client-b', { cols: 40, rows: 18 })
 
-      const pendingRestore = Reflect.get(runtime, 'pendingRestoreTimers') as Map<string, unknown>
+      const pendingRestore = runtime.restoreTimers
       pendingRestore.set('pty-1', { timer: setTimeout(() => {}, 60_000), clientId: 'client-b' })
-      const pendingSoft = Reflect.get(runtime, 'pendingSoftLeavers') as Map<string, unknown>
+      const pendingSoft = runtime.softLeavers
       expect(pendingSoft.has('pty-1')).toBe(true)
       await runtime.reclaimTerminalForDesktop('pty-1')
       expect(pendingRestore.has('pty-1')).toBe(false)
@@ -878,10 +894,10 @@ describe('mobile subscribe integration', () => {
       const { runtime } = createRuntime()
       await runtime.handleMobileSubscribe('pty-1', 'client-a', { cols: 45, rows: 20 })
       runtime.handleMobileUnsubscribe('pty-1', 'client-a')
-      ;(Reflect.get(runtime, 'terminalFitOverrides') as Map<string, unknown>).delete('pty-1')
+      runtime.fitOverrides.delete('pty-1')
 
-      const pendingRestore = Reflect.get(runtime, 'pendingRestoreTimers') as Map<string, unknown>
-      const pendingSoft = Reflect.get(runtime, 'pendingSoftLeavers') as Map<string, unknown>
+      const pendingRestore = runtime.restoreTimers
+      const pendingSoft = runtime.softLeavers
       await runtime.reclaimTerminalForDesktop('pty-1')
       expect(pendingRestore.has('pty-1')).toBe(false)
       expect(pendingSoft.has('pty-1')).toBe(false)
@@ -891,15 +907,11 @@ describe('mobile subscribe integration', () => {
       const { runtime } = createRuntime()
       await runtime.handleMobileSubscribe('pty-1', 'client-a', { cols: 45, rows: 20 })
       runtime.handleMobileUnsubscribe('pty-1', 'client-a')
-      ;(Reflect.get(runtime, 'terminalFitOverrides') as Map<string, unknown>).delete('pty-1')
-      ;(
-        Reflect.get(runtime, 'terminalDrivers') as {
-          set: (ptyId: string, driver: { kind: 'idle' }) => void
-        }
-      ).set('pty-1', { kind: 'idle' })
+      runtime.fitOverrides.delete('pty-1')
+      runtime.drivers.set('pty-1', { kind: 'idle' })
 
-      const pendingRestore = Reflect.get(runtime, 'pendingRestoreTimers') as Map<string, unknown>
-      const pendingSoft = Reflect.get(runtime, 'pendingSoftLeavers') as Map<string, unknown>
+      const pendingRestore = runtime.restoreTimers
+      const pendingSoft = runtime.softLeavers
       expect(await runtime.reclaimTerminalForDesktop('pty-1')).toBe(false)
       expect(pendingRestore.has('pty-1')).toBe(false)
       expect(pendingSoft.has('pty-1')).toBe(false)

tests/e2e/linear-url-workspace-entry.spec.ts
@@ -25,6 +25,14 @@ const LINEAR_ISSUE: LinearIssue = {
   updatedAt: '2026-08-12T00:00:00.000Z'
 }
 
+declare global {
+  // oxlint-disable-next-line typescript-eslint/consistent-type-definitions -- declaration merging requires interface
+  interface Window {
+    // Set by the fixture below while a Linear lookup is deliberately held open.
+    __mantaTestReleaseLinearLookup?: () => void
+  }
+}
+
 function pasteChord(): string {
   return process.platform === 'darwin' ? 'Meta+V' : 'Control+V'
 }
@@ -86,7 +94,7 @@ async function installLinearFixture(
 
 async function releaseHeldLinearLookup(page: Page): Promise<void> {
   await page.evaluate(() => {
-    const release = Reflect.get(window, '__mantaTestReleaseLinearLookup')
+    const release = window.__mantaTestReleaseLinearLookup
     if (typeof release !== 'function') {
       throw new Error('Linear lookup is not held')
     }
@@ -131,9 +139,7 @@ test.describe('Linear URL workspace entry', () => {
     await pasteLinearUrl(mantaPage, input)
     await expect
       .poll(() =>
-        mantaPage.evaluate(
-          () => typeof Reflect.get(window, '__mantaTestReleaseLinearLookup') === 'function'
-        )
+        mantaPage.evaluate(() => typeof window.__mantaTestReleaseLinearLookup === 'function')
       )
       .toBe(true)
     await input.press('Enter')

ivanvykopal/orca

src/main/runtime/mobile-subscribe-integration.test.ts
@@ -87,8 +87,24 @@ const store = {
   }
 }
 
+/** Reclaim clears protected retention maps that no public reader exposes. */
+class ObservableRuntime extends OrcaRuntimeService {
+  get restoreTimers(): typeof this.pendingRestoreTimers {
+    return this.pendingRestoreTimers
+  }
+  get softLeavers(): typeof this.pendingSoftLeavers {
+    return this.pendingSoftLeavers
+  }
+  get fitOverrides(): typeof this.terminalFitOverrides {
+    return this.terminalFitOverrides
+  }
+  get drivers(): typeof this.terminalDrivers {
+    return this.terminalDrivers
+  }
+}
+
 function createRuntime() {
-  const runtime = new OrcaRuntimeService(store)
+  const runtime = new ObservableRuntime(store)
   const ptySizes = new Map<string, { cols: number; rows: number }>()
   ptySizes.set('pty-1', { cols: 150, rows: 40 })
   ptySizes.set('pty-2', { cols: 120, rows: 35 })
@@ -865,9 +881,9 @@ describe('mobile subscribe integration', () => {
       runtime.handleMobileUnsubscribe('pty-1', 'client-a')
       await runtime.handleMobileSubscribe('pty-1', 'client-b', { cols: 40, rows: 18 })
 
-      const pendingRestore = Reflect.get(runtime, 'pendingRestoreTimers') as Map<string, unknown>
+      const pendingRestore = runtime.restoreTimers
       pendingRestore.set('pty-1', { timer: setTimeout(() => {}, 60_000), clientId: 'client-b' })
-      const pendingSoft = Reflect.get(runtime, 'pendingSoftLeavers') as Map<string, unknown>
+      const pendingSoft = runtime.softLeavers
       expect(pendingSoft.has('pty-1')).toBe(true)
       await runtime.reclaimTerminalForDesktop('pty-1')
       expect(pendingRestore.has('pty-1')).toBe(false)
@@ -878,10 +894,10 @@ describe('mobile subscribe integration', () => {
       const { runtime } = createRuntime()
       await runtime.handleMobileSubscribe('pty-1', 'client-a', { cols: 45, rows: 20 })
       runtime.handleMobileUnsubscribe('pty-1', 'client-a')
-      ;(Reflect.get(runtime, 'terminalFitOverrides') as Map<string, unknown>).delete('pty-1')
+      runtime.fitOverrides.delete('pty-1')
 
-      const pendingRestore = Reflect.get(runtime, 'pendingRestoreTimers') as Map<string, unknown>
-      const pendingSoft = Reflect.get(runtime, 'pendingSoftLeavers') as Map<string, unknown>
+      const pendingRestore = runtime.restoreTimers
+      const pendingSoft = runtime.softLeavers
       await runtime.reclaimTerminalForDesktop('pty-1')
       expect(pendingRestore.has('pty-1')).toBe(false)
       expect(pendingSoft.has('pty-1')).toBe(false)
@@ -891,15 +907,11 @@ describe('mobile subscribe integration', () => {
       const { runtime } = createRuntime()
       await runtime.handleMobileSubscribe('pty-1', 'client-a', { cols: 45, rows: 20 })
       runtime.handleMobileUnsubscribe('pty-1', 'client-a')
-      ;(Reflect.get(runtime, 'terminalFitOverrides') as Map<string, unknown>).delete('pty-1')
-      ;(
-        Reflect.get(runtime, 'terminalDrivers') as {
-          set: (ptyId: string, driver: { kind: 'idle' }) => void
-        }
-      ).set('pty-1', { kind: 'idle' })
+      runtime.fitOverrides.delete('pty-1')
+      runtime.drivers.set('pty-1', { kind: 'idle' })
 
-      const pendingRestore = Reflect.get(runtime, 'pendingRestoreTimers') as Map<string, unknown>
-      const pendingSoft = Reflect.get(runtime, 'pendingSoftLeavers') as Map<string, unknown>
+      const pendingRestore = runtime.restoreTimers
+      const pendingSoft = runtime.softLeavers
       expect(await runtime.reclaimTerminalForDesktop('pty-1')).toBe(false)
       expect(pendingRestore.has('pty-1')).toBe(false)
       expect(pendingSoft.has('pty-1')).toBe(false)

tests/e2e/linear-url-workspace-entry.spec.ts
@@ -25,6 +25,14 @@ const LINEAR_ISSUE: LinearIssue = {
   updatedAt: '2026-08-12T00:00:00.000Z'
 }
 
+declare global {
+  // oxlint-disable-next-line typescript-eslint/consistent-type-definitions -- declaration merging requires interface
+  interface Window {
+    // Set by the fixture below while a Linear lookup is deliberately held open.
+    __orcaTestReleaseLinearLookup?: () => void
+  }
+}
+
 function pasteChord(): string {
   return process.platform === 'darwin' ? 'Meta+V' : 'Control+V'
 }
@@ -86,7 +94,7 @@ async function installLinearFixture(
 
 async function releaseHeldLinearLookup(page: Page): Promise<void> {
   await page.evaluate(() => {
-    const release = Reflect.get(window, '__orcaTestReleaseLinearLookup')
+    const release = window.__orcaTestReleaseLinearLookup
     if (typeof release !== 'function') {
       throw new Error('Linear lookup is not held')
     }
@@ -131,9 +139,7 @@ test.describe('Linear URL workspace entry', () => {
     await pasteLinearUrl(orcaPage, input)
     await expect
       .poll(() =>
-        orcaPage.evaluate(
-          () => typeof Reflect.get(window, '__orcaTestReleaseLinearLookup') === 'function'
-        )
+        orcaPage.evaluate(() => typeof window.__orcaTestReleaseLinearLookup === 'function')
       )
       .toBe(true)
     await input.press('Enter')