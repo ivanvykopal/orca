paidaxingyo666/Manta

feat(mobile-web-bundle): serve the packaged mobile web bundle over RPC (OTA phase A, 3/5) (#21348)
* feat(mobile-web-bundle): serve the bundle manifest and chunks over RPC

Two paired-runtime methods on the already-authenticated connection:
`mobileWeb.bundle.manifest` returns this install's manifest plus the chunk
size it advertises, and `mobileWeb.bundle.chunk` returns one aligned range of
one asset with the whole asset's length and hash, so a single chunk describes
what it belongs to.

`path` is accepted only by exact match against a manifest member, so traversal
is unreachable rather than mitigated. Each asset's on-disk sha256 is verified
once and the verdict remembered, concurrent first readers sharing one hash.
Reads are capped at four in flight per connection, and a disconnected client
stops costing reads at the next checkpoint.

No SSH or relay proxying: a runtime answers only out of its own install.

Claude-Session: https://claude.ai/code/session_01JNnE9qzUZMMnqpZWCqM3nb

* test(mobile-web-bundle): pin the three buildId serializers against each other

The canonical serialization exists in the builder, the packaging guard, and the
shared contract, because the two packaging scripts run on bare node before any
build output exists and cannot import TypeScript. A divergence in any one would
reject every honest bundle at packaging, or ship a bundle whose id the phone
recomputes differently and re-downloads forever. Proved red by swapping the
guard's code-unit sort for localeCompare: five of six cases fail.

Exports the guard's serializer for the test; no packaging behaviour changes.

Claude-Session: https://claude.ai/code/session_01JNnE9qzUZMMnqpZWCqM3nb

* test(mobile-web-bundle): cover every error code and a multi-chunk paging round trip

Against a synthetic bundle in a temp dir, because the real builder's largest
asset is under one chunk and CI unit jobs never build out/mobile-web. The
fixture's script spans three chunks, its stylesheet is exactly one, and one

src/main/runtime/rpc/methods/mobile-web-bundle.test-fixture.ts
@@ -1,0 +1,106 @@
+import { createHash } from 'node:crypto'
+import { mkdirSync, writeFileSync } from 'node:fs'
+import { join } from 'node:path'
+import { installFakeAppEnvironment } from '../../../../../config/scripts/vitest-host-ports-setup'
+import type { MantaRuntimeService } from '../../manta-runtime'
+import { RpcDispatcher } from '../dispatcher'
+import { MOBILE_WEB_BUNDLE_METHODS } from './mobile-web-bundle'
+import { MOBILE_WEB_BUNDLE_CHUNK_BYTES } from '../../../../shared/mobile-web-bundle/bundle-rpc-contract'
+import {
+  computeMobileWebBundleId,
+  type MobileWebBundleAsset
+} from '../../../../shared/mobile-web-bundle/manifest-contract'
+
+export const sha256Hex = (bytes: Buffer): string => createHash('sha256').update(bytes).digest('hex')
+
+/** Deterministic, varied bytes, so a read at the wrong offset cannot accidentally look right. */
+export function mobileWebBundleFiller(byteLength: number, seed: number): Buffer {
+  const bytes = Buffer.alloc(byteLength)
+  for (let index = 0; index < byteLength; index++) {
+    bytes[index] = (index * 31 + seed * 17) % 256
+  }
+  return bytes
+}
+
+type SyntheticAsset = { path: string; bytes: Buffer; contentType: string }
+
+/**
+ * A bundle the real builder cannot produce today: its largest asset spans three chunks, where every
+ * asset the Phase A bootstrap emits is under one. Multi-chunk paging has to be exercised rather than
+ * assumed, and CI unit jobs never build out/mobile-web, so the fixture is synthetic on purpose.
+ */
+function syntheticAssets(seed: number): SyntheticAsset[] {
+  const script = mobileWebBundleFiller(MOBILE_WEB_BUNDLE_CHUNK_BYTES * 2 + 1024, seed)
+  const stylesheet = mobileWebBundleFiller(MOBILE_WEB_BUNDLE_CHUNK_BYTES, seed + 1)
+  const mark = Buffer.alloc(0)
+  return [
+    {
+      path: 'index.html',
+      bytes: mobileWebBundleFiller(640, seed + 2),
+      contentType: 'text/html; charset=utf-8'
+    },
+    {
+      path: `assets/${sha256Hex(script)}.js`,
+      bytes: script,
+      contentType: 'text/javascript; charset=utf-8'
+    },
+    { path: `assets/${sha256Hex(stylesheet)}.css`, bytes: stylesheet, contentType: 'text/css' },
+    { path: `assets/${sha256Hex(mark)}.png`, bytes: mark, contentType: 'image/png' }
+  ]
+}
+
+export type SyntheticMobileWebBundle = {
+  root: string
+  buildId: string
+  assets: MobileWebBundleAsset[]
+}
+
+export function writeSyntheticMobileWebBundle(
+  root: string,
+  seed: number
+): SyntheticMobileWebBundle {
+  mkdirSync(join(root, 'assets'), { recursive: true })
+  const written = syntheticAssets(seed)
+  for (const asset of written) {
+    writeFileSync(join(root, asset.path), asset.bytes)
+  }
+  const assets = written
+    .map((asset) => ({
+      path: asset.path,
+      sha256: sha256Hex(asset.bytes),
+      byteLength: asset.bytes.byteLength,
+      contentType: asset.contentType
+    }))
+    .sort((left, right) => (left.path < right.path ? -1 : 1))
+  const buildId = computeMobileWebBundleId(assets)
+  writeFileSync(
+    join(root, 'manifest.json'),
+    JSON.stringify({
+      schemaVersion: 1,
+      buildId,
+      desktopVersion: '1.4.200',
+      minCompatibleRuntimeProtocolVersion: 2,
+      runtimeProtocolVersion: 2,
+      entrypoint: 'index.html',
+      totalBytes: assets.reduce((total, asset) => total + asset.byteLength, 0),
+      assets
+    }),
+    'utf8'
+  )
+  return { root, buildId, assets }
+}
+
+/** A dispatcher carrying only these methods. Nothing here reaches the runtime service: the bundle is
+ *  read off the install, so the dispatcher's one call into it is the envelope's runtime id. */
+export function mobileWebBundleDispatcher(): RpcDispatcher {
+  // oxlint-disable-next-line typescript/consistent-type-assertions -- SAFETY: neither mobileWeb.bundle method takes a runtime argument, so getRuntimeId (read once, to stamp the envelope) is the only member this dispatcher can reach.
+  const runtime = { getRuntimeId: () => 'test-runtime' } as unknown as MantaRuntimeService
+  return new RpcDispatcher({ runtime, methods: MOBILE_WEB_BUNDLE_METHODS })
+}
+
+/** The install root the resolver probes. Installed through the port, not an electron mock: the
+ *  resolver is reachable from the runtime's import graph and so must never import electron. The
+ *  shared setup reinstalls a default environment before every test, so nothing here needs undoing. */
+export function installMobileWebBundleAppPath(appPath: string): void {
+  installFakeAppEnvironment({ getAppPath: () => appPath, getPath: () => appPath })
+}

src/main/runtime/rpc/methods/mobile-web-bundle.ts
@@ -1,0 +1,145 @@
+/**
+ * Serves this install's mobile web bundle to the paired client over the already-authenticated RPC
+ * connection: one call for the manifest, then one call per 48 KiB chunk of each asset.
+ *
+ * No SSH or relay proxying, ever. The bundle is an artifact of the desktop the phone paired with,
+ * not something a remote execution host owns, so a runtime answers only out of its own install and
+ * never forwards these methods to another host.
+ *
+ * `asContractError` is a total catch over the verify-and-read block: every host-side failure in
+ * there, whatever its cause, reaches the client as `mobile_web_bundle_asset_changed`.
+ */
+import {
+  MOBILE_WEB_BUNDLE_CHUNK_BYTES,
+  MOBILE_WEB_BUNDLE_CHUNK_METHOD,
+  MOBILE_WEB_BUNDLE_MANIFEST_METHOD,
+  MobileWebBundleChunkParamsSchema,
+  type MobileWebBundleChunkResult,
+  type MobileWebBundleErrorCode,
+  type MobileWebBundleManifestResult
+} from '../../../../shared/mobile-web-bundle/bundle-rpc-contract'
+import type { MobileWebBundleAsset } from '../../../../shared/mobile-web-bundle/manifest-contract'
+import {
+  loadBundledMobileWebBundle,
+  type BundledMobileWebBundle
+} from '../../bundled-mobile-web-bundle'
+import { isClientDisconnectedError } from '../../manta-runtime-core'
+import { defineMethod, InvalidArgumentError, type RpcContext } from '../core'
+import {
+  readMobileWebBundleAssetChunk,
+  verifyMobileWebBundleAsset
+} from './mobile-web-bundle-asset-reader'
+import {
+  acquireMobileWebBundleReadSlot,
+  mobileWebBundleReadBucket
+} from './mobile-web-bundle-read-admission'
+
+/** The code IS the message: `InvalidArgumentError` carries no data field, so the message is the only
+ *  place a stable code can travel, and a client must be able to branch without matching prose. */
+function bundleError(code: MobileWebBundleErrorCode): InvalidArgumentError {
+  return new InvalidArgumentError(code)
+}
+
+function requireBundle(): BundledMobileWebBundle {
+  const bundle = loadBundledMobileWebBundle()
+  if (!bundle) {
+    throw bundleError('mobile_web_bundle_unavailable')
+  }
+  return bundle
+}
+
+function abortIfDisconnected(ctx: RpcContext): void {
+  if (ctx.signal?.aborted) {
+    throw new Error('client_disconnected')
+  }
+}
+
+/** Every other way a read can fail — the asset unlinked, unreadable, or shorter than the manifest
+ *  promised — is one thing to a client: this bundle no longer matches the manifest it was handed.
+ *  The host path stays on the host; the reply carries only the code. */
+function asContractError(error: unknown, path: string): unknown {
+  if (error instanceof InvalidArgumentError || isClientDisconnectedError(error)) {
+    return error
+  }
+  console.warn(`[mobile-web-bundle] read failed for ${path}:`, error)
+  return bundleError('mobile_web_bundle_asset_changed')
+}
+
+/** Exact match against a manifest member. `path` is never joined, normalised, or prefix-matched, so
+ *  traversal is not mitigated here — it is unreachable. */
+function findAsset(bundle: BundledMobileWebBundle, path: string): MobileWebBundleAsset {
+  const asset = bundle.manifest.assets.find((candidate) => candidate.path === path)
+  if (!asset) {
+    throw bundleError('mobile_web_bundle_asset_unknown')
+  }
+  return asset
+}
+
+/** Alignment is against the size the manifest reply advertised, which the contract deliberately
+ *  leaves off `offset` so the host can shrink the chunk without a client release. Offset 0 is always
+ *  in range, so a zero-byte asset is still fetchable and still reports eof. */
+function assertOffsetAddressesAChunk(offset: number, asset: MobileWebBundleAsset): void {
+  if (offset % MOBILE_WEB_BUNDLE_CHUNK_BYTES !== 0) {
+    throw bundleError('mobile_web_bundle_offset_invalid')
+  }
+  if (offset > 0 && offset >= asset.byteLength) {
+    throw bundleError('mobile_web_bundle_offset_invalid')
+  }
+}
+
+export const MOBILE_WEB_BUNDLE_METHODS = [
+  defineMethod({
+    name: MOBILE_WEB_BUNDLE_MANIFEST_METHOD,
+    params: null,
+    handler: async (): Promise<MobileWebBundleManifestResult> => ({
+      manifest: requireBundle().manifest,
+      chunkBytes: MOBILE_WEB_BUNDLE_CHUNK_BYTES
+    })
+  }),
+  defineMethod({
+    name: MOBILE_WEB_BUNDLE_CHUNK_METHOD,
+    params: MobileWebBundleChunkParamsSchema,
+    handler: async (params, ctx): Promise<MobileWebBundleChunkResult> => {
+      const bundle = requireBundle()
+      // Checked before the asset lookup: a desktop that auto-updated mid-download must tell the
+      // client to restart from the manifest, not that its path went missing.
+      if (params.buildId !== bundle.manifest.buildId) {
+        throw bundleError('mobile_web_bundle_build_changed')
+      }
+      const asset = findAsset(bundle, params.path)
+      assertOffsetAddressesAChunk(params.offset, asset)
+
+      const release = acquireMobileWebBundleReadSlot(mobileWebBundleReadBucket(ctx))
+      if (!release) {
+        throw bundleError('mobile_web_bundle_read_limited')
+      }
+      try {
+        abortIfDisconnected(ctx)
+        if (!(await verifyMobileWebBundleAsset(bundle.root, bundle.manifest.buildId, asset))) {
+          throw bundleError('mobile_web_bundle_asset_changed')
+        }
+        abortIfDisconnected(ctx)
+        const data = await readMobileWebBundleAssetChunk(
+          bundle.root,
+          asset,
+          params.offset,
+          MOBILE_WEB_BUNDLE_CHUNK_BYTES
+        )
+        return {
+          buildId: bundle.manifest.buildId,
+          path: asset.path,
+          offset: params.offset,
+          // The whole asset's length and hash, so one chunk describes the asset it belongs to.
+          assetByteLength: asset.byteLength,
+          sha256: asset.sha256,
+          dataBase64: data.toString('base64'),
+          eof: params.offset + data.byteLength >= asset.byteLength
+        }
+      } catch (error) {
+        throw asContractError(error, asset.path)
+      } finally {
+        release()
+      }
+    }
+  })
+]

ivanvykopal/orca

src/main/runtime/rpc/methods/mobile-web-bundle.test-fixture.ts
@@ -1,0 +1,106 @@
+import { createHash } from 'node:crypto'
+import { mkdirSync, writeFileSync } from 'node:fs'
+import { join } from 'node:path'
+import { installFakeAppEnvironment } from '../../../../../config/scripts/vitest-host-ports-setup'
+import type { OrcaRuntimeService } from '../../orca-runtime'
+import { RpcDispatcher } from '../dispatcher'
+import { MOBILE_WEB_BUNDLE_METHODS } from './mobile-web-bundle'
+import { MOBILE_WEB_BUNDLE_CHUNK_BYTES } from '../../../../shared/mobile-web-bundle/bundle-rpc-contract'
+import {
+  computeMobileWebBundleId,
+  type MobileWebBundleAsset
+} from '../../../../shared/mobile-web-bundle/manifest-contract'
+
+export const sha256Hex = (bytes: Buffer): string => createHash('sha256').update(bytes).digest('hex')
+
+/** Deterministic, varied bytes, so a read at the wrong offset cannot accidentally look right. */
+export function mobileWebBundleFiller(byteLength: number, seed: number): Buffer {
+  const bytes = Buffer.alloc(byteLength)
+  for (let index = 0; index < byteLength; index++) {
+    bytes[index] = (index * 31 + seed * 17) % 256
+  }
+  return bytes
+}
+
+type SyntheticAsset = { path: string; bytes: Buffer; contentType: string }
+
+/**
+ * A bundle the real builder cannot produce today: its largest asset spans three chunks, where every
+ * asset the Phase A bootstrap emits is under one. Multi-chunk paging has to be exercised rather than
+ * assumed, and CI unit jobs never build out/mobile-web, so the fixture is synthetic on purpose.
+ */
+function syntheticAssets(seed: number): SyntheticAsset[] {
+  const script = mobileWebBundleFiller(MOBILE_WEB_BUNDLE_CHUNK_BYTES * 2 + 1024, seed)
+  const stylesheet = mobileWebBundleFiller(MOBILE_WEB_BUNDLE_CHUNK_BYTES, seed + 1)
+  const mark = Buffer.alloc(0)
+  return [
+    {
+      path: 'index.html',
+      bytes: mobileWebBundleFiller(640, seed + 2),
+      contentType: 'text/html; charset=utf-8'
+    },
+    {
+      path: `assets/${sha256Hex(script)}.js`,
+      bytes: script,
+      contentType: 'text/javascript; charset=utf-8'
+    },
+    { path: `assets/${sha256Hex(stylesheet)}.css`, bytes: stylesheet, contentType: 'text/css' },
+    { path: `assets/${sha256Hex(mark)}.png`, bytes: mark, contentType: 'image/png' }
+  ]
+}
+
+export type SyntheticMobileWebBundle = {
+  root: string
+  buildId: string
+  assets: MobileWebBundleAsset[]
+}
+
+export function writeSyntheticMobileWebBundle(
+  root: string,
+  seed: number
+): SyntheticMobileWebBundle {
+  mkdirSync(join(root, 'assets'), { recursive: true })
+  const written = syntheticAssets(seed)
+  for (const asset of written) {
+    writeFileSync(join(root, asset.path), asset.bytes)
+  }
+  const assets = written
+    .map((asset) => ({
+      path: asset.path,
+      sha256: sha256Hex(asset.bytes),
+      byteLength: asset.bytes.byteLength,
+      contentType: asset.contentType
+    }))
+    .sort((left, right) => (left.path < right.path ? -1 : 1))
+  const buildId = computeMobileWebBundleId(assets)
+  writeFileSync(
+    join(root, 'manifest.json'),
+    JSON.stringify({
+      schemaVersion: 1,
+      buildId,
+      desktopVersion: '1.4.200',
+      minCompatibleRuntimeProtocolVersion: 2,
+      runtimeProtocolVersion: 2,
+      entrypoint: 'index.html',
+      totalBytes: assets.reduce((total, asset) => total + asset.byteLength, 0),
+      assets
+    }),
+    'utf8'
+  )
+  return { root, buildId, assets }
+}
+
+/** A dispatcher carrying only these methods. Nothing here reaches the runtime service: the bundle is
+ *  read off the install, so the dispatcher's one call into it is the envelope's runtime id. */
+export function mobileWebBundleDispatcher(): RpcDispatcher {
+  // oxlint-disable-next-line typescript/consistent-type-assertions -- SAFETY: neither mobileWeb.bundle method takes a runtime argument, so getRuntimeId (read once, to stamp the envelope) is the only member this dispatcher can reach.
+  const runtime = { getRuntimeId: () => 'test-runtime' } as unknown as OrcaRuntimeService
+  return new RpcDispatcher({ runtime, methods: MOBILE_WEB_BUNDLE_METHODS })
+}
+
+/** The install root the resolver probes. Installed through the port, not an electron mock: the
+ *  resolver is reachable from the runtime's import graph and so must never import electron. The
+ *  shared setup reinstalls a default environment before every test, so nothing here needs undoing. */
+export function installMobileWebBundleAppPath(appPath: string): void {
+  installFakeAppEnvironment({ getAppPath: () => appPath, getPath: () => appPath })
+}

src/main/runtime/rpc/methods/mobile-web-bundle.ts
@@ -1,0 +1,145 @@
+/**
+ * Serves this install's mobile web bundle to the paired client over the already-authenticated RPC
+ * connection: one call for the manifest, then one call per 48 KiB chunk of each asset.
+ *
+ * No SSH or relay proxying, ever. The bundle is an artifact of the desktop the phone paired with,
+ * not something a remote execution host owns, so a runtime answers only out of its own install and
+ * never forwards these methods to another host.
+ *
+ * `asContractError` is a total catch over the verify-and-read block: every host-side failure in
+ * there, whatever its cause, reaches the client as `mobile_web_bundle_asset_changed`.
+ */
+import {
+  MOBILE_WEB_BUNDLE_CHUNK_BYTES,
+  MOBILE_WEB_BUNDLE_CHUNK_METHOD,
+  MOBILE_WEB_BUNDLE_MANIFEST_METHOD,
+  MobileWebBundleChunkParamsSchema,
+  type MobileWebBundleChunkResult,
+  type MobileWebBundleErrorCode,
+  type MobileWebBundleManifestResult
+} from '../../../../shared/mobile-web-bundle/bundle-rpc-contract'
+import type { MobileWebBundleAsset } from '../../../../shared/mobile-web-bundle/manifest-contract'
+import {
+  loadBundledMobileWebBundle,
+  type BundledMobileWebBundle
+} from '../../bundled-mobile-web-bundle'
+import { isClientDisconnectedError } from '../../orca-runtime-core'
+import { defineMethod, InvalidArgumentError, type RpcContext } from '../core'
+import {
+  readMobileWebBundleAssetChunk,
+  verifyMobileWebBundleAsset
+} from './mobile-web-bundle-asset-reader'
+import {
+  acquireMobileWebBundleReadSlot,
+  mobileWebBundleReadBucket
+} from './mobile-web-bundle-read-admission'
+
+/** The code IS the message: `InvalidArgumentError` carries no data field, so the message is the only
+ *  place a stable code can travel, and a client must be able to branch without matching prose. */
+function bundleError(code: MobileWebBundleErrorCode): InvalidArgumentError {
+  return new InvalidArgumentError(code)
+}
+
+function requireBundle(): BundledMobileWebBundle {
+  const bundle = loadBundledMobileWebBundle()
+  if (!bundle) {
+    throw bundleError('mobile_web_bundle_unavailable')
+  }
+  return bundle
+}
+
+function abortIfDisconnected(ctx: RpcContext): void {
+  if (ctx.signal?.aborted) {
+    throw new Error('client_disconnected')
+  }
+}
+
+/** Every other way a read can fail — the asset unlinked, unreadable, or shorter than the manifest
+ *  promised — is one thing to a client: this bundle no longer matches the manifest it was handed.
+ *  The host path stays on the host; the reply carries only the code. */
+function asContractError(error: unknown, path: string): unknown {
+  if (error instanceof InvalidArgumentError || isClientDisconnectedError(error)) {
+    return error
+  }
+  console.warn(`[mobile-web-bundle] read failed for ${path}:`, error)
+  return bundleError('mobile_web_bundle_asset_changed')
+}
+
+/** Exact match against a manifest member. `path` is never joined, normalised, or prefix-matched, so
+ *  traversal is not mitigated here — it is unreachable. */
+function findAsset(bundle: BundledMobileWebBundle, path: string): MobileWebBundleAsset {
+  const asset = bundle.manifest.assets.find((candidate) => candidate.path === path)
+  if (!asset) {
+    throw bundleError('mobile_web_bundle_asset_unknown')
+  }
+  return asset
+}
+
+/** Alignment is against the size the manifest reply advertised, which the contract deliberately
+ *  leaves off `offset` so the host can shrink the chunk without a client release. Offset 0 is always
+ *  in range, so a zero-byte asset is still fetchable and still reports eof. */
+function assertOffsetAddressesAChunk(offset: number, asset: MobileWebBundleAsset): void {
+  if (offset % MOBILE_WEB_BUNDLE_CHUNK_BYTES !== 0) {
+    throw bundleError('mobile_web_bundle_offset_invalid')
+  }
+  if (offset > 0 && offset >= asset.byteLength) {
+    throw bundleError('mobile_web_bundle_offset_invalid')
+  }
+}
+
+export const MOBILE_WEB_BUNDLE_METHODS = [
+  defineMethod({
+    name: MOBILE_WEB_BUNDLE_MANIFEST_METHOD,
+    params: null,
+    handler: async (): Promise<MobileWebBundleManifestResult> => ({
+      manifest: requireBundle().manifest,
+      chunkBytes: MOBILE_WEB_BUNDLE_CHUNK_BYTES
+    })
+  }),
+  defineMethod({
+    name: MOBILE_WEB_BUNDLE_CHUNK_METHOD,
+    params: MobileWebBundleChunkParamsSchema,
+    handler: async (params, ctx): Promise<MobileWebBundleChunkResult> => {
+      const bundle = requireBundle()
+      // Checked before the asset lookup: a desktop that auto-updated mid-download must tell the
+      // client to restart from the manifest, not that its path went missing.
+      if (params.buildId !== bundle.manifest.buildId) {
+        throw bundleError('mobile_web_bundle_build_changed')
+      }
+      const asset = findAsset(bundle, params.path)
+      assertOffsetAddressesAChunk(params.offset, asset)
+
+      const release = acquireMobileWebBundleReadSlot(mobileWebBundleReadBucket(ctx))
+      if (!release) {
+        throw bundleError('mobile_web_bundle_read_limited')
+      }
+      try {
+        abortIfDisconnected(ctx)
+        if (!(await verifyMobileWebBundleAsset(bundle.root, bundle.manifest.buildId, asset))) {
+          throw bundleError('mobile_web_bundle_asset_changed')
+        }
+        abortIfDisconnected(ctx)
+        const data = await readMobileWebBundleAssetChunk(
+          bundle.root,
+          asset,
+          params.offset,
+          MOBILE_WEB_BUNDLE_CHUNK_BYTES
+        )
+        return {
+          buildId: bundle.manifest.buildId,
+          path: asset.path,
+          offset: params.offset,
+          // The whole asset's length and hash, so one chunk describes the asset it belongs to.
+          assetByteLength: asset.byteLength,
+          sha256: asset.sha256,
+          dataBase64: data.toString('base64'),
+          eof: params.offset + data.byteLength >= asset.byteLength
+        }
+      } catch (error) {
+        throw asContractError(error, asset.path)
+      } finally {
+        release()
+      }
+    }
+  })
+]