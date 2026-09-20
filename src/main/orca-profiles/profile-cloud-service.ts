paidaxingyo666/Manta

fix(cloud-auth): keep Sign in clickable during a pending browser wait (#21078)
* fix(cloud-auth): keep Sign in clickable during a pending browser wait

Closing the cloud sign-in tab used to leave every Sign in button disabled
as "Signing in…" until the 5-minute loopback timeout. A second click now
starts another wait, the first tab can still complete, and the first
successful callback wins.

STA-7610

* fix(cloud-auth): satisfy typecheck and localization after Sign in unlock

Keep the account-pane mock able to represent a missing auth status, and
drop unused Signing in catalog entries now that the wait no longer
relabels the button.

* fix(cloud-auth): ignore a stale sign-in after a later wait succeeds

A second Sign in click still starts a new loopback wait. Completing that
newer wait links the session; finishing the older tab afterwards is
cancelled instead of overwriting the linked identity or toasting again.

* test(cloud-auth): cover post-exchange stale connect and pending Sign in

Pin the branch that discards an earlier token exchange after a later wait
has already linked, keep Sign in enabled while connect is still pending,
and suppress a failed toast when auth is already connected.

* fix(cloud-auth): do not relink an in-flight sign-in after sign-out

Signing out now invalidates outstanding PKCE attempts in main and the
renderer so a later browser tab cannot restore the session.

* fix(cloud-auth): do not wipe a newer connect that finishes during sign-out

If sign-in completes while revoke is still in flight, skip session clear
and unlink so the new session survives. Do not toast signed-out when auth
is already connected again.

Mirror-Of: 2e3a24c30f4c57d040d66e3dad75315fcc379eb0

src/main/manta-profiles/profile-cloud-service.ts
@@ -36,6 +36,14 @@ import { selectCloudOrgWithMutationFence } from './profile-cloud-org-selection'
 
 export { refreshCurrentMantaProfileAuth } from './profile-cloud-capability-refresh'
 
+let nextCloudConnectAttempt = 0
+let linkedCloudConnectAttempt = 0
+
+function invalidateOutstandingCloudConnectAttempts(): void {
+  nextCloudConnectAttempt += 1
+  linkedCloudConnectAttempt = nextCloudConnectAttempt
+}
+
 function isUserCancelledAuthError(message: string): boolean {
   return message === 'manta_cloud_auth_timeout' || message === 'manta_cloud_auth_denied'
 }
@@ -73,14 +81,28 @@ export async function connectCurrentMantaProfile(
     }
   }
 
+  const attempt = ++nextCloudConnectAttempt
   try {
     const code = await beginMantaCloudPkceFlow(configState.config, active.profile.id)
+    if (attempt < linkedCloudConnectAttempt) {
+      return {
+        status: 'cancelled',
+        auth: getCurrentMantaProfileAuthStatus(userDataPath)
+      }
+    }
     const exchange = await exchangeMantaCloudAuthCode(configState.config, {
       ...code,
       localProfileId: active.profile.id
     })
+    if (attempt < linkedCloudConnectAttempt) {
+      return {
+        status: 'cancelled',
+        auth: getCurrentMantaProfileAuthStatus(userDataPath)
+      }
+    }
     saveMantaCloudSessionExchange(active.profile.id, userDataPath, exchange)
     const list = linkMantaProfileToCloud(active.profile.id, exchange.cloud, userDataPath)
+    linkedCloudConnectAttempt = attempt
     return {
       status: 'connected',
       auth: getCurrentMantaProfileAuthStatus(userDataPath),
@@ -106,6 +128,10 @@ export async function connectCurrentMantaProfile(
 export async function signOutCurrentMantaProfile(
   userDataPath: string
 ): Promise<SignOutCurrentMantaProfileResult> {
+  // Why: a Sign in click still waiting in the browser must not relink after
+  // the user explicitly signed out.
+  invalidateOutstandingCloudConnectAttempts()
+  const signOutEpoch = linkedCloudConnectAttempt
   const active = ensureActiveMantaProfile(userDataPath)
   const configState = getMantaCloudAuthConfig()
   const session = readMantaCloudSession(active.profile.id, userDataPath)
@@ -119,6 +145,15 @@ export async function signOutCurrentMantaProfile(
   }
   if (!isMantaCloudDevAuthEnabled() && configState.configured && session.status === 'found') {
     await revokeMantaCloudSession(configState.config, session.session).catch(() => undefined)
+  }
+  if (linkedCloudConnectAttempt > signOutEpoch) {
+    const current = ensureActiveMantaProfile(userDataPath)
+    return {
+      status: 'signed-out',
+      auth: getCurrentMantaProfileAuthStatus(userDataPath),
+      activeProfileId: current.index.activeProfileId,
+      profiles: current.index.profiles
+    }
   }
   clearMantaCloudSession(active.profile.id, userDataPath)
   const list = unlinkMantaProfileFromCloud(active.profile.id, userDataPath)

src/renderer/src/components/artifacts/ArtifactsPageStates.tsx
@@ -22,13 +22,11 @@ export function ArtifactsPageErrorBanner({
 }
 
 export function ArtifactsPageAuthState({
-  connecting,
   needsReconnect,
   configured,
   onConnect,
   onOpenAccountSettings
 }: {
-  connecting: boolean
   needsReconnect: boolean
   configured: boolean
   onConnect: () => void
@@ -62,15 +60,13 @@ export function ArtifactsPageAuthState({
         </p>
       </div>
       {configured ? (
-        <Button size="sm" disabled={connecting} onClick={onConnect}>
-          {connecting
-            ? translate('auto.components.artifacts.ArtifactsPage.signingIn', 'Signing in…')
-            : needsReconnect
-              ? translate(
-                  'auto.components.artifacts.ArtifactsPage.signInAgainAction',
-                  'Sign in again'
-                )
-              : translate('auto.components.artifacts.ArtifactsPage.signIn', 'Sign in to Manta')}
+        <Button size="sm" onClick={onConnect}>
+          {needsReconnect
+            ? translate(
+                'auto.components.artifacts.ArtifactsPage.signInAgainAction',
+                'Sign in again'
+              )
+            : translate('auto.components.artifacts.ArtifactsPage.signIn', 'Sign in to Manta')}
         </Button>
       ) : (
         <div className="flex flex-col items-center gap-2">

src/renderer/src/components/settings/ShareSkillsSettingsPane.tsx
@@ -1,6 +1,6 @@
 import { ArrowRight, BookOpen } from 'lucide-react'
 import { Button } from '@/components/ui/button'
-import { useOrcaProfileAuthStatusRefresh } from '@/hooks/use-orca-profile-auth-status-refresh'
+import { useMantaProfileAuthStatusRefresh } from '@/hooks/use-manta-profile-auth-status-refresh'
 import { translate } from '@/i18n/i18n'
 import { isWebClientLocation } from '@/lib/web-client-location'
 import { useAppStore } from '@/store'
@@ -14,13 +14,12 @@ export function ShareSkillsSettingsPane(): React.JSX.Element {
   const settings = useAppStore((state) => state.settings)
   const updateSettings = useAppStore((state) => state.updateSettings)
   const authStatus = useAppStore((state) => state.mantaProfileAuthStatus)
-  const connecting = useAppStore((state) => state.mantaProfileConnecting)
   const connect = useAppStore((state) => state.connectCurrentMantaProfile)
   const signedIn = authStatus?.state === 'connected'
   const isWebClient = isWebClientLocation()
   const agentSharingEnabled = settings?.agentSkillSharingEnabled === true
 
-  useOrcaProfileAuthStatusRefresh()
+  useMantaProfileAuthStatusRefresh()
 
   const steps: HowToStep[] = [
     {
@@ -135,14 +134,12 @@ export function ShareSkillsSettingsPane(): React.JSX.Element {
             <Button
               type="button"
               size="sm"
-              disabled={connecting || authStatus?.configured !== true}
+              disabled={authStatus?.configured !== true}
               onClick={() => void connect()}
             >
-              {connecting
-                ? translate('auto.components.settings.shareSkills.signingIn', 'Signing in…')
-                : authStatus?.state === 'reconnect-required'
-                  ? translate('auto.components.settings.shareSkills.signInAgain', 'Sign in again')
-                  : translate('auto.components.settings.shareSkills.signIn', 'Sign in to Manta')}
+              {authStatus?.state === 'reconnect-required'
+                ? translate('auto.components.settings.shareSkills.signInAgain', 'Sign in again')
+                : translate('auto.components.settings.shareSkills.signIn', 'Sign in to Manta')}
             </Button>
           ) : null}
         </section>

ivanvykopal/orca

src/main/orca-profiles/profile-cloud-service.ts
@@ -36,6 +36,14 @@ import { selectCloudOrgWithMutationFence } from './profile-cloud-org-selection'
 
 export { refreshCurrentOrcaProfileAuth } from './profile-cloud-capability-refresh'
 
+let nextCloudConnectAttempt = 0
+let linkedCloudConnectAttempt = 0
+
+function invalidateOutstandingCloudConnectAttempts(): void {
+  nextCloudConnectAttempt += 1
+  linkedCloudConnectAttempt = nextCloudConnectAttempt
+}
+
 function isUserCancelledAuthError(message: string): boolean {
   return message === 'orca_cloud_auth_timeout' || message === 'orca_cloud_auth_denied'
 }
@@ -73,14 +81,28 @@ export async function connectCurrentOrcaProfile(
     }
   }
 
+  const attempt = ++nextCloudConnectAttempt
   try {
     const code = await beginOrcaCloudPkceFlow(configState.config, active.profile.id)
+    if (attempt < linkedCloudConnectAttempt) {
+      return {
+        status: 'cancelled',
+        auth: getCurrentOrcaProfileAuthStatus(userDataPath)
+      }
+    }
     const exchange = await exchangeOrcaCloudAuthCode(configState.config, {
       ...code,
       localProfileId: active.profile.id
     })
+    if (attempt < linkedCloudConnectAttempt) {
+      return {
+        status: 'cancelled',
+        auth: getCurrentOrcaProfileAuthStatus(userDataPath)
+      }
+    }
     saveOrcaCloudSessionExchange(active.profile.id, userDataPath, exchange)
     const list = linkOrcaProfileToCloud(active.profile.id, exchange.cloud, userDataPath)
+    linkedCloudConnectAttempt = attempt
     return {
       status: 'connected',
       auth: getCurrentOrcaProfileAuthStatus(userDataPath),
@@ -106,6 +128,10 @@ export async function connectCurrentOrcaProfile(
 export async function signOutCurrentOrcaProfile(
   userDataPath: string
 ): Promise<SignOutCurrentOrcaProfileResult> {
+  // Why: a Sign in click still waiting in the browser must not relink after
+  // the user explicitly signed out.
+  invalidateOutstandingCloudConnectAttempts()
+  const signOutEpoch = linkedCloudConnectAttempt
   const active = ensureActiveOrcaProfile(userDataPath)
   const configState = getOrcaCloudAuthConfig()
   const session = readOrcaCloudSession(active.profile.id, userDataPath)
@@ -119,6 +145,15 @@ export async function signOutCurrentOrcaProfile(
   }
   if (!isOrcaCloudDevAuthEnabled() && configState.configured && session.status === 'found') {
     await revokeOrcaCloudSession(configState.config, session.session).catch(() => undefined)
+  }
+  if (linkedCloudConnectAttempt > signOutEpoch) {
+    const current = ensureActiveOrcaProfile(userDataPath)
+    return {
+      status: 'signed-out',
+      auth: getCurrentOrcaProfileAuthStatus(userDataPath),
+      activeProfileId: current.index.activeProfileId,
+      profiles: current.index.profiles
+    }
   }
   clearOrcaCloudSession(active.profile.id, userDataPath)
   const list = unlinkOrcaProfileFromCloud(active.profile.id, userDataPath)

src/renderer/src/components/artifacts/ArtifactsPageStates.tsx
@@ -22,13 +22,11 @@ export function ArtifactsPageErrorBanner({
 }
 
 export function ArtifactsPageAuthState({
-  connecting,
   needsReconnect,
   configured,
   onConnect,
   onOpenAccountSettings
 }: {
-  connecting: boolean
   needsReconnect: boolean
   configured: boolean
   onConnect: () => void
@@ -62,15 +60,13 @@ export function ArtifactsPageAuthState({
         </p>
       </div>
       {configured ? (
-        <Button size="sm" disabled={connecting} onClick={onConnect}>
-          {connecting
-            ? translate('auto.components.artifacts.ArtifactsPage.signingIn', 'Signing in…')
-            : needsReconnect
-              ? translate(
-                  'auto.components.artifacts.ArtifactsPage.signInAgainAction',
-                  'Sign in again'
-                )
-              : translate('auto.components.artifacts.ArtifactsPage.signIn', 'Sign in to Orca')}
+        <Button size="sm" onClick={onConnect}>
+          {needsReconnect
+            ? translate(
+                'auto.components.artifacts.ArtifactsPage.signInAgainAction',
+                'Sign in again'
+              )
+            : translate('auto.components.artifacts.ArtifactsPage.signIn', 'Sign in to Orca')}
         </Button>
       ) : (
         <div className="flex flex-col items-center gap-2">

src/renderer/src/components/settings/ShareSkillsSettingsPane.tsx
@@ -14,7 +14,6 @@ export function ShareSkillsSettingsPane(): React.JSX.Element {
   const settings = useAppStore((state) => state.settings)
   const updateSettings = useAppStore((state) => state.updateSettings)
   const authStatus = useAppStore((state) => state.orcaProfileAuthStatus)
-  const connecting = useAppStore((state) => state.orcaProfileConnecting)
   const connect = useAppStore((state) => state.connectCurrentOrcaProfile)
   const signedIn = authStatus?.state === 'connected'
   const isWebClient = isWebClientLocation()
@@ -135,14 +134,12 @@ export function ShareSkillsSettingsPane(): React.JSX.Element {
             <Button
               type="button"
               size="sm"
-              disabled={connecting || authStatus?.configured !== true}
+              disabled={authStatus?.configured !== true}
               onClick={() => void connect()}
             >
-              {connecting
-                ? translate('auto.components.settings.shareSkills.signingIn', 'Signing in…')
-                : authStatus?.state === 'reconnect-required'
-                  ? translate('auto.components.settings.shareSkills.signInAgain', 'Sign in again')
-                  : translate('auto.components.settings.shareSkills.signIn', 'Sign in to Orca')}
+              {authStatus?.state === 'reconnect-required'
+                ? translate('auto.components.settings.shareSkills.signInAgain', 'Sign in again')
+                : translate('auto.components.settings.shareSkills.signIn', 'Sign in to Orca')}
             </Button>
           ) : null}
         </section>