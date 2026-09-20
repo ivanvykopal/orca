paidaxingyo666/Manta

fix(native-chat): release sessions after provider root exit (#20502)
* fix(native-chat): bound structured chat launch

* Fix post-merge test hygiene

* Make structured fallback settlement exhaustive

* fix(native-chat): release sessions after root exit

* chore(i18n): remove legacy fallback copy

* test(native-chat): remove terminal fallback census

* docs(native-chat): clarify root-exit lease proof

* chore(native-chat): drop unrelated formatting

* fix native chat launch visibility

* test(native-chat): split message rail windowing coverage

* fix(native-chat): keep transport gating render-pure

* fix(native-chat): coordinate launch prompt settlement

* test(native-chat): align unified close ownership

* fix(native-chat): correct lifecycle imports and test typing

* fix(native-chat): fence restored launch cancellations

* fix(native-chat): fence authoritative cancellation snapshots

Mirror-Of: 1457d3966cf7b4165d17b18c57668d1e829f7674

src/renderer/src/lib/structured-agent-session-launch-persistence.test.ts
@@ -1,0 +1,69 @@
+// @vitest-environment happy-dom
+
+import { beforeEach, describe, expect, it } from 'vitest'
+import {
+  hasStructuredAgentLaunchCancellationTombstonePersisted,
+  readStructuredAgentLaunchRecord,
+  resetStructuredAgentLaunchPersistenceForTests,
+  retireStructuredAgentLaunchCancellationTombstonePersisted,
+  writeStructuredAgentLaunchRecord,
+  markStructuredAgentLaunchCancelledPersisted
+} from './structured-agent-session-launch-persistence'
+
+describe('structured agent launch persistence', () => {
+  beforeEach(() => {
+    localStorage.clear()
+    resetStructuredAgentLaunchPersistenceForTests()
+  })
+
+  it('normalizes pending launches after a renderer reload', () => {
+    localStorage.setItem(
+      'manta:structuredAgentLaunches:v1',
+      JSON.stringify([
+        {
+          sessionId: 'codex_session',
+          agent: 'codex',
+          lifecycle: 'pending',
+          clientOperationId: 'operation-1',
+          payloadFingerprint: 'fingerprint-1',
+          expectedRuntimeFence: null
+        }
+      ])
+    )
+
+    expect(readStructuredAgentLaunchRecord('codex_session')).toMatchObject({
+      lifecycle: 'visibility-unknown',
+      clientOperationId: 'operation-1'
+    })
+  })
+
+  it('stores only content-free identity and preserves operation identity', () => {
+    writeStructuredAgentLaunchRecord({
+      sessionId: 'claude_session',
+      agent: 'claude',
+      lifecycle: 'visibility-unknown',
+      clientOperationId: 'operation-2',
+      payloadFingerprint: 'fingerprint-2',
+      expectedRuntimeFence: null,
+      resumeFrom: { providerSessionId: 'provider-thread' }
+    })
+
+    const raw = localStorage.getItem('manta:structuredAgentLaunches:v1') ?? ''
+    expect(raw).toContain('claude_session')
+    expect(raw).toContain('operation-2')
+    expect(raw).not.toContain('prompt')
+    expect(raw).not.toContain('branch')
+    expect(raw).not.toContain('path')
+    expect(readStructuredAgentLaunchRecord('claude_session')?.clientOperationId).toBe('operation-2')
+  })
+
+  it('persists cancellation tombstones by session id and retires them', () => {
+    markStructuredAgentLaunchCancelledPersisted('codex_session')
+    expect(hasStructuredAgentLaunchCancellationTombstonePersisted('codex_session')).toBe(true)
+    expect(localStorage.getItem('manta:structuredAgentLaunchCancelledSessions:v1')).toBe(
+      '["codex_session"]'
+    )
+    expect(retireStructuredAgentLaunchCancellationTombstonePersisted('codex_session')).toBe(true)
+    expect(hasStructuredAgentLaunchCancellationTombstonePersisted('codex_session')).toBe(false)
+  })
+})

src/renderer/src/lib/structured-agent-session-launch-reload.test.ts
@@ -1,0 +1,181 @@
+// @vitest-environment happy-dom
+
+import { beforeEach, describe, expect, it, vi } from 'vitest'
+import type { RuntimeMobileSessionTabsResult } from '../../../shared/runtime-session-contracts'
+import type { StructuredAgentSessionLaunchIntent } from '@/lib/launch-structured-agent-session'
+import type * as RecoveryModule from '@/lib/structured-agent-session-launch-recovery'
+
+const mocks = vi.hoisted(() => ({
+  abandonIntent: vi.fn(),
+  callStructuredAgentSession: vi.fn(),
+  launch: vi.fn(),
+  restoreIntent: vi.fn(),
+  retryIntent: vi.fn(),
+  seedDraft: vi.fn(),
+  clearDraft: vi.fn(),
+  refresh: vi.fn()
+}))
+
+vi.mock('@/lib/launch-structured-agent-session', () => {
+  class StructuredAgentSessionCreateRefusalError extends Error {}
+  return {
+    abandonStructuredAgentSessionLaunchIntent: mocks.abandonIntent,
+    createStructuredAgentSessionLaunchIntent: vi.fn(),
+    launchStructuredAgentSession: mocks.launch,
+    restoreStructuredAgentSessionLaunchIntent: mocks.restoreIntent,
+    retryStructuredAgentSessionLaunchIntent: mocks.retryIntent,
+    StructuredAgentSessionCreateRefusalError
+  }
+})
+
+vi.mock('@/lib/structured-agent-session-launch-recovery', async () => {
+  const actual = await vi.importActual<typeof RecoveryModule>(
+    '@/lib/structured-agent-session-launch-recovery'
+  )
+  return { ...actual, launchAndReconcile: vi.fn(actual.launchAndReconcile) }
+})
+
+vi.mock('@/runtime/local-structured-session-tabs-sync', () => ({
+  refreshLocalStructuredSessionTabs: mocks.refresh
+}))
+
+vi.mock('@/runtime/structured-agent-session-client', () => ({
+  callStructuredAgentSession: mocks.callStructuredAgentSession
+}))
+
+vi.mock('@/store', () => ({
+  useAppStore: {
+    getState: () => ({
+      seedNativeChatLaunchDraft: mocks.seedDraft,
+      clearNativeChatLaunchDraft: mocks.clearDraft
+    })
+  }
+}))
+
+vi.mock('@/i18n/i18n', () => ({
+  translate: (_key: string, fallback: string) => fallback
+}))
+
+vi.mock('@/lib/agent-catalog', () => ({
+  getAgentLabel: (agent: string) => (agent === 'codex' ? 'Codex' : 'Claude')
+}))
+
+import { retryStructuredAgentSessionLaunch } from './structured-agent-session-launch'
+import { resetStructuredAgentLaunchPersistenceForTests } from './structured-agent-session-launch-persistence'
+import { resetStructuredAgentLaunchRegistryForTests } from './structured-agent-session-launch-registry'
+
+function launchIntent(worktreeId: string, sessionId: string): StructuredAgentSessionLaunchIntent {
+  return {
+    worktreeId,
+    sessionId,
+    agent: 'codex',
+    params: {
+      envelope: {
+        sessionId,
+        clientOperationId: 'operation-reloaded',
+        expectedRuntimeFence: null,
+        payloadFingerprint: 'fingerprint-reloaded'
+      },
+      worktree: `id:${worktreeId}`,
+      agent: 'codex'
+    }
+  }
+}
+
+function publishedSnapshot(worktreeId: string, sessionId: string): RuntimeMobileSessionTabsResult {
+  return {
+    worktree: worktreeId,
+    publicationEpoch: 'epoch-1',
+    snapshotVersion: 1,
+    activeGroupId: null,
+    activeTabId: null,
+    activeTabType: null,
+    tabs: [
+      {
+        type: 'agent-session',
+        id: 'tab-1',
+        title: 'Codex',
+        sessionId,
+        agent: 'codex',
+        isActive: true
+      }
+    ]
+  }
+}
+
+async function flushLaunchSettlement(): Promise<void> {
+  for (let index = 0; index < 20; index += 1) {
+    await Promise.resolve()
+  }
+}
+
+describe('structured agent launch reload recovery', () => {
+  beforeEach(() => {
+    vi.clearAllMocks()
+    localStorage.clear()
+    resetStructuredAgentLaunchPersistenceForTests()
+    resetStructuredAgentLaunchRegistryForTests()
+    mocks.callStructuredAgentSession.mockResolvedValue({ ok: true, page: { fence: 4 } })
+    mocks.restoreIntent.mockImplementation(
+      (args: {
+        worktreeId: string
+        sessionId: string
+        agent: 'claude' | 'codex'
+        clientOperationId: string
+        payloadFingerprint: string
+        expectedRuntimeFence: number | null
+      }) => ({
+        ...launchIntent(args.worktreeId, args.sessionId),
+        agent: args.agent,
+        params: {
+          ...launchIntent(args.worktreeId, args.sessionId).params,
+          agent: args.agent,
+          envelope: {
+            sessionId: args.sessionId,
+            clientOperationId: args.clientOperationId,
+            payloadFingerprint: args.payloadFingerprint,
+            expectedRuntimeFence: args.expectedRuntimeFence
+          }
+        }
+      })
+    )
+  })
+
+  it('retries a reload-interrupted launch with the persisted operation identity', async () => {
+    const worktreeId = 'wt-reload'
+    const sessionId = 'codex-reload-session'
+    localStorage.setItem(
+      'manta:structuredAgentLaunches:v1',
+      JSON.stringify([
+        {
+          sessionId,
+          agent: 'codex',
+          lifecycle: 'pending',
+          clientOperationId: 'operation-reloaded',
+          payloadFingerprint: 'fingerprint-reloaded',
+          expectedRuntimeFence: null
+        }
+      ])
+    )
+    mocks.launch.mockResolvedValueOnce({ sessionId, fence: 4 })
+    mocks.refresh
+      .mockResolvedValueOnce([])
+      .mockResolvedValueOnce([publishedSnapshot(worktreeId, sessionId)])
+
+    expect(retryStructuredAgentSessionLaunch(worktreeId, sessionId)).toBe(true)
+    await flushLaunchSettlement()
+
+    expect(mocks.launch).toHaveBeenCalledWith(
+      expect.objectContaining({
+        sessionId,
+        worktreeId,
+        params: expect.objectContaining({
+          envelope: expect.objectContaining({
+            clientOperationId: 'operation-reloaded',
+            payloadFingerprint: 'fingerprint-reloaded'
+          })
+        })
+      })
+    )
+  })
+})

ivanvykopal/orca

src/renderer/src/lib/structured-agent-session-launch-persistence.test.ts
@@ -1,0 +1,69 @@
+// @vitest-environment happy-dom
+
+import { beforeEach, describe, expect, it } from 'vitest'
+import {
+  hasStructuredAgentLaunchCancellationTombstonePersisted,
+  readStructuredAgentLaunchRecord,
+  resetStructuredAgentLaunchPersistenceForTests,
+  retireStructuredAgentLaunchCancellationTombstonePersisted,
+  writeStructuredAgentLaunchRecord,
+  markStructuredAgentLaunchCancelledPersisted
+} from './structured-agent-session-launch-persistence'
+
+describe('structured agent launch persistence', () => {
+  beforeEach(() => {
+    localStorage.clear()
+    resetStructuredAgentLaunchPersistenceForTests()
+  })
+
+  it('normalizes pending launches after a renderer reload', () => {
+    localStorage.setItem(
+      'orca:structuredAgentLaunches:v1',
+      JSON.stringify([
+        {
+          sessionId: 'codex_session',
+          agent: 'codex',
+          lifecycle: 'pending',
+          clientOperationId: 'operation-1',
+          payloadFingerprint: 'fingerprint-1',
+          expectedRuntimeFence: null
+        }
+      ])
+    )
+
+    expect(readStructuredAgentLaunchRecord('codex_session')).toMatchObject({
+      lifecycle: 'visibility-unknown',
+      clientOperationId: 'operation-1'
+    })
+  })
+
+  it('stores only content-free identity and preserves operation identity', () => {
+    writeStructuredAgentLaunchRecord({
+      sessionId: 'claude_session',
+      agent: 'claude',
+      lifecycle: 'visibility-unknown',
+      clientOperationId: 'operation-2',
+      payloadFingerprint: 'fingerprint-2',
+      expectedRuntimeFence: null,
+      resumeFrom: { providerSessionId: 'provider-thread' }
+    })
+
+    const raw = localStorage.getItem('orca:structuredAgentLaunches:v1') ?? ''
+    expect(raw).toContain('claude_session')
+    expect(raw).toContain('operation-2')
+    expect(raw).not.toContain('prompt')
+    expect(raw).not.toContain('branch')
+    expect(raw).not.toContain('path')
+    expect(readStructuredAgentLaunchRecord('claude_session')?.clientOperationId).toBe('operation-2')
+  })
+
+  it('persists cancellation tombstones by session id and retires them', () => {
+    markStructuredAgentLaunchCancelledPersisted('codex_session')
+    expect(hasStructuredAgentLaunchCancellationTombstonePersisted('codex_session')).toBe(true)
+    expect(localStorage.getItem('orca:structuredAgentLaunchCancelledSessions:v1')).toBe(
+      '["codex_session"]'
+    )
+    expect(retireStructuredAgentLaunchCancellationTombstonePersisted('codex_session')).toBe(true)
+    expect(hasStructuredAgentLaunchCancellationTombstonePersisted('codex_session')).toBe(false)
+  })
+})

src/renderer/src/lib/structured-agent-session-launch-reload.test.ts
@@ -1,0 +1,181 @@
+// @vitest-environment happy-dom
+
+import { beforeEach, describe, expect, it, vi } from 'vitest'
+import type { RuntimeMobileSessionTabsResult } from '../../../shared/runtime-session-contracts'
+import type { StructuredAgentSessionLaunchIntent } from '@/lib/launch-structured-agent-session'
+import type * as RecoveryModule from '@/lib/structured-agent-session-launch-recovery'
+
+const mocks = vi.hoisted(() => ({
+  abandonIntent: vi.fn(),
+  callStructuredAgentSession: vi.fn(),
+  launch: vi.fn(),
+  restoreIntent: vi.fn(),
+  retryIntent: vi.fn(),
+  seedDraft: vi.fn(),
+  clearDraft: vi.fn(),
+  refresh: vi.fn()
+}))
+
+vi.mock('@/lib/launch-structured-agent-session', () => {
+  class StructuredAgentSessionCreateRefusalError extends Error {}
+  return {
+    abandonStructuredAgentSessionLaunchIntent: mocks.abandonIntent,
+    createStructuredAgentSessionLaunchIntent: vi.fn(),
+    launchStructuredAgentSession: mocks.launch,
+    restoreStructuredAgentSessionLaunchIntent: mocks.restoreIntent,
+    retryStructuredAgentSessionLaunchIntent: mocks.retryIntent,
+    StructuredAgentSessionCreateRefusalError
+  }
+})
+
+vi.mock('@/lib/structured-agent-session-launch-recovery', async () => {
+  const actual = await vi.importActual<typeof RecoveryModule>(
+    '@/lib/structured-agent-session-launch-recovery'
+  )
+  return { ...actual, launchAndReconcile: vi.fn(actual.launchAndReconcile) }
+})
+
+vi.mock('@/runtime/local-structured-session-tabs-sync', () => ({
+  refreshLocalStructuredSessionTabs: mocks.refresh
+}))
+
+vi.mock('@/runtime/structured-agent-session-client', () => ({
+  callStructuredAgentSession: mocks.callStructuredAgentSession
+}))
+
+vi.mock('@/store', () => ({
+  useAppStore: {
+    getState: () => ({
+      seedNativeChatLaunchDraft: mocks.seedDraft,
+      clearNativeChatLaunchDraft: mocks.clearDraft
+    })
+  }
+}))
+
+vi.mock('@/i18n/i18n', () => ({
+  translate: (_key: string, fallback: string) => fallback
+}))
+
+vi.mock('@/lib/agent-catalog', () => ({
+  getAgentLabel: (agent: string) => (agent === 'codex' ? 'Codex' : 'Claude')
+}))
+
+import { retryStructuredAgentSessionLaunch } from './structured-agent-session-launch'
+import { resetStructuredAgentLaunchPersistenceForTests } from './structured-agent-session-launch-persistence'
+import { resetStructuredAgentLaunchRegistryForTests } from './structured-agent-session-launch-registry'
+
+function launchIntent(worktreeId: string, sessionId: string): StructuredAgentSessionLaunchIntent {
+  return {
+    worktreeId,
+    sessionId,
+    agent: 'codex',
+    params: {
+      envelope: {
+        sessionId,
+        clientOperationId: 'operation-reloaded',
+        expectedRuntimeFence: null,
+        payloadFingerprint: 'fingerprint-reloaded'
+      },
+      worktree: `id:${worktreeId}`,
+      agent: 'codex'
+    }
+  }
+}
+
+function publishedSnapshot(worktreeId: string, sessionId: string): RuntimeMobileSessionTabsResult {
+  return {
+    worktree: worktreeId,
+    publicationEpoch: 'epoch-1',
+    snapshotVersion: 1,
+    activeGroupId: null,
+    activeTabId: null,
+    activeTabType: null,
+    tabs: [
+      {
+        type: 'agent-session',
+        id: 'tab-1',
+        title: 'Codex',
+        sessionId,
+        agent: 'codex',
+        isActive: true
+      }
+    ]
+  }
+}
+
+async function flushLaunchSettlement(): Promise<void> {
+  for (let index = 0; index < 20; index += 1) {
+    await Promise.resolve()
+  }
+}
+
+describe('structured agent launch reload recovery', () => {
+  beforeEach(() => {
+    vi.clearAllMocks()
+    localStorage.clear()
+    resetStructuredAgentLaunchPersistenceForTests()
+    resetStructuredAgentLaunchRegistryForTests()
+    mocks.callStructuredAgentSession.mockResolvedValue({ ok: true, page: { fence: 4 } })
+    mocks.restoreIntent.mockImplementation(
+      (args: {
+        worktreeId: string
+        sessionId: string
+        agent: 'claude' | 'codex'
+        clientOperationId: string
+        payloadFingerprint: string
+        expectedRuntimeFence: number | null
+      }) => ({
+        ...launchIntent(args.worktreeId, args.sessionId),
+        agent: args.agent,
+        params: {
+          ...launchIntent(args.worktreeId, args.sessionId).params,
+          agent: args.agent,
+          envelope: {
+            sessionId: args.sessionId,
+            clientOperationId: args.clientOperationId,
+            payloadFingerprint: args.payloadFingerprint,
+            expectedRuntimeFence: args.expectedRuntimeFence
+          }
+        }
+      })
+    )
+  })
+
+  it('retries a reload-interrupted launch with the persisted operation identity', async () => {
+    const worktreeId = 'wt-reload'
+    const sessionId = 'codex-reload-session'
+    localStorage.setItem(
+      'orca:structuredAgentLaunches:v1',
+      JSON.stringify([
+        {
+          sessionId,
+          agent: 'codex',
+          lifecycle: 'pending',
+          clientOperationId: 'operation-reloaded',
+          payloadFingerprint: 'fingerprint-reloaded',
+          expectedRuntimeFence: null
+        }
+      ])
+    )
+    mocks.launch.mockResolvedValueOnce({ sessionId, fence: 4 })
+    mocks.refresh
+      .mockResolvedValueOnce([])
+      .mockResolvedValueOnce([publishedSnapshot(worktreeId, sessionId)])
+
+    expect(retryStructuredAgentSessionLaunch(worktreeId, sessionId)).toBe(true)
+    await flushLaunchSettlement()
+
+    expect(mocks.launch).toHaveBeenCalledWith(
+      expect.objectContaining({
+        sessionId,
+        worktreeId,
+        params: expect.objectContaining({
+          envelope: expect.objectContaining({
+            clientOperationId: 'operation-reloaded',
+            payloadFingerprint: 'fingerprint-reloaded'
+          })
+        })
+      })
+    )
+  })
+})