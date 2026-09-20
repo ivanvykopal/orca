paidaxingyo666/Manta

fix(settings): keep integration connect dialog drafts on backdrop click (#20932)
* fix(settings): keep integration connect dialog drafts on backdrop click

A backdrop click in the Settings → Integrations Jira/Linear/Bitbucket
connect dialogs dismissed the Radix modal, and each dialog's reset-on-open
then wiped the typed credential.

Generalize SshTargetForm's dirty-gated outside dismissal into a shared
preventOutsideDismissWhenDirty factory and wire it into the three dialogs
(and SshTargetForm), so an accidental backdrop click no longer discards a
draft while Escape / Cancel / × remain the explicit discard paths.

Bitbucket compares email/baseUrl against a props-seeded baseline and only
counts the active auth mode's fields, so a mid-edit status refresh and a
mode toggle cannot make the form sticky.

STA-7332

* test(e2e): drop ticket id from dismiss spec comment

Mirror-Of: bfc297df9933e5e4e419ad8519a67f86ba1a9496

tests/e2e/settings-integration-dialog-dismiss.spec.ts
@@ -1,0 +1,91 @@
+/**
+ * A backdrop click in the Settings → Integrations Linear and Jira connect dialogs must
+ * not close the dialog and discard typed credentials. Escape / Cancel stay the explicit discard
+ * paths. (Bitbucket's baseline-seeded predicate is covered by the component tests.)
+ *
+ * Mirrors the SSH host form modal guard (tests/e2e/ssh-host-form-modal.spec.ts).
+ */
+
+import type { Page } from '@stablyai/playwright-test'
+import { expect, test } from './helpers/manta-app'
+import { dismissTransientAnnouncement } from './helpers/ssh-config-host-picker'
+import { waitForSessionReady } from './helpers/store'
+
+async function openIntegrationsSettings(page: Page): Promise<void> {
+  await page.evaluate(async () => {
+    const store = window.__store
+    if (!store) {
+      throw new Error('window.__store is not available')
+    }
+    // Why: the spec asserts on English strings; the host may run a non-English locale.
+    await store.getState().updateSettings({ uiLanguage: 'en' })
+    store.getState().openSettingsTarget({ pane: 'integrations', repoId: null })
+    store.getState().openSettingsPage()
+  })
+  await expect(page.getByPlaceholder('Search settings')).toBeVisible({ timeout: 10_000 })
+  await dismissTransientAnnouncement(page)
+}
+
+async function clickBackdrop(page: Page): Promise<void> {
+  // Why: the overlay is fixed inset-0 and the dialog sits over its center, so click near a corner.
+  await page.locator('[data-slot="dialog-overlay"]').click({ position: { x: 8, y: 8 } })
+}
+
+test.describe('Settings integrations connect dialogs', () => {
+  test.beforeEach(async ({ mantaPage }) => {
+    await waitForSessionReady(mantaPage)
+    await openIntegrationsSettings(mantaPage)
+  })
+
+  test('Linear API key draft survives a backdrop click but clears on cancel', async ({
+    mantaPage
+  }) => {
+    const card = mantaPage.locator('[data-settings-section="integrations-linear"]')
+    // Why: the button label depends on connection state; a fresh profile is disconnected.
+    const openButton = card.getByRole('button', {
+      name: /^(Add Linear access|Add workspace access)$/
+    })
+    await expect(openButton).toBeVisible({ timeout: 15_000 })
+    await openButton.click()
+
+    const dialog = mantaPage.getByRole('dialog', { name: 'Add Linear access' })
+    await expect(dialog).toBeVisible()
+    const keyInput = dialog.locator('input[type="password"]')
+    await keyInput.fill('lin_api_e2e_secret')
+
+    await clickBackdrop(mantaPage)
+    // Why: assert the settled open state, not the exit-animation frame a broken guard would leave.
+    await expect(dialog).toHaveAttribute('data-state', 'open')
+    await expect(keyInput).toHaveValue('lin_api_e2e_secret')
+
+    await dialog.getByRole('button', { name: 'Cancel' }).click()
+    await expect(dialog).toBeHidden()
+
+    // Explicit cancel discards; reopening starts empty.
+    await openButton.click()
+    await expect(dialog.locator('input[type="password"]')).toHaveValue('')
+    await dialog.getByRole('button', { name: 'Cancel' }).click()
+    await expect(dialog).toBeHidden()
+  })
+
+  test('Jira site URL draft survives a backdrop click', async ({ mantaPage }) => {
+    const card = mantaPage.locator('[data-settings-section="integrations-jira"]')
+    // Why: the button label depends on connection state; a fresh profile is disconnected.
+    const openButton = card.getByRole('button', { name: /^(Connect Jira|Add Jira site)$/ })
+    await expect(openButton).toBeVisible({ timeout: 15_000 })
+    await openButton.click()
+
+    const dialog = mantaPage.getByRole('dialog', { name: 'Connect Jira site' })
+    await expect(dialog).toBeVisible()
+    const siteUrlInput = dialog.locator('input[placeholder="https://example.atlassian.net"]')
+    await siteUrlInput.fill('https://acme.atlassian.net')
+
+    await clickBackdrop(mantaPage)
+    // Why: assert the settled open state, not the exit-animation frame a broken guard would leave.
+    await expect(dialog).toHaveAttribute('data-state', 'open')
+    await expect(siteUrlInput).toHaveValue('https://acme.atlassian.net')
+
+    await dialog.getByRole('button', { name: 'Cancel' }).click()
+    await expect(dialog).toBeHidden()
+  })
+})

ivanvykopal/orca

tests/e2e/settings-integration-dialog-dismiss.spec.ts
@@ -1,0 +1,91 @@
+/**
+ * A backdrop click in the Settings → Integrations Linear and Jira connect dialogs must
+ * not close the dialog and discard typed credentials. Escape / Cancel stay the explicit discard
+ * paths. (Bitbucket's baseline-seeded predicate is covered by the component tests.)
+ *
+ * Mirrors the SSH host form modal guard (tests/e2e/ssh-host-form-modal.spec.ts).
+ */
+
+import type { Page } from '@stablyai/playwright-test'
+import { expect, test } from './helpers/orca-app'
+import { dismissTransientAnnouncement } from './helpers/ssh-config-host-picker'
+import { waitForSessionReady } from './helpers/store'
+
+async function openIntegrationsSettings(page: Page): Promise<void> {
+  await page.evaluate(async () => {
+    const store = window.__store
+    if (!store) {
+      throw new Error('window.__store is not available')
+    }
+    // Why: the spec asserts on English strings; the host may run a non-English locale.
+    await store.getState().updateSettings({ uiLanguage: 'en' })
+    store.getState().openSettingsTarget({ pane: 'integrations', repoId: null })
+    store.getState().openSettingsPage()
+  })
+  await expect(page.getByPlaceholder('Search settings')).toBeVisible({ timeout: 10_000 })
+  await dismissTransientAnnouncement(page)
+}
+
+async function clickBackdrop(page: Page): Promise<void> {
+  // Why: the overlay is fixed inset-0 and the dialog sits over its center, so click near a corner.
+  await page.locator('[data-slot="dialog-overlay"]').click({ position: { x: 8, y: 8 } })
+}
+
+test.describe('Settings integrations connect dialogs', () => {
+  test.beforeEach(async ({ orcaPage }) => {
+    await waitForSessionReady(orcaPage)
+    await openIntegrationsSettings(orcaPage)
+  })
+
+  test('Linear API key draft survives a backdrop click but clears on cancel', async ({
+    orcaPage
+  }) => {
+    const card = orcaPage.locator('[data-settings-section="integrations-linear"]')
+    // Why: the button label depends on connection state; a fresh profile is disconnected.
+    const openButton = card.getByRole('button', {
+      name: /^(Add Linear access|Add workspace access)$/
+    })
+    await expect(openButton).toBeVisible({ timeout: 15_000 })
+    await openButton.click()
+
+    const dialog = orcaPage.getByRole('dialog', { name: 'Add Linear access' })
+    await expect(dialog).toBeVisible()
+    const keyInput = dialog.locator('input[type="password"]')
+    await keyInput.fill('lin_api_e2e_secret')
+
+    await clickBackdrop(orcaPage)
+    // Why: assert the settled open state, not the exit-animation frame a broken guard would leave.
+    await expect(dialog).toHaveAttribute('data-state', 'open')
+    await expect(keyInput).toHaveValue('lin_api_e2e_secret')
+
+    await dialog.getByRole('button', { name: 'Cancel' }).click()
+    await expect(dialog).toBeHidden()
+
+    // Explicit cancel discards; reopening starts empty.
+    await openButton.click()
+    await expect(dialog.locator('input[type="password"]')).toHaveValue('')
+    await dialog.getByRole('button', { name: 'Cancel' }).click()
+    await expect(dialog).toBeHidden()
+  })
+
+  test('Jira site URL draft survives a backdrop click', async ({ orcaPage }) => {
+    const card = orcaPage.locator('[data-settings-section="integrations-jira"]')
+    // Why: the button label depends on connection state; a fresh profile is disconnected.
+    const openButton = card.getByRole('button', { name: /^(Connect Jira|Add Jira site)$/ })
+    await expect(openButton).toBeVisible({ timeout: 15_000 })
+    await openButton.click()
+
+    const dialog = orcaPage.getByRole('dialog', { name: 'Connect Jira site' })
+    await expect(dialog).toBeVisible()
+    const siteUrlInput = dialog.locator('input[placeholder="https://example.atlassian.net"]')
+    await siteUrlInput.fill('https://acme.atlassian.net')
+
+    await clickBackdrop(orcaPage)
+    // Why: assert the settled open state, not the exit-animation frame a broken guard would leave.
+    await expect(dialog).toHaveAttribute('data-state', 'open')
+    await expect(siteUrlInput).toHaveValue('https://acme.atlassian.net')
+
+    await dialog.getByRole('button', { name: 'Cancel' }).click()
+    await expect(dialog).toBeHidden()
+  })
+})