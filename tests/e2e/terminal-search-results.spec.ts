paidaxingyo666/Manta

feat(terminal): search match count + Cmd+F focus parity (#9035)
* feat(terminal): show search match count and keep Cmd+F from closing search

Bring the terminal search bar to parity with the editor find bars:

- Show a live match indicator (0/0, current/total, "No results", or
  <count>+ past the highlight limit) driven by the xterm SearchAddon
  onDidChangeResults event.
- A repeat Cmd+F while the search is open now re-focuses and selects the
  query instead of toggling the panel closed; Esc remains the close path.

Adds unit coverage for the indicator states and the toggle decision.

* Use auto-generated localization key for TerminalSearch no-results (#9035)

Replace the hand-written "noResults" i18n key with the SHA1-based auto key
(auto.components.TerminalSearch.10e039b591) to match the repo's auto-keying
convention, and sync the key across all locale catalogs.

Addresses CodeRabbit review feedback.

* test(terminal): cover search dispatch after keyboard module split

* fix(terminal): refocus search from its input and verify real matches

* test(terminal): use portable echo commands for search proof

* refactor(terminal): keep search subscription and cleanup together

---------

Co-authored-by: Neil <neil@stably.ai>

Mirror-Of: de15227a1d321840ea35c6bb2d0cc01e3409e5f1

tests/e2e/terminal-search-results.spec.ts
@@ -1,0 +1,57 @@
+import { test, expect } from './helpers/manta-app'
+import { ensureTerminalVisible, waitForActiveWorktree, waitForSessionReady } from './helpers/store'
+import {
+  execInTerminal,
+  focusActiveTerminalInput,
+  getTerminalContent,
+  waitForActivePanePtyId,
+  waitForActiveTerminalManager
+} from './helpers/terminal'
+
+test('terminal search counts real matches and repeat find selects the query', async ({
+  mantaPage
+}, testInfo) => {
+  await waitForSessionReady(mantaPage)
+  await waitForActiveWorktree(mantaPage)
+  await ensureTerminalVisible(mantaPage)
+  await waitForActiveTerminalManager(mantaPage, 30_000)
+  const ptyId = await waitForActivePanePtyId(mantaPage)
+  for (let line = 0; line < 3; line++) {
+    await execInTerminal(mantaPage, ptyId, 'echo manta-search-proof')
+  }
+  await expect
+    .poll(
+      async () =>
+        (await getTerminalContent(mantaPage))
+          .split(/\r?\n/)
+          .filter((line) => line.trim() === 'manta-search-proof').length
+    )
+    .toBe(3)
+  await focusActiveTerminalInput(mantaPage)
+  const modifier = process.platform === 'darwin' ? 'Meta' : 'Control'
+  await mantaPage.keyboard.press(`${modifier}+f`)
+  const search = mantaPage.locator('[data-terminal-search-root]')
+  const input = search.locator('input')
+  await expect(input).toBeFocused()
+  await search.getByTitle('Regex', { exact: true }).click()
+  const query = '^manta-search-proof$'
+  await input.fill(query)
+  await expect(search).toContainText(/[1-3]\/3/)
+  const initialCount = await search.innerText()
+  await input.press('Enter')
+  await expect.poll(() => search.innerText()).not.toBe(initialCount)
+  await mantaPage.screenshot({ path: testInfo.outputPath('search-results.png') })
+  await input.press('ArrowLeft')
+  await mantaPage.keyboard.press(`${modifier}+f`)
+  await expect(input).toBeFocused()
+  await expect
+    .poll(() =>
+      input.evaluate((element) => ({ start: element.selectionStart, end: element.selectionEnd }))
+    )
+    .toEqual({ start: 0, end: query.length })
+  await mantaPage.screenshot({ path: testInfo.outputPath('search-query-selected.png') })
+  await input.fill('no-such-search-result-314159')
+  await expect(search).toContainText('No results')
+  await input.press('Escape')
+  await expect(search).toBeHidden()
+})

ivanvykopal/orca

tests/e2e/terminal-search-results.spec.ts
@@ -1,0 +1,57 @@
+import { test, expect } from './helpers/orca-app'
+import { ensureTerminalVisible, waitForActiveWorktree, waitForSessionReady } from './helpers/store'
+import {
+  execInTerminal,
+  focusActiveTerminalInput,
+  getTerminalContent,
+  waitForActivePanePtyId,
+  waitForActiveTerminalManager
+} from './helpers/terminal'
+
+test('terminal search counts real matches and repeat find selects the query', async ({
+  orcaPage
+}, testInfo) => {
+  await waitForSessionReady(orcaPage)
+  await waitForActiveWorktree(orcaPage)
+  await ensureTerminalVisible(orcaPage)
+  await waitForActiveTerminalManager(orcaPage, 30_000)
+  const ptyId = await waitForActivePanePtyId(orcaPage)
+  for (let line = 0; line < 3; line++) {
+    await execInTerminal(orcaPage, ptyId, 'echo orca-search-proof')
+  }
+  await expect
+    .poll(
+      async () =>
+        (await getTerminalContent(orcaPage))
+          .split(/\r?\n/)
+          .filter((line) => line.trim() === 'orca-search-proof').length
+    )
+    .toBe(3)
+  await focusActiveTerminalInput(orcaPage)
+  const modifier = process.platform === 'darwin' ? 'Meta' : 'Control'
+  await orcaPage.keyboard.press(`${modifier}+f`)
+  const search = orcaPage.locator('[data-terminal-search-root]')
+  const input = search.locator('input')
+  await expect(input).toBeFocused()
+  await search.getByTitle('Regex', { exact: true }).click()
+  const query = '^orca-search-proof$'
+  await input.fill(query)
+  await expect(search).toContainText(/[1-3]\/3/)
+  const initialCount = await search.innerText()
+  await input.press('Enter')
+  await expect.poll(() => search.innerText()).not.toBe(initialCount)
+  await orcaPage.screenshot({ path: testInfo.outputPath('search-results.png') })
+  await input.press('ArrowLeft')
+  await orcaPage.keyboard.press(`${modifier}+f`)
+  await expect(input).toBeFocused()
+  await expect
+    .poll(() =>
+      input.evaluate((element) => ({ start: element.selectionStart, end: element.selectionEnd }))
+    )
+    .toEqual({ start: 0, end: query.length })
+  await orcaPage.screenshot({ path: testInfo.outputPath('search-query-selected.png') })
+  await input.fill('no-such-search-result-314159')
+  await expect(search).toContainText('No results')
+  await input.press('Escape')
+  await expect(search).toBeHidden()
+})