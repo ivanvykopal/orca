paidaxingyo666/Manta

fix(omp): preserve status after terminal title owner rewrite (#20610)
Validated and independently reviewed OMP integration fix.

Co-authored-by: shahidbeig-a11y <258701601+shahidbeig-a11y@users.noreply.github.com>

Mirror-Of: 3632311d0b8ae24a9bc5a9d50bb66ca26b25dd18

tests/e2e/omp-title-marker.spec.ts
@@ -1,0 +1,43 @@
+import { writeFile } from 'node:fs/promises'
+import { buildShellCommandFromArgv } from '../../src/shared/tui-agent-startup-shell'
+import { test, expect } from './helpers/manta-app'
+import { ensureTerminalVisible, waitForActiveWorktree, waitForSessionReady } from './helpers/store'
+import {
+  execInTerminal,
+  sendToTerminal,
+  waitForActivePanePtyId,
+  waitForActiveTerminalManager
+} from './helpers/terminal'
+
+test('OMP spaced-colon title renders working and clears on idle', async ({
+  mantaPage
+}, testInfo) => {
+  test.skip(
+    process.platform === 'win32',
+    'POSIX title replay; Windows formatter bytes have separate coverage'
+  )
+  await waitForSessionReady(mantaPage)
+  await waitForActiveWorktree(mantaPage)
+  await ensureTerminalVisible(mantaPage)
+  await waitForActiveTerminalManager(mantaPage)
+  const ptyId = await waitForActivePanePtyId(mantaPage)
+  const script = testInfo.outputPath('title-replay.cjs')
+  await writeFile(
+    script,
+    `
+process.stdout.write('\\x1b]0;OMP : Image review\\x07')
+process.stdin.on('data', () => process.stdout.write('\\x1b]0;OMP > Image review\\x07'))
+`
+  )
+  await execInTerminal(
+    mantaPage,
+    ptyId,
+    buildShellCommandFromArgv([process.execPath, script], 'posix')
+  )
+  const working = mantaPage.locator('[aria-label="Working"]')
+  await expect(working.first()).toBeVisible({ timeout: 15000 })
+  await mantaPage.screenshot({ path: testInfo.outputPath('omp-title-working.png') })
+  await sendToTerminal(mantaPage, ptyId, '\r')
+  await expect(working).toHaveCount(0)
+  await mantaPage.screenshot({ path: testInfo.outputPath('omp-title-idle.png') })
+})

ivanvykopal/orca

tests/e2e/omp-title-marker.spec.ts
@@ -1,0 +1,43 @@
+import { writeFile } from 'node:fs/promises'
+import { buildShellCommandFromArgv } from '../../src/shared/tui-agent-startup-shell'
+import { test, expect } from './helpers/orca-app'
+import { ensureTerminalVisible, waitForActiveWorktree, waitForSessionReady } from './helpers/store'
+import {
+  execInTerminal,
+  sendToTerminal,
+  waitForActivePanePtyId,
+  waitForActiveTerminalManager
+} from './helpers/terminal'
+
+test('OMP spaced-colon title renders working and clears on idle', async ({
+  orcaPage
+}, testInfo) => {
+  test.skip(
+    process.platform === 'win32',
+    'POSIX title replay; Windows formatter bytes have separate coverage'
+  )
+  await waitForSessionReady(orcaPage)
+  await waitForActiveWorktree(orcaPage)
+  await ensureTerminalVisible(orcaPage)
+  await waitForActiveTerminalManager(orcaPage)
+  const ptyId = await waitForActivePanePtyId(orcaPage)
+  const script = testInfo.outputPath('title-replay.cjs')
+  await writeFile(
+    script,
+    `
+process.stdout.write('\\x1b]0;OMP : Image review\\x07')
+process.stdin.on('data', () => process.stdout.write('\\x1b]0;OMP > Image review\\x07'))
+`
+  )
+  await execInTerminal(
+    orcaPage,
+    ptyId,
+    buildShellCommandFromArgv([process.execPath, script], 'posix')
+  )
+  const working = orcaPage.locator('[aria-label="Working"]')
+  await expect(working.first()).toBeVisible({ timeout: 15000 })
+  await orcaPage.screenshot({ path: testInfo.outputPath('omp-title-working.png') })
+  await sendToTerminal(orcaPage, ptyId, '\r')
+  await expect(working).toHaveCount(0)
+  await orcaPage.screenshot({ path: testInfo.outputPath('omp-title-idle.png') })
+})