paidaxingyo666/Manta

feat(mobile-web-bundle): advertise the bundle capability where a bundle ships (OTA phase A, 4/5) (#21376)
* feat(mobile-web-bundle): advertise the bundle capability where one ships

status.get pushes mobileWeb.bundle.v1 only when the install's bundle resolves and
its manifest parses, beside the other conditional capabilities. Dev trees and
`orca serve` installs may carry no out/mobile-web, and a static entry there would
promise a download that only ever answers mobile_web_bundle_unavailable.

No protocol version bump: protocol-version.ts asks for one when a method or a
required field is removed or changes meaning, not when a capability is added.

Claude-Session: https://claude.ai/code/session_01JNnE9qzUZMMnqpZWCqM3nb

* test(mobile): pin that mobileWeb.bundle.v1 is inert on a released client

Derives the old desktop's reply by removing the one capability from what the new
one sends, rather than writing down what the old client had, and asserts every
released read of status.get lands identically apart from that string: the gate
hook, the three transport readers, the quick-command predicate and the
worktree-create support probe.

Proved red against three mutants: a closed enum on the capability schema (the
salvaged field drops whole, so nothing publishes), a client-side filter over the
new name, and a gate that changes floatingWorkspaceEnabled when it sees it.

Claude-Session: https://claude.ai/code/session_01JNnE9qzUZMMnqpZWCqM3nb

* chore(mobile): name the invariant behind the fake client's cast

The changed-code casting gate wants the rationale on the line, and the reason is
narrow enough to state: every reader under test reaches the client through an rpc
operation's `request`, which uses sendRequest alone.

Claude-Session: https://claude.ai/code/session_01JNnE9qzUZMMnqpZWCqM3nb

Mirror-Of: b90837ee469da2aee17b3ea8a2c4bc1ad42974d7

src/main/runtime/manta-runtime-tests/mobile-web-bundle-capability.spec.ts
@@ -1,0 +1,65 @@
+import { mkdtempSync, rmSync, writeFileSync } from 'node:fs'
+import { tmpdir } from 'node:os'
+import { join } from 'node:path'
+import { afterEach, describe, expect, it } from 'vitest'
+import { createRuntime } from '../manta-runtime-test-fixtures.spec'
+import { resetBundledMobileWebBundleCacheForTests } from '../bundled-mobile-web-bundle'
+import {
+  installMobileWebBundleAppPath,
+  writeSyntheticMobileWebBundle
+} from '../rpc/methods/mobile-web-bundle.test-fixture'
+import { MOBILE_WEB_BUNDLE_CAPABILITY } from '../../../shared/mobile-web-bundle/mobile-web-bundle-capability'
+
+// The resolver caches per process and the aggregated suite shares that process, so each case
+// installs its own root and hands the cache back the way it found it.
+function statusCapabilitiesForInstall(appPath: string): readonly string[] {
+  resetBundledMobileWebBundleCacheForTests()
+  installMobileWebBundleAppPath(appPath)
+  const { capabilities } = createRuntime().getStatus()
+  if (!capabilities) {
+    // A status carrying no list at all would let every absence assertion below pass vacuously.
+    throw new Error('status.get answered no capability list')
+  }
+  return capabilities
+}
+
+function withInstallRoot(prefix: string, run: (install: string) => void): void {
+  const install = mkdtempSync(join(tmpdir(), prefix))
+  try {
+    run(install)
+  } finally {
+    rmSync(install, { recursive: true, force: true })
+  }
+}
+
+describe('MantaRuntimeService mobile web bundle capability', () => {
+  afterEach(() => {
+    resetBundledMobileWebBundleCacheForTests()
+  })
+
+  // The mixed-version guarantee: a dev tree or an `manta serve` install that never built
+  // out/mobile-web must not promise a bundle it can only refuse.
+  it('omits the mobile web bundle capability where the install carries no bundle', () => {
+    withInstallRoot('orca-mobile-web-absent-', (install) => {
+      expect(statusCapabilitiesForInstall(install)).not.toContain(MOBILE_WEB_BUNDLE_CAPABILITY)
+    })
+  })
+
+  it('advertises the mobile web bundle capability where the install carries one', () => {
+    withInstallRoot('orca-mobile-web-present-', (install) => {
+      writeSyntheticMobileWebBundle(join(install, 'out', 'mobile-web'), 4)
+      expect(statusCapabilitiesForInstall(install)).toContain(MOBILE_WEB_BUNDLE_CAPABILITY)
+    })
+  })
+
+  // A manifest the contract rejects is the same answer as no bundle, so advertising off the
+  // directory's existence rather than off a parsed manifest would promise a download that errors.
+  it('omits the capability where the bundle manifest does not parse', () => {
+    withInstallRoot('orca-mobile-web-unparseable-', (install) => {
+      const root = join(install, 'out', 'mobile-web')
+      writeSyntheticMobileWebBundle(root, 5)
+      writeFileSync(join(root, 'manifest.json'), '{"schemaVersion":2}', 'utf8')
+      expect(statusCapabilitiesForInstall(install)).not.toContain(MOBILE_WEB_BUNDLE_CAPABILITY)
+    })
+  })
+})

ivanvykopal/orca

src/main/runtime/orca-runtime-tests/mobile-web-bundle-capability.spec.ts
@@ -1,0 +1,65 @@
+import { mkdtempSync, rmSync, writeFileSync } from 'node:fs'
+import { tmpdir } from 'node:os'
+import { join } from 'node:path'
+import { afterEach, describe, expect, it } from 'vitest'
+import { createRuntime } from '../orca-runtime-test-fixtures.spec'
+import { resetBundledMobileWebBundleCacheForTests } from '../bundled-mobile-web-bundle'
+import {
+  installMobileWebBundleAppPath,
+  writeSyntheticMobileWebBundle
+} from '../rpc/methods/mobile-web-bundle.test-fixture'
+import { MOBILE_WEB_BUNDLE_CAPABILITY } from '../../../shared/mobile-web-bundle/mobile-web-bundle-capability'
+
+// The resolver caches per process and the aggregated suite shares that process, so each case
+// installs its own root and hands the cache back the way it found it.
+function statusCapabilitiesForInstall(appPath: string): readonly string[] {
+  resetBundledMobileWebBundleCacheForTests()
+  installMobileWebBundleAppPath(appPath)
+  const { capabilities } = createRuntime().getStatus()
+  if (!capabilities) {
+    // A status carrying no list at all would let every absence assertion below pass vacuously.
+    throw new Error('status.get answered no capability list')
+  }
+  return capabilities
+}
+
+function withInstallRoot(prefix: string, run: (install: string) => void): void {
+  const install = mkdtempSync(join(tmpdir(), prefix))
+  try {
+    run(install)
+  } finally {
+    rmSync(install, { recursive: true, force: true })
+  }
+}
+
+describe('OrcaRuntimeService mobile web bundle capability', () => {
+  afterEach(() => {
+    resetBundledMobileWebBundleCacheForTests()
+  })
+
+  // The mixed-version guarantee: a dev tree or an `orca serve` install that never built
+  // out/mobile-web must not promise a bundle it can only refuse.
+  it('omits the mobile web bundle capability where the install carries no bundle', () => {
+    withInstallRoot('orca-mobile-web-absent-', (install) => {
+      expect(statusCapabilitiesForInstall(install)).not.toContain(MOBILE_WEB_BUNDLE_CAPABILITY)
+    })
+  })
+
+  it('advertises the mobile web bundle capability where the install carries one', () => {
+    withInstallRoot('orca-mobile-web-present-', (install) => {
+      writeSyntheticMobileWebBundle(join(install, 'out', 'mobile-web'), 4)
+      expect(statusCapabilitiesForInstall(install)).toContain(MOBILE_WEB_BUNDLE_CAPABILITY)
+    })
+  })
+
+  // A manifest the contract rejects is the same answer as no bundle, so advertising off the
+  // directory's existence rather than off a parsed manifest would promise a download that errors.
+  it('omits the capability where the bundle manifest does not parse', () => {
+    withInstallRoot('orca-mobile-web-unparseable-', (install) => {
+      const root = join(install, 'out', 'mobile-web')
+      writeSyntheticMobileWebBundle(root, 5)
+      writeFileSync(join(root, 'manifest.json'), '{"schemaVersion":2}', 'utf8')
+      expect(statusCapabilitiesForInstall(install)).not.toContain(MOBILE_WEB_BUNDLE_CAPABILITY)
+    })
+  })
+})