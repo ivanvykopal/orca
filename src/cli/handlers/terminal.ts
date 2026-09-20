paidaxingyo666/Manta

fix(terminal): let a runtime-created Windows terminal BE the requested shell (#20825)
* fix(terminal): let a runtime-created Windows terminal BE the requested shell

`orca terminal create --environment <windows-host> --command 'cmd.exe'` never
created a cmd terminal. `--command` is text the provider TYPES into whatever
shell it spawned, so the PTY stayed the host's default shell with cmd running
inside it. Captured on `awin`, whose default is Git Bash:

    $ orca terminal create --environment awin --command 'cmd.exe' --json
    $ orca terminal send --environment awin --terminal term_10656cf7... \
        --text exit --enter
    $ orca terminal read --environment awin --terminal term_10656cf7... --screen
      neil@awin MINGW64 ~/orca/orca ((30f820708f...))
      $ cmd.exe
      Microsoft Windows [Version 10.0.26200.9445]
      C:\Users\neil\orca\orca>exit
      neil@awin MINGW64 ~/orca/orca ((30f820708f...))
      $

The handle is alive the whole time and `terminal list` shows one healthy
terminal, because the PTY never changed — so the only symptom is that the
caller's terminal is now a shell it never asked for, and every later `send` is
quoted for the wrong one. On `win-lowspec` (default pwsh) the same create lands
cmd inside PowerShell.

Root cause
----------
There are two spawn preflights and they are twins:

- `src/main/ipc/pty/ipc/spawn-preflight.ts` — renderer/IPC spawns, i.e. a
  terminal tab opened in the app.
- `src/main/ipc/pty/runtime/spawn-preflight.ts` — runtime spawns: the CLI's
  `terminal.create`, headless `orca serve`, and every paired remote
  environment.

Only the IPC twin read the caller's requested shell. The runtime twin passed a
literal `requestedShellOverride: undefined`, so a runtime-created terminal on
Windows could only ever be the host default. Everything downstream of that
point — `spawn-options`, the daemon, `resolvePtyShellOverride` in the relay,
`local-pty-launch-plan` — already honoured `shellOverride`; nothing upstream
could supp

src/cli/handlers/terminal.ts
@@ -32,6 +32,11 @@ import {
 } from '../omitted-host-scope-selectors'
 import { RuntimeClientError } from '../runtime-client'
 import {
+  isSupportedWindowsShellOverride,
+  listSupportedWindowsShellOverrides
+} from '../../shared/windows-terminal-shell'
+import { TERMINAL_CREATE_SHELL_SELECTION_RUNTIME_CAPABILITY } from '../../shared/protocol-version'
+import {
   getBrowserWorktreeSelector,
   getOptionalWorktreeSelector,
   getRequiredWorktreeSelector,
@@ -153,9 +158,40 @@ export const TERMINAL_HANDLERS: Record<string, CommandHandler> = {
     const useRendererBackedInteractiveTerminal =
       !client.isRemote && shouldUseRendererBackedInteractiveTerminal(command)
     const focus = flags.get('focus') === true
+    const shell = getOptionalStringFlag(flags, 'shell')
+    if (shell !== undefined) {
+      if (!isSupportedWindowsShellOverride(shell)) {
+        throw new RuntimeClientError(
+          'invalid_argument',
+          `--shell must be one of: ${listSupportedWindowsShellOverrides().join(', ')}`
+        )
+      }
+      // Why refused rather than sent hopefully: an older host strips the unknown param and hands
+      // back a healthy terminal running its DEFAULT shell. Nothing in that reply says the shell
+      // was ignored, so a caller that wanted cmd would drive a PowerShell session believing it won.
+      const status = await client.getCliStatus()
+      // An unreachable host reports no capabilities at all; that is not evidence it lacks --shell.
+      if (!status.result.runtime.reachable) {
+        throw new RuntimeClientError(
+          'runtime_unavailable',
+          'Manta could not verify --shell support on the execution host, so no terminal was created. Wait for the execution host to become reachable and retry.'
+        )
+      }
+      if (
+        status.result.runtime.capabilities?.includes(
+          TERMINAL_CREATE_SHELL_SELECTION_RUNTIME_CAPABILITY
+        ) !== true
+      ) {
+        throw new RuntimeClientError(
+          'incompatible_runtime',
+          'This Manta host does not support --shell, and would silently create a terminal running its default shell instead. No terminal was created; update Manta on the execution host.'
+        )
+      }
+    }
     const result = await client.call<{ terminal: RuntimeTerminalCreate }>('terminal.create', {
       worktree: await getBrowserWorktreeSelector(flags, cwd, client),
       command,
+      ...(shell !== undefined ? { shell } : {}),
       title: getOptionalStringFlag(flags, 'title'),
       // Why: interactive local agent TUIs need the renderer-backed terminal
       // path for browser-side features, but CLI creates must stay backgrounded

src/cli/root-help-text-secondary.ts
@@ -62,7 +62,7 @@ export const ROOT_HELP_TEXT_SECONDARY = [
   '  manta terminal read [--terminal <handle>] [--cursor <n>] [--limit <n>] [--json]',
   '  manta terminal send [--terminal <handle>] [--text <text>] [--enter] [--interrupt] [--wait-submit <seconds>] [--retry-request <id>] [--json]',
   '  manta terminal wait [--terminal <handle>] --for exit|tui-idle [--timeout-ms <ms>] [--json]',
-  '  manta terminal create [--worktree <selector>] [--title <name>] [--command <text>] [--focus] [--json]',
+  '  manta terminal create [--worktree <selector>] [--title <name>] [--command <text>] [--shell <shell>] [--focus] [--json]',
   '  manta terminal split [--terminal <handle>] [--direction horizontal|vertical] [--json]',
   '  manta terminal switch [--terminal <handle>] [--json]',
   '  manta terminal close ([--terminal <handle>] [--tab] | --worktree <selector> --all) [--json]',

src/cli/specs/core.ts
@@ -250,17 +250,20 @@ export const CORE_COMMAND_SPECS: CommandSpec[] = [
     path: ['terminal', 'create'],
     summary: 'Create a terminal session in the current worktree',
     usage:
-      'manta terminal create [--worktree <selector>] [--title <name>] [--command <text>] [--focus] [--json]',
-    allowedFlags: [...GLOBAL_FLAGS, 'worktree', 'command', 'title', 'focus'],
+      'manta terminal create [--worktree <selector>] [--title <name>] [--command <text>] [--shell <shell>] [--focus] [--json]',
+    allowedFlags: [...GLOBAL_FLAGS, 'worktree', 'command', 'shell', 'title', 'focus'],
     notes: [
       'Creates a visible terminal tab without switching focus when possible; falls back to a background handle if the UI cannot adopt it. Pass --focus to switch to it.',
-      'Use this, not worktree create, for a fresh agent in the current checkout.'
+      'Use this, not worktree create, for a fresh agent in the current checkout.',
+      '--shell picks the shell the terminal IS on a Windows host (cmd.exe, powershell.exe, pwsh.exe, wsl.exe, bash.exe, git-bash); --command is typed into whatever shell the host started, so `--command cmd.exe` leaves a cmd running INSIDE the default shell and exiting it drops back to that shell.',
+      'A host that cannot apply --shell refuses the create rather than quietly spawning its default shell: macOS and Linux execution hosts spawn the login shell, terminals routed over SSH resolve their shell on the SSH host, a --shell that contradicts the project execution runtime (WSL vs Windows host) is refused, and a Manta host older than --shell is refused by the CLI.'
     ],
     examples: [
       'manta terminal create --json',
       'manta terminal create --worktree active --command "codex" --json',
       'manta terminal create --worktree path:/projects/myapp --title "RUNNER" --command "opencode"',
-      'manta terminal create --worktree path:/projects/myapp --command "opencode" --focus'
+      'manta terminal create --worktree path:/projects/myapp --command "opencode" --focus',
+      'manta terminal create --worktree path:C:/src/app --shell cmd.exe --json'
     ]
   },
   {

src/main/runtime/terminal-shell-override-host-support.ts
@@ -1,0 +1,90 @@
+import { isWslShellName } from '../../shared/local-windows-terminal-runtime'
+import type { ProjectExecutionRuntimeResolution } from '../../shared/project-execution-runtime'
+import { parseWslUncPath } from '../../shared/wsl-paths'
+
+/**
+ * Whether the host about to spawn a terminal can honour a requested Windows shell.
+ *
+ * The whole point of `--shell` is that the caller stops having to guess which shell it got. A host
+ * that cannot apply the pick must say so: silently spawning its default shell returns a healthy
+ * terminal running something else, which is the exact failure `--shell` exists to remove.
+ */
+export function terminalShellOverrideRefusal(args: {
+  shellOverride: string | undefined
+  connectionId: string | null
+  platform: NodeJS.Platform
+  projectRuntime: ProjectExecutionRuntimeResolution | undefined
+  /** The cwd the PTY will spawn in, resolved the same way the spawn lanes resolve it. */
+  cwd: string
+  workspacePath: string
+}): Error | null {
+  if (!args.shellOverride) {
+    return null
+  }
+  if (args.connectionId) {
+    // The shell runs on the SSH host, whose platform and installed shells this runtime cannot see.
+    return new Error(
+      `This workspace runs its terminals over SSH, and Manta cannot apply --shell ${args.shellOverride} there. No terminal was created. Omit --shell, or create the terminal on the execution host itself.`
+    )
+  }
+  if (args.platform !== 'win32') {
+    return new Error(
+      `--shell ${args.shellOverride} names a Windows shell, and this execution host is ${args.platform}, which spawns the user's login shell. No terminal was created; omit --shell.`
+    )
+  }
+  return (
+    projectRuntimeShellConflict(args.shellOverride, args.projectRuntime) ??
+    wslUncPathShellConflict(args.shellOverride, args.cwd, args.workspacePath)
+  )
+}
+
+/**
+ * The project's execution runtime decides which MACHINE the shell runs on, so it outranks a
+ * per-terminal pick — and `resolveLocalWindowsTerminalRuntimeOptions` enforces that by rewriting
+ * the value: a WSL project forces `wsl.exe`, and a Windows-host project discards a WSL name in
+ * favour of the host shell. Either rewrite hands back a terminal running something the caller did
+ * not ask for, and it also splits the startup-command quoting from the shell that receives it
+ * (POSIX args typed into cmd, or cmd args typed into a WSL shell). Refusing the contradiction is
+ * the only answer that keeps the request and the terminal describing the same thing.
+ */
+function projectRuntimeShellConflict(
+  shellOverride: string,
+  projectRuntime: ProjectExecutionRuntimeResolution | undefined
+): Error | null {
+  if (projectRuntime?.status !== 'resolved') {
+    return null
+  }
+  const runsInWsl = projectRuntime.runtime.kind === 'wsl'
+  if (runsInWsl === isWslShellName(shellOverride)) {
+    return null
+  }
+  return new Error(
+    runsInWsl
+      ? `This workspace's project runs its terminals in WSL, so --shell ${shellOverride} cannot be applied. No terminal was created. Use --shell wsl.exe, or change the project's execution runtime.`
+      : `This workspace's project runs its terminals on the Windows host, so --shell ${shellOverride} cannot be applied. No terminal was created. Change the project's execution runtime to WSL, or pass a Windows shell.`
+  )
+}
+
+/**
+ * A `\\wsl$\<distro>\...` path only exists inside that distro, so the providers force `wsl.exe`
+ * for it whatever shell was requested (`resolveWslSessionContext` keys off the cwd, then the
+ * workspace path behind the session). That rewrite is right for the global shell setting and
+ * wrong for `--shell`, which promised the caller the shell it named. This is the only check that
+ * still fires for a folder workspace, which has no project runtime to disagree with.
+ */
+function wslUncPathShellConflict(
+  shellOverride: string,
+  cwd: string,
+  workspacePath: string
+): Error | null {
+  if (isWslShellName(shellOverride)) {
+    return null
+  }
+  const wslPath = [cwd, workspacePath].find((path) => parseWslUncPath(path) !== null)
+  if (wslPath === undefined) {
+    return null
+  }
+  return new Error(
+    `This terminal would run inside WSL because ${wslPath} lives in a WSL distro, so --shell ${shellOverride} cannot be applied. No terminal was created. Use --shell wsl.exe, or omit --shell.`
+  )
+}

ivanvykopal/orca

src/cli/handlers/terminal.ts
@@ -32,6 +32,11 @@ import {
 } from '../omitted-host-scope-selectors'
 import { RuntimeClientError } from '../runtime-client'
 import {
+  isSupportedWindowsShellOverride,
+  listSupportedWindowsShellOverrides
+} from '../../shared/windows-terminal-shell'
+import { TERMINAL_CREATE_SHELL_SELECTION_RUNTIME_CAPABILITY } from '../../shared/protocol-version'
+import {
   getBrowserWorktreeSelector,
   getOptionalWorktreeSelector,
   getRequiredWorktreeSelector,
@@ -153,9 +158,40 @@ export const TERMINAL_HANDLERS: Record<string, CommandHandler> = {
     const useRendererBackedInteractiveTerminal =
       !client.isRemote && shouldUseRendererBackedInteractiveTerminal(command)
     const focus = flags.get('focus') === true
+    const shell = getOptionalStringFlag(flags, 'shell')
+    if (shell !== undefined) {
+      if (!isSupportedWindowsShellOverride(shell)) {
+        throw new RuntimeClientError(
+          'invalid_argument',
+          `--shell must be one of: ${listSupportedWindowsShellOverrides().join(', ')}`
+        )
+      }
+      // Why refused rather than sent hopefully: an older host strips the unknown param and hands
+      // back a healthy terminal running its DEFAULT shell. Nothing in that reply says the shell
+      // was ignored, so a caller that wanted cmd would drive a PowerShell session believing it won.
+      const status = await client.getCliStatus()
+      // An unreachable host reports no capabilities at all; that is not evidence it lacks --shell.
+      if (!status.result.runtime.reachable) {
+        throw new RuntimeClientError(
+          'runtime_unavailable',
+          'Orca could not verify --shell support on the execution host, so no terminal was created. Wait for the execution host to become reachable and retry.'
+        )
+      }
+      if (
+        status.result.runtime.capabilities?.includes(
+          TERMINAL_CREATE_SHELL_SELECTION_RUNTIME_CAPABILITY
+        ) !== true
+      ) {
+        throw new RuntimeClientError(
+          'incompatible_runtime',
+          'This Orca host does not support --shell, and would silently create a terminal running its default shell instead. No terminal was created; update Orca on the execution host.'
+        )
+      }
+    }
     const result = await client.call<{ terminal: RuntimeTerminalCreate }>('terminal.create', {
       worktree: await getBrowserWorktreeSelector(flags, cwd, client),
       command,
+      ...(shell !== undefined ? { shell } : {}),
       title: getOptionalStringFlag(flags, 'title'),
       // Why: interactive local agent TUIs need the renderer-backed terminal
       // path for browser-side features, but CLI creates must stay backgrounded

src/cli/root-help-text-secondary.ts
@@ -62,7 +62,7 @@ export const ROOT_HELP_TEXT_SECONDARY = [
   '  orca terminal read [--terminal <handle>] [--cursor <n>] [--limit <n>] [--json]',
   '  orca terminal send [--terminal <handle>] [--text <text>] [--enter] [--interrupt] [--wait-submit <seconds>] [--retry-request <id>] [--json]',
   '  orca terminal wait [--terminal <handle>] --for exit|tui-idle [--timeout-ms <ms>] [--json]',
-  '  orca terminal create [--worktree <selector>] [--title <name>] [--command <text>] [--focus] [--json]',
+  '  orca terminal create [--worktree <selector>] [--title <name>] [--command <text>] [--shell <shell>] [--focus] [--json]',
   '  orca terminal split [--terminal <handle>] [--direction horizontal|vertical] [--json]',
   '  orca terminal switch [--terminal <handle>] [--json]',
   '  orca terminal close ([--terminal <handle>] [--tab] | --worktree <selector> --all) [--json]',

src/cli/specs/core.ts
@@ -250,17 +250,20 @@ export const CORE_COMMAND_SPECS: CommandSpec[] = [
     path: ['terminal', 'create'],
     summary: 'Create a terminal session in the current worktree',
     usage:
-      'orca terminal create [--worktree <selector>] [--title <name>] [--command <text>] [--focus] [--json]',
-    allowedFlags: [...GLOBAL_FLAGS, 'worktree', 'command', 'title', 'focus'],
+      'orca terminal create [--worktree <selector>] [--title <name>] [--command <text>] [--shell <shell>] [--focus] [--json]',
+    allowedFlags: [...GLOBAL_FLAGS, 'worktree', 'command', 'shell', 'title', 'focus'],
     notes: [
       'Creates a visible terminal tab without switching focus when possible; falls back to a background handle if the UI cannot adopt it. Pass --focus to switch to it.',
-      'Use this, not worktree create, for a fresh agent in the current checkout.'
+      'Use this, not worktree create, for a fresh agent in the current checkout.',
+      '--shell picks the shell the terminal IS on a Windows host (cmd.exe, powershell.exe, pwsh.exe, wsl.exe, bash.exe, git-bash); --command is typed into whatever shell the host started, so `--command cmd.exe` leaves a cmd running INSIDE the default shell and exiting it drops back to that shell.',
+      'A host that cannot apply --shell refuses the create rather than quietly spawning its default shell: macOS and Linux execution hosts spawn the login shell, terminals routed over SSH resolve their shell on the SSH host, a --shell that contradicts the project execution runtime (WSL vs Windows host) is refused, and an Orca host older than --shell is refused by the CLI.'
     ],
     examples: [
       'orca terminal create --json',
       'orca terminal create --worktree active --command "codex" --json',
       'orca terminal create --worktree path:/projects/myapp --title "RUNNER" --command "opencode"',
-      'orca terminal create --worktree path:/projects/myapp --command "opencode" --focus'
+      'orca terminal create --worktree path:/projects/myapp --command "opencode" --focus',
+      'orca terminal create --worktree path:C:/src/app --shell cmd.exe --json'
     ]
   },
   {

src/main/runtime/terminal-shell-override-host-support.ts
@@ -1,0 +1,90 @@
+import { isWslShellName } from '../../shared/local-windows-terminal-runtime'
+import type { ProjectExecutionRuntimeResolution } from '../../shared/project-execution-runtime'
+import { parseWslUncPath } from '../../shared/wsl-paths'
+
+/**
+ * Whether the host about to spawn a terminal can honour a requested Windows shell.
+ *
+ * The whole point of `--shell` is that the caller stops having to guess which shell it got. A host
+ * that cannot apply the pick must say so: silently spawning its default shell returns a healthy
+ * terminal running something else, which is the exact failure `--shell` exists to remove.
+ */
+export function terminalShellOverrideRefusal(args: {
+  shellOverride: string | undefined
+  connectionId: string | null
+  platform: NodeJS.Platform
+  projectRuntime: ProjectExecutionRuntimeResolution | undefined
+  /** The cwd the PTY will spawn in, resolved the same way the spawn lanes resolve it. */
+  cwd: string
+  workspacePath: string
+}): Error | null {
+  if (!args.shellOverride) {
+    return null
+  }
+  if (args.connectionId) {
+    // The shell runs on the SSH host, whose platform and installed shells this runtime cannot see.
+    return new Error(
+      `This workspace runs its terminals over SSH, and Orca cannot apply --shell ${args.shellOverride} there. No terminal was created. Omit --shell, or create the terminal on the execution host itself.`
+    )
+  }
+  if (args.platform !== 'win32') {
+    return new Error(
+      `--shell ${args.shellOverride} names a Windows shell, and this execution host is ${args.platform}, which spawns the user's login shell. No terminal was created; omit --shell.`
+    )
+  }
+  return (
+    projectRuntimeShellConflict(args.shellOverride, args.projectRuntime) ??
+    wslUncPathShellConflict(args.shellOverride, args.cwd, args.workspacePath)
+  )
+}
+
+/**
+ * The project's execution runtime decides which MACHINE the shell runs on, so it outranks a
+ * per-terminal pick — and `resolveLocalWindowsTerminalRuntimeOptions` enforces that by rewriting
+ * the value: a WSL project forces `wsl.exe`, and a Windows-host project discards a WSL name in
+ * favour of the host shell. Either rewrite hands back a terminal running something the caller did
+ * not ask for, and it also splits the startup-command quoting from the shell that receives it
+ * (POSIX args typed into cmd, or cmd args typed into a WSL shell). Refusing the contradiction is
+ * the only answer that keeps the request and the terminal describing the same thing.
+ */
+function projectRuntimeShellConflict(
+  shellOverride: string,
+  projectRuntime: ProjectExecutionRuntimeResolution | undefined
+): Error | null {
+  if (projectRuntime?.status !== 'resolved') {
+    return null
+  }
+  const runsInWsl = projectRuntime.runtime.kind === 'wsl'
+  if (runsInWsl === isWslShellName(shellOverride)) {
+    return null
+  }
+  return new Error(
+    runsInWsl
+      ? `This workspace's project runs its terminals in WSL, so --shell ${shellOverride} cannot be applied. No terminal was created. Use --shell wsl.exe, or change the project's execution runtime.`
+      : `This workspace's project runs its terminals on the Windows host, so --shell ${shellOverride} cannot be applied. No terminal was created. Change the project's execution runtime to WSL, or pass a Windows shell.`
+  )
+}
+
+/**
+ * A `\\wsl$\<distro>\...` path only exists inside that distro, so the providers force `wsl.exe`
+ * for it whatever shell was requested (`resolveWslSessionContext` keys off the cwd, then the
+ * workspace path behind the session). That rewrite is right for the global shell setting and
+ * wrong for `--shell`, which promised the caller the shell it named. This is the only check that
+ * still fires for a folder workspace, which has no project runtime to disagree with.
+ */
+function wslUncPathShellConflict(
+  shellOverride: string,
+  cwd: string,
+  workspacePath: string
+): Error | null {
+  if (isWslShellName(shellOverride)) {
+    return null
+  }
+  const wslPath = [cwd, workspacePath].find((path) => parseWslUncPath(path) !== null)
+  if (wslPath === undefined) {
+    return null
+  }
+  return new Error(
+    `This terminal would run inside WSL because ${wslPath} lives in a WSL distro, so --shell ${shellOverride} cannot be applied. No terminal was created. Use --shell wsl.exe, or omit --shell.`
+  )
+}