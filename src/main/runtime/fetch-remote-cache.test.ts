paidaxingyo666/Manta

fix(worktree-create): prioritize creation Git and defer background preparation (#20722)
* fix(worktree-create): run create git commands at interactive tier, defer pool side jobs, bound queue wait by timeout

Creating a worktree on a busy machine stalled for minutes because the create's
own git competed for the same admission budget as everything else.

- The create path never set an admission tier, so it defaulted to 'status' and
  could never use the scheduler's headroom slots. It now tags the option objects
  that reach git directly: the add, the post-add listing, the base-ref probes and
  the prepared-checkout finalize. The speculative warm-up and the SSH path are
  unchanged.
- The prepared-pool re-arm is a full `reset --hard`; it ran mid-create and held a
  general slot. `consumePreparedWorktreeCreate` now returns it as a thunk the
  create runs after the startup terminal is spawned. Stale-preparation
  reclamation (`worktree unlock` / `worktree remove`) drops to 'background'.
- A command's timeout only armed once its child spawned, so a saturated queue
  could hold a 1s command indefinitely. Admission now takes the same deadline and
  raises GitCommandTimeoutError without spawning; a caller abort still reports as
  an abort.

The tier is kept off the `{ wslDistro }` routing objects: several callers test
those for emptiness to decide whether a repo has local git routing at all.

* fix(worktree-create): keep a bounded queue wait from reading as an absent base ref

The admission deadline added in the previous commit made every create-path probe's
15s/120s budget cover the queue wait. The default-base and worktree-base probes answer
`false`/`null` for any failure, so a saturated queue reported a repo that has origin/main
as having no default base and the create refused to start. Both probe families now let
`GitCommandTimeoutError` through, and the branch-name resolution loop, the push-target
configuration and the post-add listing run at the create's interactive ti

src/main/runtime/fetch-remote-cache.test.ts
@@ -1,3 +1,5 @@
+import { worktreeCreateGit } from '../git/worktree-create-git-executor'
+import { resolveGitAdmissionTier } from '../git/command-runner/git-operation-executor'
 import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
 
 // Why: these tests cover the §3.3 Lifecycle rules on
@@ -78,6 +80,41 @@ function mockFetchResults(results: unknown[]): void {
 }
 
 describe('MantaRuntimeService.fetchRemoteWithCache', () => {
+  it.each([undefined, 'Ubuntu'])(
+    'inherits create priority through fetch adapters on %s',
+    async (wslDistro) =>
+      worktreeCreateGit.run(async () => {
+        gitExecFileAsyncMock.mockImplementation(async (argv: string[]) => {
+          expect(resolveGitAdmissionTier()).toBe('interactive')
+          return {
+            stdout: argv[0] === 'remote' ? 'origin\n' : '/priority-repo/.git\n',
+            stderr: ''
+          }
+        })
+        const runtime = new MantaRuntimeService()
+        const options = wslDistro ? { wslDistro } : {}
+        const base = await runtime.resolveRemoteTrackingBase(
+          '/priority-repo',
+          'origin/main',
+          options
+        )
+        expect(base).not.toBeNull()
+        if (!base) {
+          throw new Error('expected a remote base')
+        }
+        await expect(runtime.hasRemoteTrackingRef('/priority-repo', base, options)).resolves.toBe(
+          true
+        )
+        await expect(
+          runtime.getOrStartRemoteTrackingBaseRefresh('/priority-repo', base, options)
+        ).resolves.toEqual({ ok: true })
+        expect(fetchCallCount()).toBe(1)
+        for (const [, execOptions] of gitExecFileAsyncMock.mock.calls) {
+          expect(execOptions).toMatchObject({ cwd: '/priority-repo', ...options })
+        }
+      })
+  )
+
   beforeEach(() => {
     gitExecFileAsyncMock.mockReset()
   })

src/main/runtime/runtime-local-create-rearm-ordering.test.ts
@@ -1,0 +1,133 @@
+// Re-arming the prepared-checkout pool is a full `reset --hard`. Firing it before the create's
+// terminals are launched puts that checkout in front of the startup agent's first git reads.
+import { beforeEach, describe, expect, it, vi } from 'vitest'
+
+vi.mock('electron', () => ({
+  BrowserWindow: { fromId: vi.fn(() => null) },
+  webContents: { fromId: vi.fn(() => null) },
+  ipcMain: { on: vi.fn(), removeListener: vi.fn() },
+  app: { getPath: vi.fn(() => '/tmp'), isPackaged: false }
+}))
+
+const calls = vi.hoisted(() => ({ order: new Array<string>() }))
+
+const createLocalMock = vi.hoisted(() => vi.fn())
+vi.mock('./runtime-local-worktree-create', () => ({
+  createRuntimeLocalManagedWorktree: createLocalMock
+}))
+
+const startTerminalsMock = vi.hoisted(() => vi.fn())
+vi.mock('./runtime-local-worktree-terminal-startup', () => ({
+  startRuntimeLocalWorktreeTerminals: startTerminalsMock
+}))
+
+vi.mock('./runtime-local-worktree-setup', () => ({
+  prepareRuntimeLocalWorktreeSetup: vi.fn(async () => ({
+    setup: undefined,
+    defaultTabs: undefined,
+    warning: undefined,
+    effectiveDecision: 'skip',
+    hookFound: false,
+    shouldRunSetup: false,
+    didStartInProcessSetupHook: false
+  }))
+}))
+
+vi.mock('../ipc/filesystem-auth', () => ({ invalidateAuthorizedRootsCache: vi.fn() }))
+
+import { MantaRuntimeService } from './manta-runtime'
+
+const repo = { id: 'repo-1', path: '/repo', displayName: 'Repo', badgeColor: 'blue', kind: 'git' }
+
+const worktree = { id: 'wt-1', path: '/worktrees/app', branch: 'app', repoId: repo.id }
+
+type RuntimeInternals = {
+  resolveRepoSelector: (selector: string) => Promise<unknown>
+  resolveLineageForWorktreeCreate: (input: unknown) => Promise<unknown>
+  recordCreatedWorktreeLineage: (created: unknown, resolution: unknown) => unknown
+  getLocalGitExecutionOptionArgs: (repo: unknown) => unknown[]
+  getHostedReviewExecutionOptions: (repo: unknown) => unknown
+  invalidateResolvedWorktreeCache: () => void
+  invalidateWorktreeScanCacheForRepo: (repoId: string) => void
+  notifyWorktreesChanged: (repoId: string) => void
+  emitWorktreeLifecycle: (event: unknown) => void
+}
+
+function makeRuntime(): MantaRuntimeService {
+  const store = {
+    getSettings: () => ({ disabledTuiAgents: [], workspaceDir: '/worktrees' }),
+    getProjectHostSetups: () => []
+  }
+  // oxlint-disable-next-line typescript/consistent-type-assertions -- SAFETY: every store method this create path reaches is supplied above.
+  const runtime = new MantaRuntimeService(store as never)
+  // oxlint-disable-next-line typescript/consistent-type-assertions -- SAFETY: the named members all exist on the service; the cast only exposes non-public ones to the spies.
+  const internals = runtime as unknown as RuntimeInternals
+  vi.spyOn(internals, 'resolveRepoSelector').mockResolvedValue(repo)
+  vi.spyOn(internals, 'resolveLineageForWorktreeCreate').mockResolvedValue(null)
+  vi.spyOn(internals, 'recordCreatedWorktreeLineage').mockReturnValue({
+    lineage: null,
+    workspaceLineage: null,
+    warnings: []
+  })
+  vi.spyOn(internals, 'getLocalGitExecutionOptionArgs').mockReturnValue([{}])
+  vi.spyOn(internals, 'getHostedReviewExecutionOptions').mockReturnValue(undefined)
+  vi.spyOn(internals, 'invalidateResolvedWorktreeCache').mockReturnValue(undefined)
+  vi.spyOn(internals, 'invalidateWorktreeScanCacheForRepo').mockReturnValue(undefined)
+  vi.spyOn(internals, 'notifyWorktreesChanged').mockReturnValue(undefined)
+  vi.spyOn(internals, 'emitWorktreeLifecycle').mockReturnValue(undefined)
+  return runtime
+}
+
+describe('runtime local create prepared-pool re-arm ordering', () => {
+  beforeEach(() => {
+    calls.order = []
+    createLocalMock.mockReset()
+    startTerminalsMock.mockReset()
+    createLocalMock.mockImplementation(async (args: { rearm: { fire: () => void } }) => {
+      args.rearm.fire = () => calls.order.push('rearm')
+      return {
+        worktree,
+        worktreePath: worktree.path,
+        includeCopyWarning: undefined,
+        created: { path: worktree.path, head: 'abc', branch: 'app' },
+        addResult: {},
+        metadataResult: { lineage: null, workspaceLineage: null, warnings: [] }
+      }
+    })
+    startTerminalsMock.mockImplementation(async () => {
+      calls.order.push('terminals')
+      return {
+        warning: undefined,
+        returnedSetup: undefined,
+        didSpawnSetup: false,
+        didSpawnStartup: false,
+        setupTerminalHandle: undefined,
+        startupTerminalHandle: undefined,
+        startupTerminalTabId: undefined,
+        startupTerminalPaneKey: undefined,
+        startupTerminalPtyId: undefined
+      }
+    })
+  })
+
+  it('arms the pool only after the startup terminals are launched', async () => {
+    const runtime = makeRuntime()
+
+    await runtime.createManagedWorktree({ repoSelector: 'repo-1', name: 'app' })
+
+    expect(startTerminalsMock).toHaveBeenCalledOnce()
+    expect(calls.order).toEqual(['terminals', 'rearm'])
+  })
+
+  it('still arms the pool when terminal launch fails', async () => {
+    startTerminalsMock.mockRejectedValue(new Error('spawn failed'))
+    const runtime = makeRuntime()
+
+    await expect(
+      runtime.createManagedWorktree({ repoSelector: 'repo-1', name: 'app' })
+    ).rejects.toThrow('spawn failed')
+
+    // The prepared checkout was consumed before the failure, so the replacement is still owed.
+    expect(calls.order).toEqual(['rearm'])
+  })
+})

ivanvykopal/orca

src/main/runtime/fetch-remote-cache.test.ts
@@ -1,3 +1,5 @@
+import { worktreeCreateGit } from '../git/worktree-create-git-executor'
+import { resolveGitAdmissionTier } from '../git/command-runner/git-operation-executor'
 import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
 
 // Why: these tests cover the §3.3 Lifecycle rules on
@@ -78,6 +80,41 @@ function mockFetchResults(results: unknown[]): void {
 }
 
 describe('OrcaRuntimeService.fetchRemoteWithCache', () => {
+  it.each([undefined, 'Ubuntu'])(
+    'inherits create priority through fetch adapters on %s',
+    async (wslDistro) =>
+      worktreeCreateGit.run(async () => {
+        gitExecFileAsyncMock.mockImplementation(async (argv: string[]) => {
+          expect(resolveGitAdmissionTier()).toBe('interactive')
+          return {
+            stdout: argv[0] === 'remote' ? 'origin\n' : '/priority-repo/.git\n',
+            stderr: ''
+          }
+        })
+        const runtime = new OrcaRuntimeService()
+        const options = wslDistro ? { wslDistro } : {}
+        const base = await runtime.resolveRemoteTrackingBase(
+          '/priority-repo',
+          'origin/main',
+          options
+        )
+        expect(base).not.toBeNull()
+        if (!base) {
+          throw new Error('expected a remote base')
+        }
+        await expect(runtime.hasRemoteTrackingRef('/priority-repo', base, options)).resolves.toBe(
+          true
+        )
+        await expect(
+          runtime.getOrStartRemoteTrackingBaseRefresh('/priority-repo', base, options)
+        ).resolves.toEqual({ ok: true })
+        expect(fetchCallCount()).toBe(1)
+        for (const [, execOptions] of gitExecFileAsyncMock.mock.calls) {
+          expect(execOptions).toMatchObject({ cwd: '/priority-repo', ...options })
+        }
+      })
+  )
+
   beforeEach(() => {
     gitExecFileAsyncMock.mockReset()
   })

src/main/runtime/runtime-local-create-rearm-ordering.test.ts
@@ -1,0 +1,133 @@
+// Re-arming the prepared-checkout pool is a full `reset --hard`. Firing it before the create's
+// terminals are launched puts that checkout in front of the startup agent's first git reads.
+import { beforeEach, describe, expect, it, vi } from 'vitest'
+
+vi.mock('electron', () => ({
+  BrowserWindow: { fromId: vi.fn(() => null) },
+  webContents: { fromId: vi.fn(() => null) },
+  ipcMain: { on: vi.fn(), removeListener: vi.fn() },
+  app: { getPath: vi.fn(() => '/tmp'), isPackaged: false }
+}))
+
+const calls = vi.hoisted(() => ({ order: new Array<string>() }))
+
+const createLocalMock = vi.hoisted(() => vi.fn())
+vi.mock('./runtime-local-worktree-create', () => ({
+  createRuntimeLocalManagedWorktree: createLocalMock
+}))
+
+const startTerminalsMock = vi.hoisted(() => vi.fn())
+vi.mock('./runtime-local-worktree-terminal-startup', () => ({
+  startRuntimeLocalWorktreeTerminals: startTerminalsMock
+}))
+
+vi.mock('./runtime-local-worktree-setup', () => ({
+  prepareRuntimeLocalWorktreeSetup: vi.fn(async () => ({
+    setup: undefined,
+    defaultTabs: undefined,
+    warning: undefined,
+    effectiveDecision: 'skip',
+    hookFound: false,
+    shouldRunSetup: false,
+    didStartInProcessSetupHook: false
+  }))
+}))
+
+vi.mock('../ipc/filesystem-auth', () => ({ invalidateAuthorizedRootsCache: vi.fn() }))
+
+import { OrcaRuntimeService } from './orca-runtime'
+
+const repo = { id: 'repo-1', path: '/repo', displayName: 'Repo', badgeColor: 'blue', kind: 'git' }
+
+const worktree = { id: 'wt-1', path: '/worktrees/app', branch: 'app', repoId: repo.id }
+
+type RuntimeInternals = {
+  resolveRepoSelector: (selector: string) => Promise<unknown>
+  resolveLineageForWorktreeCreate: (input: unknown) => Promise<unknown>
+  recordCreatedWorktreeLineage: (created: unknown, resolution: unknown) => unknown
+  getLocalGitExecutionOptionArgs: (repo: unknown) => unknown[]
+  getHostedReviewExecutionOptions: (repo: unknown) => unknown
+  invalidateResolvedWorktreeCache: () => void
+  invalidateWorktreeScanCacheForRepo: (repoId: string) => void
+  notifyWorktreesChanged: (repoId: string) => void
+  emitWorktreeLifecycle: (event: unknown) => void
+}
+
+function makeRuntime(): OrcaRuntimeService {
+  const store = {
+    getSettings: () => ({ disabledTuiAgents: [], workspaceDir: '/worktrees' }),
+    getProjectHostSetups: () => []
+  }
+  // oxlint-disable-next-line typescript/consistent-type-assertions -- SAFETY: every store method this create path reaches is supplied above.
+  const runtime = new OrcaRuntimeService(store as never)
+  // oxlint-disable-next-line typescript/consistent-type-assertions -- SAFETY: the named members all exist on the service; the cast only exposes non-public ones to the spies.
+  const internals = runtime as unknown as RuntimeInternals
+  vi.spyOn(internals, 'resolveRepoSelector').mockResolvedValue(repo)
+  vi.spyOn(internals, 'resolveLineageForWorktreeCreate').mockResolvedValue(null)
+  vi.spyOn(internals, 'recordCreatedWorktreeLineage').mockReturnValue({
+    lineage: null,
+    workspaceLineage: null,
+    warnings: []
+  })
+  vi.spyOn(internals, 'getLocalGitExecutionOptionArgs').mockReturnValue([{}])
+  vi.spyOn(internals, 'getHostedReviewExecutionOptions').mockReturnValue(undefined)
+  vi.spyOn(internals, 'invalidateResolvedWorktreeCache').mockReturnValue(undefined)
+  vi.spyOn(internals, 'invalidateWorktreeScanCacheForRepo').mockReturnValue(undefined)
+  vi.spyOn(internals, 'notifyWorktreesChanged').mockReturnValue(undefined)
+  vi.spyOn(internals, 'emitWorktreeLifecycle').mockReturnValue(undefined)
+  return runtime
+}
+
+describe('runtime local create prepared-pool re-arm ordering', () => {
+  beforeEach(() => {
+    calls.order = []
+    createLocalMock.mockReset()
+    startTerminalsMock.mockReset()
+    createLocalMock.mockImplementation(async (args: { rearm: { fire: () => void } }) => {
+      args.rearm.fire = () => calls.order.push('rearm')
+      return {
+        worktree,
+        worktreePath: worktree.path,
+        includeCopyWarning: undefined,
+        created: { path: worktree.path, head: 'abc', branch: 'app' },
+        addResult: {},
+        metadataResult: { lineage: null, workspaceLineage: null, warnings: [] }
+      }
+    })
+    startTerminalsMock.mockImplementation(async () => {
+      calls.order.push('terminals')
+      return {
+        warning: undefined,
+        returnedSetup: undefined,
+        didSpawnSetup: false,
+        didSpawnStartup: false,
+        setupTerminalHandle: undefined,
+        startupTerminalHandle: undefined,
+        startupTerminalTabId: undefined,
+        startupTerminalPaneKey: undefined,
+        startupTerminalPtyId: undefined
+      }
+    })
+  })
+
+  it('arms the pool only after the startup terminals are launched', async () => {
+    const runtime = makeRuntime()
+
+    await runtime.createManagedWorktree({ repoSelector: 'repo-1', name: 'app' })
+
+    expect(startTerminalsMock).toHaveBeenCalledOnce()
+    expect(calls.order).toEqual(['terminals', 'rearm'])
+  })
+
+  it('still arms the pool when terminal launch fails', async () => {
+    startTerminalsMock.mockRejectedValue(new Error('spawn failed'))
+    const runtime = makeRuntime()
+
+    await expect(
+      runtime.createManagedWorktree({ repoSelector: 'repo-1', name: 'app' })
+    ).rejects.toThrow('spawn failed')
+
+    // The prepared checkout was consumed before the failure, so the replacement is still owed.
+    expect(calls.order).toEqual(['rearm'])
+  })
+})